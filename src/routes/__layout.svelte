3cae24ffa2b5129cf58ac8c106c8ea38f4ddbbf2
Steve Kinney <hello@stevekinney.net>
2021-07-07 11:29:47 -0400

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+  <title>Temporal</title>
+
+  <link rel="manifest" href="site.webmanifest" />
+  <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+
+  <meta property="og:title" content="Temporal" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://temporal.io" />
+  <meta property="og:image" content="/banner.png" />
+</svelte:head>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<h1>Temporal UI</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
