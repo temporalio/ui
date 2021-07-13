4090a0c08e737a8ccfb02275d87eefcc799eacbb
Steve Kinney <hello@stevekinney.net>
2021-07-13 12:39:43 -0400

    Add fixture data for tests

--- cypress/integration/hello-world.js
+++ cypress/integration/hello-world.js
@@ -1,7 +1,7 @@
 describe('A Basic Sanity Check', () => {
   it('verifies that the word "Temporal" is somewhere on the page', () => {
+    cy.intercept('/api/workflows.json', { fixture: 'workflows.json' });
     cy.visit('http://localhost:3000');
-
     cy.contains('Temporal');
   });
 });
