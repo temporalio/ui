ade9b6a0e43fed0a7722a8df9e5d2fe6f01a8d69
Steve Kinney <hello@stevekinney.net>
2021-07-13 12:59:57 -0400

    One more time with feeling

--- cypress/integration/hello-world.js
+++ cypress/integration/hello-world.js
@@ -1,6 +1,8 @@
 describe('A Basic Sanity Check', () => {
   it('verifies that the word "Temporal" is somewhere on the page', () => {
-    cy.intercept('/api/workflows.json', { fixture: 'workflows.json' });
+    cy.intercept(`${process.env.CYPRESS_API_HOST}/api/workflows.json`, {
+      fixture: 'workflows.json',
+    });
     cy.visit('http://localhost:3000');
     cy.contains('Temporal');
   });
