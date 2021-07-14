430688693b022dfed0fed057393c277417a0e82e
MrSirDev <blankworldwide@gmail.com>
2021-07-14 12:48:46 -0700

    basic storybook with test button implemented

---
+++ src/lib/components/button-test.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  export let text = '';
+  const dispatch = createEventDispatcher();
+  function onClick(event) {
+    dispatch('click', event);
+  }
+</script>
+
+<button on:click={onClick}>
+  {text}
+</button>
