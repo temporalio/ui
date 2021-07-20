44afb334ccec61ed308a3de4089e866db3617fe9
Steve Kinney <hello@stevekinney.net>
2021-07-20 11:56:38 -0400

    Add support for query parameters in path matching (#13)
    * Add support for query parameters in path matching
    
    * Fix typos in test names
    
    * Change the last test back to what it's supposed to be

--- src/lib/utilities/path-matches.test.ts
+++ src/lib/utilities/path-matches.test.ts
@@ -12,4 +12,32 @@ describe(pathMatches, () => {
   it('should return false if the second path is not a subdirectory of the first', () => {
     expect(pathMatches('/workflows', '/queries/abc')).toBe(false);
   });
+
+  it('should ignore query params if missing from the first path and the paths otherwise match', () => {
+    expect(pathMatches('/workflows', '/workflows?query=param')).toBe(true);
+  });
+
+  it('should ignore query params if missing from the second path and the paths otherwise match', () => {
+    expect(pathMatches('/workflows?query=param', '/workflows')).toBe(true);
+  });
+
+  it('should ignore query params if missing from the first path and the paths do not match', () => {
+    expect(pathMatches('/queries', '/workflows?query=param')).toBe(false);
+  });
+
+  it('should ignore query params if missing from the second path and the paths do not match', () => {
+    expect(pathMatches('/workflows?query=param', '/queries')).toBe(false);
+  });
+
+  it('should ignore query params if the paths match but the parameters do not', () => {
+    expect(pathMatches('/workflows?foo=bar', '/workflows?query=param')).toBe(
+      true,
+    );
+  });
+
+  it('should ignore query params if both the paths and parameters do not match', () => {
+    expect(pathMatches('/workflows?query=param', '/queries?foo=bar')).toBe(
+      false,
+    );
+  });
 });
