a7ffdc42d6095e29acf43688226da682ce04ed5f
Steve Kinney <hello@stevekinney.net>
2021-07-20 12:03:58 -0400

    Add types to endpoints

--- src/routes/api/workflows/[workflow]/[run]/history.ts
+++ src/routes/api/workflows/[workflow]/[run]/history.ts
@@ -1,6 +1,8 @@
+import type { Request, EndpointOutput } from '@sveltejs/kit';
+
 const apiHost = process.env.TEMPORAL_API_HOST || 'http://localhost:8088';
 
-export async function get({ params }) {
+export async function get({ params }: Request): Promise<EndpointOutput> {
   const { workflow: workflowId, run: runId } = params;
 
   const response = await fetch(
