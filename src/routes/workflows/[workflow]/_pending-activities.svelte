54da7b4e36ba4695140bc9a9cbe82bf392032e4b
Steve Kinney <hello@stevekinney.net>
2021-07-30 11:32:03 -0600

    Add pending activities

---
+++ src/routes/workflows/[workflow]/_pending-activities.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { PendingActivityInfo } from '$types/temporal/api/workflow/v1/message';
+
+  import PendingActivity from './_pending-activity.svelte';
+
+  export let activities: PendingActivityInfo[];
+</script>
+
+<div>
+  <h3 class="text-lg mt-6 mb-2">Pending Activities</h3>
+  {#each activities as activity}
+    <PendingActivity {activity} />
+  {/each}
+</div>
