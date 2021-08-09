fd887c58d2a3dd8e3394e2e43772739502227705
Steve Kinney <hello@stevekinney.net>
2021-08-09 12:07:55 -0600

    Use an abstraction to fetch at runtime
    This will allow us to do stuff like automatic pagination and re-validating and/or updating data with polling or whatever.

---
+++ src/lib/stores/data.ts
@@ -0,0 +1,55 @@
+import { derived, get, Readable, writable } from 'svelte/store';
+
+export type Slice<T> = {
+  isLoading: boolean;
+  isError: boolean;
+  data: T | null;
+};
+
+export type SliceStore<T> = Readable<Slice<T>>;
+
+export type SliceRequestConfiguration<T, U> = {
+  initialData?: U;
+  format?: (response: T) => U;
+};
+
+const store = writable({});
+
+const update = (key: string) => (data: Slice<any>) =>
+  store.update((s) => ({ ...s, [key]: { ...data } }));
+
+const newSlice = <T>(initialState?: T): Slice<T> => ({
+  isLoading: true,
+  isError: false,
+  data: initialState || null,
+});
+
+function request<T, U>(
+  key: string,
+  request: () => Promise<T>,
+  configuration: SliceRequestConfiguration<T, U> = {},
+): SliceStore<U> {
+  const { initialData, format = (i) => i } = configuration;
+  const currentState: Slice<U> = get(store)[key] || newSlice(initialData);
+  const sliceStore = derived(store, (s) => s[key]);
+  const updateKey = update(key);
+
+  updateKey({ ...currentState, isLoading: true });
+
+  request().then((data) => {
+    updateKey({
+      isLoading: false,
+      isError: false,
+      data: format(data),
+    });
+  });
+
+  return {
+    subscribe: sliceStore.subscribe,
+  };
+}
+
+export const query = {
+  subscribe: store.subscribe,
+  request,
+};
