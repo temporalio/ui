03ac36e5c80dec52f8d1fd677ba417c5d4cd21ed
Steve Kinney <hello@stevekinney.net>
2021-08-10 15:51:47 -0600

    Reorganize files and implement tabs (#43)
    * Reorganize files and implement tabs
    
    * Remove extra code

---
+++ src/routes/workflows/[workflow]/[run]/index.svelte
@@ -0,0 +1,53 @@
+<script context="module" lang="ts">
+  import { WorkflowExecutionAPI } from '$lib/services/workflow-execution-service';
+
+  import type { LoadInput } from '@sveltejs/kit';
+
+  export async function load({ fetch, page }: LoadInput) {
+    const { workflow: executionId, run: runId } = page.params;
+
+    const { execution } = await WorkflowExecutionAPI.get(
+      {
+        executionId,
+        runId,
+      },
+      fetch,
+    );
+
+    return {
+      props: {
+        execution,
+      },
+    };
+  }
+</script>
+
+<script lang="typescript">
+  import { isFullScreen } from '$lib/stores/full-screen';
+
+  import type { DescribeWorkflowExecutionResponse } from '$types/temporal/api/workflowservice/v1/request_response';
+
+  import { toWorkflowExecution } from '$lib/models/workflow-execution';
+
+  import ExecutionInformation from './_execution-information.svelte';
+  import PendingActivities from './_pending-activities.svelte';
+
+  export let execution: DescribeWorkflowExecutionResponse;
+
+  $: workflow = toWorkflowExecution(execution);
+</script>
+
+<div class="execution-information px-6 py-6 flex flex-col">
+  <div class={$isFullScreen ? 'w-1/3' : 'w-full'}>
+    <ExecutionInformation title="Start Time" value={workflow.startTime} />
+    <ExecutionInformation title="End Time" value={workflow.endTime} />
+    <ExecutionInformation title="Task Queue" value={workflow.taskQueue} />
+    <ExecutionInformation
+      title="History Events"
+      value={workflow.historyEvents}
+    />
+  </div>
+  <div class="pending-activities w-full">
+    <PendingActivities activities={workflow.pendingActivities} />
+  </div>
+</div>
