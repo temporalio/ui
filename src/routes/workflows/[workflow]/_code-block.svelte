ba0e37c2eef8c0765b7a1558efadd608d8023259
Joseph Rios <blankworldwide@gmail.com>
2021-08-10 20:12:51 -0700

    styling changes

--- src/routes/workflows/[workflow]/_code-block.svelte
+++ src/routes/workflows/[workflow]/_code-block.svelte
@@ -3,8 +3,12 @@
 
   export let heading = '';
   export let content = '';
+  export let copied = false;
 
-  const copy = () => navigator.clipboard.writeText(content).then(() => console.log('success')).catch(error => console.error(error));
+  const copy = () => navigator.clipboard.writeText(content).then(() => {
+    copied = !copied;
+    setTimeout(() => copied = false, 2000);
+  }).catch(error => console.error(error));
 
 </script>
 
@@ -16,7 +20,13 @@
       <pre
         class="p-6 bg-gray-200 relative">
         {JSON.stringify(content)}
-        <button on:click={copy}> <Icon src={Clipboard} class="w-8 h-8 text-purple-900 bg-gray-300 border-2 border-gray-200 absolute right-0 top-0 hidden group-hover:block hover:bg-gray-400" /></button>
+        <button on:click={copy}> 
+          {#if copied}
+          <p class="w-8 h-8 text-purple-900 absolute right-9 top-0">Copied!</p>
+          {:else}
+          <Icon src={Clipboard} class="w-8 h-8 text-purple-900 bg-gray-300 border-2 border-gray-200 absolute right-0 top-0 hidden group-hover:block hover:bg-gray-400" />
+          {/if}
+        </button>
       </pre>
     </code>
   </div>
