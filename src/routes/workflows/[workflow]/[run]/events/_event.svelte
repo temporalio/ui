78bf61d4db10ab5360683479bf9418f8bf662510
Steve Kinney <hello@stevekinney.net>
2021-08-11 14:48:25 -0600

    Create better event types

--- src/routes/workflows/[workflow]/[run]/events/_event.svelte
+++ src/routes/workflows/[workflow]/[run]/events/_event.svelte
@@ -2,18 +2,29 @@
   import beautify from 'json-beautify';
   import { formatDate } from '$lib/utilities/format-date';
   import type { HistoryEvent } from '$types/temporal/api/history/v1/message';
+  import ExecutionInformation from '../_execution-information.svelte';
 
   export let event: HistoryEvent;
   export let index: number;
+
+  let baseTypes = ['eventId', 'eventTime', 'eventType', 'version', 'taskId'];
+  let [type] = Object.keys(event).filter((key) => !baseTypes.includes(key));
 
   let even = !(index % 2);
 </script>
 
 <tr class:even>
   <td>{event.eventId}</td>
-  <td>{event.eventType}</td>
+  <td>{event.eventType} ({type})</td>
   <td>{formatDate(event.eventTime)}</td>
-  <td><pre><code>{beautify(event, null, 2, 80)}</code></pre></td>
+  <td>
+    <div class="flex">
+      <ExecutionInformation title="Version" value={event.version} />
+      <ExecutionInformation title="Task ID" value={event.taskId} />
+    </div>
+
+    <pre><code>{beautify(event, null, 2, 80)}</code></pre>
+  </td>
 </tr>
 
 <style lang="postcss">
@@ -25,6 +36,10 @@
   td {
     vertical-align: top;
     @apply p-4;
+  }
+
+  h3 {
+    @apply font-semibold;
   }
 
   .even {
