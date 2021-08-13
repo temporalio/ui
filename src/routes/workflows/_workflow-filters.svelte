da523c1e92e2aad8e21d643fe9b904efe4e61fb2
Steve Kinney <hello@stevekinney.net>
2021-08-13 12:59:17 -0600

    Add additional filters (#48)

--- src/routes/workflows/_workflow-filters.svelte
+++ src/routes/workflows/_workflow-filters.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
   import Select from '$lib/components/filter-select.svelte';
+  import Input from '$lib/components/filter-input.svelte';
 
   export let status: WorkflowStatus = null;
   export let workflowType: WorkflowType = null;
+  export let executionId: string = null;
+  export let runId: string = null;
   export let workflowTypes: string[];
+
+  function clear() {
+    status = null;
+    workflowType = null;
+    executionId = null;
+    runId = null;
+  }
 </script>
 
 <section class="p-4 flex gap-8">
-  <Select id="workflow-status" name="Workflow Status" bind:value={status}>
+  <Select
+    id="filter-by-workflow-status"
+    name="Workflow Status"
+    bind:value={status}
+  >
     <option value={null} />
     <option value="Running">Running</option>
     <option value="TimedOut">Timed Out</option>
@@ -18,10 +32,31 @@
     <option value="Canceled">Canceled</option>
     <option value="Terminated">Terminated</option>
   </Select>
-  <Select id="workflow-type" name="Workflow Type" bind:value={workflowType}>
+  <Select
+    id="filter-by-workflow-type"
+    name="Workflow Type"
+    bind:value={workflowType}
+  >
     <option value={null} />
     {#each workflowTypes as name}
       <option>{name}</option>
     {/each}
   </Select>
+  <Input
+    name="Execution ID"
+    id="filter-by-execution-id"
+    bind:value={executionId}
+  />
+  <Input name="Run ID" id="filter-by-run-id" bind:value={runId} />
+  <button on:click={clear}>Clear</button>
 </section>
+
+<style lang="postcss">
+  button {
+    @apply text-purple-700 py-4 px-6 border-purple-400 border-2 rounded-md;
+  }
+
+  button:hover {
+    @apply bg-purple-100;
+  }
+</style>
