a95a0a7b5a54bed7bba0ef2b4d0f82ec96346957
MrSirDev <blankworldwide@gmail.com>
2021-08-18 14:15:03 -0700

    added export

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/events/index.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/events/index.svelte
@@ -4,11 +4,11 @@
   import type { LoadInput } from '@sveltejs/kit';
 
   export async function load({ context }: LoadInput) {
-    const { events } = context;
-
+    const { events, execution } = context;
     return {
       props: {
         events,
+        execution,
       },
     };
   }
@@ -16,12 +16,23 @@
 
 <script lang="ts">
   import Events from './_events.svelte';
-
+  import Icon, { Download } from 'svelte-hero-icons';
   export let events: GetWorkflowExecutionHistoryResponse;
+  export let execution;
 
   let { history } = events;
+  let eventsString = JSON.stringify(events);
+  let dataUri =
+    'data:application/json;charset=utf-8,' + encodeURIComponent(eventsString);
 </script>
 
 <div class="px-6 py-6">
+  <a
+    class="text-black-500 font-bold uppercase px-3 py-1 text-xs flex"
+    href={dataUri}
+    download={`${execution.workflowExecutionInfo.type.name}.json`}
+  >
+    <Icon src={Download} class="text-black w-4 h-4" />export</a
+  >
   <Events {history} />
 </div>
