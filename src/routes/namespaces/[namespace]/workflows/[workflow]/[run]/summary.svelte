c83def314bc5fe0a42cbc258650a07f577f5a7ea
Joseph Rios <blankworldwide@gmail.com>
2021-08-23 17:10:02 -0600

    updated summary component with Task queue  component

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/summary.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/summary.svelte
@@ -19,8 +19,10 @@
 
   import { toWorkflowExecution } from '$lib/models/workflow-execution';
   import { getWorkflowStartedAndCompletedEvents } from '$lib/utilities/get-started-and-completed-events';
-
+  import { getTaskQueueUrl } from '$lib/utilities/get-task-queue-url';
   import ExecutionInformation from './_execution-information.svelte';
+  import { getContext } from 'svelte';
+  import TaskQueueInformation from './_task-queue-information.svelte';
   import PendingActivities from './_pending-activities.svelte';
   import CodeBlock from '$lib/components/code-block.svelte';
 
@@ -28,14 +30,20 @@
   export let events: GetWorkflowExecutionHistoryResponse;
 
   $: workflow = toWorkflowExecution(execution);
+  $: namespace = getContext('namespace') as string;
   $: inputAndResults = getWorkflowStartedAndCompletedEvents(events);
+  $: href = getTaskQueueUrl(namespace, workflow.taskQueue);
 </script>
 
 <div class="execution-information px-6 py-6">
   <div class="w-full flex">
     <ExecutionInformation title="Start Time" value={workflow.startTime} />
     <ExecutionInformation title="End Time" value={workflow.endTime} />
-    <ExecutionInformation title="Task Queue" value={workflow.taskQueue} />
+    <TaskQueueInformation
+      title="Task Queue"
+      value={workflow.taskQueue}
+      {href}
+    />
     <ExecutionInformation
       title="History Events"
       value={workflow.historyEvents}
