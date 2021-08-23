d237c3b4797b61167aafe2ca38a7a88a003a8bbd
Joseph Rios <blankworldwide@gmail.com>
2021-08-23 12:27:12 -0600

    added queue pollers styling

---
+++ src/routes/namespaces/[namespace]/queues/[queue]/__layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import QueuePollersTable from './_queue-pollers-table.svelte';
+  </script>
+  
+<section class="flex flex-col items-start">
+    <header>
+        <h3>Pollers</h3>
+    </header>
+      <div class="w-full h-full overflow-x-scroll">
+        <QueuePollersTable />
+
+      </div>
+  </section>
+
+  <style lang="postcss">
+    header {
+        padding: 12px;
+        margin-bottom: 18px;
+    }
+
+    h3 {
+        font-size: 24px;
+    }
+  
+  </style>
