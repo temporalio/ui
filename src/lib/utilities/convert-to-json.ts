ada05e490036c85554e75f7ee791f4277c44acd4
Steve Kinney <hello@stevekinney.net>
2021-08-31 14:21:26 -0600

    Update convert-to-json.ts

--- src/lib/utilities/convert-to-json.ts
+++ src/lib/utilities/convert-to-json.ts
@@ -1,4 +1,4 @@
-export const convertToJSON = (events: {}) => {
+export const convertToJSON = (events: Parameters<typeof JSON.stringify>) => {
   const data = JSON.stringify(events);
   const dataUri =
     'data:application/json;charset=utf-8,' + encodeURIComponent(data);
