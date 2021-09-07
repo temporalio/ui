baea575a280cd33b290fa1f69d674ba42cefc61a
Steve Kinney <hello@stevekinney.net>
2021-09-07 11:13:20 -0600

    Fix SSR issue with localStorage (#76)
    * Update namespace.ts
    
    * Infer types

--- src/lib/stores/namespace.ts
+++ src/lib/stores/namespace.ts
@@ -1,15 +1,24 @@
+import { browser } from '$app/env';
 import { page } from '$app/stores';
 import { derived } from 'svelte/store';
 import UrlPattern from 'url-pattern';
 
 const pattern = new UrlPattern('/namespaces/:namespace/*');
+const onlyInBrowser = <T>(fn: () => T): T => {
+  if (browser) return fn();
+};
 
 export const namespace = derived(page, ($page) => {
   const match = pattern.match($page.path);
-  const namespace = localStorage.getItem('currentNamespace') || 'default';
+  const namespace =
+    onlyInBrowser(
+      () => localStorage.getItem('currentNamespace') || 'default',
+    ) || 'default';
 
   if (match) {
-    localStorage.setItem('currentNamespace', match.namespace);
+    onlyInBrowser(() =>
+      localStorage.setItem('currentNamespace', match.namespace),
+    );
     return match.namespace;
   }
 
