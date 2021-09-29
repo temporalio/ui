b3f95332ed374d56c800205286af6bbc65c23826
Steve Kinney <hello@stevekinney.net>
2021-09-29 12:51:17 -0600

    Use Tailwind for Select

--- src/lib/components/filter-select.svelte
+++ src/lib/components/filter-select.svelte
@@ -7,7 +7,7 @@
 <div>
   <label for={id} class="text-gray-600 text-xs block">{name}</label>
   <select
-    class="block border-2 text-base p-2 min-w-full"
+    class="block border-2 text-base p-2 w-full h-10"
     {id}
     bind:value
     on:change
@@ -15,9 +15,3 @@
     <slot />
   </select>
 </div>
-
-<style>
-  select {
-    height: 40px;
-  }
-</style>
