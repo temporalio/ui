25e5544b00733d2554dccb10fd8694e1df7278be
Steve Kinney <hello@stevekinney.net>
2021-09-29 12:52:22 -0600

    Update filter-input.svelte

--- src/lib/components/filter-input.svelte
+++ src/lib/components/filter-input.svelte
@@ -6,11 +6,5 @@
 
 <div>
   <label for={id} class="text-gray-600 text-xs block">{name}</label>
-  <input class="block border-2 text-sm p-2" {id} bind:value />
+  <input class="block border-2 text-sm p-2 h-10" {id} bind:value />
 </div>
-
-<style>
-  input {
-    height: 40px;
-  }
-</style>
