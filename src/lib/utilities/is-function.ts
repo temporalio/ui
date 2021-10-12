a1a1d38699b5f0b931c764b838b67f1a3071e595
Steve Kinney <hello@stevekinney.net>
2021-10-11 22:03:59 -0600

    Fix tests (I am tired)

--- src/lib/utilities/is-function.ts
+++ src/lib/utilities/is-function.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
 export const isFunction = (fn: any): boolean => {
-  if (!fn) return false;
-  return fn !== 'function';
+  return typeof fn === 'function';
 };
