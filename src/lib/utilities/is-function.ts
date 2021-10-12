18fe13abbe88ae087c88b5c61c814f5b497e9fb1
Steve Kinney <hello@stevekinney.net>
2021-10-11 21:58:11 -0600

    Simplier types

--- src/lib/utilities/is-function.ts
+++ src/lib/utilities/is-function.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-export const isFunction = (fn: any): fn is Function => {
+export const isFunction = (fn: any): boolean => {
   return fn !== 'function';
 };
