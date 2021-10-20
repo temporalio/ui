f297bc2ff5692098310c22d6b2468fce1c5762f0
Joseph Rios <blankworldwide@gmail.com>
2021-10-20 13:46:06 -0700

    added dropdown feature

--- src/lib/components/terminate-workflow.svelte
+++ src/lib/components/terminate-workflow.svelte
@@ -1,17 +1,47 @@
 <script lang="ts">
   import { terminateWorkflow } from '$lib/services/terminate-service';
   import type { WorkflowExecution } from '$lib/models/workflow-execution';
+  import Icon from 'svelte-hero-icons/Icon.svelte';
+  import { ArrowCircleDown } from 'svelte-hero-icons';
 
   export let workflow: WorkflowExecution;
   export let namespace: string;
+
+  let dropdown: boolean = false;
+  let reason: string = '';
 </script>
 
-<button
-  class="mt-2 bg-red-600 p-1 text-white uppercase rounded text-sm"
-  on:click={() =>
-    terminateWorkflow({
-      workflow,
-      namespace,
-      reason: 'REPLACE_ME',
-    })}>Terminate</button
->
+<div class="flex flex-col">
+  <span on:click={() => (dropdown = !dropdown)}>
+    <Icon src={ArrowCircleDown} class={`mt-1 w-5 h-5 text-gray-500 cursor-pointer ${dropdown ? 'opened' : 'closed'}`} />
+  </span>
+
+  {#if !dropdown}
+    <div>
+      <input placeholder="Enter Reason" bind:value={reason} />
+      <button
+        class="mt-2 p-1 terminate rounded text-sm uppercase"
+        on:click={() =>
+          terminateWorkflow({
+            workflow,
+            namespace,
+            reason,
+          })}>Terminate</button
+      >
+    </div>
+  {/if}
+</div>
+
+<style lang="postcss">
+  .terminate {
+    @apply bg-red-200 text-red-600;
+  }
+
+  .closed {
+      @apply transition-transform;
+  }
+
+  .opened {
+      @apply transform rotate-180;
+  }
+</style>
