dd19d8424caf51962bb085786dd53d8370677f5e
Joseph Rios <blankworldwide@gmail.com>
2021-10-21 14:31:05 -0700

    changed namespace select to namespace dropdown

--- src/routes/_header.svelte
+++ src/routes/_header.svelte
@@ -1,8 +1,7 @@
 <script context="module" lang="ts">
   import type { LoadInput } from '@sveltejs/kit';
 
-  import NamespaceSelect from '$lib/components/namespace-select.svelte';
-  import OutsideOfDropdown from '$lib/components/outside-of-dropdown.svelte';
+  import NamespaceDropdown from '$lib/components/namespace-dropdown.svelte';
 
   export async function load({ fetch }: LoadInput) {
     const user = await fetch(import.meta.env.VITE_API + '/api/v1/me/').then(
@@ -16,9 +15,7 @@
 </script>
 
 <script lang="ts">
-  import { scale } from 'svelte/transition';
-  import Icon from 'svelte-hero-icons/Icon.svelte';
-  import { User } from 'svelte-hero-icons';
+  import { namespace } from '$lib/stores/namespace';
 
   export let user: { name?: string; email?: string; picture?: string } = {};
   export let showDropdown = false;
@@ -28,61 +25,26 @@
   id="header"
   class="h-16 w-full px-6 flex flex-row items-center justify-between static border-b-2"
 >
-  <NamespaceSelect />
-  <OutsideOfDropdown bind:showDropdown>
-    <button
-      on:click={() => (showDropdown = !showDropdown)}
-      class="menu focus:outline-none focus:shadow-solid w-8 h-8"
-    >
-      {#if user?.picture}
-        <img src={user.picture} alt={user.name} class="rounded-full" />
-      {:else}
-        <Icon src={User} class="rounded-full" />
-      {/if}
-    </button>
-
-    {#if showDropdown}
-      <div
-        in:scale={{ duration: 100, start: 0.95 }}
-        out:scale={{ duration: 75, start: 0.95 }}
-        class="z-40 origin-top-right absolute top-14 right-8 w-40 py-2 mt-1 border-2
-      rounded-md shadow-md bg-white"
-      >
-        <a
-          href="https://github.com/temporalio/web/issues/new/choose"
-          class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
-        >
-          Report Bug/Give Feedback
-        </a>
-        {#if user.email}
-          <span
-            href="/settings"
-            class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
-          >
-            {user.email}
-          </span>
-        {:else}
-          <a
-            href={import.meta.env.VITE_API + '/auth/sso'}
-            class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
-          >
-            Sign In
-          </a>
-        {/if}
-        <a
-          href="/settings"
-          class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
-          >Settings</a
-        >
-        {#if user.email}
-          <a
-            href={import.meta.env.VITE_API + '/auth/signout'}
-            class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
-          >
-            Sign Out
-          </a>
-        {/if}
-      </div>
+  <NamespaceDropdown />
+  <div class="flex flex-row justify-center items-center">
+    <a href="https://github.com/temporalio/web/issues/new/choose">
+      Report Bug/Give Feedback
+    </a>
+    {#if user.email}
+      <span href={`/namespaces/${$namespace}/settings`}>
+        {user.email}
+      </span>
+    {:else}
+      <a href={import.meta.env.VITE_API + '/auth/sso'}> Sign In </a>
     {/if}
-  </OutsideOfDropdown>
+  </div>
 </div>
+
+<style lang="postcss">
+  a {
+    @apply block px-4 py-2 text-sm;
+  }
+  a:hover {
+    @apply bg-purple-100 rounded-md;
+  }
+</style>
