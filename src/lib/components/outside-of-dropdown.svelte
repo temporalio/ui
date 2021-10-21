ddaf4183b0f6fea0724a98e2f4f2d5f1810450c6
Joseph Rios <blankworldwide@gmail.com>
2021-10-21 13:23:49 -0700

    dropdowns are now dissmissible when clicking out

---
+++ src/lib/components/outside-of-dropdown.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  // When using outside of dropdown component binded component must be named showDropdown
+  export let showDropdown: boolean;
+  let node: Node;
+
+  const windowClick = (event) => {
+    if (!node.contains(event.target)) {
+      showDropdown = false;
+    }
+  };
+</script>
+
+<svelte:window on:click={windowClick} />
+<div bind:this={node}>
+  <slot />
+</div>
--- src/routes/_header.svelte
+++ src/routes/_header.svelte
@@ -2,6 +2,7 @@
   import type { LoadInput } from '@sveltejs/kit';
 
   import NamespaceSelect from '$lib/components/namespace-select.svelte';
+  import OutsideOfDropdown from '$lib/components/outside-of-dropdown.svelte';
 
   export async function load({ fetch }: LoadInput) {
     const user = await fetch(import.meta.env.VITE_API + '/api/v1/me/').then(
@@ -20,7 +21,7 @@
   import { User } from 'svelte-hero-icons';
 
   export let user: { name?: string; email?: string; picture?: string } = {};
-  export let showMenu = false;
+  export let showDropdown = false;
 </script>
 
 <div
@@ -28,58 +29,60 @@
   class="h-16 w-full px-6 flex flex-row items-center justify-between static border-b-2"
 >
   <NamespaceSelect />
-  <button
-    on:click={() => (showMenu = !showMenu)}
-    class="menu focus:outline-none focus:shadow-solid w-8 h-8"
-  >
-    {#if user?.picture}
-      <img src={user.picture} alt={user.name} class="rounded-full" />
-    {:else}
-      <Icon src={User} class="rounded-full" />
-    {/if}
-  </button>
-
-  {#if showMenu}
-    <div
-      in:scale={{ duration: 100, start: 0.95 }}
-      out:scale={{ duration: 75, start: 0.95 }}
-      class="origin-top-right absolute top-14 right-8 w-40 py-2 mt-1 border-2
-      rounded-md shadow-md bg-white"
+  <OutsideOfDropdown bind:showDropdown>
+    <button
+      on:click={() => (showDropdown = !showDropdown)}
+      class="menu focus:outline-none focus:shadow-solid w-8 h-8"
     >
-      <a
-        href="https://github.com/temporalio/web/issues/new/choose"
-        class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
+      {#if user?.picture}
+        <img src={user.picture} alt={user.name} class="rounded-full" />
+      {:else}
+        <Icon src={User} class="rounded-full" />
+      {/if}
+    </button>
+
+    {#if showDropdown}
+      <div
+        in:scale={{ duration: 100, start: 0.95 }}
+        out:scale={{ duration: 75, start: 0.95 }}
+        class="z-40 origin-top-right absolute top-14 right-8 w-40 py-2 mt-1 border-2
+      rounded-md shadow-md bg-white"
       >
-        Report Bug/Give Feedback
-      </a>
-      {#if user.email}
-        <span
+        <a
+          href="https://github.com/temporalio/web/issues/new/choose"
+          class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
+        >
+          Report Bug/Give Feedback
+        </a>
+        {#if user.email}
+          <span
+            href="/settings"
+            class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
+          >
+            {user.email}
+          </span>
+        {:else}
+          <a
+            href={import.meta.env.VITE_API + '/auth/sso'}
+            class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
+          >
+            Sign In
+          </a>
+        {/if}
+        <a
           href="/settings"
           class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
+          >Settings</a
         >
-          {user.email}
-        </span>
-      {:else}
-        <a
-          href={import.meta.env.VITE_API + '/auth/sso'}
-          class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
-        >
-          Sign In
-        </a>
-      {/if}
-      <a
-        href="/settings"
-        class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
-        >Settings</a
-      >
-      {#if user.email}
-        <a
-          href={import.meta.env.VITE_API + '/auth/signout'}
-          class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
-        >
-          Sign Out
-        </a>
-      {/if}
-    </div>
-  {/if}
+        {#if user.email}
+          <a
+            href={import.meta.env.VITE_API + '/auth/signout'}
+            class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
+          >
+            Sign Out
+          </a>
+        {/if}
+      </div>
+    {/if}
+  </OutsideOfDropdown>
 </div>
