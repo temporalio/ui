0761e5add5768feb90a2a4c90b0f80fb8caa8f35
Rob Holland <rob@temporal.io>
2021-10-29 17:38:51 +0100

    Fix typo. (#132)

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/events/_event.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/events/_event.svelte
@@ -42,7 +42,7 @@
     {#if isDetailsJSONView}
       <CodeBlock
         content={{
-          verison: event.version,
+          version: event.version,
           TaskId: event.taskId,
         }}
       />
