2da6cd3956cd5d8f469802bd154bd89d63ecbb51
Steve Kinney <hello@stevekinney.net>
2021-11-29 13:23:55 -0700

    Use template literals

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/_tabs.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/_tabs.svelte
@@ -7,8 +7,8 @@
   const { namespace, workflow: id, run: runId } = $page.params;
 
   $: workflowUrl = getWorkflowExecutionUrl(namespace, { id, runId });
-  $: eventsUrl = workflowUrl + '/events';
-  $: activitiesUrl = workflowUrl + '/activities';
+  $: eventsUrl = `${workflowUrl}/events`;
+  $: activitiesUrl = `${workflowUrl}/activities`;
 </script>
 
 <nav class="mt-6 border-b-2 px-2 flex">
