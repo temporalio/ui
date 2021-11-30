e58237d90e124eee609f201e03f2cf80a3e64b2f
Steve Kinney <hello@stevekinney.net>
2021-11-29 17:24:29 -0700

    Swap in select field that sync with query params

---
+++ src/lib/utilities/update-query-parameters.ts
@@ -0,0 +1,23 @@
+type UpdateQueryParams = {
+  parameter: string;
+  value: string;
+  query: URLSearchParams;
+  path: string;
+  goto: (href: string, options: { replaceState: boolean }) => Promise<any>;
+  invalidate?: (href: string) => Promise<any>;
+};
+
+export const updateQueryParameters = ({
+  parameter,
+  value,
+  query,
+  path,
+  goto,
+}: UpdateQueryParams) => {
+  if (value) {
+    query.set(parameter, value);
+  } else {
+    query.delete(parameter);
+  }
+  goto(`${path}?${query}`, { replaceState: true });
+};
--- src/routes/namespaces/[namespace]/workflows/_workflow-filters.svelte
+++ src/routes/namespaces/[namespace]/workflows/_workflow-filters.svelte
@@ -1,18 +1,45 @@
 <script lang="ts">
-  import { namespace } from '$lib/stores/namespace';
-  import { createWorkflowStore } from '$lib/stores/workflows';
-
   import Select from '$lib/components/filter-select.svelte';
-  import TimeRangeSelect from '$lib/components/time-range-select.svelte';
+  import QuerySelect from '$lib/components/query-param-select.svelte';
 
   export let timeFormat: string = 'relative';
 
-  $: store = createWorkflowStore($namespace);
-  $: range = store.range;
+  const durations = [
+    '10 minutes',
+    '60 minutes',
+    '3 hours',
+    '24 hours',
+    '3 days',
+    '7 days',
+    '30 days',
+    '90 days',
+  ];
+
+  const statuses = {
+    All: null,
+    Running: 'Running',
+    TimedOut: 'Timed Out',
+    Completed: 'Completed',
+    Failed: 'Failed',
+    'Continued as New': 'ContinuedAsNew',
+    Canceled: 'Canceled',
+    Terminated: 'Terminated',
+  };
 </script>
 
 <section class="p-4 flex gap-8">
-  <TimeRangeSelect {range} key="workflows" />
+  <QuerySelect
+    label="Time Range"
+    parameter="time-range"
+    options={durations}
+    value="24 hours"
+  />
+  <QuerySelect
+    label="Workflow Status"
+    parameter="status"
+    options={statuses}
+    value={null}
+  />
   <Select
     id="filter-by-relative-time"
     name="Time Format"
