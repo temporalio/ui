e1e663b2df9e242de8b09084c9e9ae53be88e8d8
Steve Kinney <hello@stevekinney.net>
2021-11-30 08:57:43 -0700

    Remove query param persistence

--- src/lib/utilities/get-workflow-execution-url.test.ts
+++ src/lib/utilities/get-workflow-execution-url.test.ts
@@ -9,23 +9,4 @@ describe(getWorkflowExecutionUrl, () => {
       '/namespaces/default/workflows/id/run',
     );
   });
-
-  it('should display a workflow URL with query params', () => {
-    const workflow = { id: 'id', runId: 'run' } as WorkflowExecution;
-    const query = new URLSearchParams({ fullScreen: 'true' });
-
-    expect(getWorkflowExecutionUrl('default', workflow, query)).toBe(
-      '/namespaces/default/workflows/id/run?fullScreen=true',
-    );
-  });
-
-  it('should display a workflow URL with overiding query params', () => {
-    const workflow = { id: 'id', runId: 'run' } as WorkflowExecution;
-    const query = new URLSearchParams({ a: '1', b: '2' });
-    const overrides = new URLSearchParams({ b: '3' });
-
-    expect(getWorkflowExecutionUrl('default', workflow, query, overrides)).toBe(
-      '/namespaces/default/workflows/id/run?a=1&b=3',
-    );
-  });
 });
