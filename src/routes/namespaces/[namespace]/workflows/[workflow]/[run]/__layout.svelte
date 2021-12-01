11fa0db2000ab4cc3a219e741167e1c5c5c17bcc
Steve Kinney <hello@stevekinney.net>
2021-12-01 13:33:44 -0800

    Pull events from the API

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/__layout.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/__layout.svelte
@@ -1,16 +1,18 @@
 <script context="module" lang="ts">
   import type { LoadInput } from '@sveltejs/kit';
-  import { fetchWorkflow } from '$lib/services/workflow-service';
-  import { fetchEvents } from '$lib/services/events-service';
+  import { fetchWorkflowWithEventHistory } from '$lib/services/workflow-service';
 
   export async function load({ page }: LoadInput) {
     const { workflow: executionId, run: runId, namespace } = page.params;
-    const workflow = await fetchWorkflow({ namespace, executionId, runId });
-    const events = await fetchEvents({ namespace, executionId, runId });
+    const { workflow, events } = await fetchWorkflowWithEventHistory({
+      namespace,
+      executionId,
+      runId,
+    });
 
     return {
       props: { workflow },
-      stuff: { workflow, events: events.history.events },
+      stuff: { workflow, events },
     };
   }
 </script>
