c8dce9d8f912a5fcedfbfe329b3253f8ceb0e9b8
Joseph Rios <blankworldwide@gmail.com>
2021-12-01 12:11:01 -0800

    changed namespace select

---
+++ src/lib/components/select/option.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let value: string | boolean = null;
+</script>
+
+<option {value} class="border-0"><slot /></option>
+
+<style lang="postcss">
+  option {
+    color: gray;
+  }
+</style>
---
+++ src/lib/components/select/select.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let id: string = null;
+  export let value: string | boolean;
+  export let type: string = 'default';
+  export let condensed = false;
+</script>
+
+<div class="flex flex-col items-start justify-center">
+  <select
+    class="inline border-2 text-base p-2 w-full h-10 rounded-lg"
+    {id}
+    bind:value
+    on:change
+  >
+    <slot />
+  </select>
+</div>
