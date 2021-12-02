b5f040ff98d0e8d9995a23ef304bef6856bbba65
Joseph Rios <blankworldwide@gmail.com>
2021-12-02 15:42:59 -0800

    final test

--- src/lib/utilities/history-summary-plucker.test.ts
+++ src/lib/utilities/history-summary-plucker.test.ts
@@ -1,5 +1,27 @@
 import { getHistorySummary } from './history-summary-plucker';
 
 describe(getHistorySummary, () => {
-  it('should return back unchanged ev', () => {});
+  it('should return back unchanged ev', () => {
+    const event = {
+      workflowExecutionStartedEventAttributes: {
+        attempt: 1,
+        workflowRunTimeout: '100s',
+        identity: 'some identity',
+        input: 'true',
+        Parent: undefined,
+        workflowType: {
+          name: 'Rainbow Workflow',
+        },
+      },
+      eventType: 'WorkflowExecutionStarted',
+    };
+
+    expect(getHistorySummary(event)).toStrictEqual({
+      'Close Timeout': '100s',
+      identity: 'some identity',
+      input: 'true',
+      Parent: undefined,
+      Workflow: 'Rainbow Workflow',
+    });
+  });
 });
