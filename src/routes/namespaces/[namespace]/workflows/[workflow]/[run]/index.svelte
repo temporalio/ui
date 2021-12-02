514a48815d8ed2832ef29a1e1060a9394a1feded
Joseph Rios <blankworldwide@gmail.com>
2021-12-02 12:07:11 -0800

    reformated workflow details

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/index.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/index.svelte
@@ -20,39 +20,21 @@
 </script>
 
 <script lang="ts">
-  import { getWorkflowStartedAndCompletedEvents } from '$lib/utilities/get-started-and-completed-events';
-  import { getTaskQueueUrl } from '$lib/utilities/get-task-queue-url';
-
-  import ExecutionInformation from './_execution-information.svelte';
-  import TaskQueueInformation from './_task-queue-information.svelte';
   import PendingActivities from './_pending-activities.svelte';
   import CodeBlock from '$lib/components/code-block.svelte';
-  import TerminateWorkflow from '$lib/components/terminate-workflow.svelte';
   import Event from './_event.svelte';
+  import Tabs from './_tabs.svelte';
 
   export let workflow: WorkflowExecution;
   export let events: HistoryEvent[];
-  export let namespace: string;
 
   let format: EventFormat = 'grid';
 
-  $: inputAndResults = getWorkflowStartedAndCompletedEvents(events);
   $: pendingActivities = workflow?.pendingActivities;
-  $: taskQueue = workflow?.taskQueue;
-  $: historyEvents = workflow?.historyEvents;
-  $: href = getTaskQueueUrl(namespace, taskQueue);
 </script>
 
 <div class="execution-information px-6 py-6">
-  <div class="w-full flex">
-    <ExecutionInformation title="Start Time" value={workflow.startTime} />
-    <ExecutionInformation title="End Time" value={workflow.endTime} />
-    <TaskQueueInformation title="Task Queue" value={taskQueue} {href} />
-    <ExecutionInformation title="History Events" value={historyEvents} />
-    <CodeBlock heading="Input" content={inputAndResults.input} />
-    <CodeBlock heading="Result" content={inputAndResults.result} />
-  </div>
-  <TerminateWorkflow {namespace} {workflow} />
+  <Tabs />
   <div class="flex w-full mt-4">
     <PendingActivities activities={pendingActivities} />
   </div>
