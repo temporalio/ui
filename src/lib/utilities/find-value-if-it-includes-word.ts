314c1512624e35c50b5677753b375908b21728bf
Joseph Rios <blankworldwide@gmail.com>
2021-12-03 14:33:14 -0800

    removed

--- src/lib/utilities/find-value-if-it-includes-word.ts
+++
@@ -1,7 +0,0 @@
-export const findValueIfItIncludesWord = (object, word) => {
-  for (var property in object) {
-    if (object.hasOwnProperty(property) && property.toString().includes(word)) {
-      return object[property];
-    }
-  }
-};
