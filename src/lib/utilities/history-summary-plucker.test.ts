137b22501ffc7751152d277839f1e91066d4d8cc
Joseph Rios <blankworldwide@gmail.com>
2021-12-02 16:48:01 -0800

    format

--- src/lib/utilities/history-summary-plucker.test.ts
+++ src/lib/utilities/history-summary-plucker.test.ts
@@ -3,20 +3,20 @@ import { getHistorySummary } from './history-summary-plucker';
 describe(getHistorySummary, () => {
   it('should return back unchanged ev', () => {
     const event = {
-      "eventId": "11",
-      "eventTime": "2021-12-02T21:51:06.992475543Z",
-      "eventType": "WorkflowExecutionCompleted",
-      "version": "0",
-      "taskId": "1048850",
-      "workflowExecutionCompletedEventAttributes": {
-          "result": null,
-          "workflowTaskCompletedEventId": "10"
+      eventId: '11',
+      eventTime: '2021-12-02T21:51:06.992475543Z',
+      eventType: 'WorkflowExecutionCompleted',
+      version: '0',
+      taskId: '1048850',
+      workflowExecutionCompletedEventAttributes: {
+        result: null,
+        workflowTaskCompletedEventId: '10',
       },
-      "id": "11"
-  }
+      id: '11',
+    };
 
     expect(getHistorySummary(event)).toStrictEqual({
-      result: null
+      result: null,
     });
   });
 });
