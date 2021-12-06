c2dca92979f3131df83bd8a36804b36366091d79
Alex Thomsen <alex.scott.thomsen@gmail.com>
2021-12-06 16:19:36 -0700

    Add isPromise and tests for isPromise

--- src/lib/utilities/is-promise.test.ts
+++ src/lib/utilities/is-promise.test.ts
@@ -1,1 +1,79 @@
-// I promise to write some is promise tests.
+import { isPromise } from './is-promise';
+
+describe(isPromise, () => {
+  it('should return true if an object is a resolved promise', async () => {
+    expect(isPromise(Promise.resolve())).toBe(true);
+  });
+
+  it('should return true if an object is a rejected promise', async () => {
+    try {
+      expect(isPromise(await Promise.reject().catch())).toBe(true);
+    } catch (er) {}
+  });
+
+  it('should return true if an object is a promise', () => {
+    expect(isPromise(new Promise(() => {}))).toBe(true);
+  });
+
+  it('should return false if an object is a function', () => {
+    expect(isPromise(() => {})).toBe(false);
+  });
+
+  it('should return false if an object is partially promise like with a .then', () => {
+    expect(isPromise({ then: () => {} })).toBe(false);
+  });
+
+  it('should return false if an object is partially promise like with a .catch', () => {
+    expect(isPromise({ catch: () => {} })).toBe(false);
+  });
+
+  it('should return false if an object is partially promise like with a .finally', () => {
+    expect(isPromise({ finally: () => {} })).toBe(false);
+  });
+
+  it('should return false if an object is promise like (contains .then, .catch. finally)', () => {
+    expect(
+      isPromise({
+        then: () => {},
+        catch: () => {},
+        finally: () => {},
+      }),
+    ).toBe(false);
+  });
+
+  it('should return false if an object is a string', () => {
+    expect(isPromise('')).toBe(false);
+  });
+
+  it('should return false if an object is a number', () => {
+    expect(isPromise(1234)).toBe(false);
+  });
+
+  it('should return false if an object is -Infinity', () => {
+    expect(isPromise(-Infinity)).toBe(false);
+  });
+
+  it('should return false if an object is Infinity', () => {
+    expect(isPromise(Infinity)).toBe(false);
+  });
+
+  it('should return false if an object is NaN', () => {
+    expect(isPromise(NaN)).toBe(false);
+  });
+
+  it('should return false if an object is -NaN', () => {
+    expect(isPromise(-NaN)).toBe(false);
+  });
+
+  it('should return false if an object is an object', () => {
+    expect(isPromise({})).toBe(false);
+  });
+
+  it('should return false if an object is null', () => {
+    expect(isPromise(null)).toBe(false);
+  });
+
+  it('should return false if an object is undefined', () => {
+    expect(isPromise(undefined)).toBe(false);
+  });
+});
--- src/lib/utilities/is-promise.ts
+++ src/lib/utilities/is-promise.ts
@@ -2,7 +2,8 @@ export function isPromise(obj: any): obj is Promise<unknown> {
   if (
     typeof obj?.then === 'function' &&
     typeof obj?.catch === 'function' &&
-    typeof obj?.finally === 'function'
+    typeof obj?.finally === 'function' &&
+    obj.toString() === '[object Promise]'
   ) {
     return true;
   }
