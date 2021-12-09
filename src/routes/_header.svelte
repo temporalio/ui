83d4919eeb819bfe2d608499c058daede0184ed7
Steve Kinney <hello@stevekinney.net>
2021-12-09 09:23:43 -0700

    Remove side navigation; break out DataConvertorStatus (#163)

--- src/routes/_header.svelte
+++ src/routes/_header.svelte
@@ -15,39 +15,17 @@
 
 <script lang="ts">
   import { namespace } from '$lib/stores/namespace';
-  import { StatusOnline, StatusOffline } from 'svelte-hero-icons';
-  import {
-    dataConverterPort,
-    lastDataConverterStatus,
-  } from '$lib/stores/data-converter-config';
-  import Icon from 'svelte-hero-icons/Icon.svelte';
-  import Tooltip from '$lib/components/tooltip.svelte';
+  import DataConvertorStatus from '$lib/components/data-convertor-status.svelte';
   export let user: { name?: string; email?: string; picture?: string } = {};
 </script>
 
 <div
   id="header"
-  class="h-16 w-full px-6 flex flex-row items-center justify-between static border-b-2"
+  class="h-16 w-full px-6 flex flex-row items-center justify-between border-b-2"
 >
   <NamespaceSelect />
   <div class="flex flex-row justify-center items-center">
-    {#if $dataConverterPort}
-      {#if $lastDataConverterStatus === 'notRequested'}
-        <Tooltip text={'Data converter is configured'}>
-          <Icon src={StatusOffline} class="mx-auto text-gray-400 w-8 h-8" />
-        </Tooltip>
-      {:else if $lastDataConverterStatus === 'error'}
-        <Tooltip
-          text={`Data converter couldn't connect to the remote converter`}
-        >
-          <Icon src={StatusOffline} class="mx-auto text-red-400 w-8 h-8" />
-        </Tooltip>
-      {:else if $lastDataConverterStatus === 'success'}
-        <Tooltip text={'Data converter succesfully converted content'}>
-          <Icon src={StatusOnline} class="mx-auto text-green-500 w-8 h-8" />
-        </Tooltip>
-      {/if}
-    {/if}
+    <DataConvertorStatus />
     <a href="https://github.com/temporalio/web/issues/new/choose">
       Report Bug/Give Feedback
     </a>
@@ -65,6 +43,7 @@
   a {
     @apply block px-4 py-2 text-sm;
   }
+
   a:hover {
     @apply bg-purple-100 rounded-md;
   }
