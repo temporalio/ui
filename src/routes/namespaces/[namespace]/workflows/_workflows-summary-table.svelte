6790d4fe2c180fde6275c6d34dc89e3b4c63ccdc
Joseph <joseph@temporal.io>
2021-12-10 09:49:16 -0800

    removed styling (#167)
    Update workflows table to render properly.

--- src/routes/namespaces/[namespace]/workflows/_workflows-summary-table.svelte
+++ src/routes/namespaces/[namespace]/workflows/_workflows-summary-table.svelte
@@ -1,5 +1,5 @@
 <section
-  class="workflow-table border-collapse border-2 border-gray-300 overflow-hidden w-full rounded-lg"
+  class="workflow-table border-collapse border-2 border-gray-300 overflow-hidden w-full h-screen rounded-lg"
 >
   <div class="bg-gray-200 flex flex-row p-2">
     <div class="w-3/12 text-left">Workflow ID</div>
@@ -10,11 +10,3 @@
   </div>
   <slot />
 </section>
-
-<!--  this style is mainly for keeping the height a certain level so the scroll
-will feel more natural | may need revisions -->
-<style lang="postcss">
-  .workflow-table {
-    height: calc(100vh - 25%);
-  }
-</style>
