74a4c12ed3d6402ce4882db9e2e3749d63da50a1
Steve Kinney <hello@stevekinney.net>
2021-12-26 07:33:37 -0800

    Refactor out the event table

---
+++ src/lib/components/event-table.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { setContext } from 'svelte';
+
+  import Event from './event.svelte';
+  import VirtualList from '@sveltejs/svelte-virtual-list';
+
+  export let events: (HistoryEventWithId | PendingActivity)[];
+</script>
+
+<section
+  class="flex flex-col border-2 border-gray-300 rounded-lg w-full event-history"
+>
+  <div class="flex w-full">
+    <header class="table-header border-r-2 rounded-tl-lg w-1/3">
+      <h3>Summary</h3>
+      <slot name="filters" />
+    </header>
+    <header class="table-header rounded-tr-lg w-2/3">
+      <h3>Details</h3>
+    </header>
+  </div>
+  <div class="flex h-full overflow-y-hidden">
+    <div
+      class="flex flex-col h-full w-1/3 border-r-2 border-gray-300 rounded-bl-lg"
+    >
+      <div class="h-full rounded-bl-lg overflow-y-scroll">
+        <VirtualList items={events} let:item>
+          <Event event={item} />
+        </VirtualList>
+      </div>
+    </div>
+    <div class="flex flex-col h-full w-2/3">
+      <div
+        class="h-full  overflow-y-scroll overflow-x-hidden rounded-br-lg px-4"
+      >
+        <slot name="details">
+          <div class="flex items-center justify-center w-full h-full">
+            <p class="text-gray-600 italic">(Nothing selected.)</p>
+          </div>
+        </slot>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style lang="postcss">
+  .event-history {
+    height: calc(100vh - 360px);
+  }
+
+  .table-header {
+    @apply bg-gray-100 text-gray-800 font-semibold p-4 border-b-2 flex justify-between items-center;
+  }
+</style>
--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/history/full/__layout.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/history/full/__layout.svelte
@@ -29,10 +29,9 @@
 
   import { eventTypeInCategory } from '$lib/utilities/get-event-categorization';
 
-  import Event from '$lib/components/event.svelte';
-  import VirtualList from '@sveltejs/svelte-virtual-list';
   import FilterSelect from '$lib/components/select/filter-select.svelte';
   import Option from '$lib/components/select/option.svelte';
+  import EventTable from '$lib/components/event-table.svelte';
 
   export let events: HistoryEventWithId[];
   export let pendingActivities: PendingActivity[];
@@ -46,54 +45,19 @@
   $: eventsAndActivities = [...pendingActivities, ...visibleEvents];
 </script>
 
-<section
-  class="flex flex-col border-2 border-gray-300 rounded-lg w-full event-history"
->
-  <div class="flex w-full">
-    <header class="table-header border-r-2 rounded-tl-lg w-1/3">
-      <h3>Summary</h3>
-      <div>
-        <FilterSelect parameter="event-type" bind:value={category}>
-          <Option value={null}>All</Option>
-          <Option value="activity">Activity</Option>
-          <Option value="command">Command</Option>
-          <Option value="signal">Signal</Option>
-          <Option value="timer">Timer</Option>
-          <Option value="child-workflow">Child Workflow</Option>
-          <Option value="workflow">Workflow</Option>
-        </FilterSelect>
-      </div>
-    </header>
-    <header class="table-header rounded-tr-lg w-2/3">
-      <h3>Details</h3>
-    </header>
+<EventTable events={eventsAndActivities}>
+  <div slot="filters">
+    <FilterSelect parameter="event-type" bind:value={category}>
+      <Option value={null}>All</Option>
+      <Option value="activity">Activity</Option>
+      <Option value="command">Command</Option>
+      <Option value="signal">Signal</Option>
+      <Option value="timer">Timer</Option>
+      <Option value="child-workflow">Child Workflow</Option>
+      <Option value="workflow">Workflow</Option>
+    </FilterSelect>
   </div>
-  <div class="flex h-full overflow-y-hidden">
-    <div
-      class="flex flex-col h-full w-1/3 border-r-2 border-gray-300 rounded-bl-lg"
-    >
-      <div class="h-full rounded-bl-lg overflow-y-scroll">
-        <VirtualList items={eventsAndActivities} let:item>
-          <Event event={item} />
-        </VirtualList>
-      </div>
-    </div>
-    <div class="flex flex-col h-full w-2/3">
-      <div
-        class="h-full  overflow-y-scroll overflow-x-hidden rounded-br-lg px-4"
-      >
-        <slot />
-      </div>
-    </div>
+  <div slot="details">
+    <slot />
   </div>
-</section>
-
-<style lang="postcss">
-  .event-history {
-    height: calc(100vh - 360px);
-  }
-
-  .table-header {
-    @apply bg-gray-100 text-gray-800 font-semibold p-4 border-b-2 flex justify-between items-center;
-  }
-</style>
+</EventTable>
