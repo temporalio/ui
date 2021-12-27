4972b3a16a177056375a1ad7365431ae89c9e429
Steve Kinney <hello@stevekinney.net>
2021-12-27 11:51:32 -0800

    Swap in routeFor

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/history/__layout.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/history/__layout.svelte
@@ -1,16 +1,13 @@
 <script context="module" lang="ts">
-  import { getWorkflowExecutionUrl } from '$lib/utilities/get-workflow-execution-url';
   import type { LoadInput } from '@sveltejs/kit';
+  import type { WorkflowParameters } from '$lib/utilities/route-for';
 
-  export async function load({ stuff, page }: LoadInput) {
-    const { workflow, run: runId, namespace } = page.params;
-
-    const path =
-      getWorkflowExecutionUrl(namespace, { id: workflow, runId }) + '/history';
+  export async function load({ page }: LoadInput) {
+    const { workflow: workflowId, run: runId, namespace } = page.params;
 
     return {
       props: {
-        path,
+        workflow: { workflowId, runId, namespace },
       },
     };
   }
@@ -26,8 +23,9 @@
 
   import ToggleButton from '$lib/components/toggle-button.svelte';
   import ToggleButtons from '$lib/components/toggle-buttons.svelte';
+  import { routeFor } from '$lib/utilities/route-for';
 
-  export let path: string;
+  export let workflow: WorkflowParameters;
 </script>
 
 <section class="flex flex-col gap-4">
@@ -35,9 +33,18 @@
     <h3 class="text-lg font-semibold">Event History</h3>
     <div class="flex gap-4">
       <ToggleButtons>
-        <ToggleButton icon={faStream} href="{path}/full" />
-        <ToggleButton icon={faLayerGroup} href="{path}/compact" />
-        <ToggleButton icon={faCode} href="{path}/json" />
+        <ToggleButton
+          icon={faStream}
+          href={routeFor('workflow.events.full', workflow)}
+        />
+        <ToggleButton
+          icon={faLayerGroup}
+          href={routeFor('workflow.events.compact', workflow)}
+        />
+        <ToggleButton
+          icon={faCode}
+          href={routeFor('workflow.events.json', workflow)}
+        />
       </ToggleButtons>
       <ToggleButton icon={faDownload} href="#no-implemented" />
     </div>
