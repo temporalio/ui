71f41ec75850b12299e83ca54b489c431a188eea
Steve Kinney <hello@stevekinney.net>
2021-12-28 13:10:53 -0800

    Update __layout.svelte

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/history/compact/__layout.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/history/compact/__layout.svelte
@@ -16,17 +16,11 @@
 
 <script lang="ts">
   import { Activities } from '$lib/models/activity';
-  import { eventTypeInCategory } from '$lib/utilities/get-event-categorization';
-
-  import FilterSelect from '$lib/components/select/filter-select.svelte';
-  import Option from '$lib/components/select/option.svelte';
   import EventTable from '$lib/components/event-table.svelte';
 
   export let events: HistoryEventWithId[];
 
   let activities = new Activities(events);
-
-  let category: EventTypeCategory = null;
 </script>
 
 <EventTable events={activities}>
