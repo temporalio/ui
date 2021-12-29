c2fbd280a02839919012c2ff3e6ae44cba6f2b48
Joseph Rios <blankworldwide@gmail.com>
2021-12-29 01:35:07 -0800

    current time

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/stack-trace.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/stack-trace.svelte
@@ -3,15 +3,18 @@
   import Icon from 'svelte-fa';
   import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
   import Button from '$lib/components/button.svelte';
+
+  let currentdate = new Date();
+  $: datetime = currentdate.toLocaleTimeString();
 </script>
 
 <section>
   <div class="flex items-center">
-    <Button>
+    <Button on:click={() => (currentdate = new Date())}>
       <Icon icon={faSyncAlt} scale={0.8} class="block w-full h-full" />
       Refresh</Button
     >
-    <p class="ml-9">Stack Trace at</p>
+    <p class="ml-9">Stack Trace at {datetime}</p>
   </div>
-  <CodeBlock content={'console.log(hello);'} />
+  <CodeBlock content={'test'} />
 </section>
