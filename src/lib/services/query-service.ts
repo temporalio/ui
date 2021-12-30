b45ddfa0fbc31d6e28c701ad3b037ae1e3927a75
Joseph Rios <blankworldwide@gmail.com>
2021-12-30 02:07:07 -0800

    added stack trace

---
+++ src/lib/services/query-service.ts
@@ -0,0 +1,28 @@
+import type { WorkflowExecution } from '$lib/models/workflow-execution';
+import { requestFromAPI } from '$lib/utilities/request-from-api';
+
+type StackTraceOptions = {
+  workflow: WorkflowExecution;
+  namespace: string;
+};
+
+export async function getWorkflowStackTrace({
+  workflow,
+  namespace,
+}: StackTraceOptions): Promise<null> {
+  return await requestFromAPI<null>(
+    `/namespaces/${namespace}/workflows/${workflow.id}/executions/${workflow.runId}/query`,
+    {
+      options: { method: 'POST', body: JSON.stringify({
+        "execution": {
+          "workflowId": workflow.id,
+          "runId": workflow.runId
+        },
+        "query": {
+          "queryType": "__stack_trace"
+        }
+      }) },
+      shouldRetry: false,
+    },
+  );
+}
--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/stack-trace.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/stack-trace.svelte
@@ -1,20 +1,55 @@
+<script context="module" lang="ts">
+  import type { LoadInput } from '@sveltejs/kit';
+  import type { WorkflowExecution } from '$lib/models/workflow-execution';
+
+  export async function load({ stuff }: LoadInput) {
+    const { workflow } = stuff as {
+      workflow: WorkflowExecution;
+    };
+
+    return {
+      props: {
+        workflow,
+      },
+    };
+  }
+</script>
+
 <script lang="ts">
   import CodeBlock from '$lib/components/code-block.svelte';
   import Icon from 'svelte-fa';
+  import { namespace } from '$lib/stores/namespace';
   import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
   import Button from '$lib/components/button.svelte';
+  import { getWorkflowStackTrace } from '$lib/services/query-service';
+
+  export let workflow: WorkflowExecution;
 
   let currentdate = new Date();
   $: datetime = currentdate.toLocaleTimeString();
+  $: data = getWorkflowStackTrace({ workflow, namespace: $namespace });
 </script>
 
 <section>
-  <div class="flex items-center">
-    <Button on:click={() => (currentdate = new Date())}>
-      <Icon icon={faSyncAlt} scale={0.8} class="block w-full h-full" />
-      Refresh</Button
-    >
-    <p class="ml-9">Stack Trace at {datetime}</p>
-  </div>
-  <CodeBlock content={'test'} />
+  {#if String(workflow.status) === 'Running'}
+    <div class="flex items-center">
+      <Button
+        on:click={() => {
+          data = getWorkflowStackTrace({ workflow, namespace: $namespace });
+          currentdate = new Date();
+        }}
+      >
+        <Icon icon={faSyncAlt} scale={0.8} class="block w-full h-full" />
+        Refresh</Button
+      >
+      <p class="ml-9">Stack Trace at {datetime}</p>
+    </div>
+    {#await data}
+      <div>loading</div>
+    {:then { queryResult }}
+      <CodeBlock content={queryResult.payloads[0].data} />
+    {/await}
+  {:else}
+    <h2>Will add empty state</h2>
+  {/if}
 </section>
