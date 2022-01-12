c8666fdf8cf24aa17a69b53b9757c67f81e9e047
Steve Kinney <hello@stevekinney.net>
2022-01-12 09:41:58 -0700

    Adjust spacing from header (#223)

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/__layout.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/__layout.svelte
@@ -31,7 +31,7 @@
   $: setContext('workflow', workflow);
 </script>
 
-<main class="flex flex-col gap-4 h-full">
+<main class="flex flex-col gap-6 h-full">
   <Header {namespace} />
   <slot />
 </main>
