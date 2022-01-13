f1f66eaeb258903f1bd6f4619861cc0b5b9e094c
Alex Thomsen <alex.thomsen@temporal.io>
2022-01-13 08:53:55 -0700

    Convertor -> Converter

---
+++ src/lib/components/data-converter-status.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import Icon from 'svelte-fa';
+
+  import {
+    faLongArrowAltDown,
+    faLongArrowAltUp,
+  } from '@fortawesome/free-solid-svg-icons';
+
+  import {
+    dataConverterPort,
+    lastDataConverterStatus,
+  } from '$lib/stores/data-converter-config';
+
+  import Tooltip from '$lib/components/tooltip.svelte';
+</script>
+
+{#if $dataConverterPort}
+  {#if $lastDataConverterStatus === 'notRequested'}
+    <Tooltip text={'Data converter is configured'}>
+      <div class="flex">
+        <Icon
+          icon={faLongArrowAltDown}
+          scale={1}
+          class="text-blue-200 block w-full h-full"
+        />
+        <Icon
+          icon={faLongArrowAltUp}
+          scale={1}
+          class="text-blue-200 block w-full h-full"
+        />
+      </div>
+    </Tooltip>
+  {:else if $lastDataConverterStatus === 'error'}
+    <Tooltip text={`Data converter couldn't connect to the remote converter`}>
+      <div class="flex">
+        <Icon
+          icon={faLongArrowAltDown}
+          scale={1}
+          class="text-red-400 block w-full h-full"
+        />
+        <Icon
+          icon={faLongArrowAltUp}
+          scale={1}
+          class="text-red-400 block w-full h-full"
+        />
+      </div>
+    </Tooltip>
+  {:else if $lastDataConverterStatus === 'success'}
+    <Tooltip text={'Data converter succesfully converted content'}>
+      <div class="flex">
+        <Icon
+          icon={faLongArrowAltDown}
+          scale={1}
+          class="text-blue-200 block w-full h-full"
+        />
+        <Icon
+          icon={faLongArrowAltUp}
+          scale={1}
+          class="text-blue-200 block w-full h-full"
+        />
+      </div>
+    </Tooltip>
+  {/if}
+{/if}
--- src/routes/_header.svelte
+++ src/routes/_header.svelte
@@ -4,7 +4,7 @@
 
 <script lang="ts">
   import { namespace } from '$lib/stores/namespace';
-  import DataConvertorStatus from '$lib/components/data-convertor-status.svelte';
+  import DataConverterStatus from '$lib/components/data-converter-status.svelte';
   import { settings } from '$lib/stores/settings';
   import { user } from '$lib/stores/user';
   import NavigationLink from './_navigation-link.svelte';
@@ -27,10 +27,9 @@
       Settings
     </NavigationLink>
   </div>
-
   <div class="flex justify-end gap-4 col-span-3 items-center">
     <div class="text-right">
-      <DataConvertorStatus />
+      <DataConverterStatus />
     </div>
     <a
       class="header-button"
