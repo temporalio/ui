151a2ad72e31361dbd00ae9691bc312f8f42a963
Alex Thomsen <alex.scott.thomsen@gmail.com>
2022-01-14 15:26:32 -0700

    Add comment for app/env mock

--- src/lib/utilities/__mocks__/$app/env.ts
+++ src/lib/utilities/__mocks__/$app/env.ts
@@ -1,1 +1,2 @@
-export const browser = jest.fn();
+// jsDom works for some dom implementation stuffs but $app/env browser fails whenever it's imported so lets mock it instead
+export const browser = jest.fn(() => false);
