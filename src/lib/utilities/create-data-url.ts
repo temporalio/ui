5c9f4cb69f126de6456231bfee0dd7236dd05804
feedmeapples <aksenov.ro@outlook.com>
2022-01-14 09:25:31 -0500

    remove unused create-data-url

--- src/lib/utilities/create-data-url.ts
+++
@@ -1,9 +0,0 @@
-export const createDataUrl = (
-  events: Parameters<typeof JSON.stringify>[0],
-): string => {
-  const data = JSON.stringify(events, undefined, 2);
-  const dataUri =
-    'data:application/json;charset=utf-8,' + encodeURIComponent(data);
-
-  return dataUri;
-};
