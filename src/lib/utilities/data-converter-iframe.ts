33315e2ec85acf049490b08c95fbb1648f9c9d69
Rob Holland <rob@temporal.io>
2022-02-08 16:44:35 +0000

    Simple fetch.

--- src/lib/utilities/data-converter-iframe.ts
+++ src/lib/utilities/data-converter-iframe.ts
@@ -19,56 +19,13 @@ export const createIframe = (
     };
   }
 
-  const iframe = document.createElement('iframe');
-  iframe.style.display = 'none';
-  document.body.appendChild(iframe);
-
-  const open: Promise<boolean> = new Promise((resolve) => {
-    iframe.src = endpoint + '/js';
-    // Note: the load event will always fire, it does not indicate success.
-    // There is no way to detect cross-origin iframe load errors :(
-    iframe.addEventListener('load', () => resolve(true));
-  });
-
-  let target: MessageEventSource;
-
-  window.addEventListener('message', (event) => {
-    if (event.origin != endpoint) {
-      return;
-    }
-
-    switch (event.data.type) {
-      case 'data_encoder_ready':
-        target = event.source;
-        break;
-      case 'encoded':
-      case 'decoded':
-        requests[event.data.requestId](event.data.payload);
-        delete requests[event.data.requestId];
-        break;
-    }
-  });
-
-  let requestId = 0;
-  const requests = {};
-
-  const nextRequestId = (): number => {
-    return (requestId += 1);
-  };
-
   return {
     configured: true,
-    isOpened: () => !!target,
-    open: () => open,
+    isOpened: () => true,
+    open: () => Promise.resolve(true),
     decode: async (payload: Payload): Promise<Payload> => {
-      const id = nextRequestId();
-      return new Promise((resolve) => {
-        requests[id] = resolve;
-        target.postMessage(
-          { requestId: id, type: 'decode', payload: payload },
-          { targetOrigin: endpoint },
-        );
-      });
+      let response = await fetch(endpoint + '/decode', { method: 'POST', body: JSON.stringify(payload) })
+      return response.json()
     },
   };
 };
