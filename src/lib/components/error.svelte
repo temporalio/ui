09fc7abed7dedf0729ff88ce40b50eb5cc80b212
Alex Thomsen <alex.scott.thomsen@gmail.com>
2022-03-01 14:47:05 -0700

    Have error pages swap out when navigating to other pages.

--- src/lib/components/error.svelte
+++ src/lib/components/error.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-  import { dev } from '$app/env';
+  import { browser, dev } from '$app/env';
   import { page } from '$app/stores';
   import { isNetworkError } from '$lib/utilities/is-network-error';
+  import { createEventDispatcher } from 'svelte';
+  import { beforeNavigate } from '$app/navigation';
 
   export let error: globalThis.Error = null;
   export let status = 500;
@@ -10,7 +12,13 @@
     status = error.statusCode;
   }
 
+  const dispatch = createEventDispatcher();
+
   $: currentLocation = $page.url.toString();
+
+  beforeNavigate(() => {
+    dispatch('clearError', {});
+  });
 </script>
 
 <section aria-roledescription="error" class="text-center align-middle mt-32">
@@ -19,6 +27,11 @@
   <p class="text-lg">
     <a
       href={currentLocation}
+      on:click={() => {
+        if (browser) {
+          window.location.reload();
+        }
+      }}
       target="_self"
       class="underline-offset-2 underline">Try a refresh</a
     >
