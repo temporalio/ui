ce50fbfe456316720f652ffb052e676f3eff7665
Steve Kinney <hello@stevekinney.net>
2022-03-14 12:24:51 -0600

    Get rid of event history being its own component

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/history/__layout.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/history/__layout.svelte
@@ -29,8 +29,7 @@
 
   import { routeFor } from '$lib/utilities/route-for';
 
-  import EventTable from '$lib/components/event-table.svelte';
-
+  import Event from '$lib/components/event.svelte';
   import ExportHistory from '$lib/components/export-history.svelte';
   import ToggleButton from '$lib/components/toggle-button.svelte';
   import ToggleButtons from '$lib/components/toggle-buttons.svelte';
@@ -45,7 +44,7 @@
   $: visibleEvents = isCompact ? eventGroups : events;
 </script>
 
-<section class="flex flex-col gap-4 max-h-screen">
+<section class="flex flex-col gap-4">
   <InputAndResults {events} />
   <section id="event-history">
     <nav class="flex gap-4 justify-between items-end">
@@ -64,20 +63,49 @@
         <ExportHistory />
       </div>
     </nav>
-    <EventTable events={visibleEvents}>
-      <div slot="filters">
-        <div>
-          <input
-            type="checkbox"
-            id="event-history-view"
-            bind:checked={isCompact}
-          />
-          <label for="event-history-view">Show compact view</label>
+    <section
+      class="flex flex-col border-2 border-gray-300 rounded-lg w-full mb-6"
+    >
+      <div class="flex w-full">
+        <header class="table-header border-r-2 rounded-tl-lg w-1/3">
+          <h3>Summary</h3>
+          <div>
+            <input
+              type="checkbox"
+              id="event-history-view"
+              bind:checked={isCompact}
+            />
+            <label for="event-history-view">Show compact view</label>
+          </div>
+        </header>
+        <header class="table-header rounded-tr-lg w-2/3">
+          <h3>Details</h3>
+        </header>
+      </div>
+      <div class="flex overflow-y-hidden">
+        <div
+          class="flex flex-col w-1/3 border-r-2 border-gray-300 rounded-bl-lg"
+        >
+          <div class="rounded-bl-lg overflow-y-scroll">
+            {#each visibleEvents as event (event.id)}
+              <Event {event} />
+            {/each}
+          </div>
+        </div>
+        <div class="flex flex-col w-2/3">
+          <div
+            class="overflow-y-scroll overflow-x-hidden rounded-br-lg px-4 w-full py-4"
+          >
+            <slot />
+          </div>
         </div>
       </div>
-      <div slot="details" class="w-full h-full py-4">
-        <slot />
-      </div>
-    </EventTable>
+    </section>
   </section>
 </section>
+
+<style lang="postcss">
+  .table-header {
+    @apply bg-gray-100 text-gray-800 font-semibold p-4 flex justify-between items-center border-b-2;
+  }
+</style>
