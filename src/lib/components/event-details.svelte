629e53c879287b0465b473b42f342c5edd937d6a
Steve Kinney <hello@stevekinney.net>
2022-03-15 11:43:52 -0600

    Hide null and undefined values in event details (#329)
    * Hide null and undefined values
    
    * Update event-details.svelte
    
    * Update event-details.svelte

--- src/lib/components/event-details.svelte
+++ src/lib/components/event-details.svelte
@@ -10,22 +10,20 @@
 </script>
 
 {#each Object.entries(attributes) as [key, value] (key)}
-  <article
-    class="flex items-center content-start w-full border-b-2 last:border-b-0 border-gray-200 py-1"
-  >
-    <h4 class="w-96 flex-grow">{format(key)}</h4>
-    <div class="flex-grow w-full">
-      {#if value === null}
-        <p class="font-mono whitespace-nowrap flex-grow">
-          {value}
-        </p>
-      {:else if typeof value === 'object'}
-        <CodeBlock content={value} />
-      {:else if value}
-        <p><span class="bg-gray-300 text-gray-700 px-2">{value}</span></p>
-      {:else}
-        <p class="text-gray-500">Undefined</p>
-      {/if}
-    </div>
-  </article>
+  {#if value !== null && value !== undefined}
+    <article
+      class="flex items-center content-start w-full border-b-2 last:border-b-0 border-gray-200 py-1"
+    >
+      <h4 class="w-96 flex-grow">{format(key)}</h4>
+      <div class="flex-grow w-full">
+        {#if typeof value === 'object'}
+          <CodeBlock content={value} />
+        {:else if value}
+          <p><span class="bg-gray-300 text-gray-700 px-2">{value}</span></p>
+        {:else}
+          <p class="text-gray-500">Undefined</p>
+        {/if}
+      </div>
+    </article>
+  {/if}
 {/each}
