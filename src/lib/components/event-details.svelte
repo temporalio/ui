408318df810b19b77166fbfaeddc4516c2c9c602
Steve Kinney <hello@stevekinney.net>
2022-03-16 08:17:02 -0600

    Don't display "0s" for timeouts, closes #335

--- src/lib/components/event-details.svelte
+++ src/lib/components/event-details.svelte
@@ -7,6 +7,7 @@
     if (value === null) return false;
     if (value === undefined) return false;
     if (value === '') return false;
+    if (value === '0s') return false;
     return true;
   };
 
