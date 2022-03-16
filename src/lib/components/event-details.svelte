d786ed3d8949d5d7764bf91288d690bf0d2ed9b4
Steve Kinney <hello@stevekinney.net>
2022-03-16 08:43:43 -0600

    Remove additional from the event details table, closes #333, #334, #335, and #336 (#340)
    * Remove empty strings, closes #336
    
    * Don't display "0s" for timeouts, closes #335
    
    * Hide the `type` property, closes #334
    
    * Remove result heading, closes #333

--- src/lib/components/event-details.svelte
+++ src/lib/components/event-details.svelte
@@ -3,6 +3,15 @@
 
   import CodeBlock from './code-block.svelte';
 
+  const shouldDisplay = (key: string, value: unknown): boolean => {
+    if (value === null) return false;
+    if (value === undefined) return false;
+    if (value === '') return false;
+    if (value === '0s') return false;
+    if (key === 'type') return false;
+    return true;
+  };
+
   export let attributes:
     | EventAttribute
     | PendingActivity
@@ -10,7 +19,7 @@
 </script>
 
 {#each Object.entries(attributes) as [key, value] (key)}
-  {#if value !== null && value !== undefined}
+  {#if shouldDisplay(key, value)}
     <article
       class="flex items-center content-start w-full border-b-2 last:border-b-0 border-gray-200 py-1"
     >
@@ -18,10 +27,8 @@
       <div class="flex-grow w-full">
         {#if typeof value === 'object'}
           <CodeBlock content={value} />
-        {:else if value}
-          <p><span class="bg-gray-300 text-gray-700 px-2">{value}</span></p>
         {:else}
-          <p class="text-gray-500">Undefined</p>
+          <p><span class="bg-gray-300 text-gray-700 px-2">{value}</span></p>
         {/if}
       </div>
     </article>
