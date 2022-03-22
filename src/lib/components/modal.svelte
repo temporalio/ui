003e8be5578f481ba5b464f26320e8cfbc0fec50
Alex Tideman <alex.tideman@gmail.com>
2022-03-22 13:50:27 -0500

    Update modal UI

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Icon from 'svelte-fa';
+  import { faTimes } from '@fortawesome/free-solid-svg-icons';
   import { createEventDispatcher } from 'svelte';
   import Button from './button.svelte';
 
@@ -12,9 +14,15 @@
   <div class="modal">
     <div class="overlay" />
     <div class="body">
+      <div
+        class="float-right cursor-pointer p-6"
+        on:click={() => dispatch('cancelModal', {})}
+      >
+        <Icon icon={faTimes} />
+      </div>
       <div class="title">
         <slot name="title">
-          <span>Title</span>
+          <h3>Title</h3>
         </slot>
       </div>
       <div class="content">
@@ -22,13 +30,11 @@
           <span>Content</span>
         </slot>
       </div>
-      <div
-        class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600"
-      >
+      <div class="flex justify-end items-center p-6 space-x-2">
         <Button secondary on:click={() => dispatch('cancelModal', {})}
           >Cancel</Button
         >
-        <Button on:click={() => dispatch('confirmModal', {})}
+        <Button destroy on:click={() => dispatch('confirmModal', {})}
           >{confirmText}</Button
         >
       </div>
@@ -46,11 +52,11 @@
   }
 
   .body {
-    @apply bg-white w-full lg:h-max lg:w-1/2  mx-auto rounded-lg shadow-xl z-50 overflow-y-auto;
+    @apply bg-white w-full md:h-max lg:w-1/3  mx-auto rounded-lg shadow-xl z-50 overflow-y-auto;
   }
 
   .title {
-    @apply bg-gray-100 py-5 px-8 text-2xl;
+    @apply bg-white pt-8 pb-0 px-8 text-2xl;
   }
 
   .content {
--- src/lib/components/terminate-workflow.svelte
+++ src/lib/components/terminate-workflow.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import { goto } from '$app/navigation';
-
-  import { routeForWorkflow } from '$lib/utilities/route-for';
   import { handleError } from '$lib/utilities/handle-error';
   import { terminateWorkflow } from '$lib/services/terminate-service';
   import { notifications } from '$lib/stores/notifications';
@@ -40,22 +37,24 @@
 </script>
 
 {#if isEligibleForTermination(workflow)}
-  <Button destroy on:click={show}>Terminate</Button>
+  <Button destroy thin on:click={show}>Terminate</Button>
   <Modal
     open={showConfirmation}
     confirmText="Terminate"
     on:cancelModal={cancel}
     on:confirmModal={terminate}
   >
-    <span slot="title">
-      Are you sure you want to terminate this workflow?
-    </span>
-    <span slot="content">
+    <h3 slot="title">Terminate Workflow</h3>
+    <div slot="content">
+      <p>
+        Are you sure you want to terminate this workflow? This action cannot be
+        undone.
+      </p>
       <input
-        class="block w-full border border-gray-200 p-4"
-        placeholder="Reason"
+        class="block w-full border border-gray-200 rounded-md p-2 mt-4"
+        placeholder="Enter a reason"
         bind:value={reason}
       />
-    </span>
+    </div>
   </Modal>
 {/if}
