776edc48071ee9d318fcbbf4aa2263fec54617b4
Steve Kinney <hello@stevekinney.net>
2022-03-22 17:02:11 -0600

    Standardize links (#369)
    * Standardize links; create Link component
    
    * Update link.svelte
    
    * Update link.svelte
    
    * Update _event-details.svelte

---
+++ src/lib/components/link.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let href: string;
+  export let active = false;
+</script>
+
+<a
+  {href}
+  class:active
+  {...$$props}
+  class="border-b-2 border-blue-600 {$$props.class} hover:text-blue-900 hover:no-underline"
+>
+  <slot />
+</a>
+
+<style lang="postcss">
+  .active {
+    @apply text-blue-700 border-b-2 border-blue-600;
+  }
+</style>
