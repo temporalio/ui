574fbc3044a00fa392e76c14abb27c11e120a780
Steve Kinney <hello@stevekinney.net>
2022-03-23 12:48:42 -0600

    Add tests for redirects

--- cypress/integration/check-redirects.spec.js
+++ cypress/integration/check-redirects.spec.js
@@ -1,11 +1,51 @@
 /// <reference types="cypress" />
 
-describe('Workflow Executions List', () => {
-  it('should redirect ', () => {
-    cy.visit('/');
-    cy.url().should(
-      'include',
-      '/namespaces/default/workflows?time-range=24+hours',
-    );
+describe('Redirect to the workflow executions list', () => {
+  const urls = ['/', '/namespaces', '/namespaces/default'];
+
+  beforeEach(() => {
+    cy.interceptApi();
   });
+
+  for (const url of urls) {
+    it(`should redirect from "${url}" to "/namespaces/default/workflows"`, () => {
+      cy.visit(url);
+      cy.url().should('include', '/namespaces/default/workflows');
+    });
+  }
+});
+
+describe.only('Redirect to the correct default namespace', () => {
+  const urls = ['/', '/namespaces'];
+
+  beforeEach(() => {
+    cy.intercept(Cypress.env('VITE_API_HOST') + '/api/v1/namespaces*', {
+      fixture: 'namespaces.json',
+    }).as('namespaces-api');
+
+    cy.intercept(
+      Cypress.env('VITE_API_HOST') + '/api/v1/namespaces/*/workflows?query=*',
+      { fixture: 'workflows.json' },
+    ).as('list-workflows-api');
+
+    cy.intercept(Cypress.env('VITE_API_HOST') + '/api/v1/cluster*', {
+      fixture: 'cluster.json',
+    }).as('settings-api');
+
+    cy.intercept(Cypress.env('VITE_API_HOST') + '/api/v1/me*', {
+      fixture: 'me.json',
+    }).as('user-api');
+
+    cy.intercept(Cypress.env('VITE_API_HOST') + '/api/v1/settings*', {
+      Auth: { Enabled: false, Options: null },
+      DefaultNamespace: 'some-other-namespace',
+    }).as('settings-api');
+  });
+
+  for (const url of urls) {
+    it(`should redirect from "${url}" to "/namespaces/default/workflows"`, () => {
+      cy.visit(url);
+      cy.url().should('include', '/namespaces/some-other-namespace/workflows');
+    });
+  }
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,26 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('interceptApi', (options = {}) => {
+  cy.intercept(Cypress.env('VITE_API_HOST') + '/api/v1/namespaces*', {
+    fixture: 'namespaces.json',
+  }).as('namespaces-api');
+
+  cy.intercept(
+    Cypress.env('VITE_API_HOST') + '/api/v1/namespaces/*/workflows?query=*',
+    { fixture: 'workflows.json' },
+  ).as('list-workflows-api');
+
+  cy.intercept(Cypress.env('VITE_API_HOST') + '/api/v1/cluster*', {
+    fixture: 'cluster.json',
+  }).as('settings-api');
+
+  cy.intercept(Cypress.env('VITE_API_HOST') + '/api/v1/me*', {
+    fixture: 'me.json',
+  }).as('user-api');
+
+  cy.intercept(Cypress.env('VITE_API_HOST') + '/api/v1/settings*', {
+    fixture: 'settings.json',
+  }).as('settings-api');
+});
