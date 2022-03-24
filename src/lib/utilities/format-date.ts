afbe95904b6dded455cdef2d572f885bf8dd4b60
Alex Tideman <alex.tideman@gmail.com>
2022-03-24 12:54:53 -0500

    Change date format to HH:mm:ss.SS

--- src/lib/utilities/format-date.ts
+++ src/lib/utilities/format-date.ts
@@ -5,7 +5,7 @@ import type { Timestamp } from '$types';
 
 type ValidTime = Parameters<typeof parseJSON>[0] | Timestamp;
 
-const pattern = 'yyyy-MM-dd z H:mm:ss:SS';
+const pattern = 'yyyy-MM-dd z HH:mm:ss.SS';
 
 export function formatDate(
   date: ValidTime,
