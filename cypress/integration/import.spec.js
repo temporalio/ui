04a237071545cf6cd1974db242b894d5265f6995
Alex Tideman <alex.tideman@gmail.com>
2022-03-28 13:32:11 -0500

    Use input[type=file]

--- cypress/integration/import.spec.js
+++ cypress/integration/import.spec.js
@@ -12,14 +12,16 @@ describe('Import JSON File for Event History', () => {
   });
 
   it('should redirect to first event on successful import', () => {
-    cy.get('[data-cy="file-input"]').selectFile('cypress/fixtures/raw_events.json');
+    cy.get('input[type=file]').selectFile('cypress/fixtures/raw_events.json');
     cy.get('.import-btn').click();
 
     cy.url().should('include', '/import/events/1');
   });
 
   it('should not redirect on unsuccessful import', () => {
-    cy.get('[data-cy="file-input"]').selectFile('cypress/fixtures/raw_events_bad.json');
+    cy.get('input[type=file]').selectFile(
+      'cypress/fixtures/raw_events_bad.json',
+    );
     cy.get('.import-btn').click();
 
     cy.url().should('not.include', '/import/events/1');
