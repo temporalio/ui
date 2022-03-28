f0cc6c3ebe43a5d85f774bf3a84cc06875e91ff8
Alex Tideman <alex.tideman@gmail.com>
2022-03-28 13:52:39 -0500

    Remove input tests again

--- cypress/integration/import.spec.js
+++ cypress/integration/import.spec.js
@@ -5,27 +5,3 @@ describe('Redirect to Events Import', () => {
     cy.url().should('include', '/import/events');
   });
 });
-
-describe('Import JSON File for Event History', () => {
-  beforeEach(() => {
-    cy.visit('/import/events');
-  });
-
-  it('should redirect to first event on successful import', () => {
-    cy.fixture('raw_events').as('events');
-
-    cy.get('input[type=file]').selectFile('@events');
-    cy.get('.import-btn').click();
-
-    cy.url().should('include', '/import/events/1');
-  });
-
-  it('should not redirect on unsuccessful import', () => {
-    cy.fixture('raw_events_bad').as('events');
-
-    cy.get('input[type=file]').selectFile('@events');
-    cy.get('.import-btn').click();
-
-    cy.url().should('not.include', '/import/events/1');
-  });
-});
