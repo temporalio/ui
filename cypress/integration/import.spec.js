faa514b7256d9bdf674bbccdc80d3ce363c501b3
Alex Tideman <alex.tideman@gmail.com>
2022-03-28 13:27:07 -0500

    Switch to cy.get('[data-cy=file-input]')

--- cypress/integration/import.spec.js
+++ cypress/integration/import.spec.js
@@ -12,14 +12,14 @@ describe('Import JSON File for Event History', () => {
   });
 
   it('should redirect to first event on successful import', () => {
-    cy.get('.import-input').selectFile('cypress/fixtures/raw_events.json');
+    cy.get('[data-cy="file-input"]').selectFile('cypress/fixtures/raw_events.json');
     cy.get('.import-btn').click();
 
     cy.url().should('include', '/import/events/1');
   });
 
   it('should not redirect on unsuccessful import', () => {
-    cy.get('.import-input').selectFile('cypress/fixtures/raw_events_bad.json');
+    cy.get('[data-cy="file-input"]').selectFile('cypress/fixtures/raw_events_bad.json');
     cy.get('.import-btn').click();
 
     cy.url().should('not.include', '/import/events/1');
