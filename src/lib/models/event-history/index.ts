3fba8401c25a2686bd731ce36927c4f339e42c30
Steve Kinney <hello@stevekinney.net>
2022-04-11 09:35:02 -0600

    Simplify event attributes with one key/value to just their value (#407)

--- src/lib/models/event-history/index.ts
+++ src/lib/models/event-history/index.ts
@@ -6,6 +6,7 @@ import { findAttributesAndKey } from '$lib/utilities/is-event-type';
 import { groupEvents } from '../group-events';
 import { getEventCategory } from './get-event-categorization';
 import { getEventClassification } from './get-event-classification';
+import { simplifyAttributes } from './simplify-attributes';
 
 export async function getEventAttributes(
   historyEvent: HistoryEvent,
@@ -27,7 +28,9 @@ const toEvent = async (
   const timestamp = formatDate(String(historyEvent.eventTime));
   const classification = getEventClassification(eventType);
   const category = getEventCategory(eventType);
-  const attributes = await getEventAttributes(historyEvent);
+  const attributes = await getEventAttributes(historyEvent).then(
+    simplifyAttributes,
+  );
 
   return {
     ...historyEvent,
