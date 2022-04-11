99f3314014afd539974917ed460c03c01f1833a0
Alex Tideman <alex.tideman@gmail.com>
2022-04-11 13:26:33 -0500

    Check if default namespace from settings exists in namespaces (#412)
    * Check if default namespace from settings existings in namespaces, if not use the first namespace
    
    * Remove unused goto

--- src/lib/utilities/get-namespace.ts
+++ src/lib/utilities/get-namespace.ts
@@ -6,6 +6,11 @@ type GetNamespaceParameters = {
   namespaces: DescribeNamespaceResponse[];
 };
 
+type GetDefaultNamespaceParameters = {
+  namespaces: DescribeNamespaceResponse[];
+  settings: Settings;
+};
+
 export const getNamespace = ({
   namespace,
   defaultNamespace,
@@ -20,4 +25,26 @@ export const getNamespace = ({
   }
 
   return defaultNamespace;
+};
+
+export const getDefaultNamespace = ({
+  namespaces,
+  settings,
+}: GetDefaultNamespaceParameters): string => {
+  const { showTemporalSystemNamespace, defaultNamespace } = settings;
+
+  const namespaceNames = namespaces
+    .map(
+      (namespace: DescribeNamespaceResponse) => namespace?.namespaceInfo?.name,
+    )
+    .filter(
+      (namespace: string) =>
+        showTemporalSystemNamespace || namespace !== 'temporal-system',
+    );
+
+  return (
+    namespaceNames.find((ns) => ns === defaultNamespace) ??
+    namespaceNames[0] ??
+    defaultNamespace
+  );
 };
