d92c3d702b9b22eb60580ef91da5052716521777
Steve Kinney <hello@stevekinney.net>
2022-04-11 15:32:16 -0600

    Show results for failure cases (#416)
    * Show results for failure cases
    
    * Update get-started-and-completed-events.ts

--- src/lib/utilities/get-started-and-completed-events.ts
+++ src/lib/utilities/get-started-and-completed-events.ts
@@ -1,6 +1,51 @@
+import { isWorkflowExecutionCompletedEvent } from './is-event-type';
+
 type WorkflowEvents = {
   input: string;
   result: string;
+};
+
+type CompletionEvent =
+  | WorkflowExecutionFailedEvent
+  | WorkflowExecutionCompletedEvent
+  | WorkflowExecutionContinuedAsNewEvent
+  | WorkflowExecutionTimedOutEvent
+  | WorkflowExecutionCanceledEvent
+  | WorkflowExecutionTerminatedEvent;
+
+const completedEventTypes = [
+  'WorkflowExecutionFailed',
+  'WorkflowExecutionCompleted',
+  'WorkflowExecutionContinuedAsNew',
+  'WorkflowExecutionTimedOut',
+  'WorkflowExecutionCanceled',
+  'WorkflowExecutionTerminated',
+] as const;
+
+const isCompletionEvent = (
+  event: HistoryEventWithId,
+): event is CompletionEvent => {
+  for (const completionType of completedEventTypes) {
+    if (event.eventType === completionType) return true;
+  }
+  return false;
+};
+
+const getWorkflowCompletedEvent = (
+  events: HistoryEventWithId[],
+): CompletionEvent => {
+  for (const event of events) {
+    if (isCompletionEvent(event)) return event;
+  }
+};
+
+const getEventResult = (event: CompletionEvent) => {
+  if (isWorkflowExecutionCompletedEvent(event)) {
+    if (event.attributes.result === null) return null;
+    return event.attributes.result.payloads;
+  }
+
+  return event.attributes;
 };
 
 export const getWorkflowStartedAndCompletedEvents = (
@@ -13,9 +58,7 @@ export const getWorkflowStartedAndCompletedEvents = (
     return !!event.workflowExecutionStartedEventAttributes;
   });
 
-  const workflowCompletedEvent: HistoryEventWithId = events?.find((event) => {
-    return !!event.workflowExecutionCompletedEventAttributes;
-  });
+  const workflowCompletedEvent = getWorkflowCompletedEvent(events);
 
   if (workflowStartedEvent) {
     input = JSON.stringify(
@@ -25,10 +68,7 @@ export const getWorkflowStartedAndCompletedEvents = (
   }
 
   if (workflowCompletedEvent) {
-    result = JSON.stringify(
-      workflowCompletedEvent?.workflowExecutionCompletedEventAttributes?.result
-        ?.payloads,
-    );
+    result = JSON.stringify(getEventResult(workflowCompletedEvent));
   }
 
   return {
