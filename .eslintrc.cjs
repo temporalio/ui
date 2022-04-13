1e6aaed79bdd76ab9779a1978c9a9c47d6a4e31e
Steve Kinney <hello@stevekinney.net>
2022-04-13 12:31:20 -0600

    Get Storybook set up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,30 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:storybook/recommended'],
   plugins: ['svelte3', '@typescript-eslint'],
   ignorePatterns: ['*.cjs', 'prism.js'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  overrides: [{
+    files: ['*.svelte'],
+    processor: 'svelte3/svelte3'
+  }],
   settings: {
-    'svelte3/typescript': () => require('typescript'),
+    'svelte3/typescript': () => require('typescript')
   },
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2019,
+    ecmaVersion: 2019
   },
   env: {
     browser: true,
     es2017: true,
-    node: true,
+    node: true
   },
-  overrides: [
-    {
-      files: ['src/**/*.test.js'],
-      extends: ['plugin:jest/recommended'],
-    },
-    {
-      files: ['cypress/**/*.js'],
-      extends: ['plugin:cypress/recommended'],
-    },
-  ],
+  overrides: [{
+    files: ['src/**/*.test.js'],
+    extends: ['plugin:jest/recommended']
+  }, {
+    files: ['cypress/**/*.js'],
+    extends: ['plugin:cypress/recommended']
+  }]
 };
---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-svelte-csf',
+  ],
+  framework: '@storybook/svelte',
+  svelteOptions: {
+    preprocess: import('../svelte.config.js').then((module) => {
+      return module.preprocess;
+    }),
+  },
+};
