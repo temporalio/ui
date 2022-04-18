2f0e8cbab37ebc3661a5d9efd07d1a7f182e12a6
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2022-04-18 19:58:31 -0400

    Correct namespace in tctl enable archival command (#454)

--- src/routes/namespaces/[namespace]/archival/index@root.svelte
+++ src/routes/namespaces/[namespace]/archival/index@root.svelte
@@ -111,13 +111,13 @@
   </h2>
   <p>Run this command to enable Archival for Event Histories:</p>
   <CodeBlock
-    content={`tctl --namespace default namespace update --has enabled`}
+    content={`tctl --namespace ${namespace} namespace update --has enabled`}
     language="text"
   />
   {#if !visibilityArchivalEnabled}
     <p>To enable Visibility Archival:</p>
     <CodeBlock
-      content="tctl --namespace default namespace update -vas enabled"
+      content={`tctl --namespace ${namespace} namespace update -vas enabled`}
       language="text"
     />
   {/if}
