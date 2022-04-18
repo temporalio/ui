0f99fe2b94c38fb1c9510ba3d0bf9a84399e8c6f
Alex Tideman <alex.tideman@gmail.com>
2022-04-18 11:16:09 -0500

    Load workflows onMount and update loadingRow (#445)
    * Load workflows onMount and use loadingRow
    
    * Remove let workflows, just use  promise
    
    * Remove workflow status
    
    * Remove unused imports

--- src/lib/components/loading-row.svelte
+++ src/lib/components/loading-row.svelte
@@ -1,5 +1,32 @@
-<tr>
-  <td colspan="5" class="m-auto p-12 text-center font-extralight text-2xl">
-    Loading
-  </td>
-</tr>
+<article class="row">
+  <div class="cell">
+    <p />
+  </div>
+  <div class="cell w-1/2">
+    <p>Loading</p>
+  </div>
+  <div class="cell">
+    <p />
+  </div>
+  <div class="cell">
+    <p />
+  </div>
+  <span class=" md:hidden"> - </span>
+  <div class="cell">
+    <p />
+  </div>
+</article>
+
+<style lang="postcss">
+  .row {
+    @apply no-underline p-2 text-sm border-b-2 items-center md:text-base md:table-row last-of-type:border-b-0;
+  }
+
+  .cell {
+    @apply md:table-cell md:border-b-2 text-left p-2;
+  }
+
+  .row:last-of-type .cell {
+    @apply border-b-0 first-of-type:rounded-bl-lg  last-of-type:rounded-br-lg;
+  }
+</style>
