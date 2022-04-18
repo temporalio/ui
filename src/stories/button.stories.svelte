c89b967a7307b58d05aa392adac4808a272c6f62
Steve Kinney <hello@stevekinney.net>
2022-04-18 09:33:58 -0600

    Add stories for the button

--- src/stories/button.stories.svelte
+++ src/stories/button.stories.svelte
@@ -2,6 +2,7 @@
   import '../app.css';
   import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
   import Button from '../lib/components/button.svelte';
+  import { faSearchDollar } from '@fortawesome/free-solid-svg-icons';
 </script>
 
 <Meta
@@ -60,3 +61,45 @@
 
 <!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
 <Story name="Default" />
+
+<Story name="Disabled" args={{ disabled: true }} />
+
+<Story name="Secondary" args={{ secondary: true }} />
+
+<Story name="Secondary, Disabled" args={{ secondary: true, disabled: true }} />
+
+<Story name="Thin, Disabled" args={{ thin: true, disabled: true }} />
+
+<Story name="Thin, Secondary" args={{ thin: true, secondary: true }} />
+
+<Story name="With Icon, Default" args={{ icon: faSearchDollar }} />
+
+<Story
+  name="With Icon, Disabled"
+  args={{ disabled: true, icon: faSearchDollar }}
+/>
+
+<Story
+  name="With Icon, Secondary"
+  args={{ secondary: true, icon: faSearchDollar }}
+/>
+
+<Story
+  name="With Icon, Secondary, Disabled"
+  args={{ secondary: true, disabled: true, icon: faSearchDollar }}
+/>
+
+<Story
+  name="With Icon, Thin, Disabled"
+  args={{ thin: true, disabled: true, icon: faSearchDollar }}
+/>
+
+<Story
+  name="With Icon, Thin, Secondary"
+  args={{ thin: true, secondary: true, icon: faSearchDollar }}
+/>
+
+<Story
+  name="Thin, Secondary, Disabled"
+  args={{ thin: true, secondary: true, disabled: true }}
+/>
