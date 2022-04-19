cdf78a0662219f908e34827c3862e87f849eccd4
Steve Kinney <hello@stevekinney.net>
2022-04-18 19:04:57 -0600

    Even more responsive tweaks

--- src/lib/components/event/event-details-row.svelte
+++ src/lib/components/event/event-details-row.svelte
@@ -19,10 +19,12 @@
   class="flex flex-col justify-between xl:flex-row xl:gap-4 gap-2 py-2 last:border-b-0 border-gray-200 first:pt-0 {$$props.class}"
 >
   {#if typeof value === 'object'}
-    <h2 class="w-full xl:w-1/4 text-sm">{format(key)}</h2>
+    <h2 class="w-full items-center xl:items-start xl:w-1/4 text-sm">
+      {format(key)}
+    </h2>
     <CodeBlock content={value} class="w-full xl:w-3/4" />
   {:else if shouldDisplayAsWorkflowLink(key)}
-    <div class="flex w-full xl:3/4">
+    <div class="flex items-center xl:items-start w-full xl:3/4">
       <h2 class="w-full xl:w-1/4 text-sm">{format(key)}</h2>
       <div class="w-full xl:w-3/4 text-sm">
         <Copyable content={value} container-class="flex-row-reverse">
@@ -33,7 +35,7 @@
       </div>
     </div>
   {:else}
-    <div class="flex w-full xl:3/4">
+    <div class="flex items-center xl:items-start w-full xl:3/4">
       <h2 class="w-full xl:w-1/4 text-sm">{format(key)}</h2>
       <p class="w-full xl:w-3/4 text-sm text-right">
         <span class="bg-gray-300 text-gray-700 px-2 select-all">{value}</span>
