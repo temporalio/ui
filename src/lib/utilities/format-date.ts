1c40af0c4de7428d059cce30996bbc70bf4df4c6
Alex Tideman <alex.tideman@gmail.com>
2022-04-20 10:33:30 -0500

    Use durationToString to get human readable interval

--- src/lib/utilities/format-date.ts
+++ src/lib/utilities/format-date.ts
@@ -4,7 +4,6 @@ import {
   formatDuration as durationToString,
   intervalToDuration,
   getMilliseconds as getSecondAsMilliseconds,
-  secondsToHours,
 } from 'date-fns';
 import * as dateTz from 'date-fns-tz'; // `build` script fails on importing some of named CommonJS modules
 
@@ -157,16 +156,9 @@ export function fromSecondsToDaysOrHours(seconds: string): string {
     seconds = seconds.replace('s', '');
   }
 
-  const hours = secondsToHours(parseInt(seconds));
-  const days = hours / 24;
-
-  if (days > 1) {
-    return `${days} days`;
-  } else if (days < 1) {
-    if (hours > 1) {
-      return `${hours} hours`;
-    }
-    return `${hours} hour`;
-  }
-  return `${days} day`;
+  const duration = intervalToDuration({
+    start: 0,
+    end: parseInt(seconds) * 1000,
+  });
+  return durationToString(duration, { format: ['days', 'hours'] });
 }
