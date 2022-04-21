bda387449e6a6e7e44d319008c9193ce5dacb1a9
Alex Tideman <alex.tideman@gmail.com>
2022-04-21 16:07:00 -0500

    Switch to IterableEvent

--- src/lib/utilities/is-subrow-activity.ts
+++ src/lib/utilities/is-subrow-activity.ts
@@ -1,3 +1,3 @@
-export const isSubrowActivity = (event: CommonHistoryEvent): boolean => {
+export const isSubrowActivity = (event: IterableEvent): boolean => {
   return Boolean((event?.attributes as any)?.workflowTaskCompletedEventId);
 };
