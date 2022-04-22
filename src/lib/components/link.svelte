ea0829437214a8bfad721dd461a9067172900b8b
Alex Tideman <alex.tideman@gmail.com>
2022-04-22 10:10:49 -0500

    Update link colors (#472)

--- src/lib/components/link.svelte
+++ src/lib/components/link.svelte
@@ -7,13 +7,13 @@
   {href}
   class:active
   {...$$props}
-  class="border-b-2 border-blue-600 {$$props.class} hover:text-blue-900 hover:no-underline"
+  class="border-b-2 border-gray-900 {$$props.class} hover:text-blue-700 hover:border-blue-700"
 >
   <slot />
 </a>
 
 <style lang="postcss">
   .active {
-    @apply text-blue-700 border-b-2 border-blue-600;
+    @apply text-blue-900 border-b-2 border-blue-900;
   }
 </style>
