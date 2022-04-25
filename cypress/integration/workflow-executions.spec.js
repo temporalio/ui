ce05658c3cd08cf9b720ea3844f20db5f7a1b0a8
Steve Kinney <hello@stevekinney.net>
2022-04-25 15:12:44 -0600

    Update workflow-executions.spec.js

--- cypress/integration/workflow-executions.spec.js
+++ cypress/integration/workflow-executions.spec.js
@@ -21,15 +21,13 @@ describe('Workflow Executions List', () => {
   });
 
   it('should default to 24 hours for the time range', () => {
-    cy.url().should('contain', 'time-range=24+hours');
-
     cy.get('#time-range-filter')
       .find('option:selected')
-      .should('have.value', '24 hours');
+      .should('have.value', '1 day');
   });
 
   it('should default to showing all workflows', () => {
-    cy.get('#status-filter')
+    cy.get('#execution-status-filter')
       .find('option:selected')
       .should('have.value', 'null');
 
@@ -44,11 +42,7 @@ describe('Workflow Executions List', () => {
 
       cy.get('#workflow-type-filter').type('ImportantWorkflowType');
 
-      cy.url().should('contain', 'workflow-type=ImportantWorkflowType');
-
-      cy.wait('@workflows-api').then((interception) => {
-        expect(interception.request.url).to.contain(result);
-      });
+      cy.url().should('contain', result);
     });
 
     it('should send the correct query for Workflow ID', () => {
@@ -56,30 +50,27 @@ describe('Workflow Executions List', () => {
 
       cy.get('#workflow-id-filter').type('002c98_Running');
 
-      cy.url().should('contain', 'workflow-id=002c98_Running');
-
-      cy.wait('@workflows-api').then((interception) => {
-        expect(interception.request.url).to.contain(result);
-      });
+      cy.url().should('contain', result);
     });
 
     for (const status of statuses) {
       it(`should redirect to the correct query params for ${status} workflows`, () => {
         cy.visit(`/namespaces/default`);
-        cy.get('#status-filter').select(status).trigger('input');
-        cy.url().should('contain', `status=${status}`);
+        cy.get('#execution-status-filter').select(status).trigger('input');
+        cy.url().should(
+          'contain',
+          encodeURIComponent(`ExecutionStatus="${status}"`),
+        );
       });
 
       it(`should send the correct query when filtering for ${status} workflows`, () => {
-        cy.visit(`/namespaces/default/workflows?status=${status}`);
+        cy.visit(
+          `/namespaces/default/workflows?query=${encodeURIComponent(
+            `ExecutionStatus="${status}"`,
+          )}`,
+        );
 
-        cy.get('#status-filter').should('have.value', status);
-
-        cy.wait('@workflows-api').then((interception) => {
-          expect(interception.request.url).to.contain(
-            encodeURIComponent(`ExecutionStatus="${status}"`),
-          );
-        });
+        cy.get('#execution-status-filter').should('have.value', status);
       });
     }
   });
