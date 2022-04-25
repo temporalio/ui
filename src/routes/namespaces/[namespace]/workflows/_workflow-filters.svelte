6739313d39e9628d006cfa68ffc9c58f93051920
Steve Kinney <hello@stevekinney.net>
2022-04-25 14:56:42 -0600

    Update _workflow-filters.svelte

--- src/routes/namespaces/[namespace]/workflows/_workflow-filters.svelte
+++ src/routes/namespaces/[namespace]/workflows/_workflow-filters.svelte
@@ -89,57 +89,57 @@
     {/if}
   </p>
 
-  <!-- {#if searchType === 'advanced'} -->
-  <Search
-    icon
-    id="advanced-search"
-    placeholder="Query…"
-    value={query}
-    on:submit={updateQuery}
-  />
-  <!-- {:else} -->
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
-    <Input
-      id="workflow-id-filter"
-      label="Workflow ID"
-      bind:value={parameters.workflowId}
-      on:input={handleParameterChange}
+  {#if searchType === 'advanced'}
+    <Search
+      icon
+      id="advanced-search"
+      placeholder="Query…"
+      value={query}
+      on:submit={updateQuery}
     />
-    <Input
-      id="workflow-type-filter"
-      label="Workflow Type"
-      bind:value={parameters.workflowType}
-      on:input={handleParameterChange}
-    />
-    <Select
-      id="time-range-filter"
-      label="Time Range"
-      bind:value={parameters.timeRange}
-      on:change={handleParameterChange}
-    >
-      <Option value={null}>All</Option>
-      {#if parameters.timeRange && !durations.includes(parameters.timeRange)}
-        <Option value={parameters.timeRange}>{parameters.timeRange}</Option>
-      {/if}
-      {#each durations as value}
-        <Option {value}>{value}</Option>
-      {/each}
-    </Select>
-    <Select
-      id="execution-status-filter"
-      label="Workflow Status"
-      bind:value={parameters.executionStatus}
-      on:change={handleParameterChange}
-    >
-      {#each Object.entries(statuses) as [label, value] (label)}
-        <Option {value}>{label}</Option>
-      {/each}
-    </Select>
-    <Select id="filter-by-relative-time" bind:value={$timeFormat}>
-      <Option value={'relative'}>Relative</Option>
-      <Option value={'UTC'}>UTC</Option>
-      <Option value={'local'}>Local</Option>
-    </Select>
-  </div>
-  <!-- {/if} -->
+  {:else}
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
+      <Input
+        id="workflow-id-filter"
+        label="Workflow ID"
+        bind:value={parameters.workflowId}
+        on:input={handleParameterChange}
+      />
+      <Input
+        id="workflow-type-filter"
+        label="Workflow Type"
+        bind:value={parameters.workflowType}
+        on:input={handleParameterChange}
+      />
+      <Select
+        id="time-range-filter"
+        label="Time Range"
+        bind:value={parameters.timeRange}
+        on:change={handleParameterChange}
+      >
+        <Option value={null}>All</Option>
+        {#if parameters.timeRange && !durations.includes(parameters.timeRange)}
+          <Option value={parameters.timeRange}>{parameters.timeRange}</Option>
+        {/if}
+        {#each durations as value}
+          <Option {value}>{value}</Option>
+        {/each}
+      </Select>
+      <Select
+        id="execution-status-filter"
+        label="Workflow Status"
+        bind:value={parameters.executionStatus}
+        on:change={handleParameterChange}
+      >
+        {#each Object.entries(statuses) as [label, value] (label)}
+          <Option {value}>{label}</Option>
+        {/each}
+      </Select>
+      <Select id="filter-by-relative-time" bind:value={$timeFormat}>
+        <Option value={'relative'}>Relative</Option>
+        <Option value={'UTC'}>UTC</Option>
+        <Option value={'local'}>Local</Option>
+      </Select>
+    </div>
+  {/if}
 </section>
