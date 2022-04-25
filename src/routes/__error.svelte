9f29f6e35009f15681dfdcc1af79a2062426d081
Alex Tideman <alex.tideman@gmail.com>
2022-04-25 12:30:18 -0500

    Redirect to login if unauthorized

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,22 +1,42 @@
 <script context="module" lang="ts">
   import type { ErrorLoad } from '@sveltejs/kit';
+  import { fetchSettings } from '$lib/services/settings-service';
+  import { fetchUser } from '$lib/services/user-service';
 
-  export const load: ErrorLoad = async function ({ error, status }) {
+  import '../app.css';
+
+  export const load: ErrorLoad = async function ({
+    error,
+    status,
+    url,
+    fetch,
+  }) {
+    const settings: Settings = await fetchSettings({ url }, fetch);
+    const user = await fetchUser(fetch);
+
     return {
+      stuff: {
+        settings,
+      },
       props: {
         error,
         status,
+        settings,
+        user,
       },
     };
   };
 </script>
 
 <script lang="ts">
+  import Header from './_header.svelte';
+  import HeaderResponsive from './_header-responsive.svelte';
   import Error from '$lib/components/error.svelte';
   import { isNetworkError } from '$lib/utilities/is-network-error';
 
   export let error: globalThis.Error;
   export let status: number;
+  export let user: User;
 
   let requestFromAPIError: Record<string, any>;
 
@@ -29,4 +49,6 @@
   } catch (e) {}
 </script>
 
+<Header {user} />
+<HeaderResponsive {user} />
 <Error {error} {status} />
