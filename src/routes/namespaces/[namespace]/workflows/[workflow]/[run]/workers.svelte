d9a3d806873efc8a4418b5d7cf93e4f4e9b53942
Steve Kinney <hello@stevekinney.net>
2022-04-26 13:36:20 -0600

    Put workers in the same table; respect time settings

--- src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/workers.svelte
+++ src/routes/namespaces/[namespace]/workflows/[workflow]/[run]/workers.svelte
@@ -16,6 +16,8 @@
 <script lang="ts">
   import Icon from 'svelte-fa';
   import { faTimes, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+  import { timeFormat } from '$lib/stores/time-format';
   import { formatDate } from '$lib/utilities/format-date';
 
   import EmptyState from '$lib/components/empty-state.svelte';
@@ -25,24 +27,28 @@
 </script>
 
 <section class="flex flex-col gap-4">
-  <h3 class="text-lg font-medium">Task Queue: {taskQueue}</h3>
-  {#each workers.pollers as poller (poller.identity)}
-    <section class="flex flex-col border-2 border-gray-900 w-full rounded-lg">
-      <div class="bg-gray-900 text-white flex flex-row p-2">
-        <div class="w-3/12 text-left">ID</div>
-        <div class="w-3/12 text-left">Last Accessed</div>
-        <div class="w-3/12 text-left">Workflow Task Handler</div>
-        <div class="w-2/12 text-left">Activity Handler</div>
-      </div>
+  <h3 class="text-lg font-medium">
+    Task Queue: <span class="select-all font-normal">{taskQueue}</span>
+  </h3>
+  <section class="flex flex-col border-2 border-gray-900 w-full rounded-lg">
+    <div class="bg-gray-900 text-white flex flex-row p-2">
+      <div class="w-3/12 text-left">ID</div>
+      <div class="w-3/12 text-left">Last Accessed</div>
+      <div class="w-3/12 text-left">Workflow Task Handler</div>
+      <div class="w-2/12 text-left">Activity Handler</div>
+    </div>
+    {#each workers.pollers as poller (poller.identity)}
       <article
         class="w-full h-full flex flex-row border-b-2 last:border-b-0 no-underline p-2"
       >
         <div class="links w-3/12 text-left">
-          {poller.identity}
+          <p class="select-all">{poller.identity}</p>
         </div>
         <div class="links w-3/12 text-left">
           <h3>
-            <p>{formatDate(poller.lastAccessTime)}</p>
+            <p class="select-all">
+              {formatDate(poller.lastAccessTime, $timeFormat)}
+            </p>
           </h3>
         </div>
         <div class="w-3/12 text-left">
@@ -60,8 +66,8 @@
           {/if}
         </div>
       </article>
-    </section>
-  {:else}
-    <EmptyState title={'No Workers Found'} />
-  {/each}
+    {:else}
+      <EmptyState title={'No Workers Found'} />
+    {/each}
+  </section>
 </section>
