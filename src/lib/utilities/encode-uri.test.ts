29dad8e23985de6e60be8ab4d2d4670408bbc3e1
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2022-04-26 20:22:50 -0400

    Fix workflow page 404 on special characters in workflow ID (#491)

---
+++ src/lib/utilities/encode-uri.test.ts
@@ -0,0 +1,19 @@
+import { encodeURIForSvelte, decodeURIForSvelte } from './encode-uri';
+
+describe(encodeURIForSvelte, () => {
+  it('should encode reserved URI characters, \\ and %', () => {
+    const path = encodeURIForSvelte(',./;\'[]-=<>?:"{}|_+!@#$%^&*()`~)');
+    expect(path).toEqual(
+      '%2C.%2F;\'[]-%3D<>%3F%3A"{}|_%2B!%40%23%24%25^%26*()`~)',
+    );
+  });
+});
+
+describe(decodeURIForSvelte, () => {
+  it('should decode reserved URI characters, \\ and %', () => {
+    const path = decodeURIForSvelte(
+      '%2C.%2F;\'[]-%3D<>%3F%3A"{}|_%2B!%40%23%24%25^%26*()`~)',
+    );
+    expect(path).toEqual(',./;\'[]-=<>?:"{}|_+!@#$%^&*()`~)');
+  });
+});
