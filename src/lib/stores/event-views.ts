07a1baf44ef625e7706ca788606118dec902d95e
Steve Kinney <hello@stevekinney.net>
2022-04-29 12:28:22 -0600

    Refactor sort order

--- src/lib/stores/event-views.ts
+++ src/lib/stores/event-views.ts
@@ -1,4 +1,20 @@
 import { persistStore } from '$lib/stores/persist-store';
 
+export type EventSortOrder = 'ascending' | 'descending';
+export type EventFilterTypeOptions = {
+  label: string;
+  option: EventSortOrder;
+}[];
+
 export const eventViewType = persistStore<EventView>('eventView', 'feed');
 export const expandAllEvents = persistStore('expandAllEvents', 'false');
+
+export const eventFilterSort = persistStore<EventSortOrder>(
+  'eventFilterSort',
+  'descending',
+);
+
+export const eventShowElapsed = persistStore<BooleanString>(
+  'eventShowElapsed',
+  'false',
+);
