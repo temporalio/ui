4ce30ee73ee96df680e4de1cd5d86d1b860b4c82
Steve Kinney <hello@stevekinney.net>
2022-05-04 11:00:14 -0600

    Fix error parsing queries from the Java SDK (#516)

--- src/lib/utilities/get-query-types-from-error.ts
+++ src/lib/utilities/get-query-types-from-error.ts
@@ -1,9 +1,17 @@
 export const getQueryTypesFromError = (message: string): string[] => {
+  console.log(message);
   const indexOfOpeningBracket = message.indexOf('[');
   const indexOfClosingBracket = message.indexOf(']');
 
   return message
     .slice(indexOfOpeningBracket + 1, indexOfClosingBracket)
     .split(' ')
-    .filter((query) => query !== '__stack_trace');
+    .filter((query: string) => query !== '__stack_trace')
+    .map((query: string) => {
+      if (query.endsWith(',')) {
+        return query.slice(0, query.length - 1);
+      } else {
+        return query;
+      }
+    });
 };
