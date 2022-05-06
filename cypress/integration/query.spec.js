cbe4a594b2029882ddf46e022663b211bf79259c
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2022-05-06 16:18:39 -0400

    Add cypress test for parsing Java query types (#523)

---
+++ cypress/integration/query.spec.js
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+import workflowCompletedFixture from '../fixtures/workflow-completed.json';
+
+const { workflowId, runId } =
+  workflowCompletedFixture.workflowExecutionInfo.execution;
+
+describe('Query', () => {
+  beforeEach(() => {
+    cy.interceptApi();
+
+    cy.intercept(
+      Cypress.env('VITE_API_HOST') +
+        `/api/v1/namespaces/default/workflows/*/runs/*/events*`,
+      { fixture: 'event-history-completed.json' },
+    ).as('event-history-api');
+
+    cy.intercept(
+      Cypress.env('VITE_API_HOST') +
+        `/api/v1/namespaces/default/workflows/${workflowId}/runs/${runId}?`,
+      { fixture: 'workflow-completed.json' },
+    ).as('workflow-api');
+
+    cy.visit('/');
+    cy.wait('@workflows-api');
+    cy.wait('@namespaces-api');
+  });
+
+  it('should render Query options for Java SDK', () => {
+    cy.visit(`/namespaces/default/workflows/${workflowId}/${runId}`);
+
+    cy.intercept(
+      Cypress.env('VITE_API_HOST') +
+        `/api/v1/namespaces/default/workflows/*/runs/*/query*`,
+      { fixture: 'query-java-error-unknown-type.json', statusCode: 400 },
+    ).as('query-unknown-type-api');
+
+    cy.on('uncaught:exception', () => {
+      return false;
+    });
+
+    cy.wait('@workflow-api');
+    cy.wait('@event-history-api');
+
+    cy.get(
+      `[href="/namespaces/default/workflows/${workflowId}/${runId}/query"]`,
+    ).click();
+
+    cy.wait('@query-unknown-type-api');
+
+    cy.get('[data-cy=query-select]').contains('query1');
+  });
+});
