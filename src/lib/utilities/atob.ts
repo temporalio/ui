fc49382ab9cff525739fa16a4dcbfaec2b0af989
feedmeapples <aksenov.ro@outlook.com>
2022-05-09 17:40:52 -0400

    Resolve unicode codeblocks showing gibberish

--- src/lib/utilities/atob.ts
+++ src/lib/utilities/atob.ts
@@ -1,3 +1,16 @@
 import { browser } from '$app/env';
 
-export const atob = browser ? window.atob : (str: string) => str;
+// decode base64 to unicode
+function atou(str: string) {
+  return decodeURIComponent(
+    window
+      .atob(str)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join(''),
+  );
+}
+
+export const atob = browser ? atou : (str: string) => str;
