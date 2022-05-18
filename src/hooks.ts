11c94888aa22cc697c07b9bf510d181aefd511a1
Alex Tideman <alex.tideman@gmail.com>
2022-05-18 12:30:12 -0500

    Remove ssr

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,7 +2,8 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event, {
-    ssr: process.env.VITE_MODE !== 'test',
+    // ssr: process.env.VITE_MODE !== 'test',
+    ssr: false
   });
 
   return response;
