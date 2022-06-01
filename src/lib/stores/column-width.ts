09950ccf4559d5c7e5fcb0a9d5f935f65649b04a
Ross Edfort <rossedfort@gmail.com>
2022-06-01 16:15:47 -0600

    lint

--- src/lib/stores/column-width.ts
+++ src/lib/stores/column-width.ts
@@ -4,4 +4,3 @@ export const workflowIdColumnWidth = writable<number>(0);
 export const workflowTypeColumnWidth = writable<number>(0);
 export const workflowSummaryColumnWidth = writable<number>(0);
 export const workflowEventsColumnWidth = writable<number>(0);
-
