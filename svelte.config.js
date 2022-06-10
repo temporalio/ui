f4b6d320c6ff614eedea0a6d1e47c264575783a0
Ross Edfort <rossedfort@gmail.com>
2022-06-10 16:33:29 -0600

    only include fiction in dev mode

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,12 @@ import path from 'path';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
-const buildTarget = process.env.VITE_TEMPORAL_UI_BUILD_TARGET || 'local';
+// Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+process.env.TAILWIND_MODE = dev ? 'watch' : 'build';
 
+const buildTarget = process.env.VITE_TEMPORAL_UI_BUILD_TARGET || 'local';
 let outputDirectory = `build-${buildTarget}`;
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -32,7 +36,9 @@ const config = {
       },
       files: (filepath) => /^(?!.*(?:test)).*\.ts$/.test(filepath),
     },
+   ...(!dev && {
     routes: (filepath) =>/^(?!.*(?:fiction)).*$/.test(filepath),
+   }),
     vite: {
       resolve: {
         alias: {
@@ -44,7 +50,3 @@ const config = {
 };
 
 export default config;
-// Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
-process.env.TAILWIND_MODE = dev ? 'watch' : 'build';
