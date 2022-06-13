2c23bf5c9bb11bcd1ba7235d726de03113b4ddd6
Ross Edfort <rossedfort@gmail.com>
2022-06-13 15:34:21 -0600

    scaffold fiction and add a few chapters (#582)
    * scaffold fiction and add a few chapters
    
    * only include fiction in dev mode
    
    * format

---
+++ src/routes/fiction/_chapter.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { currentProps } from '$lib/stores/fiction-store';
+  import type { SvelteComponentTyped } from 'svelte/internal';
+
+  type ComponentProps<T> = T extends new (
+    ...args: any[]
+  ) => SvelteComponentTyped<infer Props>
+    ? Props
+    : unknown;
+
+  type T = $$Generic;
+
+  interface $$Props {
+    description: string;
+    component: T;
+    props?: ComponentProps<T>;
+  }
+
+  export let description;
+  export let props = {};
+  let Component;
+  export { Component as component };
+
+  function handleClick() {
+    currentProps.set({ ...props });
+  }
+</script>
+
+<div
+  on:click={handleClick}
+  class="mt-4 cursor-pointer border-2 p-4 hover:bg-gray-50"
+>
+  <h2 class="mb-4">{description}</h2>
+  <svelte:component this={Component} {...props}>
+    <slot />
+  </svelte:component>
+</div>
