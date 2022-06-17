1920993baae15733cbe7a9fd5f5b3539a111fe2a
Ross Edfort <rossedfort@gmail.com>
2022-06-17 07:34:27 -0600

    [WIP] - move input to holocene and make copyable variety

---
+++ src/lib/holocene/input.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import Icon from '$lib/holocene/icon/index.svelte';
+  import type { IconName } from '$lib/holocene/icon/paths';
+
+  export let id: string;
+  export let label: string;
+  export let value: string;
+  export let icon: IconName = '';
+  export let placeholder = label;
+  export let name = id;
+  export let copyable: boolean = false;
+
+  function handleCopy() {}
+</script>
+
+<div class="input-container focus-within:border-blue-700">
+  <label for={id} class="hidden">{label}</label>
+  {#if icon !== ''}
+    <span class="icon-container">
+      <Icon name={icon} scale={0.9} stroke="gray" class="flex items-center" />
+    </span>
+  {/if}
+  <input
+    class="block w-full focus:outline-none bg-white"
+    {placeholder}
+    {id}
+    {name}
+    bind:value
+    on:input
+    on:change
+  />
+  {#if copyable}
+    <span class="icon-container cursor-pointer" on:click={handleCopy}>
+      <Icon name="copy" stroke="gray" />
+    </span>
+  {/if}
+</div>
+
+<style lang="postcss">
+  .input-container {
+    @apply relative box-border inline-flex h-10 w-full items-center rounded-lg border-2 p-2 text-base bg-white;
+  }
+
+  span {
+    @apply mr-1;
+  }
+</style>
---
+++ src/routes/fiction/chapters/input.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import Chapter from '../_chapter.svelte';
+
+  import Input from '$lib/holocene/input.svelte';
+</script>
+
+<Chapter
+  description="An input"
+  component={Input}
+  props={{ id: 'input1', label: 'Hello', value: '' }}
+/>
+
+<Chapter
+  description="An input with an icon"
+  component={Input}
+  props={{ id: 'input2', label: 'Search...', value: '', icon: 'search' }}
+/>
+
+<Chapter
+  description="A copyable input"
+  component={Input}
+  props={{
+    id: 'input2',
+    label: 'Search...',
+    value: 'Copy me!',
+    copyable: true,
+  }}
+/>
