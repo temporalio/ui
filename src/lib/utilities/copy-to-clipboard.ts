4551886b3d136be18db82c58cc1745dd8ddd66b4
Ross Edfort <rossedfort@gmail.com>
2022-06-22 13:10:38 -0600

    rm unnecessary type

--- src/lib/utilities/copy-to-clipboard.ts
+++ src/lib/utilities/copy-to-clipboard.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-export const copyToClipboard = (content: string, timeout: number = 2000) => {
+export const copyToClipboard = (content: string, timeout = 2000) => {
   const copied = writable(false);
 
   const copy = async (event: Event) => {
