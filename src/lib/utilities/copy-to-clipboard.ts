ba173812674d38bba9fe77ebdac2174363325caf
Ross Edfort <rossedfort@gmail.com>
2022-06-22 12:58:54 -0600

    format

--- src/lib/utilities/copy-to-clipboard.ts
+++ src/lib/utilities/copy-to-clipboard.ts
@@ -1,21 +1,21 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
 export const copyToClipboard = (content: string, timeout: number = 2000) => {
-  const copied = writable(false)
+  const copied = writable(false);
 
   const copy = async (event: Event) => {
     event.preventDefault();
     event.stopPropagation();
     try {
-      await navigator.clipboard.writeText(content)
-      copied.set(true)
+      await navigator.clipboard.writeText(content);
+      copied.set(true);
       setTimeout(() => {
-        copied.set(false)
-      }, timeout)
+        copied.set(false);
+      }, timeout);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  return { copy, copied }
-}
+  return { copy, copied };
+};
