ec39f168e27b6cfff0c01cd12f68421f0138e780
Ross Edfort <rossedfort@gmail.com>
2022-06-23 08:40:50 -0600

    create a copyable input component (#615)
    * [WIP] - move input to holocene and make copyable variety
    
    * finish copyable input
    
    * fix light copyable input colors
    
    * less border on chapter divs
    
    * lint
    
    * add copy-to-clipboard util and get rid of dupes
    
    * format
    
    * git rid of unnecessary parsing
    
    * rm unnecessary type

--- src/lib/components/code-block.svelte
+++ src/lib/components/code-block.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
   import Icon from '$lib/holocene/icon/index.svelte';
+  import { copyToClipboard } from '$lib/utilities/copy-to-clipboard';
 
   export let content: Parameters<typeof JSON.stringify>[0];
-  export let copied = false;
   export let inline = false;
   export let language = 'json';
 
-  $: isJSON = language === 'json';
-
-  const copy = () =>
-    navigator.clipboard
-      .writeText(isJSON ? formatJSON(content) : content)
-      .then(() => {
-        copied = !copied;
-        setTimeout(() => (copied = false), 2000);
-      })
-      .catch((error) => console.error(error));
+  let root: HTMLElement;
+  let isJSON = language === 'json';
 
   const formatJSON = (jsonData: string): string => {
     if (!jsonData) return;
@@ -30,10 +22,11 @@
     return JSON.stringify(parsedData, undefined, inline ? 0 : 2);
   };
 
-  let root: HTMLElement;
+  const parsedContent = isJSON ? formatJSON(content) : content;
+  const { copy, copied } = copyToClipboard(parsedContent);
 
   function highlight(root: HTMLElement, language: string, source: string) {
-    root.textContent = isJSON ? formatJSON(source) : source;
+    root.textContent = source;
     root.classList.forEach((item) => root.classList.remove(item));
     if (language) {
       root.classList.add(`language-${language}`);
@@ -44,12 +37,12 @@
 
   $: {
     if (root && window.Prism) {
-      highlight(root, language, content);
+      highlight(root, language, parsedContent);
     }
   }
 </script>
 
-{#if content || content === null}
+{#if parsedContent || parsedContent === null}
   <div
     class="relative h-auto w-full rounded-lg {$$props.class} {inline
       ? ''
@@ -67,7 +60,7 @@
       /></pre>
 
     <button on:click={copy} class="absolute top-4 right-4">
-      <Icon name={copied ? 'checkMark' : 'copy'} stroke="white" />
+      <Icon name={$copied ? 'checkMark' : 'copy'} stroke="white" />
     </button>
   </div>
 {/if}
---
+++ src/routes/fiction/chapters/input.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import Chapter from '../_chapter.svelte';
+
+  import Input from '$lib/holocene/input.svelte';
+</script>
+
+<Chapter
+  description="An input"
+  component={Input}
+  props={{ id: 'input1', label: 'Hello', value: '' }}
+/>
+
+<Chapter
+  description="A dark input"
+  component={Input}
+  props={{ id: 'input2', label: 'Hello', value: '', theme: 'dark' }}
+/>
+
+<Chapter
+  description="An input with an icon"
+  component={Input}
+  props={{ id: 'input3', label: 'Search...', value: '', icon: 'search' }}
+/>
+
+<Chapter
+  description="A dark input with an icon"
+  component={Input}
+  props={{
+    id: 'input4',
+    label: 'Search...',
+    icon: 'search',
+    value: '',
+    theme: 'dark',
+  }}
+/>
+
+<Chapter
+  description="A copyable input"
+  component={Input}
+  props={{
+    id: 'input5',
+    label: 'Copy me',
+    value: 'https://www.temporalurl.com/123',
+    copyable: true,
+  }}
+/>
+
+<Chapter
+  description="A copyable dark input"
+  component={Input}
+  props={{
+    id: 'input6',
+    label: 'Copy me',
+    value: 'https://www.temporalurl.com/123',
+    theme: 'dark',
+    copyable: true,
+  }}
+/>
