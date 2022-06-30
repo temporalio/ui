120e22173e3058efad230e0a5eb1f8f2737c314a
Ross Edfort <rossedfort@gmail.com>
2022-06-30 10:21:08 -0600

    implement dropdown button (#643)
    * implement dropdown button
    
    * lint
    
    * fix spelling, remove flex gap

---
+++ src/routes/fiction/chapters/dropdown-button.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import DropdownButton from '$lib/holocene/dropdown-button/index.svelte';
+  import Item from '$lib/holocene/dropdown-button/radio-menu-item.svelte';
+  import Chapter from '../_chapter.svelte';
+
+  let options = {
+    pepperoni: 'Pepperoni',
+    green_peppers: 'Green Peppers',
+    black_olives: 'Black Olives',
+  };
+
+  let favoritePizzaTopping: string;
+
+  const updateValue = (event: CustomEvent<{ value: string }>) => {
+    favoritePizzaTopping = event.detail.value;
+  };
+</script>
+
+<Chapter description="A simple dropdown menu">
+  <DropdownButton
+    label={options[favoritePizzaTopping] ?? 'Favorite Pizza Topping'}
+    icon="clock"
+  >
+    {#each Object.entries(options) as [value, label]}
+      <Item
+        on:click={updateValue}
+        name="pizza-toppings"
+        {value}
+        checked={value === favoritePizzaTopping}>{label}</Item
+      >
+    {/each}
+  </DropdownButton>
+</Chapter>
