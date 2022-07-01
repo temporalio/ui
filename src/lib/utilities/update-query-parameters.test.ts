19edb966c2716521ce40d23c3a45be78a2ee4aa6
Steve Kinney <hello@stevekinney.net>
2022-07-01 10:29:38 -0400

    Address edge case in ordering hash and search parameters (#656)
    * Address edge case in ordering hash and search parameters
    
    The hash should come after the search parameters.
    
    * Update update-query-parameters.test.ts
    
    * Don't export `gotoOptions`
    
    * Update update-query-parameters.test.ts
    
    * Always show event history in ascending order in JSON view, closes #645 (#646)
    
    * Always show events in ascending order in JSON view
    
    * Fetch separately for JSON view
    
    * Update workflow-history-json.svelte
    
    * Add stronger typing for EventSortOrder
    
    * Add tests
    
    * Export history sorts correctly
    
    * Update tests for reverse order
    
    * Ignore screenshots
    
    * Conditionally check for versions
    
    * Remove using addHashToURL in update query params
    
    * Go back to String(url)
    
    Co-authored-by: Alex Tideman <alex.tideman@gmail.com>

--- src/lib/utilities/update-query-parameters.test.ts
+++ src/lib/utilities/update-query-parameters.test.ts
@@ -1,6 +1,6 @@
 import { afterEach, describe, expect, it, vi } from 'vitest';
 
-import { updateQueryParameters } from './update-query-parameters';
+import { addHashToURL, updateQueryParameters } from './update-query-parameters';
 
 const gotoOptions = { replaceState: true, keepfocus: true, noscroll: true };
 const url = new URL('https://temporal.io');
@@ -50,7 +50,7 @@ describe('updateQueryParameters', () => {
 
   it('should call the delete method on the query when null is provided', () => {
     const parameter = 'parameter';
-    const value = null;
+    const value = null as unknown as string;
     const goto = () => Promise.resolve();
 
     const spy = vi.spyOn(url.searchParams, 'delete');
@@ -68,7 +68,10 @@ describe('updateQueryParameters', () => {
 
     updateQueryParameters({ parameter, value, url, goto });
 
-    expect(goto).toHaveBeenCalledWith(url.toString(), gotoOptions);
+    expect(goto).toHaveBeenCalledWith(
+      'https://temporal.io/?parameter=value',
+      gotoOptions,
+    );
   });
 
   it('should call `goto` with the correct path when query parameters already exist', () => {
@@ -79,18 +82,73 @@ describe('updateQueryParameters', () => {
     updateQueryParameters({ parameter, value, url, goto });
 
     expect(goto).toHaveBeenCalledWith(
-      'https://temporal.io/?parameter=newvalue#',
+      'https://temporal.io/?parameter=newvalue',
       gotoOptions,
     );
   });
 
   it('should call `goto` with without the "?" if the query params are empty', () => {
     const parameter = 'parameter';
-    const value = null;
+    const value = null as unknown as string;
     const goto = vi.fn().mockImplementation(() => Promise.resolve(null));
 
     updateQueryParameters({ parameter, value, url, goto });
 
-    expect(goto).toHaveBeenCalledWith('https://temporal.io/#', gotoOptions);
+    expect(goto).toHaveBeenCalledWith('https://temporal.io/', gotoOptions);
+  });
+
+  it('should set the parameter to an empty string if allowEmpty is set', () => {
+    const parameter = 'parameter';
+    const value = '';
+    const goto = vi.fn().mockImplementation(() => Promise.resolve(null));
+
+    updateQueryParameters({ parameter, value, url, goto, allowEmpty: true });
+
+    expect(goto).toHaveBeenCalledWith(
+      'https://temporal.io/?parameter=',
+      gotoOptions,
+    );
+  });
+});
+
+describe('addHashToURL', () => {
+  it('should add a hash to a URL', () => {
+    const url = new URL('https://temporal.io/');
+    expect(addHashToURL(url)).toBe('https://temporal.io/#');
+  });
+
+  it('should not add a duplicate hash to a URL', () => {
+    const url = new URL('https://temporal.io/#');
+    expect(addHashToURL(url)).toBe('https://temporal.io/#');
+  });
+
+  it('should put the hash before the search parameters', () => {
+    const url = new URL('https://temporal.io/?parameter=value');
+    expect(addHashToURL(url)).toBe('https://temporal.io/#?parameter=value');
+  });
+});
+
+describe('a sanity test for how URLs work in JavaScript', () => {
+  // This is primarily meant to serve as documentation for some of the
+  // decisions made above.
+
+  it('should not be able to parse the hash if it comes before the search parameters', () => {
+    const url = new URL('https://temporal.io/#hash?parameter=value');
+    expect(url.hash).not.toBe('#hash');
+  });
+
+  it('should not be able to parse the search parameters if they come after the hash', () => {
+    const url = new URL('https://temporal.io/#hash?parameter=value');
+    expect(url.search).not.toBe('?parameter=value');
+  });
+
+  it('should be able to parse the search parameters if it comes after the hash', () => {
+    const url = new URL('https://temporal.io/?parameter=value#hash');
+    expect(url.search).toBe('?parameter=value');
+  });
+
+  it('should be able to parse hash if it comes after the search parameters ', () => {
+    const url = new URL('https://temporal.io/?parameter=value#hash');
+    expect(url.hash).toBe('#hash');
   });
 });
