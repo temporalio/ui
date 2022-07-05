1b2f5bdb4a82ea03d9a4271add1ecee73a4486b8
Steve Kinney <hello@stevekinney.net>
2022-07-05 10:44:51 -0400

    Make tests for simplifyAttributes more realistic

--- src/lib/models/event-history/simplify-attributes.test.ts
+++ src/lib/models/event-history/simplify-attributes.test.ts
@@ -1,32 +1,67 @@
+import { describe, expect, it } from 'vitest';
 import { simplifyAttributes } from './simplify-attributes';
 
-const createAttributes = () => {
-  return {
-    shouldBeSimple: {
-      name: 'SimpleResult',
+const attributes = {
+  type: 'workflowExecutionStartedEventAttributes',
+  workflowType: 'workflow.advanced-visibility',
+  parentWorkflowNamespace: 'canary',
+  parentWorkflowExecution: {
+    workflowId: 'temporal.fixture.running.workflow.id',
+    runId: '8f00d989-6bc2-4826-b9f9-7c18ed90c9cc',
+  },
+  parentInitiatedEventId: '11',
+  taskQueue: {
+    name: 'canary-task-queue',
+    kind: 'Normal',
+  },
+  input: {
+    payloads: ['1656707029044596700', 'canary'],
+  },
+  workflowExecutionTimeout: '',
+  workflowRunTimeout: '20 minutes',
+  workflowTaskTimeout: '20 seconds',
+  continuedExecutionRunId: '',
+  initiator: 'Unspecified',
+  continuedFailure: null,
+  lastCompletionResult: null,
+  originalExecutionRunId: '2a7ba421-f74b-4b8b-b9d8-e6e30e4caac7',
+  identity: '',
+  firstExecutionRunId: '2a7ba421-f74b-4b8b-b9d8-e6e30e4caac7',
+  retryPolicy: null,
+  attempt: 1,
+  workflowExecutionExpirationTime: '',
+  cronSchedule: '',
+  firstWorkflowTaskBackoff: '',
+  memo: null,
+  searchAttributes: {
+    indexedFields: {
+      CustomKeywordField: 'childWorkflowValue',
     },
-    notSoSimple: {
-      name: 'NotSimple',
-      type: 'NotSimple',
-    },
-  };
-};
+  },
+  prevAutoResetPoints: null,
+  header: {
+    fields: {},
+  },
+  parentInitiatedEventVersion: '0',
+} as unknown as EventAttributesWithType<'workflowExecutionStartedEventAttributes'>;
 
 describe('simplifyAttributes', () => {
   it('should take single key attributes and reduce them down to their values', () => {
-    const attributes = createAttributes();
-    const { shouldBeSimple } = simplifyAttributes(attributes);
+    const { workflowType } = simplifyAttributes(
+      attributes,
+    ) as EventAttributesWithType<'workflowExecutionStartedEventAttributes'>;
 
-    expect(shouldBeSimple).toBe('SimpleResult');
+    expect(workflowType).toBe('workflow.advanced-visibility');
   });
 
   it('should take single key attributes and reduce them down to their values', () => {
-    const attributes = createAttributes();
-    const { notSoSimple } = simplifyAttributes(attributes);
+    const { parentWorkflowExecution } = simplifyAttributes(
+      attributes,
+    ) as EventAttributesWithType<'workflowExecutionStartedEventAttributes'>;
 
-    expect(notSoSimple).toEqual({
-      name: 'NotSimple',
-      type: 'NotSimple',
+    expect(parentWorkflowExecution).toEqual({
+      workflowId: 'temporal.fixture.running.workflow.id',
+      runId: '8f00d989-6bc2-4826-b9f9-7c18ed90c9cc',
     });
   });
 });
