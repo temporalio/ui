28b490feb9da338b72aff9934d3d96ef53a60352
Steve Kinney <hello@stevekinney.net>
2022-07-11 11:38:59 -0400

    Address Strict TypeScript errors with empty User object
    Fixes two out of 394 TypeScript errors when strict mode is turned on.

--- src/lib/utilities/has.ts
+++ src/lib/utilities/has.ts
@@ -1,3 +1,9 @@
 export const has = (target: unknown, property: string): boolean => {
   return Object.prototype.hasOwnProperty.call(target, property);
 };
+
+export const hasKeys = (obj: {
+  [key: string | number | symbol]: unknown;
+}): boolean => {
+  return !!Object.keys(obj).length;
+};
