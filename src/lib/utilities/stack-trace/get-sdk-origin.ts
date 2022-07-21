7710e94f11a8d7e4f1cf825c1ad4f86656ebca12
Nik B <63573283+BashkirovN@users.noreply.github.com>
2022-07-21 11:20:24 -0700

    Added functions that determine the origin SDK of a stack trace (#710)
    * Stack trace querty network responses added to fixtures
    
    * Decoded data value
    
    * Decoded data value
    
    * Decoded data value
    
    * Added functions that determine SDK type from a stack trace
    
    * Added a function that determines the origin SDK of a stacktrace
    
    * Run Prettier on the files
    
    * RegEx improved
    
    * Ran prettier again

---
+++ src/lib/utilities/stack-trace/get-sdk-origin.ts
@@ -0,0 +1,13 @@
+//  The function takes a decoded StackTraceQuery response as a string
+//  and returns the name of the SDK the response came from
+
+import { isFromTypeScriptSDK } from './is-from-typescript-sdk';
+import { isFromGoSDK } from './is-from-go-sdk';
+import { isFromJavaSDK } from './is-from-java-sdk';
+
+export const getSDKOrigin = (stackTraceText: string): string => {
+  if (isFromTypeScriptSDK(stackTraceText)) return 'typescript';
+  else if (isFromGoSDK(stackTraceText)) return 'go';
+  else if (isFromJavaSDK(stackTraceText)) return 'java';
+  else return null;
+};
