07587198ab8c875d186f5d34cd27270ea13f3527
Laura Whitaker <laura.whitaker@temporal.io>
2022-07-27 17:19:49 -0600

    Add textarea to holocene

---
+++ src/lib/holocene/textarea.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let value: string = '';
+  export let disabled: boolean = false;
+  export let placeholder: string = '';
+  export let rows: number = 5;
+</script>
+
+<textarea
+  class="rounded border border-gray-900 p-5 w-full min-h-fit font-mono text-sm"
+  bind:value
+  {disabled}
+  {placeholder}
+  {rows}
+/>
