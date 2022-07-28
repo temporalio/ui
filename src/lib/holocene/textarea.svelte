dffa09385053282be09462ec33f3d03d2a2c2392
Laura Whitaker <laura.k.whitaker@gmail.com>
2022-07-28 11:01:04 -0600

    create a Textarea component (#729)
    * Add textarea to holocene
    
    * Add textarea chapters
    
    * Format css classes

---
+++ src/lib/holocene/textarea.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let value: string = '';
+  export let disabled: boolean = false;
+  export let placeholder: string = '';
+  export let rows: number = 5;
+</script>
+
+<textarea
+  class="font-mono min-h-fit w-full rounded border border-gray-900 p-5 text-sm"
+  bind:value
+  {disabled}
+  {placeholder}
+  {rows}
+/>
