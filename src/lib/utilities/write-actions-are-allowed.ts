be2f4fec0ecf87f6c34b32fa31474412dcbda634
Steve Kinney <hello@stevekinney.net>
2022-07-28 18:05:14 -0600

    Update write-actions-are-allowed.ts

--- src/lib/utilities/write-actions-are-allowed.ts
+++ src/lib/utilities/write-actions-are-allowed.ts
@@ -3,6 +3,5 @@ import { settings } from '$lib/stores/settings';
 
 export const writeActionsAreAllowed = (store = settings): boolean => {
   const isDisabled = get(store).disableWriteActions;
-  console.log({ isDisabled, settings: get(store) });
   return !isDisabled;
 };
