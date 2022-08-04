8de4cba3d05fb58603898716295424c0a68113f7
Alex Tideman <alex.tideman@gmail.com>
2022-08-04 13:01:48 -0500

    Remove unused noop

--- src/lib/holocene/split-button.svelte
+++ src/lib/holocene/split-button.svelte
@@ -4,7 +4,6 @@
   import MenuButton from '$holocene/primitives/menu/menu-button.svelte';
   import Menu from '$holocene/primitives/menu/menu.svelte';
   import Button from './button.svelte';
-  import { noop } from 'svelte/internal';
 
   export let label: string;
   export let id: string;
