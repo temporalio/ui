2c7a75d75db670e11d079aa46fc00f184aa834b5
Laura Whitaker <laura.whitaker@temporal.io>
2022-08-08 14:30:08 -0600

    Make icon option in toggle-button

--- src/routes/fiction/chapters/toggle-button.svelte
+++ src/routes/fiction/chapters/toggle-button.svelte
@@ -3,20 +3,38 @@
   import ToggleButton from '$lib/holocene/toggle-button/toggle-button.svelte';
   import ToggleButtons from '$lib/holocene/toggle-button/toggle-buttons.svelte';
 
-  let tab = 'auto';
+  let tab = {
+    a: 'auto',
+    b: 'auto',
+  };
 </script>
+
+<Chapter description="A toggle button">
+  <ToggleButtons>
+    <ToggleButton
+      active={tab.a === 'auto'}
+      data-cy="auto"
+      on:click={() => (tab.a = 'auto')}>Auto</ToggleButton
+    >
+    <ToggleButton
+      active={tab.a === 'manual'}
+      data-cy="manual"
+      on:click={() => (tab.a = 'manual')}>Manual</ToggleButton
+    >
+  </ToggleButtons>
+</Chapter>
 
 <Chapter description="A toggle button with icons">
   <ToggleButtons>
     <ToggleButton
       icon="workflow"
-      active={tab === 'auto'}
-      on:click={() => (tab = 'auto')}>Auto</ToggleButton
+      active={tab.b === 'auto'}
+      on:click={() => (tab.b = 'auto')}>Auto</ToggleButton
     >
     <ToggleButton
       icon="calendarPlus"
-      active={tab === 'manual'}
-      on:click={() => (tab = 'manual')}>Manual</ToggleButton
+      active={tab.b === 'manual'}
+      on:click={() => (tab.b = 'manual')}>Manual</ToggleButton
     >
   </ToggleButtons>
 </Chapter>
