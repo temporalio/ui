b71e087c252d1259034c3dd7d40988528046b843
Alex Tideman <alex.tideman@gmail.com>
2022-08-10 12:25:12 -0500

    Native select

---
+++ src/lib/holocene/select/native-select.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import colors from 'tailwindcss/colors';
+  import Icon from '$holocene/icon/index.svelte';
+  import Menu from '$holocene/primitives/menu/menu.svelte';
+  import Option, { isOption } from '$holocene/select/option.svelte';
+  import type { Option as OptionType } from '$holocene/select/option.svelte';
+  import MenuButton from '$holocene/primitives/menu/menu-button.svelte';
+  import MenuContainer from '$holocene/primitives/menu/menu-container.svelte';
+
+  let show = false;
+
+  export let label = '';
+  export let id: string;
+  export let placeholder: string;
+  export let value = '';
+  export let options: { label: string; value: string }[] = [];
+</script>
+
+{#if label}
+  <label for={id} class={$$props.labelClass}>{label}</label>
+{/if}
+<select {value} {id} class={$$props.class} {placeholder} on:select>
+  {#each options as option}
+    <option value={option.value}>{label}</option>
+  {/each}
+</select>
