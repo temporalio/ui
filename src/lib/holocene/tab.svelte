2941c189be440d61b3c8fa2d8f2883e7bff00500
Alex Tideman <alex.tideman@gmail.com>
2022-08-15 10:26:05 -0500

    Disable tabs

--- src/lib/holocene/tab.svelte
+++ src/lib/holocene/tab.svelte
@@ -3,12 +3,14 @@
   export let label: string;
   export let amount: number | Long.Long = null;
   export let active = false;
+  export let disabled = false;
 </script>
 
 {#if href}
   <a
     class="block whitespace-nowrap border-b-2 border-white text-sm hover:border-b-2 hover:border-blue-700 md:text-base"
     class:active
+    class:disabled
     {href}
     data-cy={$$props.dataCy}
   >
@@ -23,6 +25,7 @@
   <div
     class="block cursor-pointer whitespace-nowrap border-b-2 border-white text-sm hover:border-b-2 hover:border-blue-700 md:text-base"
     class:active
+    class:disabled
     data-cy={$$props.dataCy}
     on:click
   >
@@ -39,7 +42,15 @@
   a.active {
     @apply border-b-2 border-blue-700 font-medium text-blue-700;
   }
+
+  a.disabled {
+    @apply pointer-events-none border-0 text-gray-800 opacity-50;
+  }
   div.active {
     @apply border-b-2 border-blue-700 font-medium text-blue-700;
+  }
+
+  div.disabled {
+    @apply cursor-not-allowed border-0 text-gray-800 opacity-50;
   }
 </style>
