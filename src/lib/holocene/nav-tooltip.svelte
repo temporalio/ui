612a1a79edcd9b324af3e12deaf2073ccf9d34d7
Alex Tideman <alex.tideman@gmail.com>
2022-08-16 09:10:54 -0500

    Add NavTooltip

---
+++ src/lib/holocene/nav-tooltip.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { navOpen } from '$lib/stores/nav-open';
+  import Tooltip from './tooltip.svelte';
+</script>
+
+<Tooltip hide={$navOpen} {...$$restProps}>
+  <slot />
+</Tooltip>
