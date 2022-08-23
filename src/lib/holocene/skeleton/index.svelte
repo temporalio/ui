2226a8947f15a36622ca40fe531b2aab4257fc68
Alex Tideman <alex.tideman@gmail.com>
2022-08-23 10:27:55 -0500

    WIP: skeleton

---
+++ src/lib/holocene/skeleton/index.svelte
@@ -0,0 +1,30 @@
+<div
+  class="w-full h-5 
+      bg-gradient-to-r
+      from-gray-100
+      via-gray-200
+      to-gray-300
+      rounded-full
+      background-animate 
+      {$$props.class}"
+/>
+
+<style lang="postcss">
+  .background-animate {
+    background-size: 400%;
+
+    -webkit-animation: AnimationName 3s ease infinite;
+    -moz-animation: AnimationName 3s ease infinite;
+    animation: AnimationName 3s ease infinite;
+  }
+
+  @keyframes AnimationName {
+    0%,
+    100% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+  }
+</style>
---
+++ src/lib/holocene/skeleton/table.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Table from '$lib/holocene/table/table.svelte';
+  import Skeleton from './index.svelte';
+
+  export let rows = 10;
+</script>
+
+<Table class="w-full border" variant="fancy">
+  <tr slot="headers" class="h-10">
+    <th />
+  </tr>
+  {#each Array.from(Array(rows).keys()) as _row}
+    <tr class="flex gap-4 p-2">
+      <Skeleton class="w-96" />
+      <Skeleton />
+      <Skeleton />
+      <Skeleton />
+    </tr>
+  {/each}
+</Table>
