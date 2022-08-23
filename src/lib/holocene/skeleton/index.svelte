57bd8fb2cef4726f62ce9b285c98f28bebf653e2
Alex Tideman <alex.tideman@gmail.com>
2022-08-23 11:27:18 -0500

    Skeleton and SkeletonTable Components (#787)
    * WIP: skeleton
    
    * Add Skeleton and Skeleton Table

---
+++ src/lib/holocene/skeleton/index.svelte
@@ -0,0 +1,31 @@
+<div
+  class="background-animate 
+      h-12
+      w-full
+      rounded-full
+      bg-gradient-to-r
+      from-gray-100
+      via-gray-200
+      to-gray-300 
+      {$$props.class}"
+/>
+
+<style lang="postcss">
+  .background-animate {
+    background-size: 400%;
+
+    -webkit-animation: Loading 3s ease infinite;
+    -moz-animation: Loading 3s ease infinite;
+    animation: Loading 3s ease infinite;
+  }
+
+  @keyframes Loading {
+    0%,
+    100% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+  }
+</style>
---
+++ src/lib/holocene/skeleton/table.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Table from '$lib/holocene/table/table.svelte';
+  import Skeleton from './index.svelte';
+
+  export let rows = 10;
+</script>
+
+<Table class="w-full border" variant="fancy">
+  <tr slot="headers" class="h-10">
+    <th />
+  </tr>
+  {#each Array.from(Array(rows).keys()) as _row}
+    <tr class="flex gap-4 p-2">
+      <Skeleton class="h-5 w-44" />
+      <Skeleton class="h-5" />
+      <Skeleton class="h-5 w-96" />
+      <Skeleton class="h-5 w-96" />
+    </tr>
+  {/each}
+</Table>
