202cc5f00dc643e8c732422653b0e0c2a0d92399
Steve Kinney <hello@stevekinney.net>
2022-08-25 09:33:02 -0700

    Handle edge cases

--- src/lib/utilities/to-time-difference.ts
+++ src/lib/utilities/to-time-difference.ts
@@ -1,12 +1,12 @@
-export const toTimeDifference = (date: unknown, now = Date.now()) => {
+export const toTimeDifference = (date: unknown, now = Date.now()): string => {
   if (!date) return '';
   const start = String(date);
+
   try {
     const scheduled = Number(new Date(start));
-    const timeFromNow = (scheduled - now) / 1000 + 's';
-    console.log({ date, now });
-    return timeFromNow;
+    const timeFromNow = (scheduled - now) / 1000;
+    return !isNaN(timeFromNow) ? `${timeFromNow}s` : '';
   } catch (error) {
-    return undefined;
+    return '';
   }
 };
