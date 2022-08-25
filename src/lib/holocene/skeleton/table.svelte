be197f79438b813b46d161d9ea43d4c97deaae45
Alex Tideman <alex.tideman@gmail.com>
2022-08-25 09:17:32 -0500

    Make fancy table rows .25 rem less y padding for skinny rows

--- src/lib/holocene/skeleton/table.svelte
+++ src/lib/holocene/skeleton/table.svelte
@@ -6,11 +6,11 @@
 </script>
 
 <Table class="w-full border" variant="fancy">
-  <tr slot="headers" class="h-10">
+  <tr slot="headers" class="h-8">
     <th />
   </tr>
   {#each Array.from(Array(rows).keys()) as _row}
-    <tr class="flex gap-4 p-2">
+    <tr class="flex items-center gap-4 p-3">
       <Skeleton class="h-5 w-44" />
       <Skeleton class="h-5" />
       <Skeleton class="h-5 w-96" />
