cd3e4ee4f02de600de28a6ffab280c4f33b15c59
Alex Tideman <alex.tideman@gmail.com>
2022-09-06 11:43:50 -0500

    Update to sveltekit .405, fix all the things related

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $types: path.resolve('./src/types'),
+      $holocene: path.resolve('./src/lib/holocene'),
+      $fixtures: path.resolve('./src/fixtures'),
+    },
+  },
+};
+
+export default config;
