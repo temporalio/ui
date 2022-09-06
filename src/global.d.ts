b9422ef4f0268596b8a314c48a80d80ac607ba37
Alex Tideman <alex.tideman@gmail.com>
2022-09-06 12:11:00 -0500

    Remove unnecessary unknown extension

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,7 +5,7 @@ declare module '@sveltejs/svelte-virtual-list';
 
 type NamespaceItem = { namespace: string; href: string; onClick: () => void };
 
-type Optional<T extends unknown, K extends keyof T = keyof T> = Omit<T, K> &
+type Optional<T, K extends keyof T = keyof T> = Omit<T, K> &
   Partial<Pick<T, K>>;
 
 interface Window {
