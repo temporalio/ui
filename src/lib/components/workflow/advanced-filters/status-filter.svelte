18e112df902048ab7ad44259cdcf801aee998d2e
Alex Tideman <alex.tideman@gmail.com>
2022-09-09 11:23:48 -0500

    Add search attributes

---
+++ src/lib/components/workflow/advanced-filters/status-filter.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import Select from '$lib/holocene/select/select.svelte';
+  import Option from '$lib/holocene/select/option.svelte';
+  import WorkflowStatus from '$lib/components/workflow-status.svelte';
+
+  export let value = '';
+
+  const operations = {
+    Is: 'Is',
+  };
+
+  const statuses = {
+    All: '',
+    Running: 'Running',
+    'Timed Out': 'TimedOut',
+    Completed: 'Completed',
+    Failed: 'Failed',
+    'Continued as New': 'ContinuedAsNew',
+    Canceled: 'Canceled',
+    Terminated: 'Terminated',
+  };
+
+  let operator: keyof typeof operations = 'Is';
+</script>
+
+<div class="flex gap-2">
+  <Select showIcon={false} disabled id="operator-filter" bind:value={operator}>
+    {#each Object.entries(operations) as [label, value] (label)}
+      <Option {value}>{label}</Option>
+    {/each}
+  </Select>
+  <Select
+    id="workflow-status"
+    bind:value
+    class="w-44"
+    displayValue={(value) => {
+      if (!value) return 'All';
+      return value;
+    }}
+  >
+    {#each Object.entries(statuses) as [label, value] (label)}
+      <Option value={label}>
+        {#if value}
+          <WorkflowStatus status={value} />
+        {:else}
+          {label}
+        {/if}
+      </Option>
+    {/each}
+  </Select>
+</div>
