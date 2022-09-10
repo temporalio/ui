c8db8855f2e47f44cdd375ee071c29c384a87db6
Alex Tideman <alex.tideman@gmail.com>
2022-09-09 20:51:34 -0500

    Add preview

--- src/lib/components/workflow/advanced-filter/index.svelte
+++ src/lib/components/workflow/advanced-filter/index.svelte
@@ -6,22 +6,23 @@
 
   import Select from '$lib/holocene/select/select.svelte';
   import Option from '$lib/holocene/select/option.svelte';
-  import Input from '$lib/holocene/input/input.svelte';
+  import Button from '$lib/holocene/button.svelte';
   import IconButton from '$lib/holocene/icon-button.svelte';
-  import TypeFilter from './advanced-filters/type-filter.svelte';
-  import StatusFilter from './advanced-filters/status-filter.svelte';
-  import TimeRangeFilter from './advanced-filters/time-range-filter.svelte';
-  import IdFilter from './advanced-filters/id-filter.svelte';
-  import SearchAttributeFilter from './advanced-filters/search-attribute-filter.svelte';
+  import TypeFilter from './type-filter.svelte';
+  import StatusFilter from './status-filter.svelte';
+  import TimeRangeFilter from './time-range-filter.svelte';
+  import IdFilter from './id-filter.svelte';
+  import SearchAttributeFilter from './search-attribute-filter.svelte';
   import type { FilterKey } from '$lib/utilities/query/list-workflow-query';
   import { searchAttributes } from '$lib/stores/search-attributes';
+  import { noop } from 'svelte/internal';
 
   export let filterType: FilterKey;
   export let value: string = '';
-  export let isOnly: boolean;
-  export let isLast: boolean;
-  export let addFilter: () => void;
-  export let removeFilter: () => void;
+  export let isOnly: boolean = true;
+  export let isLast: boolean = false;
+  export let addFilter: () => void = () => noop;
+  export let removeFilter: () => void = () => noop;
 
   const baseOptions = [
     { label: 'Workflow Type', value: 'workflowType', component: TypeFilter },
@@ -29,15 +30,17 @@
     { label: 'Status', value: 'executionStatus', component: StatusFilter },
     { label: 'Time Range', value: 'timeRange', component: TimeRangeFilter },
   ];
-  const searchAttributeOptions = Object.keys($searchAttributes).map((key) => {
-    return { label: key, value: key, component: SearchAttributeFilter };
-  });
+  const searchAttributeOptions = $searchAttributes
+    ? Object.keys($searchAttributes).map((key) => {
+        return { label: key, value: key, component: SearchAttributeFilter };
+      })
+    : [];
   const filterOptions = [...baseOptions, ...searchAttributeOptions];
 
-  let selected = filterOptions.find((option) => option.value === filterType);
+  let selected = filterOptions?.find((option) => option.value === filterType);
 
   $: {
-    selected = filterOptions.find((option) => option.value === filterType);
+    selected = filterOptions?.find((option) => option.value === filterType);
     value = '';
   }
 </script>
@@ -56,6 +59,9 @@
   </Select>
   <svelte:component this={selected.component} bind:value />
   <div class="flex gap-2 items-center">
+    <Button variant="secondary" classes="bg-green-200">AND</Button>
+    <Button variant="secondary" classes="bg-green-200">OR</Button>
+    <Button variant="secondary" classes="bg-green-200">()</Button>
     {#if !isOnly}
       <IconButton
         icon="close"
