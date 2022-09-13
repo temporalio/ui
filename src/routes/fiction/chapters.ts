4f6ca03cde146a8b7c07f6f1cce786eb6dde36bf
Ross Edfort <rossedfort@gmail.com>
2022-09-13 09:13:41 -0600

    fix fiction for local dev (#827)

--- src/routes/fiction/chapters.ts
+++ src/routes/fiction/chapters.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 import { readdirSync } from 'fs';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const get: RequestHandler = async () => {
+export const GET: RequestHandler = async () => {
   const files = readdirSync(join('src', 'routes', 'fiction', 'chapters'));
   const chapters = files
     .filter((file) => file.includes('.svelte'))
