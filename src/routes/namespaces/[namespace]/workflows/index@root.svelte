c8f3fed4fd3cecbe949498e1f75bd80cfb547273
Alex Tideman <alex.tideman@gmail.com>
2022-09-16 09:50:25 -0500

    WIP

--- src/routes/namespaces/[namespace]/workflows/index@root.svelte
+++ src/routes/namespaces/[namespace]/workflows/index@root.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+  import { onDestroy, getContext } from 'svelte';
   import Workflows from '$lib/pages/workflows.svelte';
+
+  const CurrentUser = getContext('CurrentUser');
+
+  console.log('$CurrentUser', CurrentUser);
 </script>
 
+<h1>Cats</h1>
 <Workflows />
