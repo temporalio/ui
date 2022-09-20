916d308b1604b262f2c1412b03195ada5e303a70
Alex Tideman <alex.tideman@gmail.com>
2022-09-20 12:33:13 -0500

    Set terminateDisabled for not found context of core user

--- src/lib/utilities/core-user.ts
+++ src/lib/utilities/core-user.ts
@@ -2,6 +2,6 @@ import { CoreUser, CoreUserKey } from '$lib/models/core-user';
 import { hasContext, getContext } from 'svelte';
 
 export const getCoreUser = (): CoreUser => {
-  if (!hasContext(CoreUserKey)) return {} as CoreUser;
+  if (!hasContext(CoreUserKey)) return { terminateDisabled: () => false };
   return getContext(CoreUserKey);
 };
