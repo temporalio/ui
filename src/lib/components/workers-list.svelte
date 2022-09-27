bc809189c0f236e86559831e09854a1e8419f3ca
Loren 🤓 <lorensr@gmail.com>
2022-09-27 01:44:44 -0400

    Add explanation to Worker list

--- src/lib/components/workers-list.svelte
+++ src/lib/components/workers-list.svelte
@@ -56,4 +56,12 @@
       <EmptyState title={'No Workers Found'} />
     {/each}
   </section>
+  <p class="mt-4 max-w-2xl text-secondary">
+    The Server records the last time a Worker sent a poll request (displayed as
+    “Last Accessed”). Poll requests can last up to a minute, so a “Last
+    Accessed” time less than a minute ago is normal. If it's over a minute ago,
+    then likely either the Worker is at capacity (all Workflow and Activity
+    slots are full) or it has shut down. Once it has been 5 minutes since the
+    last poll request, the Worker will no longer appear on the list.
+  </p>
 </section>
--- src/routes/__layout-root.svelte
+++ src/routes/__layout-root.svelte
@@ -37,7 +37,7 @@
 
     const cluster: GetClusterInfoResponse = await fetchCluster(settings, fetch);
 
-    fetchSearchAttributes(settings);
+    fetchSearchAttributes(settings, fetch);
 
     const uiVersionInfo: UiVersionInfo = {
       current: settings.version,
