c461fd4f44f8f15e5ab7f737b010df13505d65b9
Alex Tideman <alex.tideman@gmail.com>
2022-10-17 09:18:02 -0500

    Use old ID

--- src/lib/components/workflow/workflows-summary-table.svelte
+++ src/lib/components/workflow/workflows-summary-table.svelte
@@ -8,7 +8,7 @@
 <Table class="w-full md:table-fixed" {updating}>
   <TableHeaderRow slot="headers">
     <th class="hidden w-48 md:table-cell">Status</th>
-    <th class="hidden md:table-cell md:w-60 xl:w-auto">Workflow Id</th>
+    <th class="hidden md:table-cell md:w-60 xl:w-auto">Workflow ID</th>
     <th class="hidden md:table-cell md:w-60 xl:w-80">Type</th>
     <th class="hidden xl:table-cell xl:w-60">Start</th>
     <th class="hidden xl:table-cell xl:w-60">End</th>
