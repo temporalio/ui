98caf7456a3825a2cbc43589070a3498f51cc5d0
Ross Edfort <rossedfort@gmail.com>
2022-10-20 15:00:40 -0600

    add a selectable table component in holocene

---
+++ src/lib/holocene/table/example-table-row.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let status: string;
+  export let id: string;
+  export let type: string;
+  export let start: string;
+  export let end: string;
+</script>
+
+<td>{status}</td>
+<td>{id}</td>
+<td>{type}</td>
+<td>{start}</td>
+<td>{end}</td>
