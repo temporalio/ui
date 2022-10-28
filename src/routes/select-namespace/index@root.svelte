a03e4f3dcbdaa43fce1250dfdf2a463f290357cf
Laura Whitaker <laura.whitaker@temporal.io>
2022-10-28 14:44:57 -0600

    Add namespaces promise to namespaces select page

--- src/routes/select-namespace/index@root.svelte
+++ src/routes/select-namespace/index@root.svelte
@@ -2,49 +2,55 @@
   import VirtualList from '@sveltejs/svelte-virtual-list';
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
-  import type { DescribeNamespaceResponse as Namespace } from '$types';
   import { lastUsedNamespace } from '$lib/stores/namespaces';
   import { routeForWorkflows } from '$lib/utilities/route-for';
   import EmptyState from '$lib/holocene/empty-state.svelte';
   import Icon from '$lib/holocene/icon/icon.svelte';
+  import Loading from '$lib/holocene/loading.svelte';
   import PageTitle from '$lib/components/page-title.svelte';
   import { fetchWorkflowForAuthorization } from '$lib/services/workflow-service';
   import { notifications } from '$lib/stores/notifications';
+  import { fetchNamespaces } from '$lib/services/namespaces-service';
+
+  import type { DescribeNamespaceResponse as Namespace } from '$types';
+  import type { ListNamespacesResponse } from '$types';
 
   let searchField: HTMLInputElement = null;
+  let searchValue = '';
 
   const showTemporalSystemNamespace =
     $page.stuff?.settings?.showTemporalSystemNamespace;
 
-  const namespaces = ($page.stuff.namespaces || [])
-    .map((namespace: Namespace) => namespace?.namespaceInfo?.name)
-    .filter(
-      (namespace: string) =>
-        showTemporalSystemNamespace || namespace !== 'temporal-system',
-    );
-
-  const namespaceList: NamespaceItem[] = namespaces.map((namespace: string) => {
-    return {
-      namespace,
-      onClick: async (namespace: string) => {
-        const { authorized } = await fetchWorkflowForAuthorization(namespace);
-        if (authorized) {
-          $lastUsedNamespace = namespace;
-          goto(routeForWorkflows({ namespace }));
-        } else {
-          notifications.add(
-            'error',
-            'You do not have access to this namespace.',
-          );
-        }
-      },
-    };
-  });
-
-  let searchValue = '';
-  $: filteredList = namespaceList.filter(({ namespace }) =>
-    namespace.includes(searchValue),
+  const namespacesPromise: Promise<ListNamespacesResponse> = fetchNamespaces(
+    $page.stuff.settings,
   );
+
+  function getNamespaceList(namespaces: Namespace[]) {
+    const filteredNamespaces = namespaces
+      .map((namespace: Namespace) => namespace?.namespaceInfo?.name)
+      .filter(
+        (namespace: string) =>
+          showTemporalSystemNamespace || namespace !== 'temporal-system',
+      );
+
+    return filteredNamespaces.map((namespace: string) => {
+      return {
+        namespace,
+        onClick: async (namespace: string) => {
+          const { authorized } = await fetchWorkflowForAuthorization(namespace);
+          if (authorized) {
+            $lastUsedNamespace = namespace;
+            goto(routeForWorkflows({ namespace }));
+          } else {
+            notifications.add(
+              'error',
+              'You do not have access to this namespace.',
+            );
+          }
+        },
+      };
+    });
+  }
 </script>
 
 <PageTitle title="Select Namespace" url={$page.url.href} />
@@ -62,21 +68,32 @@
       bind:this={searchField}
     />
   </div>
-  <ul class="h-screen w-full">
-    {#if namespaceList.length}
-      {#if filteredList.length}
-        <VirtualList items={filteredList} let:item itemHeight={50}>
-          <li class="link-item" on:click={() => item?.onClick(item.namespace)}>
-            {item.namespace}
-          </li>
-        </VirtualList>
+  {#await namespacesPromise}
+    <Loading />
+  {:then namespacesResult}
+    {@const namespaceList = getNamespaceList(namespacesResult?.namespaces)}
+    <ul class="h-screen w-full">
+      {#if namespaceList.length}
+        {@const filteredList = namespaceList.filter(({ namespace }) =>
+          namespace.includes(searchValue),
+        )}
+        {#if filteredList.length}
+          <VirtualList items={filteredList} let:item itemHeight={50}>
+            <li
+              class="link-item"
+              on:click={() => item?.onClick(item.namespace)}
+            >
+              {item.namespace}
+            </li>
+          </VirtualList>
+        {:else}
+          <EmptyState title="No results." />
+        {/if}
       {:else}
-        <EmptyState title="No results." />
+        <EmptyState title="No Namespaces. Contact your admin to create one." />
       {/if}
-    {:else}
-      <EmptyState title="No Namespaces. Contact your admin to create one." />
-    {/if}
-  </ul>
+    </ul>
+  {/await}
 </div>
 
 <style lang="postcss">
