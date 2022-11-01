5d723368146f038a13b38c728d731ef329b41131
Laura Whitaker <laura.whitaker@temporal.io>
2022-10-31 21:15:04 -0600

    Add check for error title or copy

--- src/lib/components/workflow/workflow-typed-error.svelte
+++ src/lib/components/workflow/workflow-typed-error.svelte
@@ -5,61 +5,45 @@
   import Link from '$lib/holocene/link.svelte';
 
   const WORKFLOW_TASK_FAILED_ERROR_COPY = {
-    Unspecified: {
-      title: '',
-      copy: '',
-    },
     UnhandledCommand: {
       title: 'Unhandled Command',
       copy: 'The workflow task has failed because there are new available events since the last workflow task started. A retry workflow task has been scheduled and the workflow will have a chance to handle those new events.',
     },
     BadScheduleActivityAttributes: {
       title: 'Bad Schedule Activity Attributes',
-      copy: '',
     },
     BadRequestCancelActivityAttributes: {
       title: 'Bad Request Cancel Activity Attributes',
-      copy: '',
     },
     BadStartTimerAttributes: {
       title: 'Bad Start Timer Attributes',
-      copy: '',
     },
     BadCancelTimerAttributes: {
       title: 'Bad Cancel Timer Attributes',
-      copy: '',
     },
     BadRecordMarkerAttributes: {
       title: 'Bad Record Marker Attributes',
-      copy: '',
     },
     BadCompleteWorkflowExecutionAttributes: {
       title: 'Bad Complete Workflow Execution Attributes',
-      copy: '',
     },
     BadFailWorkflowExecutionAttribute: {
       title: 'Bad Fail Workflow Execution Attributes',
-      copy: '',
     },
     BadCancelWorkflowExecutionAttributes: {
       title: 'Bad Cancel Workflow Execution Attributes',
-      copy: '',
     },
     BadRequestCancelExternalAttributes: {
       title: 'Bad Request Cancel External Attributes',
-      copy: '',
     },
     BadContinueAsNewAttributes: {
       title: 'Bad Continue As New Attributes',
-      copy: '',
     },
     StartTimerDuplicateId: {
       title: 'Start Timer Duplicate',
-      copy: '',
     },
     ResetStickyTaskQueue: {
       title: 'Reset Sticky Task Queue',
-      copy: '',
     },
     WorkflowWorkerUnhandledFailure: {
       title: 'Workflow Worker Unhandled Failure',
@@ -69,11 +53,9 @@
     },
     BadSignalWorkflowExecutionAttributes: {
       title: 'Bad Signal Workflow Execution Attributes',
-      copy: '',
     },
     BadStartChildExecutionAttributes: {
       title: 'Bad Start Child Execution Attributes',
-      copy: '',
     },
     ForceCloseCommand: {
       title: 'Force Close Command',
@@ -85,15 +67,12 @@
     },
     BadSignalInputSize: {
       title: 'Bad Signal Input Size',
-      copy: '',
     },
     ResetWorkflow: {
       title: 'Reset Workflow',
-      copy: '',
     },
     BadBinary: {
       title: 'Bad Binary',
-      copy: '',
     },
     ScheduleActivityDuplicatId: {
       title: 'Schedule Activity Duplicate ID',
@@ -109,10 +88,6 @@
       title: 'Cause Non Deterministic Error',
       copy: 'The workflow task has failed due to non-deterministic error from workflow code. This usually means the workflow code has a non-backward compatible change without proper versioning branch.',
     },
-    BadModifyWorkflowPropertiesAttributes: {
-      title: '',
-      copy: '',
-    },
   };
 
   export let error: WorkflowTaskFailedCause;
@@ -121,7 +96,7 @@
   $: ({ title = '', copy = '', actionCopy = '', link = '' } = errorCopy);
 </script>
 
-{#if !$updating}
+{#if !$updating && (title || copy)}
   <Alert icon="warning" intent="warning" {title}>
     <p>{copy}</p>
     {#if actionCopy && link}
