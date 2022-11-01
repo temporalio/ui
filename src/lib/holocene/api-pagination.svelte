589b84b1bcb53f6cbb496d9719df7b773e71bca8
Laura Whitaker <laura.whitaker@temporal.io>
2022-11-01 11:45:40 -0600

    Add support for various action slots

--- src/lib/holocene/api-pagination.svelte
+++ src/lib/holocene/api-pagination.svelte
@@ -80,15 +80,21 @@
   <slot name="empty">No Items</slot>
 {:else}
   <div class="relative mb-8 flex flex-col gap-4">
-    <div class="flex justify-start md:justify-between">
-      <p class="mr-6 flex items-center text-gray-600">
+    <div class="flex flex-col items-center gap-4 lg:flex-row justify-between">
+      <div class="flex items-center">
+        <slot name="action-top-left" />
         {#if $store.updating}
-          Updating…
+          <p
+            class={`${
+              $$slots['action-top-left'] ? 'ml-6' : 'mr-6'
+            } text-gray-600`}
+          >
+            Updating…
+          </p>
         {/if}
-      </p>
-      <nav
-        class="flex items-center justify-start gap-4 text-sm md:justify-end md:text-base"
-      >
+      </div>
+      <nav class="flex flex-col justify-end gap-4 md:flex-row">
+        <slot name="action-top-center" />
         <FilterSelect
           label="Per Page"
           parameter={$store.key}
@@ -114,7 +120,7 @@
             <Icon name="chevron-right" />
           </button>
         </div>
-        <slot name="action" />
+        <slot name="action-top-right" />
       </nav>
     </div>
     {#if $store.loading}
@@ -123,31 +129,39 @@
       <slot visibleItems={$store.items} initialItem={[]} />
     {/if}
 
-    <nav class="flex justify-end gap-4">
-      <FilterSelect
-        label="Per Page"
-        parameter={$store.key}
-        value={String($store.pageSize)}
-        options={perPageOptions($store.pageSize)}
-      />
-      <div class="flex items-center justify-center gap-1">
-        <button
-          class="caret"
-          disabled={!$store.hasPrevious}
-          on:click={store.previousPage}
-        >
-          <Icon name="chevron-left" />
-        </button>
-        <p>
-          {$store.currentPageNumber}–{$store.endingPageNumber}
-        </p>
-        <button
-          class="caret"
-          disabled={!$store.hasNext}
-          on:click={store.nextPage}
-        >
-          <Icon name="chevron-right" />
-        </button>
+    <nav
+      class={`flex ${
+        $$slots['action-bottom-left'] ? 'justify-between' : 'justify-end'
+      }`}
+    >
+      <slot name="action-bottom-left" />
+      <div class="flex gap-4">
+        <FilterSelect
+          label="Per Page"
+          parameter={$store.key}
+          value={String($store.pageSize)}
+          options={perPageOptions($store.pageSize)}
+        />
+        <div class="flex items-center justify-center gap-1">
+          <button
+            class="caret"
+            disabled={!$store.hasPrevious}
+            on:click={store.previousPage}
+          >
+            <Icon name="chevron-left" />
+          </button>
+          <p>
+            {$store.currentPageNumber}–{$store.endingPageNumber}
+          </p>
+          <button
+            class="caret"
+            disabled={!$store.hasNext}
+            on:click={store.nextPage}
+          >
+            <Icon name="chevron-right" />
+          </button>
+        </div>
+        <slot name="action-bottom-right" />
       </div>
     </nav>
   </div>
