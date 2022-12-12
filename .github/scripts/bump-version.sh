09230db5aae2c5bf84e66285042e9f1751511c72
Ross Edfort <rossedfort@gmail.com>
2022-12-12 12:36:34 -0700

    [skip actions] fix string interpolation

---
+++ .github/scripts/bump-version.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+version=`git describe --abbrev=0 --tags`
+branch="releases/$version"
+
+git checkout -b $branch
+git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO}"
+git push --tags origin $branch
+gh pr create -B main -H $branch --title "bump version in package.json to $version"
