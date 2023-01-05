cb27e3056d35fe87d3ddf7862a4909422ae53b96
Ross Edfort <rossedfort@gmail.com>
2023-01-05 12:13:22 -0700

    no ssr for login page

--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -10,6 +10,7 @@
   import Button from '$lib/holocene/button.svelte';
   import { routeForAuthentication } from '$lib/utilities/route-for';
 
+  export const ssr = false;
   export let data: PageData;
 
   let { settings } = data;
