f36452bc5e505343d4be41f611210957768a7def
Ross Edfort <rossedfort@gmail.com>
2023-01-05 12:25:54 -0700

    move login and signing Load fn to the layout move ssr disable to the layout

--- src/routes/(login)/+layout.ts
+++ src/routes/(login)/+layout.ts
@@ -1,1 +1,20 @@
 import '../../app.css';
+
+import { error } from '@sveltejs/kit';
+import { fetchSettings } from '$lib/services/settings-service';
+
+import type { LayoutLoad } from './$types';
+
+export const ssr = false;
+
+export const load: LayoutLoad = async function ({ fetch }) {
+  const settings: Settings = await fetchSettings(fetch);
+
+  if (!settings.auth.enabled) {
+    throw error(404);
+  }
+
+  return {
+    settings,
+  };
+};
