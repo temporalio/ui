25f042deac810f348dea90b761840cbfc59d66a7
Ross Edfort <rossedfort@gmail.com>
2023-01-11 11:29:34 -0700

    clean up namespace details page

--- src/types/index.ts
+++ src/types/index.ts
@@ -120,6 +120,7 @@ export type WorkflowExecutionStatus =
 export type Severity = temporal.api.enums.v1.Severity;
 export type WorkflowTaskFailedCause =
   temporal.api.enums.v1.WorkflowTaskFailedCause;
+export type ArchivalState = temporal.api.enums.v1.ArchivalState;
 
 // api.workflow
 
