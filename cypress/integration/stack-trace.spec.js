f7e32639f576979a501649676426d2a3b012c05a
Alex Tideman <alex.tideman@gmail.com>
2023-01-12 13:43:24 -0600

    New describe block for stack-trace

--- cypress/integration/stack-trace.spec.js
+++ cypress/integration/stack-trace.spec.js
@@ -3,20 +3,12 @@
 import workflowRunningFixture from '../fixtures/workflow-running.json';
 import workflowCompletedFixture from '../fixtures/workflow-completed.json';
 
-describe('Stack Trace', () => {
+describe('Stack Trace With Completed Workflow', () => {
+  const { workflowId, runId } =
+    workflowCompletedFixture.workflowExecutionInfo.execution;
+
   beforeEach(() => {
     cy.interceptApi();
-  });
-
-  it('should show No Stack Trace for completed workflow', () => {
-    const { workflowId, runId } =
-      workflowCompletedFixture.workflowExecutionInfo.execution;
-
-    cy.intercept(
-      Cypress.env('VITE_API_HOST') +
-        `/api/v1/namespaces/default/workflows/${workflowId}/runs/${runId}?`,
-      { fixture: 'workflow-completed.json' },
-    ).as('workflow-api');
 
     cy.intercept(
       Cypress.env('VITE_API_HOST') +
@@ -36,6 +28,14 @@ describe('Stack Trace', () => {
       { fixture: 'event-history-completed-reverse.json' },
     ).as('event-history-descending');
 
+    cy.intercept(
+      Cypress.env('VITE_API_HOST') +
+        `/api/v1/namespaces/default/workflows/${workflowId}/runs/${runId}?`,
+      { fixture: 'workflow-completed.json' },
+    ).as('workflow-api');
+  });
+
+  it('should show No Stack Trace for completed workflow', () => {
     cy.visit(`/namespaces/default/workflows/${workflowId}/${runId}`);
 
     cy.wait('@workflow-api');
@@ -47,56 +47,80 @@ describe('Stack Trace', () => {
     cy.get('[data-cy=stack-trace-tab]').click();
 
     cy.wait('@workflow-api');
-    cy.wait('@task-queues-api');
 
     cy.get('[data-cy="query-stack-trace-empty"]').contains(
       'No Stack Traces Found',
     );
   });
+});
 
-  it('should show stack trace for running workflow', () => {
-    const { workflowId, runId } =
-      workflowRunningFixture.workflowExecutionInfo.execution;
+describe('Stack Trace with Running Workflow', () => {
+  const { workflowId, runId } =
+    workflowRunningFixture.workflowExecutionInfo.execution;
+
+  beforeEach(() => {
+    cy.interceptApi();
+
+    cy.intercept(
+      Cypress.env('VITE_API_HOST') +
+        `/api/v1/namespaces/default/workflows/${workflowId}/runs/${runId}/events?maximumPageSize=20`,
+      { fixture: 'event-history-completed.json' },
+    ).as('event-history-start');
+
+    cy.intercept(
+      Cypress.env('VITE_API_HOST') +
+        `/api/v1/namespaces/default/workflows/${workflowId}/runs/${runId}/events/reverse?maximumPageSize=20`,
+      { fixture: 'event-history-completed-reverse.json' },
+    ).as('event-history-end');
+
+    cy.intercept(
+      Cypress.env('VITE_API_HOST') +
+        `/api/v1/namespaces/default/workflows/${workflowId}/runs/${runId}/events/reverse?`,
+      { fixture: 'event-history-completed-reverse.json' },
+    ).as('event-history-descending');
 
     cy.intercept(
       Cypress.env('VITE_API_HOST') +
         `/api/v1/namespaces/default/workflows/${workflowId}/runs/${runId}?`,
       { fixture: 'workflow-running.json' },
     ).as('workflow-api');
+  });
 
-    cy.visit(
-      `/namespaces/default/workflows/${workflowId}/${runId}/stack-trace`,
-    );
+  it('should show stack trace for running workflow', () => {
+    cy.visit(`/namespaces/default/workflows/${workflowId}/${runId}`);
 
     cy.wait('@workflow-api');
     cy.wait('@task-queues-api');
+    cy.wait('@event-history-start');
+    cy.wait('@event-history-end');
+    cy.wait('@event-history-descending');
+
+    cy.get('[data-cy=stack-trace-tab]').click();
+
+    cy.wait('@workflow-api');
     cy.wait('@query-api');
 
     cy.get('[data-cy="query-stack-trace"]').contains('go.temporal.io/sdk');
   });
 
   it('should handle errors when the stack trace is not formatted as we expect', () => {
-    const { workflowId, runId } =
-      workflowRunningFixture.workflowExecutionInfo.execution;
-
     cy.intercept(
       Cypress.env('VITE_API_HOST') +
         `/api/v1/namespaces/default/workflows/*/runs/*/query*`,
       { fixture: 'query-stack-trace-error.json' },
     ).as('query-api-error');
 
-    cy.intercept(
-      Cypress.env('VITE_API_HOST') +
-        `/api/v1/namespaces/default/workflows/${workflowId}/runs/${runId}?`,
-      { fixture: 'workflow-running.json' },
-    ).as('workflow-api');
-
-    cy.visit(
-      `/namespaces/default/workflows/${workflowId}/${runId}/stack-trace`,
-    );
+    cy.visit(`/namespaces/default/workflows/${workflowId}/${runId}`);
 
     cy.wait('@workflow-api');
     cy.wait('@task-queues-api');
+    cy.wait('@event-history-start');
+    cy.wait('@event-history-end');
+    cy.wait('@event-history-descending');
+
+    cy.get('[data-cy=stack-trace-tab]').click();
+
+    cy.wait('@workflow-api');
     cy.wait('@query-api-error');
 
     cy.get('[data-cy="query-stack-trace"]').contains('[{"an":"error"}]');
