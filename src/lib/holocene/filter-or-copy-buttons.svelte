50c98e528371c27ea651459702c7c17b1626afa2
Ross Edfort <rossedfort@gmail.com>
2023-01-19 09:05:02 -0700

    fix filter icon - it was using stroke instead of fill

--- src/lib/holocene/filter-or-copy-buttons.svelte
+++ src/lib/holocene/filter-or-copy-buttons.svelte
@@ -28,7 +28,7 @@
         class:filtered
       >
         {#key filtered}
-          <Icon name="filter" class="h-5 w-5" />
+          <Icon name="filter" />
         {/key}
       </button>
     {/if}
@@ -37,11 +37,7 @@
         class="copy-or-filter-button"
         on:click|preventDefault|stopPropagation={(e) => copy(e, content)}
       >
-        <Icon
-          name={$copied ? 'checkmark' : 'copy'}
-          stroke="#000"
-          class="h-5 w-5"
-        />
+        <Icon name={$copied ? 'checkmark' : 'copy'} stroke="#000" />
       </button>
     {/if}
   </div>
