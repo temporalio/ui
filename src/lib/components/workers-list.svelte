1f090f7c97d25a222b94a31a77dfdd3fb0b8a46c
Laura Whitaker <laura.whitaker@temporal.io>
2023-01-23 15:40:56 -0700

    Refactor workers list to use Table component

--- src/lib/components/workers-list.svelte
+++ src/lib/components/workers-list.svelte
@@ -5,6 +5,8 @@
   import { formatDate } from '$lib/utilities/format-date';
 
   import EmptyState from '$lib/holocene/empty-state.svelte';
+  import Table from '$lib/holocene/table/table.svelte';
+  import TableHeaderRow from '$lib/holocene/table/table-header-row.svelte';
   import type { GetPollersResponse } from '$lib/services/pollers-service';
 
   export let taskQueue: string;
@@ -15,47 +17,46 @@
   <h2 class="text-lg font-medium">
     Task Queue: <span class="select-all font-normal">{taskQueue}</span>
   </h2>
-  <section
-    class="flex w-full flex-col rounded-lg border-[3px] border-gray-900 bg-white"
-  >
-    <div class="flex flex-row bg-gray-900 p-2 text-white">
-      <div class="w-6/12 text-left">ID</div>
-      <div class="w-2/12 text-left">Last Accessed</div>
-      <div class="w-2/12 text-center">Workflow Task Handler</div>
-      <div class="w-2/12 text-center">Activity Handler</div>
-    </div>
+  <Table class="mb-6 w-full min-w-[600px] table-fixed">
+    <TableHeaderRow slot="headers">
+      <th class="table-cell w-6/12">ID</th>
+      <th class="table-cell w-2/12">Last Accessed</th>
+      <th class="table-cell w-2/12">
+        <p class="text-center">Workflow Task Handler</p>
+      </th>
+      <th class="table-cell w-2/12 text-center">
+        <p class="text-center">Activity Handler</p>
+      </th>
+    </TableHeaderRow>
     {#each workers?.pollers as poller (poller.identity)}
-      <article
-        class="flex h-full w-full flex-row border-b-2 p-2 no-underline last:border-b-0"
-        data-cy="worker-row"
-      >
-        <div class="links w-6/12 text-left" data-cy="worker-identity">
+      <tr class="table-row w-full" data-cy="worker-row">
+        <td />
+        <td class="text-left" data-cy="worker-identity">
           <p class="select-all">{poller.identity}</p>
-        </div>
-        <div class="links w-2/12 text-left" data-cy="worker-last-access-time">
-          <h3>
-            <p class="select-all">
-              {formatDate(poller.lastAccessTime, $timeFormat)}
-            </p>
-          </h3>
-        </div>
-        <div class="flex w-2/12 justify-center" data-cy="workflow-poller">
+        </td>
+        <td class="text-left" data-cy="worker-last-access-time">
+          <p class="select-all">
+            {formatDate(poller.lastAccessTime, $timeFormat)}
+          </p>
+        </td>
+        <td data-cy="workflow-poller">
           {#if poller.taskQueueTypes.includes('WORKFLOW')}
-            <Icon name="checkmark" class="text-blue-700" />
+            <Icon name="checkmark" class="m-auto text-blue-700" />
           {:else}
-            <Icon name="close" class="text-primary" />
+            <Icon name="close" class="m-auto text-primary" />
           {/if}
-        </div>
-        <div class="flex w-2/12 justify-center" data-cy="activity-poller">
+        </td>
+        <td data-cy="activity-poller">
           {#if poller.taskQueueTypes.includes('ACTIVITY')}
-            <Icon name="checkmark" class="text-blue-700" />
+            <Icon name="checkmark" class="m-auto text-blue-700" />
           {:else}
-            <Icon name="close" class="text-primary" />
+            <Icon name="close" class="m-auto text-primary" />
           {/if}
-        </div>
-      </article>
+        </td>
+        <td />
+      </tr>
     {:else}
       <EmptyState title={'No Workers Found'} />
     {/each}
-  </section>
+  </Table>
 </section>
