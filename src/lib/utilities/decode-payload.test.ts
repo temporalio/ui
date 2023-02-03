e2b15bd687955a55cbcfd40c3b47b948f442013c
Alex Tideman <alex.tideman@gmail.com>
2023-02-03 10:24:23 -0600

    Preserve 'constructor' keyword in json-bigint and add console.warn on decode error (#1109)
    * WIP
    
    * Set constructorAction: 'preserve' and use JSONBigNative const to get options
    
    * Remove commented out code
    
    * Add unit test
    
    * Fix lint issue and unit test

--- src/lib/utilities/decode-payload.test.ts
+++ src/lib/utilities/decode-payload.test.ts
@@ -83,7 +83,16 @@ const JsonObjectEncoded = {
   data: 'eyAiVHJhbnNmb3JtZXIiOiAiT3B0aW11c1ByaW1lIiB9',
 };
 
+const JsonObjectEncodedWithConstructor = {
+  metadata: {
+    encoding: 'anNvbi9wbGFpbg==',
+    type: 'S2V5d29yZA==',
+  },
+  data: 'eyAiQ29uc3RydWN0b3JPdXRwdXQiOiAiT3B0aW11c1ByaW1lIiB9',
+};
+
 const JsonObjectDecoded = { Transformer: 'OptimusPrime' };
+const JsonObjectDecodedWithConstructor = { ConstructorOutput: 'OptimusPrime' };
 
 describe('decodePayload', () => {
   it('Should not decode a payload with encoding binary/encrypted', () => {
@@ -105,6 +114,11 @@ describe('decodePayload', () => {
   });
   it('Should decode a json payload with encoding json/plain', () => {
     expect(decodePayload(JsonObjectEncoded)).toEqual(JsonObjectDecoded);
+  });
+  it('Should decode a json payload with constructor keyword with encoding json/plain', () => {
+    expect(decodePayload(JsonObjectEncodedWithConstructor)).toEqual(
+      JsonObjectDecodedWithConstructor,
+    );
   });
 });
 
