59e0dbdbf990db6a4cb7a46dc540d6b976facbce
Alex Tideman <alex.tideman@gmail.com>
2023-02-07 13:43:17 -0600

    Add margin top to auto-refresh

--- src/lib/components/auto-refresh-workflow.svelte
+++ src/lib/components/auto-refresh-workflow.svelte
@@ -9,7 +9,9 @@
   $: checked = $autoRefreshWorkflow === 'on';
 </script>
 
-<label for="autorefresh" class="flex items-center gap-4 font-secondary text-sm"
+<label
+  for="autorefresh"
+  class="mt-2 flex items-center gap-4 font-secondary text-sm"
   >Auto refresh
   <Tooltip bottomLeft text="15 second page refresh">
     <ToggleSwitch id="autorefresh" {checked} on:change={onChange} />
