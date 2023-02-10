8adb7ad95fe35a7466575d818aa6082c7a6f19ad
Steve Kinney <hello@stevekinney.net>
2023-02-10 12:51:01 -0700

    Use a single command

---
+++ plugins/vite-plugin-temporal-server.ts
@@ -0,0 +1,26 @@
+import { TestWorkflowEnvironment } from '@temporalio/testing';
+import type { Plugin } from 'vite';
+
+export function temporalServer(): Plugin {
+  return {
+    name: 'vite-plugin-temporal-server',
+    configureServer(server) {
+      let temporalServer: TestWorkflowEnvironment;
+      const port = +server.config.env.VITE_TEMPORAL_PORT;
+      const api = server.config.env.VITE_UI_SERVER_PORT;
+      const extraArgs = [`--ui-port=${api}`];
+
+      console.log({ port, api, extraArgs });
+
+      server.httpServer?.on('listening', async () => {
+        temporalServer = await TestWorkflowEnvironment.createLocal({
+          server: { port, ui: true, extraArgs },
+        });
+      });
+
+      server.httpServer?.on('close', () => {
+        temporalServer.teardown();
+      });
+    },
+  };
+}
