38fc7389226df5734d0cf7bb9c6be5a3e779b9dc
Steve Kinney <hello@stevekinney.net>
2023-02-17 16:31:06 -0700

    Update copy

--- src/routes/(app)/namespaces/[namespace]/archival/+page.svelte
+++ src/routes/(app)/namespaces/[namespace]/archival/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import type { PageData } from './$types';
+  import { timeFormat } from '$lib/stores/time-format';
+  import { page } from '$app/stores';
+
   import WorkflowsSummaryTable from '$lib/components/workflow/workflows-summary-table.svelte';
   import WorkflowsSummaryRow from '$lib/components/workflow/workflows-summary-row.svelte';
   import WorkflowFilters from './_workflow-filters.svelte';
   import Pagination from '$lib/holocene/pagination.svelte';
   import EmptyState from '$lib/holocene/empty-state.svelte';
   import CodeBlock from '$lib/holocene/code-block.svelte';
-  import { timeFormat } from '$lib/stores/time-format';
   import PageTitle from '$lib/components/page-title.svelte';
-  import { page } from '$app/stores';
 
   export let data: PageData;
 
@@ -52,47 +53,43 @@
   {/if}
 {:else if archivalEnabled}
   <h1 class="text-2xl" data-testid="visibility-disabled-title">
-    This Namespace is currently enabled for archival but visibility is not
+    This namespace is currently enabled for archival but visibility is not
     enabled.
   </h1>
-  <p>To enable Visibility Archival:</p>
+  <p>To enable <a
+    class="text-blue-700 underline"
+    href="https://docs.temporal.io/clusters#archival"
+    target="_blank"
+    rel="noreferrer">archival visibility</a
+  >archival visibility</p>:</p>
   <CodeBlock
     content={`tctl --namespace ${namespaceName} namespace update -vas enabled`}
     language="text"
     inline
   />
-  <p>
-    For more details, please check out <a
-      class="text-blue-700 underline"
-      href="https://docs.temporal.io/clusters#archival"
-      target="_blank"
-      rel="noreferrer">Archival documentation</a
-    >.
-  </p>
 {:else}
   <h1 class="text-2xl" data-testid="archived-disabled-title">
-    This Namespace is currently not enabled for archival.
+    This namespace is currently not enabled for archival.
   </h1>
-  <p>Run this command to enable Archival for Event Histories:</p>
+  <p>Run this command to enable archival visibility for event histories:</p>
   <CodeBlock
     content={`tctl --namespace ${namespaceName} namespace update --has enabled`}
     language="text"
     inline
   />
   {#if !visibilityArchivalEnabled}
-    <p>To enable Visibility Archival:</p>
+    <p>
+      To enable <a
+        class="text-blue-700 underline"
+        href="https://docs.temporal.io/clusters#archival"
+        target="_blank"
+        rel="noreferrer">archival visibility</a
+      >:
+    </p>
     <CodeBlock
       content={`tctl --namespace ${namespaceName} namespace update -vas enabled`}
       language="text"
       inline
     />
   {/if}
-  <p>
-    For more details, please check out <a
-      class="text-blue-700 underline"
-      href="https://docs.temporal.io/clusters#archival"
-      target="_blank"
-      rel="noreferrer">Archival documentation</a
-    >.
-  </p>
 {/if}
