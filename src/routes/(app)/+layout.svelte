b6556d6fd97941742d8e5c1bc39f18f5ca92522c
Alex Tideman <alex.tideman@gmail.com>
2023-03-01 12:07:19 -0600

    Negative top margin on layout to move up content

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -35,7 +35,7 @@
   <div class="sticky top-0 z-20 h-screen w-auto">
     <SideNavigation />
   </div>
-  <main id="content" class="min-h-screen w-max flex-auto bg-gray-100">
+  <main id="content" class="min-h-screen w-max flex-auto bg-gray-100 -mt-8">
     <TopNavigation {logout} />
     <Banners {uiVersionInfo} />
     <div class="z-10 flex flex-col gap-4 px-10 pb-10 pt-8">
