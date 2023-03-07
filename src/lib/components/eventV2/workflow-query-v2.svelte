409930874db401e7fc9ba95d3cc5c3eff8ba3f06
Alex Tideman <alex.tideman@gmail.com>
2023-03-06 23:19:02 -0600

    Add query

---
+++ src/lib/components/eventV2/workflow-query-v2.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import { getQuery, getQueryTypes } from '$lib/services/query-service';
+
+  import CodeBlock from '$lib/holocene/code-block.svelte';
+  import Select from '$lib/holocene/select/simple-select.svelte';
+  import Option from '$lib/holocene/select/simple-option.svelte';
+  import EmptyState from '$lib/holocene/empty-state.svelte';
+  import Button from '$lib/holocene/button.svelte';
+  import Loading from '$lib/holocene/loading.svelte';
+  import { authUser } from '$lib/stores/auth-user';
+  import ToggleSwitch from '$lib/holocene/toggle-switch.svelte';
+  import Accordion from '$lib/holocene/accordion.svelte';
+
+  const { namespace, workflow: workflowId, run: runId } = $page.params;
+
+  const params = {
+    id: workflowId,
+    runId,
+  };
+
+  let queryType: string;
+  let isLoading = false;
+
+  let queryTypes = getQueryTypes({
+    namespace,
+    workflow: params,
+  }).then((queryTypes) => {
+    queryType = queryType || queryTypes[0];
+    return queryTypes;
+  });
+
+  let queryResult: Promise<string>;
+
+  const query = (queryType: string) => {
+    queryResult = getQuery(
+      {
+        namespace,
+        workflow: params,
+        queryType,
+      },
+      $page.data?.settings,
+      $authUser?.accessToken,
+    );
+  };
+
+  $: {
+    queryType && query(queryType);
+  }
+
+  let jsonFormatting = true;
+</script>
+
+<Accordion title="Query">
+  {#await queryTypes}
+    <div class="text-center">
+      <Loading />
+      <p>(This will fail if you have no workers running.)</p>
+    </div>
+  {:then types}
+    <div class="h-[200px] overflow-auto">
+      <div class="flex flex-row justify-between">
+        <div class="flex items-center gap-4">
+          <Select
+            id="query-select"
+            label="Query Type"
+            bind:value={queryType}
+            testId="query-select"
+          >
+            {#each types as value}
+              <Option {value}>{value}</Option>
+            {/each}
+          </Select>
+          <Button
+            on:click={() => query(queryType)}
+            icon="retry"
+            loading={isLoading}
+          >
+            Refresh
+          </Button>
+        </div>
+        <div class="flex flex-col">
+          <label
+            for="json-formatting"
+            class="flex text-center items-center gap-2 font-secondary text-sm"
+            >JSON Formatting
+            <ToggleSwitch
+              id="json-formatting"
+              checked={jsonFormatting}
+              on:change={() => (jsonFormatting = !jsonFormatting)}
+            />
+          </label>
+        </div>
+      </div>
+      <div class="flex items-start h-full">
+        {#await queryResult then result}
+          <CodeBlock
+            content={result}
+            language={jsonFormatting ? 'json' : 'text'}
+          />
+        {/await}
+      </div>
+    </div>
+  {:catch _error}
+    <EmptyState
+      title="An Error Occurred"
+      content="Please make sure you have at least one worker running."
+      error={_error?.message}
+    />
+  {/await}
+</Accordion>
