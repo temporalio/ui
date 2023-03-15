7d0c8f5c7cf3e45d4eefe48c9efb047b1143822f
Laura Whitaker <laura.whitaker@temporal.io>
2023-03-14 21:32:48 -0600

    Add table v2 with grid layout and resizable columns

---
+++ src/lib/holocene/table-v2/table-row.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  interface $$Props extends HTMLAttributes<HTMLTableRowElement> {
+    href?: string;
+    'data-testid'?: String;
+  }
+
+  let className = '';
+  export { className as class };
+  export let href: string = '';
+</script>
+
+{#if href}
+  <a class="row {className}" {href} {...$$restProps}>
+    <slot />
+  </a>
+{:else}
+  <tr on:click|stopPropagation class={className} {...$$restProps}>
+    <slot />
+  </tr>
+{/if}
+
+<style lang="postcss">
+  .row {
+    @apply table-row;
+
+    :global(td) {
+      @apply flex items-center;
+    }
+  }
+</style>
