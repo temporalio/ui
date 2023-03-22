519ea3d1cf0517f91391f5c6e87536e55a95adb8
Alex Tideman <alex.tideman@gmail.com>
2023-03-22 15:13:26 -0500

    Bump UIVersion to 2.12.0 (#1231)
    * Bump version to 2.11.3
    '
    
    * Use 2.12.0 instead

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.11.2"
+	UIVersion               = "2.12.0"
 )
