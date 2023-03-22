ff6fb622c8d8475e195cc6f2cd51cd9a3136ca32
Alex Tideman <alex.tideman@gmail.com>
2023-03-22 15:05:06 -0500

    Use 2.12.0 instead

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.11.3"
+	UIVersion               = "2.12.0"
 )
