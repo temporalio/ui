0c0d0873a977fb25fcdcf7cadc3f8020344c4ab6
feedmeapples <aksenov.ro@outlook.com>
2023-03-28 18:30:52 -0400

    Bump ui-server version to 2.13.2

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.13.1"
+	UIVersion               = "2.13.2"
 )
