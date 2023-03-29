de741cab36a0f9fc327d7dc9c00437de1c1d91bf
Alex Thomsen <alex.scott.thomsen@gmail.com>
2023-03-29 14:22:41 -0600

    Try to use the new package api

---
+++ src/types/holocene.d.ts
@@ -0,0 +1,10 @@
+export type DataAttributes = {
+  'data-testid'?: string;
+};
+export type ToastVariant = 'success' | 'error' | 'info' | 'warning' | 'primary';
+export interface Toast {
+  message: string;
+  variant?: ToastVariant;
+  id?: string;
+  duration?: number;
+}
