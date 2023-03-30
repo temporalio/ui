0d4464efe7ca057b07cdc5224d3e59e51b56a94c
Alex Tideman <alex.tideman@gmail.com>
2023-03-30 12:44:07 -0500

    Show code from source location

--- src/lib/components/eventV2/initial-event-card.svelte
+++ src/lib/components/eventV2/initial-event-card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import CodeBlock from '$lib/holocene/code-block.svelte';
+  import { parseWithBigInt } from '$lib/utilities/parse-with-big-int';
   import Card from './event-summary-card/card.svelte';
   import Collapsed from './event-summary-card/collapsed.svelte';
   import EnhancedStackTrace from './event-summary-card/enhanced-stack-trace.svelte';
@@ -9,14 +10,22 @@
   export let events: IterableEvent[];
   export let content: string = '';
   export let showStackTrace = false;
+
+  $: parsedContent = showStackTrace && parseWithBigInt(content);
+  $: stackTraceContent =
+    showStackTrace &&
+    parsedContent?.sources[
+      parsedContent?.stacks[0]?.locations[0]?.filePath
+    ]?.[0].content;
 </script>
 
 <div class="flex gap-2">
   <div class="w-[160px] min-w-[160px]" />
   <div class="flex grow flex-col overflow-auto">
     <CodeBlock
-      {content}
+      content={showStackTrace ? stackTraceContent : content}
       title={showStackTrace ? 'Stack Trace' : 'Input'}
+      language={showStackTrace ? 'text' : 'json'}
       icon="json"
       class="h-auto"
     />
