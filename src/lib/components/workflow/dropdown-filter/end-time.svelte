52d5ae406a50c73e9a1cd03bca60aa4550ae7986
Ross Edfort <rossedfort@gmail.com>
2023-04-10 08:23:51 -0600

    fix dropdown menu so it overflows table content

--- src/lib/components/workflow/dropdown-filter/end-time.svelte
+++ src/lib/components/workflow/dropdown-filter/end-time.svelte
@@ -13,7 +13,6 @@
   disabledText="Sort disabled"
   value={typeSort?.value ?? ''}
   keepOpen
-  right
 >
   <svelte:fragment slot="label">End Time</svelte:fragment>
   <div class="flex w-44 flex-col gap-2 p-2">
