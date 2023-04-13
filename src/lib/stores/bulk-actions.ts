7f03017abe478072123a617693cdc6117e68eb6a
Ross Edfort <rossedfort@gmail.com>
2023-04-13 14:17:03 -0600

    fix `supportsBulkActions` helper for cloud

--- src/lib/stores/bulk-actions.ts
+++ src/lib/stores/bulk-actions.ts
@@ -2,15 +2,15 @@ import { derived } from 'svelte/store';
 
 import { isVersionNewer } from '$lib/utilities/version-check';
 
-import { supportsAdvancedVisibility } from './advanced-visibility';
+import { supportsAdvancedVisibility, isCloud } from './advanced-visibility';
 import { temporalVersion } from './versions';
 import { settings } from './settings';
 import { bulkActionsEnabled } from '$lib/utilities/bulk-actions-enabled';
 
 export const supportsBulkActions = derived(
-  [temporalVersion, supportsAdvancedVisibility, settings],
-  ([$temporalVersion, $supportsAdvancedVisibility, $settings]) =>
-    isVersionNewer($temporalVersion, '1.18.0') &&
+  [temporalVersion, supportsAdvancedVisibility, settings, isCloud],
+  ([$temporalVersion, $supportsAdvancedVisibility, $settings, $isCloud]) =>
+    ($isCloud ? true : isVersionNewer($temporalVersion, '1.18.0')) &&
     $supportsAdvancedVisibility &&
     bulkActionsEnabled($settings),
 );
