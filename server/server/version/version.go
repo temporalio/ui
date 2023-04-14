1c376b2b9537173271e2b60b66e16d84eeb58302
Alex Tideman <alex.tideman@gmail.com>
2023-04-14 14:54:17 -0500

    Bump to 2.14.0

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.13.3"
+	UIVersion               = "2.14.0"
 )
