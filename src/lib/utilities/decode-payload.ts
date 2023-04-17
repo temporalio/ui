404f1e5cd660b19d59cdc2c82f65a1415bfb04d8
Alex Tideman <alex.tideman@gmail.com>
2023-04-17 12:52:01 -0500

    Remove unused imports

--- src/lib/utilities/decode-payload.ts
+++ src/lib/utilities/decode-payload.ts
@@ -9,12 +9,6 @@ import type {
   passAccessToken,
   includeCredentials,
 } from '$lib/stores/data-encoder-config';
-import type { Optional, Replace, Settings } from 'src/types/global';
-import type {
-  EventAttribute,
-  EventRequestMetadata,
-  Payload,
-} from 'src/types/events';
 
 import { atob } from './atob';
 import { parseWithBigInt } from './parse-with-big-int';
