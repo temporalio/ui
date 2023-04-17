08837f798e1eb1385518d306f5488bfe5fefea20
Alex Tideman <alex.tideman@gmail.com>
2023-04-17 14:53:53 -0500

    Don't decode if null value (#1302)
    * Don't decode if null value
    
    * Remove .only

--- src/lib/utilities/decode-payload-test-fixtures.ts
+++ src/lib/utilities/decode-payload-test-fixtures.ts
@@ -308,3 +308,57 @@ export const getTestPayloadEvent = () => ({
   memo: null,
   prevAutoResetPoints: null,
 });
+
+export const getTestPayloadEventWithNullEncodedAttributes = () => ({
+  type: 'workflowExecutionStartedEventAttributes',
+  workflowType: {
+    name: 'BackgroundCheck',
+  },
+  parentWorkflowNamespace: '',
+  parentWorkflowExecution: null,
+  parentInitiatedEventId: '0',
+  taskQueue: {
+    name: 'background-checks-main',
+    kind: 'Normal',
+  },
+  input: {
+    payloads: [
+      {
+        metadata: {
+          encoding: 'anNvbi9wbGFpbg==',
+        },
+        data: 'InRlc3RAdGVzdC5jb20i',
+      },
+    ],
+  },
+  details: {
+    detail1: {
+      payloads: [
+        {
+          metadata: {
+            encoding: 'anNvbi9wbGFpbg==',
+          },
+          data: 'eyAidGVzdCI6ICJkZXRhaWwiIH0=',
+        },
+      ],
+    },
+  },
+  encodedAttributes: null,
+  workflowExecutionTimeout: '0s',
+  workflowRunTimeout: '0s',
+  workflowTaskTimeout: '10s',
+  continuedExecutionRunId: '',
+  initiator: 'Unspecified',
+  continuedFailure: null,
+  lastCompletionResult: null,
+  originalExecutionRunId: 'b4351cb3-f54f-4ed4-be5a-b13ef429b861',
+  identity: '1@ac0cd56257aa@',
+  firstExecutionRunId: 'b4351cb3-f54f-4ed4-be5a-b13ef429b861',
+  retryPolicy: null,
+  attempt: 1,
+  workflowExecutionExpirationTime: null,
+  cronSchedule: '',
+  firstWorkflowTaskBackoff: '0s',
+  memo: null,
+  prevAutoResetPoints: null,
+});
