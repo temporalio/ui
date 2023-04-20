99cffd3d348b8c8622da65573bc74aed776ccfba
Alex Thomsen <alex.scott.thomsen@gmail.com>
2023-04-20 12:38:54 -0600

    Make an error boundary with fallback to be more flexible of a boundary

--- src/lib/holocene/error-boundary.ts
+++ src/lib/holocene/error-boundary.ts
@@ -1,4 +1,6 @@
 import Component from './error-boundary.svelte';
+import ErrorBoundryFallback from './error-boundary-fallback.svelte';
 import { createBoundary } from '@crownframework/svelte-error-boundary';
 
 export const ErrorBoundary = createBoundary(Component);
+export const ErrorBoundaryWithFallback = createBoundary(ErrorBoundryFallback);
