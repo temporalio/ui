ed7a1f612c2c2daa44219b285d5cef1b55a76e8e
Laura Whitaker <laura.whitaker@temporal.io>
2023-05-08 13:59:28 -0600

    Show none for next retry for canceled pending activities (#1343)
    * Display None for Next retry if negative
    
    * Add negative default to toTimeDifference

---
+++ src/lib/utilities/to-time-difference.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from 'vitest';
+import { toTimeDifference } from './to-time-difference';
+
+describe('toTimeDifference', () => {
+  const negativeDefault = 'None';
+
+  it('should correctly parse a Timestamp', () => {
+    const now = 1649867374630;
+    const date = '2022-04-13T16:29:35.630571Z';
+    expect(toTimeDifference({ date, now, negativeDefault })).toEqual('1s');
+  });
+
+  it('should correctly parse a negative difference', () => {
+    const now = 1683060815883;
+    const date = '2022-04-13T16:29:35.630571Z';
+    expect(toTimeDifference({ date, now })).toEqual('-33193440.253s');
+  });
+
+  it('should correctly parse a negative difference with a default', () => {
+    const now = 1683060815883;
+    const date = '2022-04-13T16:29:35.630571Z';
+    expect(toTimeDifference({ date, now, negativeDefault })).toEqual('None');
+  });
+
+  it('should correctly parse an invalid date', () => {
+    expect(toTimeDifference({ date: null })).toEqual('');
+    expect(toTimeDifference({ date: '' })).toEqual('');
+    expect(toTimeDifference({ date: undefined })).toEqual('');
+    expect(toTimeDifference({ date: 'date' })).toEqual('');
+  });
+});
--- src/lib/utilities/to-time-difference.ts
+++ src/lib/utilities/to-time-difference.ts
@@ -1,10 +1,25 @@
-export const toTimeDifference = (date: unknown, now = Date.now()): string => {
+import type { Timestamp } from '$lib/types';
+
+export const toTimeDifference = ({
+  date,
+  now = Date.now(),
+  negativeDefault,
+}: {
+  date: Timestamp;
+  now?: number;
+  negativeDefault?: string;
+}): string => {
   if (!date) return '';
   const start = String(date);
 
   try {
     const scheduled = Number(new Date(start));
     const timeFromNow = (scheduled - now) / 1000;
+
+    if (negativeDefault && timeFromNow < 0) {
+      return negativeDefault;
+    }
+
     return !isNaN(timeFromNow) ? `${timeFromNow}s` : '';
   } catch (error) {
     return '';
