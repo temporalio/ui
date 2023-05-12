139f6ba158b8ae43f36806ea340bd71f6baa8c42
Ross Edfort <rossedfort@gmail.com>
2023-05-12 16:51:26 -0600

    add smoe comments and logging

--- scripts/generate-locales.ts
+++ scripts/generate-locales.ts
@@ -1,5 +1,5 @@
 import ts from 'typescript';
-import path from 'path';
+import path, { relative } from 'path';
 import { writeFile, mkdir, stat } from 'fs/promises';
 import { glob, chalk } from 'zx';
 
@@ -9,6 +9,10 @@ const DEST_DIR = path.resolve('./static/i18n/locales');
 const logAndExit = (msg: string) => {
   console.error(chalk.red(msg));
   process.exit(0);
+};
+
+const relativeTo = (path: string) => {
+  return relative('./', path);
 };
 
 const pattern = path.join(SRC_DIR, '*/*.ts');
@@ -38,31 +42,37 @@ filesToTranslate.map(async (source) => {
     logAndExit(`No default export in source: ${source}`);
   }
 
-  const jsObject = {};
-
+  const jsObject: Record<string, string> = {};
   if (defaultExport?.declarations && defaultExport.declarations[0]) {
     defaultExport.declarations[0].forEachChild((child) => {
       if (child.kind === ts.SyntaxKind.ObjectLiteralExpression) {
         child.forEachChild((grandchild) => {
           if (grandchild.kind === ts.SyntaxKind.PropertyAssignment) {
+            let key = '';
+            let value = '';
             const [keyNode, valueNode] = [
               grandchild.getChildAt(0),
               grandchild.getChildAt(2),
             ];
 
+            /**
+             * when an object is defined with strings as keys, i.e. { 'my-key': 'my-value' },
+             * both Nodes will be ts.SyntaxKind.StringLiteral
+             * when an object is defined as identifier as keys, i.e. { myKey: "my-value" },
+             * the key Node will be ts.SyntaxKind.Identifier and the value Node will be ts.SyntaxKind.StringLiteral
+             */
             if (ts.isStringLiteral(keyNode) && ts.isStringLiteral(valueNode)) {
-              const key = keyNode.text;
-              const value = valueNode.text;
-              jsObject[key] = value;
+              key = keyNode.text;
+              value = valueNode.text;
             } else if (
               ts.isIdentifier(keyNode) &&
               ts.isStringLiteral(valueNode)
             ) {
-              const key = keyNode.getText();
-              const value = valueNode.text;
-
-              jsObject[key] = value;
+              key = keyNode.getText();
+              value = valueNode.text;
             }
+
+            jsObject[key] = value;
           }
         });
       }
@@ -80,8 +90,8 @@ filesToTranslate.map(async (source) => {
   }
 
   try {
-    console.log(jsObject);
     await writeFile(fullPath, JSON.stringify(jsObject));
+    console.log(chalk.green(`Parsed locale file: ${relativeTo(source)}`));
   } catch (error) {
     logAndExit(`Unable to write file: ${fullPath} with JSON content`);
   }
