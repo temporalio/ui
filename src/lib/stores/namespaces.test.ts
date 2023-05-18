5b3b6fdb3513d70b8c0e3b7b599fa1a78c542180
Steve Kinney <hello@stevekinney.net>
2023-05-18 10:47:07 -0600

    Add tests for namespaces store

---
+++ src/lib/stores/namespaces.test.ts
@@ -0,0 +1,47 @@
+import { get } from 'svelte/store';
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest';
+import { lastUsedNamespace, namespaces } from './namespaces';
+
+describe('lastUsedNamespace', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+    vi.restoreAllMocks();
+  });
+
+  it('should default to "default"', () => {
+    expect(get(lastUsedNamespace)).toEqual('default');
+  });
+
+  it('should be able to set a value', () => {
+    lastUsedNamespace.set('test');
+    expect(get(lastUsedNamespace)).toEqual('test');
+  });
+
+  it('should store values in localStorage', () => {
+    lastUsedNamespace.set('test');
+    expect(localStorage.getItem('lastNamespace')).toMatch('test');
+  });
+
+  it('should store values in localStorage', () => {
+    lastUsedNamespace.set('test');
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'lastNamespace',
+      JSON.stringify('test'),
+    );
+  });
+});
+
+describe('namespaces', () => {
+  beforeEach(() => {
+    namespaces.set([]);
+  });
+
+  it('should be able to set a value', () => {
+    namespaces.set([{ name: 'default' }]);
+    expect(get(namespaces)).toEqual([{ name: 'default' }]);
+  });
+});
