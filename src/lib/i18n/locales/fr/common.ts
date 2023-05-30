d9ba213e9a74c2e2e3919b9bdab748c82bf3349c
Ross Edfort <rossedfort@gmail.com>
2023-05-30 09:21:06 -0600

    remove unused languages

--- src/lib/i18n/locales/fr/common.ts
+++
@@ -1,9 +0,0 @@
-export const Namespace = 'common' as const;
-
-export const Strings = {
-  'recent-workflows': 'Recent Workflows',
-  'workflows-count_one': '{{count}} workflow',
-  'workflows-count_other': '{{count}} workflows',
-  'filtered-workflows-count':
-    'Results {{filtered}} of $t(workflows-count, { "count": {{total}} })',
-} as const;
