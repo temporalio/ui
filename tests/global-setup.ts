1834ec084c82d58eb5c2f1612be194d42896618d
Ross Edfort <rossedfort@gmail.com>
2023-06-15 11:11:43 -0600

    rm temporal server and ui server from test setup

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -1,8 +1,6 @@
 import { FullConfig, chromium } from '@playwright/test';
 
-import { createTemporalServer } from '../utilities/temporal-server';
 import { createCodecServer } from '../temporal/codec-server';
-import { createUIServer } from '../utilities/ui-server';
 import { runWorker } from '../temporal/workers';
 import { startWorkflows } from '../temporal/client';
 import { connect } from '../temporal/client';
@@ -10,12 +8,8 @@ import { setLocalStorage } from './test-utilities/mock-local-storage';
 
 const setupDependencies = async () => {
   const codecServer = await createCodecServer({ port: 8888 });
-  const temporalServer = await createTemporalServer();
-  const uiServer = await createUIServer('e2e');
 
-  await uiServer.ready();
   await codecServer.start();
-  await temporalServer.ready();
 
   const client = await connect();
   await runWorker();
