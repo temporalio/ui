4b0e06e24d890184376c15895feab83a2de114f4
Steve Kinney <hello@stevekinney.net>
2023-07-03 17:19:23 -0600

    Update ui-server.ts

--- utilities/ui-server.ts
+++ utilities/ui-server.ts
@@ -1,6 +1,6 @@
 import { join } from 'path';
 import waitForPort from 'wait-port';
-import { $, chalk } from 'zx';
+import { $ } from 'zx';
 
 export type UIServer = {
   shutdown: () => Promise<number | null>;
