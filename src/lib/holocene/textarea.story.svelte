e5bf430e0b9436a46f21bf6e6a72b1dccd864bed
Steve Kinney <hello@stevekinney.net>
2023-07-05 09:24:50 -0600

    Add label to text area (#1467)
    * Add label to text area
    
    * Add required to story

--- src/lib/holocene/textarea.story.svelte
+++ src/lib/holocene/textarea.story.svelte
@@ -8,14 +8,33 @@
   let isValid = true;
   let error = '';
   let value = '';
+  let label = 'Label';
+  let required = true;
 </script>
 
 <Hst.Story>
-  <Textarea {disabled} {isValid} {error} bind:value />
+  <Textarea id="textarea" {disabled} {isValid} {error} bind:value />
 
   <svelte:fragment slot="controls">
     <Hst.Checkbox title="Disabled: " bind:value={disabled} />
     <Hst.Checkbox title="Valid: " bind:value={isValid} />
     <Hst.Text title="Error: " bind:value={error} />
+  </svelte:fragment>
+</Hst.Story>
+
+<Hst.Story>
+  <Textarea
+    id="textarea-with-label"
+    {disabled}
+    {isValid}
+    {error}
+    {label}
+    {required}
+    bind:value
+  />
+
+  <svelte:fragment slot="controls">
+    <Hst.Text title="Label: " bind:value={label} />
+    <Hst.Checkbox title="Required: " bind:value={required} />
   </svelte:fragment>
 </Hst.Story>
