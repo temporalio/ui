34457ae7439d74b2b74ddb80c124b8be37f58b27
Alex Tideman <alex.tideman@gmail.com>
2023-07-18 15:27:44 -0500

    Remove viewBox prop

--- src/lib/holocene/icon/svg/svg.svelte
+++ src/lib/holocene/icon/svg/svg.svelte
@@ -2,13 +2,12 @@
   export let width = 24;
   export let height = 24;
   export let title = '';
-  export let viewBox = '0 0 24 24';
 </script>
 
 <svg
   {width}
   {height}
-  {viewBox}
+  viewBox="0 0 24 24"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
   class={$$props.class}
