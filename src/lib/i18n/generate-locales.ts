8c00ddb07e907c469d210a54f3c189ec04e8c786
Ross Edfort <rossedfort@gmail.com>
2023-07-24 12:00:52 -0600

    fix generate-locales script to only use dynamic import on JS

--- src/lib/i18n/generate-locales.ts
+++ src/lib/i18n/generate-locales.ts
@@ -1,71 +1,90 @@
-import { mkdir, stat, writeFile } from 'fs/promises';
-import path, { relative } from 'path';
+import { mkdir, readFile, stat, writeFile } from 'fs/promises';
+import path from 'path';
 
 import { rimraf } from 'rimraf';
-import { $, chalk, glob } from 'zx';
+import ts from 'typescript';
+import { chalk, glob } from 'zx';
 
 const SRC_DIR = path.resolve('./src/lib/i18n/locales');
 const DEST_DIR = path.resolve('./static/i18n/locales');
 
-const logAndExit = (msg: string, logError: (msg: string) => void) => {
-  logError(chalk.red(msg));
-
-  if (logError === console.error) {
-    process.exit(0);
+const writeLocaleFile = async (
+  originalPath: string,
+  parentPath: string,
+  filePath: string,
+  exports: { Strings?: object },
+) => {
+  if (!Object.getOwnPropertyNames(exports).includes('Strings')) {
+    console.log(`No 'Strings' named export in ${originalPath}`);
+    return;
   }
-};
 
-const relativeTo = (path: string) => {
-  return relative('./', path);
+  try {
+    await stat(parentPath);
+    await rimraf(`${parentPath}/*`);
+  } catch {
+    await mkdir(parentPath, { recursive: true });
+  }
+
+  try {
+    await writeFile(
+      filePath,
+      `${JSON.stringify(exports.Strings, undefined, 2)}\n`,
+    );
+    console.log(
+      chalk.bold(chalk.green('Parsed locale file:')),
+      `${path.relative('.', originalPath)} to ${path.relative('.', filePath)}`,
+    );
+  } catch (error) {
+    console.log(`Unable to write file: ${path.relative('.', filePath)}`);
+    console.error(error);
+  }
 };
 
 export const generateLocales = async (
   src: string = SRC_DIR,
   dest: string = DEST_DIR,
   ext: 'ts' | 'js' = 'ts',
-  logError: (msg: string) => void = console.error,
 ) => {
   const pattern = path.join(src, `*/*.${ext}`);
   const files = await glob(pattern);
-
   if (files.length === 0) {
     console.log(chalk.yellow(`No files matching ${pattern}`));
     return;
   }
 
-  files.map(async (file) => {
-    const locale = await import(file);
+  await Promise.all(
+    files.map(async (file) => {
+      const subDir = file.split(src)[1];
+      const regex = new RegExp(`[a-zA-Z0-9_-]+\\.${ext}`);
+      const parentPath = path.join(dest, subDir.split(regex)[0]);
+      const filePath = path.join(dest, subDir.replace(`.${ext}`, '.json'));
 
-    if (!Object.getOwnPropertyNames(locale).includes('Strings')) {
-      logAndExit(`No 'Strings' named export in ${file}`, logError);
-    }
+      if (ext === 'ts') {
+        const fileContents = await readFile(file);
+        const result = ts.transpileModule(fileContents.toString(), {
+          compilerOptions: {
+            module: ts.ModuleKind.ESNext,
+            target: ts.ScriptTarget.ESNext,
+            esModuleInterop: true,
+          },
+        });
 
-    const subDir = file.split(src)[1];
-    const regex = new RegExp(`[a-zA-Z0-9_-]+\\.${ext}`);
-    const fullDir = path.join(dest, subDir.split(regex)[0]);
-    const fullPath = path.join(dest, subDir.replace(`.${ext}`, '.json'));
+        const buf = Buffer.from(result.outputText);
+        const exports = await import(
+          `data:text/javascript;base64,${buf.toString('base64')}`
+        );
 
-    try {
-      await stat(fullDir);
-      await rimraf(`${fullDir}/*`);
-    } catch {
-      await mkdir(fullDir, { recursive: true });
-    }
+        return writeLocaleFile(file, parentPath, filePath, exports);
+      } else if (ext === 'js') {
+        const exports = await import(file);
 
-    try {
-      const { Strings } = locale;
-      await writeFile(fullPath, JSON.stringify(Strings));
-      $.cwd = process.cwd();
-      await $`prettier --write --plugin-search-dir=. --no-error-on-unmatched-pattern ./static/i18n/locales/*/*.json`.quiet();
-      console.log(
-        chalk.bold(chalk.green('Parsed locale file:')),
-        `${relativeTo(file)} to ${relativeTo(fullPath)}`,
-      );
-    } catch (error) {
-      logAndExit(
-        `Unable to write file: ${relativeTo(fullPath)} with JSON content`,
-        logError,
-      );
-    }
-  });
+        return writeLocaleFile(file, parentPath, filePath, exports);
+      } else {
+        return Promise.reject(
+          `Unknown extension: ${ext}, please specify either 'js' or 'ts'`,
+        );
+      }
+    }),
+  );
 };
