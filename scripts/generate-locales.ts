6f0623b4513c53829d39f7867a1f71e67f5399ea
Ross Edfort <rossedfort@gmail.com>
2023-08-02 12:09:21 -0600

    get rid of http backend and JSON files for i18n
    use resources and import.meta.glob instead

--- scripts/generate-locales.ts
+++
@@ -1,3 +0,0 @@
-import { generateLocales } from '../src/lib/i18n/generate-locales';
-
-await generateLocales();
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,5 @@
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend, { type HttpBackendOptions } from 'i18next-http-backend';
 
 import type { LayoutData, LayoutLoad } from './$types';
 
@@ -8,23 +7,41 @@ import { I18nNamespaces } from '$lib/i18n';
 
 export const ssr = false;
 
-export const load: LayoutLoad = function (): LayoutData {
-  i18next
-    .use(Backend)
-    .use(LanguageDetector)
-    .init<HttpBackendOptions>({
-      fallbackLng: 'en',
-      load: 'languageOnly',
-      ns: I18nNamespaces,
-      defaultNS: 'common',
-      detection: {
-        order: ['querystring', 'localStorage', 'navigator'],
-        caches: ['localStorage'],
-        lookupQuerystring: 'lng',
-        lookupLocalStorage: 'locale',
-      },
-      backend: {
-        loadPath: '/i18n/locales/{{lng}}/{{ns}}.json',
-      },
-    });
+export const load: LayoutLoad = async function (): LayoutData {
+  const locales = import.meta.glob('/src/lib/i18n/locales/*/*.ts');
+
+  const resources = {};
+
+  for (const key in locales) {
+    const [_, lang] = key.split(/\/src\/lib\/i18n\/locales\/(\w+)\/.+\.ts/);
+    if (!resources[lang]) {
+      resources[lang] = {
+        translations: {},
+      };
+    }
+
+    const { Strings, Namespace } = (await locales[key]()) as {
+      Strings: object;
+      Namespace: string;
+    };
+
+    resources[lang] = {
+      ...resources[lang],
+      [Namespace]: Strings,
+    };
+  }
+
+  i18next.use(LanguageDetector).init({
+    fallbackLng: 'en',
+    load: 'languageOnly',
+    ns: I18nNamespaces,
+    defaultNS: 'common',
+    detection: {
+      order: ['querystring', 'localStorage', 'navigator'],
+      caches: ['localStorage'],
+      lookupQuerystring: 'lng',
+      lookupLocalStorage: 'locale',
+    },
+    resources,
+  });
 };
