cd327f63970925024e95c35e1863f3e32d002eea
Alex Tideman <alex.tideman@gmail.com>
2023-08-09 11:51:21 -0500

    Versioning Warnings -> No Workers or Incompatible Workers (#1536)
    * Add max version sets warning
    
    * Refactor reachability code to fetch on workflow run layout. Add warning for incompatible workers
    
    * Fix check for non-versioned compatiblity check
    
    * Remove unused import and static json files

--- src/lib/pages/workflow-workers.svelte
+++ src/lib/pages/workflow-workers.svelte
@@ -2,8 +2,12 @@
   import { workflowRun } from '$lib/stores/workflow-run';
 
   import WorkersList from '$lib/components/workers-list.svelte';
-
-  $: ({ workers, workflow, compatibility } = $workflowRun);
+  $: ({ workers, workflow, compatibility, reachability } = $workflowRun);
 </script>
 
-<WorkersList taskQueue={workflow?.taskQueue} {workers} {compatibility} />
+<WorkersList
+  taskQueue={workflow?.taskQueue}
+  {workers}
+  {compatibility}
+  {reachability}
+/>
--- src/lib/utilities/task-queue-compatibility.ts
+++ src/lib/utilities/task-queue-compatibility.ts
@@ -1,4 +1,7 @@
-import type { TaskQueueCompatibility } from '$lib/services/pollers-service';
+import type {
+  PollerWithTaskQueueTypes,
+  TaskQueueCompatibility,
+} from '$lib/services/pollers-service';
 import type { PollerInfo, TaskQueueCompatibleVersionSet } from '$lib/types';
 import type { WorkflowExecution } from '$lib/types/workflows';
 
@@ -61,4 +64,36 @@ export const getCurrentPollerBuildId = (
     return poller.workerVersionCapabilities?.buildId || undefined;
   }
   return undefined;
+};
+
+export const getUniqueBuildIdsFromPollers = (
+  pollers: PollerWithTaskQueueTypes[],
+): string[] => {
+  const buildIds = [
+    ...new Set(pollers.map(getCurrentPollerBuildId).filter(Boolean)),
+  ];
+  return buildIds;
+};
+
+export const pollerHasVersioning = (
+  pollers: PollerWithTaskQueueTypes[],
+): boolean => {
+  return pollers.some(
+    (poller) => poller?.workerVersionCapabilities?.useVersioning,
+  );
+};
+
+export const workflowIsCompatibleWithWorkers = (
+  workflow: WorkflowExecution,
+  pollers: PollerWithTaskQueueTypes[],
+  compatibility: TaskQueueCompatibility | undefined,
+): boolean => {
+  const workflowBuildId = getCurrentWorkflowBuildId(workflow);
+  if (!workflowBuildId) return true;
+  const defaultVersionForBuildId = getDefaultVersionForSetFromABuildId(
+    compatibility,
+    workflowBuildId,
+  );
+  const buildIds = getUniqueBuildIdsFromPollers(pollers);
+  return buildIds.includes(defaultVersionForBuildId);
 };
