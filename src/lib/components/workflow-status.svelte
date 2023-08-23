e57c242f70d6602b95804dfdf62875574405915c
Steve Kinney <hello@stevekinney.net>
2023-08-23 16:20:15 -0600

    Use CVA

--- src/lib/components/workflow-status.svelte
+++ src/lib/components/workflow-status.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { cva } from 'class-variance-authority';
+
   import Badge from '$lib/holocene/badge.svelte';
   import { translate } from '$lib/i18n/translate';
   import type { WorkflowStatus } from '$lib/types/workflows';
@@ -11,7 +13,7 @@
   export let delay = 0;
   export let status: Status = 'Running';
 
-  const humanFriendlyNames: Partial<Record<Status, string>> = {
+  const humanFriendlyNames: Record<Status, string> = {
     Running: translate('workflows', 'running'),
     TimedOut: translate('workflows', 'timed-out'),
     Completed: translate('workflows', 'completed'),
@@ -20,26 +22,34 @@
     Canceled: translate('workflows', 'canceled'),
     Terminated: translate('workflows', 'terminated'),
     Paused: translate('workflows', 'paused'),
-    Canceled: {
-      label: translate('workflows', 'canceled'),
-      className: 'status-canceled',
-    },
-    Terminated: {
-      label: translate('workflows', 'terminated'),
-      className: 'status-terminated',
-    },
-    Paused: {
-      label: translate('workflows', 'paused'),
-      className: 'status-paused',
-    },
   };
+
+  const workflowStatus = cva(
+    [
+      'flex items-center rounded-sm px-1 py-0.5 font-secondary whitespace-nowrap',
+    ],
+    {
+      variants: {
+        status: {
+          Running: 'bg-blue-100 text-blue-700',
+          TimedOut: 'bg-orange-100 text-orange-700',
+          Completed: 'bg-green-100 text-green-700',
+          Failed: 'bg-red-100 text-red-700',
+          ContinuedAsNew: 'bg-gray-200 text-gray-900',
+          Canceled: 'bg-yellow-100 text-yellow-900',
+          Terminated: 'bg-red-100 text-red-700',
+          Paused: 'bg-yellow-100 text-yellow-700',
+        },
+      },
+    },
+  );
 </script>
 
-<Badge class="status-{formatStatus(status)}">
-  <span class="whitespace-nowrap">
+<div class="flex text-center text-sm font-medium leading-4">
+  <span class={workflowStatus({ status })}>
     {humanFriendlyNames[status]}
+    {#if status === 'Running'}
+      <HeartBeat {delay} />
+    {/if}
   </span>
-  {#if status === 'Running'}
-    <HeartBeat {delay} />
-  {/if}
-</Badge>
+</div>
--- src/lib/models/workflow-status.ts
+++ src/lib/models/workflow-status.ts
@@ -1,9 +1,8 @@
 import type { WorkflowStatus } from '$lib/types/workflows';
 
-export type WorkflowStatuses = readonly WorkflowStatus[];
 export type WorkflowFilters = readonly (WorkflowStatus | 'All')[];
 
-export const workflowStatuses: WorkflowStatuses = [
+export const workflowStatuses: WorkflowStatus[] = [
   'Running',
   'TimedOut',
   'Completed',
@@ -12,8 +11,6 @@ export const workflowStatuses: WorkflowStatuses = [
   'Canceled',
   'Terminated',
 ] as const;
-
-export const scheduleStatuses = ['Paused', 'Running'] as const;
 
 export function isWorkflowStatusType(value: string): value is WorkflowStatus {
   return workflowStatuses.includes(value as WorkflowStatus);
