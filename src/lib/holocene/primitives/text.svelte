63c9c59c257870391f3e1a364d3ac11c8b93bd26
Steve Kinney <hello@stevekinney.net>
2023-08-30 07:54:33 -0600

    Add proposal for Box and Text
    I don't feel super strongly about either of these proposals, but they're mostly meant to get the conversation started.
    
    The `Text` component just renders a string, but takes care of the translation. The cool thing about it is that it doesn't render the slot. So, you _could_ just put a string of text in there like "Recent Workflows" just like we had before we did internationalization.
    
    The `Box` component is an abstraction around the native DOM. It does support the same types as the DOM element that it wraps, but it gives us an inflection point where we could do stuff like dark mode logic or adding a standardized format for `data-test-id` or whatever.
    
    The idea is that we could use this as the basis for the common set of things we use in the UI (e.g. `Input`, `Heading`, etc.) and have a place to handle accessibility or theming logic for us.

---
+++ src/lib/holocene/primitives/text.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" generics="Namespace extends I18nNamespace">
+  import type { I18nKey, I18nNamespace, I18nReplace } from '$lib/i18n';
+  import { translate } from '$lib/i18n/translate';
+
+  type $$Props = {
+    namespace: Namespace;
+    key: I18nKey<Namespace>;
+    replace?: I18nReplace;
+    count?: number;
+  };
+
+  export let namespace: Namespace = 'common' as Namespace;
+  export let key: I18nKey<Namespace>;
+  export let replace: I18nReplace = undefined;
+  export let count: number = undefined;
+</script>
+
+{translate(namespace, key, count, replace)}
