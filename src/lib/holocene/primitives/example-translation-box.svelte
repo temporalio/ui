75eceaa7bb9397661889a82827437dd5413310ba
Steve Kinney <hello@stevekinney.net>
2023-08-30 13:48:39 -0600

    Show example of component that handles translation
    This is imperfect because I didn't add support for options, but it demonstrates the point. With some more work and TypeScript 5, I could easily expand this to take multiple keys—and ideally support multiple namespaces. But, in terms of ergonomics, this is a decent proof of concept.

---
+++ src/lib/holocene/primitives/example-translation-box.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Box from './box-with-translate.svelte';
+</script>
+
+<Box
+  as="section"
+  namespace="workflows"
+  translation="activity-id"
+  let:translation
+>
+  <p>{translation}</p>
+</Box>
