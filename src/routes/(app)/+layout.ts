203bfa482ec41d02153c28a1a064eac0c4ef9943
Steve Kinney <hello@stevekinney.net>
2023-08-31 09:01:26 -0600

    Update +layout.ts

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -3,12 +3,11 @@ import { redirect } from '@sveltejs/kit';
 import type { LayoutData, LayoutLoad } from './$types';
 
 import { fetchCluster } from '$lib/services/cluster-service';
-import { fetchLatestUiVersion } from '$lib/services/github-service';
 import { fetchNamespaces } from '$lib/services/namespaces-service';
 import { fetchSettings } from '$lib/services/settings-service';
 import { getAuthUser, setAuthUser } from '$lib/stores/auth-user';
 import type { GetClusterInfoResponse } from '$lib/types';
-import type { Settings, UiVersionInfo } from '$lib/types/global';
+import type { Settings } from '$lib/types/global';
 import {
   cleanAuthUserCookie,
   getAuthUserCookie,
@@ -41,16 +40,8 @@ export const load: LayoutLoad = async function ({
 
   const cluster: GetClusterInfoResponse = await fetchCluster(settings, fetch);
 
-  const uiVersionInfo: UiVersionInfo = {
-    current: settings.version,
-    recommended: settings.notifyOnNewVersion
-      ? fetchLatestUiVersion(fetch)
-      : undefined,
-  };
-
   return {
     user,
-    uiVersionInfo,
     settings,
     cluster,
   };
