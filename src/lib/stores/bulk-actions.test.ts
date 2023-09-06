143bedb212cd5736759eda1530fbacf98deb19a9
Alex Tideman <alex.tideman@gmail.com>
2023-09-06 12:22:04 -0500

    Update unit tests

--- src/lib/stores/bulk-actions.test.ts
+++ src/lib/stores/bulk-actions.test.ts
@@ -3,8 +3,6 @@ import { get, type writable as writableFunc } from 'svelte/store';
 import { beforeEach, describe, expect, test, vi } from 'vitest';
 
 import { supportsBulkActions } from './bulk-actions';
-import { cluster } from './cluster';
-import { settings } from './settings';
 
 const mockedPageStore = await vi.hoisted(async () => {
   const { writable } = await vi.importActual<{
@@ -31,9 +29,14 @@ describe('supportsBulkActions store', () => {
     });
 
     test('returns true when batch actions are enabled, and visibility store is elasticsearch regardless of server version', () => {
-      cluster.set({ serverVersion: '1.0.0', visibilityStore: 'elasticsearch' });
-      settings.set({
-        batchActionsDisabled: false,
+      mockedPageStore.mockSetSubscribeValue({
+        data: {
+          settings: {
+            runtimeEnvironment: { isCloud: true },
+            batchActionsDisabled: false,
+          },
+          cluster: { serverVersion: '1.0.0', visibilityStore: 'elasticsearch' },
+        },
       });
 
       expect(get(supportsBulkActions)).toBe(true);
@@ -47,44 +50,62 @@ describe('supportsBulkActions store', () => {
       });
     });
     test('returns true when version is newer than 1.18.0, advanced visibility is supported, and batch actions are enabled', () => {
-      cluster.set({
-        serverVersion: '1.19.0',
-        visibilityStore: 'elasticsearch',
-      });
-      settings.set({
-        batchActionsDisabled: false,
+      mockedPageStore.mockSetSubscribeValue({
+        data: {
+          cluster: {
+            serverVersion: '1.19.0',
+            visibilityStore: 'elasticsearch',
+          },
+        },
       });
 
       expect(get(supportsBulkActions)).toBe(true);
     });
 
     test('returns false when version is older, even if visibility store is elasticsearch and batch actions are enabled', () => {
-      cluster.set({
-        serverVersion: '1.17.0',
-        visibilityStore: 'elasticsearch',
-      });
-      settings.set({
-        batchActionsDisabled: false,
+      mockedPageStore.mockSetSubscribeValue({
+        data: {
+          settings: {
+            runtimeEnvironment: { isCloud: false },
+            batchActionsDisabled: false,
+          },
+          cluster: {
+            serverVersion: '1.17.0',
+            visibilityStore: 'elasticsearch',
+          },
+        },
       });
 
       expect(get(supportsBulkActions)).toBe(false);
     });
 
     test('returns false when advanced visibility store is not elasticsearch, even if version is newer and batch actions are enabled', () => {
-      cluster.set({ serverVersion: '1.19.0', visibilityStore: 'mysql' });
-      settings.set({
-        batchActionsDisabled: false,
+      mockedPageStore.mockSetSubscribeValue({
+        data: {
+          settings: {
+            runtimeEnvironment: { isCloud: false },
+            batchActionsDisabled: false,
+          },
+          cluster: { serverVersion: '1.19.0', visibilityStore: 'mysql' },
+        },
       });
 
       expect(get(supportsBulkActions)).toBe(false);
     });
 
     test('returns false when batch actions are not enabled, even if version is newer and advanced visibility is supported', () => {
-      cluster.set({
-        serverVersion: '1.19.0',
-        visibilityStore: 'elasticsearch',
+      mockedPageStore.mockSetSubscribeValue({
+        data: {
+          settings: {
+            runtimeEnvironment: { isCloud: false },
+            batchActionsDisabled: true,
+          },
+          cluster: {
+            serverVersion: '1.19.0',
+            visibilityStore: 'elasticsearch',
+          },
+        },
       });
-      settings.set({ batchActionsDisabled: true });
 
       expect(get(supportsBulkActions)).toBe(false);
     });
