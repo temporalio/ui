fe3740e9bc3b931d2894e612d55e67b58352d553
Ross Edfort <rossedfort@gmail.com>
2023-09-06 15:16:39 -0600

    toggle button a11y
    - make toggle button either a button or anchor element (previously was a noninteractive div)
    - add some hover and focus styles for visual consistency with other interactive elements

--- src/lib/holocene/toggle-button/toggle-button.svelte
+++ src/lib/holocene/toggle-button/toggle-button.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-  
-  import Icon from '$lib/holocene/icon/icon.svelte';
-import type { IconName } from '$lib/holocene/icon/paths';
-import { getAppContext } from '$lib/utilities/get-context';
+  import type {
+    HTMLAnchorAttributes,
+    HTMLButtonAttributes,
+  } from 'svelte/elements';
 
+  import { page } from '$app/stores';
+
+  import Icon from '$lib/holocene/icon/icon.svelte';
+  import type { IconName } from '$lib/holocene/icon/paths';
+  import { getAppContext } from '$lib/utilities/get-context';
+
+  type BaseProps = {
+    icon?: IconName;
+    group?: boolean;
+    active?: boolean;
+    'data-testid'?: string;
+  };
+
+  type AnchorProps = BaseProps &
+    HTMLAnchorAttributes & {
+      href: string;
+      base?: string;
+    };
+
+  type ButtonProps = BaseProps &
+    HTMLButtonAttributes & {
+      href?: never;
+      base?: never;
+    };
+
+  type $$Props = AnchorProps | ButtonProps;
+
+  let className = '';
+  export { className as class };
   export let icon: IconName = null;
   export let group = getAppContext('group');
   export let href = '';
@@ -12,50 +40,35 @@ import { getAppContext } from '$lib/utilities/get-context';
   export let active = false;
 </script>
 
-{#if href}
-  <a
-    class="flex items-center justify-center border border-gray-900 py-2 px-4 text-sm {$$props.class}"
-    class:rounded-lg={!group}
-    class:active={$page.url.pathname.includes(base) || active}
-    class:group
-    href={href + $page.url.search}
-    id={$$props.id}
-    {...$$props}
-    on:click
-  >
-    {#if icon}
-      <div class="flex items-center gap-2">
-        <Icon name={icon} />
-        <span class="hidden md:block"><slot /></span>
-      </div>
-    {:else}
-      <slot />
-    {/if}
-  </a>
-{:else}
-  <div
-    class="flex cursor-pointer items-center justify-center border border-gray-900 py-2 px-4 text-sm {$$props.class}"
-    class:rounded-lg={!group}
-    class:active
-    class:group
-    id={$$props.id}
-    {...$$props}
-    on:click
-  >
-    {#if icon}
-      <div class="flex items-center gap-2">
-        <Icon name={icon} />
-        <span class="hidden md:block"><slot /></span>
-      </div>
-    {:else}
-      <slot />
-    {/if}
-  </div>
-{/if}
+<svelte:element
+  this={href ? 'a' : 'button'}
+  class="toggle-button {className}"
+  class:group
+  class:active={href ? $page.url.pathname.includes(base) : active}
+  href={href ? href + $page.url.search : null}
+  class:rounded-lg={!group}
+  on:click
+  role="button"
+  tabindex="0"
+  {...$$restProps}
+>
+  {#if icon}
+    <div class="flex items-center gap-2">
+      <Icon name={icon} />
+      <span class="hidden md:block"><slot /></span>
+    </div>
+  {:else}
+    <slot />
+  {/if}
+</svelte:element>
 
 <style lang="postcss">
-  .active {
-    @apply bg-gray-900 text-white hover:bg-gray-900;
+  .toggle-button {
+    @apply flex items-center justify-center border-2 border-gray-900 py-2 px-4 text-sm transition-colors hover:bg-gradient-to-br hover:from-blue-100 hover:to-purple-100 hover:text-primary focus:outline-none focus-visible:border-indigo-600 focus-visible:shadow-focus focus-visible:shadow-blue-600/50 focus-visible:bg-gradient-to-br focus:from-blue-100 focus:to-purple-100;
+  }
+
+  .toggle-button.active {
+    @apply bg-gray-900 text-white hover:text-primary focus-visible:text-primary;
   }
 
   .group:first-child {
