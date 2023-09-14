db4b61420bd37c09a43911e3309dd0fac53eef96
Alex Tideman <alex.tideman@gmail.com>
2023-09-14 14:18:41 -0500

    Remove custom filters

--- src/lib/components/workflow/workflow-counts.svelte
+++ src/lib/components/workflow/workflow-counts.svelte
@@ -1,149 +1,65 @@
 <script lang="ts">
-  import { page } from '$app/stores';
+import { page } from '$app/stores';
 
-  import type { WorkflowFilter } from '$lib/models/workflow-filters';
-  import { workflowStatuses } from '$lib/models/workflow-status';
-  import { workflowFilters } from '$lib/stores/filters';
-  import { groupByCountEnabled } from '$lib/stores/group-by-enabled';
-  import { workflowCount, workflowsQuery } from '$lib/stores/workflows';
-  import { decodePayload } from '$lib/utilities/decode-payload';
-  import { isStatusFilter } from '$lib/utilities/query/filter-search';
-  import { toListWorkflowQueryFromFilters } from '$lib/utilities/query/filter-workflow-query';
-  import { combineFilters } from '$lib/utilities/query/to-list-workflow-filters';
-  import { requestFromAPI } from '$lib/utilities/request-from-api';
-  import { routeForApi } from '$lib/utilities/route-for-api';
-  import { updateQueryParameters } from '$lib/utilities/update-query-parameters';
+import { workflowStatuses } from '$lib/models/workflow-status';
+import { workflowFilters } from '$lib/stores/filters';
+import { groupByCountEnabled } from '$lib/stores/group-by-enabled';
+import { workflowCount, workflowsQuery } from '$lib/stores/workflows';
+import { decodePayload } from '$lib/utilities/decode-payload';
+import { toListWorkflowQueryFromFilters } from '$lib/utilities/query/filter-workflow-query';
+import { combineFilters } from '$lib/utilities/query/to-list-workflow-filters';
+import { requestFromAPI } from '$lib/utilities/request-from-api';
+import { routeForApi } from '$lib/utilities/route-for-api';
 
-  import WorkflowCountAll from './workflow-count-all.svelte';
-  import WorkflowCountStatus from './workflow-count-status.svelte';
+import WorkflowCountAll from './workflow-count-all.svelte';
+import WorkflowCountStatus from './workflow-count-status.svelte';
 
-  $: namespace = $page.params.namespace;
+$: namespace = $page.params.namespace;
 
-  let totalCount = 0;
-  let statusGroups = [];
+let totalCount = 0;
+let statusGroups = [];
 
-  $: statusFilters = $workflowFilters.filter((filter) =>
-    isStatusFilter(filter.attribute),
-  );
+const fetchCounts = async () => {
+  if (groupByCountEnabled) {
+    const groupByClause = 'GROUP BY ExecutionStatus';
+    const countRoute = routeForApi('workflows.count', {
+      namespace,
+    });
 
-  function mapStatusToFilter(value: string) {
-    return {
-      attribute: 'ExecutionStatus',
-      value,
-      operator: '',
-      parenthesis: '',
-      conditional: '=',
-    };
-  }
-
-  function mapStatusesToFilters(filters: WorkflowFilter[]) {
-    if (filters.length === 1) {
-      return [mapStatusToFilter(filters[0].value)];
-    } else {
-      return filters.map((filter, i) => {
-        const operator = i === filters.length - 1 ? '' : 'OR';
-        return {
-          ...filter,
-          operator,
-        };
-      });
-    }
-  }
-
-  const onStatusClick = (status: string) => {
-    if (status === 'all') {
-      $workflowFilters = $workflowFilters.filter(
-        (f) => !isStatusFilter(f.attribute),
-      );
-    } else {
-      if (statusFilters.find((s) => s.value === status)) {
-        const nonStatusFilters = $workflowFilters.filter(
-          (f) => !isStatusFilter(f.attribute),
-        );
-        $workflowFilters = [
-          ...nonStatusFilters,
-          ...mapStatusesToFilters(
-            statusFilters.filter((s) => s.value !== status),
-          ),
-        ];
-      } else {
-        if (!statusFilters.length) {
-          $workflowFilters = [...$workflowFilters, mapStatusToFilter(status)];
-        } else {
-          const nonStatusFilters = $workflowFilters.filter(
-            (f) => !isStatusFilter(f.attribute),
-          );
-          $workflowFilters = [
-            ...nonStatusFilters,
-            ...mapStatusesToFilters([
-              ...statusFilters,
-              mapStatusToFilter(status),
-            ]),
-          ];
-        }
-      }
-    }
-
-    const searchQuery = toListWorkflowQueryFromFilters(
+    const query = toListWorkflowQueryFromFilters(
       combineFilters($workflowFilters),
     );
-    updateQueryParameters({
-      url: $page.url,
-      parameter: 'query',
-      value: searchQuery,
-      allowEmpty: true,
+    const { count, groups } = await requestFromAPI<{
+      count: string;
+      groups: unknown[];
+    }>(countRoute, {
+      params: { query: `${query} ${groupByClause}` },
+      notifyOnError: false,
     });
-  };
+    totalCount = parseInt(count);
+    $workflowCount.totalCount = totalCount;
+    statusGroups = groups.map((group) => {
+      const value = decodePayload(group?.groupValues[0]);
+      const count = parseInt(group.count);
+      // if ()
+      return {
+        value,
+        count,
+      };
+    });
+  }
+};
 
-  const fetchCounts = async () => {
-    if (groupByCountEnabled) {
-      const groupByClause = 'GROUP BY ExecutionStatus';
-      const countRoute = routeForApi('workflows.count', {
-        namespace,
-      });
-
-      const query = toListWorkflowQueryFromFilters(
-        combineFilters(
-          $workflowFilters.filter((f) => !isStatusFilter(f.attribute)),
-        ),
-      );
-      const { count, groups } = await requestFromAPI<{
-        count: string;
-        groups: unknown[];
-      }>(countRoute, {
-        params: { query: `${query} ${groupByClause}` },
-        notifyOnError: false,
-      });
-      totalCount = parseInt(count);
-      $workflowCount.totalCount = totalCount;
-      statusGroups = groups.map((group) => {
-        const value = decodePayload(group?.groupValues[0]);
-        const count = parseInt(group.count);
-        // if ()
-        return {
-          value,
-          count,
-        };
-      });
-    }
-  };
-
-  $: $workflowsQuery, namespace, fetchCounts();
+$: $workflowsQuery, namespace, fetchCounts();
 </script>
 
 {#if groupByCountEnabled}
   <div class="flex flex flex-wrap gap-1">
-    <WorkflowCountAll
-      count={totalCount}
-      {onStatusClick}
-      active={!$workflowFilters.length}
-    />
+    <WorkflowCountAll count={totalCount} />
     {#each workflowStatuses as status}
       <WorkflowCountStatus
-        {status}
-        {onStatusClick}
+        status={status}
         count={statusGroups.find((group) => group.value === status)?.count ?? 0}
-        active={statusFilters.some((filter) => filter.value === status)}
       />
     {/each}
   </div>
