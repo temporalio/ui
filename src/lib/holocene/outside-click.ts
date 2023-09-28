5d3de5fbb2c0128c83c28a68b99db8f75384f08c
Alex Tideman <alex.tideman@gmail.com>
2023-09-28 11:30:27 -0500

    Remove commented out code

--- src/lib/holocene/outside-click.ts
+++ src/lib/holocene/outside-click.ts
@@ -1,26 +1,3 @@
-// export const clickoutside: Action<Element, (event: MouseEvent) => void> = (
-//   node: Element,
-//   handler: (event: MouseEvent) => void,
-// ): { destroy: () => void } => {
-//   const handleClick = (event: MouseEvent) => {
-//     if (
-//       node &&
-//       !node.contains(event.target as HTMLElement) &&
-//       !event.defaultPrevented
-//     ) {
-//       handler(event);
-//     }
-//   };
-
-//   document.addEventListener('click', handleClick, true);
-
-//   return {
-//     destroy() {
-//       document.removeEventListener('click', handleClick, true);
-//     },
-//   };
-// };
-
 export function clickOutside(node: Element): { destroy: () => void } {
   const handleClick = (event: Event) => {
     if (
