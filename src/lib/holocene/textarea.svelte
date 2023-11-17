98138d7850224cc57e4e7ac7301a01053d8aea61
Laura Whitaker <laura.whitaker@temporal.io>
2023-11-17 16:44:27 -0700

    Add optional maxLength to Textarea

--- src/lib/holocene/textarea.svelte
+++ src/lib/holocene/textarea.svelte
@@ -1,4 +1,22 @@
 <script lang="ts">
+  import type { HTMLTextareaAttributes } from 'svelte/elements';
+
+  type $$Props = HTMLTextareaAttributes & {
+    disabled?: boolean;
+    error?: string;
+    isValid?: boolean;
+    placeholder?: string;
+    rows?: number;
+    spellcheck?: boolean;
+    value: string;
+    label: string;
+    labelHidden?: boolean;
+    id: string;
+    required?: boolean;
+    description?: string;
+    maxLength?: number;
+  };
+
   export let disabled = false;
   export let error = '';
   export let isValid = true;
@@ -11,6 +29,7 @@
   export let id: string;
   export let required = false;
   export let description = '';
+  export let maxLength = 0;
 
   let className = '';
   export { className as class };
@@ -21,22 +40,38 @@
   {#if description}
     <p class="text-sm">{description}</p>
   {/if}
-  <textarea
-    class="min-h-fit w-full rounded border border-gray-900 py-2 px-3 font-mono text-sm"
-    class:error={!isValid}
-    {id}
-    bind:value
-    {disabled}
-    {placeholder}
-    {rows}
-    {spellcheck}
-    on:input
-    on:change
-    on:focus
-    on:blur
-    on:keydown|stopPropagation
-  />
-  <div class="error-msg" aria-live={isValid ? 'off' : 'assertive'}>
+  <div class="relative">
+    <textarea
+      class="min-h-fit w-full rounded border border-gray-900 py-2 px-3 font-mono text-sm"
+      class:error={!isValid}
+      {id}
+      bind:value
+      {disabled}
+      {placeholder}
+      {rows}
+      {spellcheck}
+      on:input
+      on:change
+      on:focus
+      on:blur
+      on:keydown|stopPropagation
+      maxlength={maxLength > 0 ? maxLength : undefined}
+    />
+    {#if maxLength && !disabled}
+      <span class="count">
+        <span
+          class="text-blue-700"
+          class:warn={maxLength - value?.length <= 5}
+          class:error={maxLength === value?.length}>{value?.length ?? 0}</span
+        >&nbsp;/&nbsp;{maxLength}
+      </span>
+    {/if}
+  </div>
+  <div
+    class="error-msg"
+    class:min-width={maxLength}
+    aria-live={isValid ? 'off' : 'assertive'}
+  >
     {#if !isValid}
       {#if error}
         <p>{error}</p>
@@ -60,6 +95,26 @@
   }
 
   .error-msg {
-    @apply border-danger font-primary text-sm font-normal text-danger;
+    @apply break-words border-danger font-primary text-sm font-normal text-danger;
+  }
+
+  .error-msg.min-width {
+    @apply w-[calc(100%-6rem)];
+  }
+
+  .count {
+    @apply invisible absolute -bottom-5 right-0 font-secondary text-sm font-medium text-primary;
+  }
+
+  .count > .warn {
+    @apply text-orange-600;
+  }
+
+  .count > .error {
+    @apply text-red-700;
+  }
+
+  textarea:focus + .count {
+    @apply visible;
   }
 </style>
