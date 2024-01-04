8de6b50abbf024e3b24887e46d0c49bc49963e58
Alex Tideman <alex.tideman@gmail.com>
2024-01-04 15:21:09 -0600

    Update version.go to 2.21.4 (#1762)

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.21.3"
+	UIVersion               = "2.21.4"
 )
