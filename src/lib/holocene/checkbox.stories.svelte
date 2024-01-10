51ee2cf7e6341896786292d7cecf9ae5c53fb583
Alex Thomsen <alex.scott.thomsen@gmail.com>
2024-01-10 12:51:47 -0700

    Checkboxiis are now laid out with flex instead of only using absolute… (#1788)
    * Checkboxiis are now laid out with flex instead of only using absolute. Hover element is a pseudo element now and seems to be generally centered. Removed some of the extra fluff/padding that hover-able introduced around elements. Made it so text can wrap on multiple lines and checkbox stays correct. Used sr-only for input.
    
    * Demonstrate we can still nowrap checkbox
    
    * Don't center use items-start
    
    * We can do this for checkbox hovers too
    
    * Moved input and fixed css selector to get siblings instead of the exact "next" element
    
    * Fix focus css
    
    * Go back to the way it was

--- src/lib/holocene/checkbox.stories.svelte
+++ src/lib/holocene/checkbox.stories.svelte
@@ -3,6 +3,9 @@
   import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
 
   import Checkbox from '$lib/holocene/checkbox.svelte';
+
+  const loremIpsum =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porttitor nec nulla et elementum. Nulla eget fermentum magna. Ut vel turpis at nulla interdum auctor. Etiam semper arcu non lectus porta, sit amet hendrerit ipsum ultricies. Suspendisse potenti. Nulla facilisi. Donec sit amet risus placerat, volutpat enim ac, elementum turpis. Etiam faucibus nisl ac sollicitudin placerat. Etiam in tincidunt erat. Pellentesque in tempor tortor.';
 </script>
 
 <Meta
@@ -12,12 +15,70 @@
 />
 
 <Template let:args>
-  <Checkbox
-    {...args}
-    on:change={action('change')}
-    on:click={action('click')}
-    on:keypress={action('keypress')}
-  />
+  <div class:bg-black={args.onDark}>
+    <Checkbox
+      {...args}
+      on:change={action('change')}
+      on:click={action('click')}
+      on:keypress={action('keypress')}
+    />
+  </div>
 </Template>
 
-<Story name="checkbox" args={{ label: 'Check Me', onDark: false }} />
+<Story
+  name="checkbox"
+  args={{ label: 'Check Me', onDark: false, hoverable: true }}
+/>
+
+<Story
+  name="So Many"
+  template="Many"
+  args={{ label: loremIpsum, onDark: false }}
+/>
+
+<Template id="Many" let:args>
+  <div
+    class:bg-black={args.onDark}
+    class="border-bl flex max-w-xl flex-col gap-5 border-2 p-5"
+  >
+    <Checkbox
+      label={loremIpsum}
+      hoverable
+      {...args}
+      on:change={action('change')}
+      on:click={action('click')}
+      on:keypress={action('keypress')}
+    />
+    <Checkbox
+      label={loremIpsum}
+      {...args}
+      on:change={action('change')}
+      on:click={action('click')}
+      on:keypress={action('keypress')}
+    />
+    <Checkbox
+      {...args}
+      class="whitespace-nowrap"
+      label="This one can intentionally break out of the mold to demonstrate we still have nowrap as an option"
+      hoverable
+      on:change={action('change')}
+      on:click={action('click')}
+      on:keypress={action('keypress')}
+    />
+    <Checkbox
+      label={loremIpsum}
+      {...args}
+      on:change={action('change')}
+      on:click={action('click')}
+      on:keypress={action('keypress')}
+    />
+    <Checkbox
+      label={loremIpsum}
+      hoverable
+      {...args}
+      on:change={action('change')}
+      on:click={action('click')}
+      on:keypress={action('keypress')}
+    />
+  </div>
+</Template>
