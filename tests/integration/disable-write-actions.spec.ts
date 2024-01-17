e994a35510f04e3715b0c6fd3797088efdf87d78
Alex Tideman <alex.tideman@gmail.com>
2024-01-17 10:18:51 -0600

    Add disable write action and schedules tests

--- tests/integration/disable-write-actions.spec.ts
+++ tests/integration/disable-write-actions.spec.ts
@@ -1,9 +1,11 @@
 import { expect, test } from '@playwright/test';
 
 import { mockSettingsApi, mockWorkflowApis } from '~/test-utilities/mock-apis';
+import { mockSchedulesApis } from '~/test-utilities/mock-apis';
 import { mockWorkflow } from '~/test-utilities/mocks/workflow';
 
 const workflowUrl = `/namespaces/default/workflows/${mockWorkflow.workflowExecutionInfo.execution.workflowId}/${mockWorkflow.workflowExecutionInfo.execution.runId}/history`;
+const schedulesUrl = '/namespaces/default/schedules';
 
 test.describe('Enable write actions by default', () => {
   test.beforeEach(async ({ page }) => {
@@ -19,7 +21,7 @@ test.describe('Enable write actions by default', () => {
   });
 });
 
-test.describe('Disable write actions', () => {
+test.describe('Disable write actions on workflow actions', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(workflowUrl);
     await mockWorkflowApis(page);
@@ -31,5 +33,23 @@ test.describe('Disable write actions', () => {
       '#workflow-actions-primary-button',
     );
     await expect(workflowActionButton).toBeDisabled();
+  });
+});
+
+test.describe('Disable write actions on empty schedules list actions', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(schedulesUrl);
+    await mockSchedulesApis(page, true);
+    await mockSettingsApi(page, { DisableWriteActions: true });
+  });
+
+  test('Create Schedule button is disabled', async ({ page }) => {
+    await page.goto(schedulesUrl);
+
+    const namespace = await page.locator('h1').innerText();
+    expect(namespace).toBe('Schedules');
+
+    const createButton = page.getByTestId('create-schedule');
+    await expect(createButton).toBeDisabled();
   });
 });
