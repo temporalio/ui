4f69c6267ee5cf612bf0b9c7be3d923ffd95c097
Steve Kinney <hello@stevekinney.net>
2024-01-30 15:43:37 -0700

    Remove unused theme properties

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,13 @@
 import type { Config } from 'tailwindcss';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 
-import temporalColors from './colors';
+import colors from './colors';
 
 const config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   darkMode: 'class',
   theme: {
-    colors: temporalColors,
-    textColor: ({ theme }) => theme('colors'),
-    backgroundColor: ({ theme }) => theme('colors'),
+    colors,
     fontFamily: {
       primary: ['Inter Variable', ...fontFamily.sans],
       secondary: ['Poppins', ...fontFamily.sans],
