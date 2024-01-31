96991cf131e2877f3ada16e4dde7f3035feda40c
Steve Kinney <hello@stevekinney.net>
2024-01-31 08:37:38 -0700

    Spike out Tailwind plugin

---
+++ src/theme/index.ts
@@ -0,0 +1,49 @@
+import plugin from 'tailwindcss/plugin';
+
+import colors, { getColor } from './colors';
+import { rgb } from './utilities';
+
+const variables = {
+  '--color-primary': rgb(colors.primary),
+  '--color-secondary': rgb(colors.secondary),
+  '--color-text-primary': rgb(colors.primary),
+  '--color-text-secondary': rgb(colors.secondary),
+  '--color-surface-primary': rgb(colors.white),
+  '--color-surface-secondary': rgb(getColor('slate', 100)),
+} as const;
+
+const css = (variable: keyof typeof variables) => `rgb(var(${variable}))`;
+
+const temporal = plugin(
+  ({ addUtilities, addComponents, addBase }) => {
+    addBase({
+      ':root': variables,
+    });
+
+    addUtilities({
+      '.text-primary': { color: css('--color-text-primary') },
+      '.text-secondary': { color: css('--color-text-secondary') },
+      '.bg-primary': { backgroundColor: css('--color-primary') },
+      '.border-primary': { borderColor: css('--color-primary') },
+    });
+
+    addComponents({
+      '.surface-primary': {
+        backgroundColor: css('--color-surface-primary'),
+        color: css('--color-text-primary'),
+      },
+      '.surface-secondary': {
+        backgroundColor: css('--color-surface-secondary'),
+        color: css('--color-text-primary'),
+      },
+    });
+  },
+  {
+    theme: {
+      colors,
+    },
+  },
+);
+
+export default temporal;
+export { colors };
