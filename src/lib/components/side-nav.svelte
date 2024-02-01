4ec1c0db733a51dada31829b176afbae437ad325
Steve Kinney <hello@stevekinney.net>
2024-01-31 18:16:25 -0700

    Simplify side-nav

--- src/lib/components/side-nav.svelte
+++ src/lib/components/side-nav.svelte
@@ -2,13 +2,10 @@
   import FeatureGuard from '$lib/components/feature-guard.svelte';
   import IsCloudGuard from '$lib/components/is-cloud-guard.svelte';
   import IsLegacyCloudGuard from '$lib/components/is-legacy-cloud-guard.svelte';
-  import Icon from '$lib/holocene/icon/icon.svelte';
   import NavContainer from '$lib/holocene/navigation/nav-container.svelte';
   import NavRow from '$lib/holocene/navigation/nav-row.svelte';
-  import NavTooltip from '$lib/holocene/navigation/nav-tooltip.svelte';
   import { translate } from '$lib/i18n/translate';
   import { inProgressBatchOperation } from '$lib/stores/batch-operations';
-  // import { labsMode } from '$lib/stores/labs-mode';
 
   import type { DescribeNamespaceResponse as Namespace } from '$types';
 
@@ -28,66 +25,48 @@
 
 <NavContainer {isCloud} {linkList} aria-label={translate('common.primary')}>
   <svelte:fragment slot="top">
-    <NavRow link={linkList.workflows} {isCloud} data-testid="workflows-button">
-      <NavTooltip text={translate('common.workflows')}>
-        <div class="nav-icon">
-          <Icon name="workflow" />
-        </div>
-      </NavTooltip>
-      <div class="nav-title">{translate('common.workflows')}</div>
-    </NavRow>
+    <NavRow
+      link={linkList.workflows}
+      data-testid="workflows-button"
+      label={translate('common.workflows')}
+      icon="workflow"
+    />
     <IsCloudGuard {isCloud}>
       <FeatureGuard
         enabled={Boolean(activeNamespace?.namespaceInfo?.supportsSchedules)}
       >
         <NavRow
           link={linkList.schedules}
-          {isCloud}
           data-testid="schedules-button"
-        >
-          <NavTooltip text={translate('common.schedules')}>
-            <div class="nav-icon">
-              <Icon name="schedules" height={25} width={24} />
-            </div>
-          </NavTooltip>
-          <div class="nav-title">{translate('common.schedules')}</div>
-        </NavRow>
+          label={translate('common.schedules')}
+          icon="schedules"
+        />
       </FeatureGuard>
     </IsCloudGuard>
     <slot name="top" />
     <IsCloudGuard {isCloud}>
-      <NavRow link={linkList.archive} {isCloud} data-testid="archive-button">
-        <NavTooltip text={translate('common.archive')}>
-          <div class="nav-icon">
-            <Icon name="archives" />
-          </div>
-        </NavTooltip>
-        <div class="nav-title">{translate('common.archive')}</div>
-      </NavRow>
+      <NavRow
+        link={linkList.archive}
+        data-testid="archive-button"
+        label={translate('common.archive')}
+        icon="archives"
+      />
     </IsCloudGuard>
-    <NavRow link={linkList.batchOperations} {isCloud}>
-      <NavTooltip text={translate('batch.list-page-title')}>
-        <div class="nav-icon">
-          <Icon name="batch-operation" animate={$inProgressBatchOperation} />
-        </div>
-      </NavTooltip>
-      <div class="nav-title">{translate('batch.nav-title')}</div>
-    </NavRow>
+    <NavRow
+      link={linkList.batchOperations}
+      label={translate('batch.nav-title')}
+      tooltip={translate('batch.list-page-title')}
+      icon={batchOperationIcon}
+    />
   </svelte:fragment>
   <svelte:fragment slot="middle">
     <IsLegacyCloudGuard {isCloud}>
       <NavRow
         link={linkList.namespaces}
-        {isCloud}
         data-testid="namespaces-button"
-      >
-        <NavTooltip text={translate('common.namespaces')}>
-          <div class="nav-icon">
-            <Icon name="namespace" />
-          </div>
-        </NavTooltip>
-        <div class="nav-title">{translate('common.namespaces')}</div>
-      </NavRow>
+        label={translate('common.namespaces')}
+        icon="namespace"
+      />
     </IsLegacyCloudGuard>
     <slot name="middle" />
   </svelte:fragment>
@@ -95,37 +74,20 @@
     <slot name="bottom" />
     <slot name="import">
       <IsCloudGuard {isCloud}>
-        <NavRow link={linkList.import} {isCloud} data-testid="import-button">
-          <NavTooltip text={translate('common.import')}>
-            <div class="nav-icon">
-              <Icon name="import" />
-            </div>
-          </NavTooltip>
-          <div class="nav-title">{translate('common.import')}</div>
-        </NavRow>
+        <NavRow
+          link={linkList.import}
+          data-testid="import-button"
+          label={translate('common.import')}
+          icon="import"
+        />
       </IsCloudGuard>
     </slot>
     <slot name="feedback">
-      <NavRow link={linkList.feedback} {isCloud}>
-        <NavTooltip text={translate('common.feedback')}>
-          <div class="nav-icon">
-            <Icon name="feedback" />
-          </div>
-        </NavTooltip>
-        <div class="nav-title">{translate('common.feedback')}</div>
-      </NavRow>
+      <NavRow
+        link={linkList.feedback}
+        label={translate('common.feedback')}
+        icon="feedback"
+      />
     </slot>
   </svelte:fragment>
 </NavContainer>
-
-<style lang="postcss">
-  .nav-icon {
-    @apply ml-2 mr-2 mt-0 h-6 cursor-pointer;
-  }
-
-  .nav-title {
-    width: 100px;
-    overflow: hidden;
-    transition: width 0.15s linear;
-  }
-</style>
--- src/lib/holocene/navigation/nav-row.svelte
+++ src/lib/holocene/navigation/nav-row.svelte
@@ -1,23 +1,34 @@
 <script lang="ts">
-  export let link: string = '';
+  import Icon from '../icon/icon.svelte';
+  import type { IconName } from '../icon/paths';
+
+  import NavTooltip from './nav-tooltip.svelte';
+
+  export let link: string;
+  export let label: string;
+  export let icon: IconName;
+  export let tooltip = label;
   export let externalLink = false;
 
-  let attributes = {
-    rel: externalLink ? 'noreferrer' : undefined,
-    target: externalLink ? '_blank' : undefined,
-  };
+  const attributes = externalLink
+    ? {
+        rel: 'noreferrer',
+        target: '_blank',
+      }
+    : {};
 </script>
 
 <li data-testid={$$props['data-testid']}>
-  {#if link}
-    <a
-      href={link}
-      {...attributes}
-      class="flex flex-row items-center whitespace-nowrap rounded-lg py-1 text-sm font-medium group-[.surface-inverse]:hover:bg-slate-950 group-[.surface-primary]:hover:bg-slate-200"
-    >
-      <slot />
-    </a>
-  {:else}
-    <slot />
-  {/if}
+  <a
+    href={link}
+    {...attributes}
+    class="flex flex-row items-center whitespace-nowrap rounded-lg py-1 text-sm font-medium group-[.surface-inverse]:hover:bg-slate-950 group-[.surface-primary]:hover:bg-slate-200"
+  >
+    <NavTooltip text={tooltip}>
+      <div class="ml-2 mr-2 mt-0 h-6 cursor-pointer">
+        <Icon name={icon} />
+      </div>
+    </NavTooltip>
+    <div class="overflow-hidden">{label}</div>
+  </a>
 </li>
