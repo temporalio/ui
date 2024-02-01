4cb38ecb53e0b45f0256b73093eb402e41d66af0
Steve Kinney <hello@stevekinney.net>
2024-01-31 17:52:48 -0700

    Simplify nav-container and nav-row

--- src/lib/holocene/navigation/nav-row.svelte
+++ src/lib/holocene/navigation/nav-row.svelte
@@ -1,63 +1,23 @@
 <script lang="ts">
-  export let link: string | null = null;
-  export let isCloud: boolean;
-  export let noFilter = false;
-  export let wrap = false;
+  export let link: string = '';
   export let externalLink = false;
-  export let handleClick: () => void = null;
-  let classes = `nav-row ${$$props.class} ${isCloud ? 'cloud' : 'local'}`;
+
+  let attributes = {
+    rel: externalLink ? 'noreferrer' : undefined,
+    target: externalLink ? '_blank' : undefined,
+  };
 </script>
 
-<li
-  class={link ? '' : classes}
-  class:noFilter
-  class:wrap
-  class:button={handleClick !== null}
-  data-testid={$$props['data-testid']}
->
-  {#if handleClick !== null}
-    <button on:click={handleClick} class="{classes} text-left">
-      <slot />
-    </button>
-  {:else if link && externalLink}
-    <a href={link} rel="noreferrer" target="_blank" class={classes}>
-      <slot />
-    </a>
-  {:else if link}
-    <a href={link} class={classes}>
+<li data-testid={$$props['data-testid']}>
+  {#if link}
+    <a
+      href={link}
+      {...attributes}
+      class="flex flex-row items-center whitespace-nowrap rounded-lg py-1 text-sm font-medium group-[.surface-inverse]:hover:bg-slate-950 group-[.surface-primary]:hover:bg-slate-200"
+    >
       <slot />
     </a>
   {:else}
     <slot />
   {/if}
 </li>
-
-<style lang="postcss">
-  .nav-row {
-    @apply flex flex-row items-center whitespace-nowrap rounded-lg py-1 text-sm font-medium;
-  }
-
-  .nav-row.button {
-    @apply py-0;
-  }
-
-  .wrap {
-    @apply whitespace-normal;
-  }
-
-  .local {
-    @apply text-white;
-  }
-
-  .cloud {
-    @apply text-primary;
-  }
-
-  .local:hover {
-    @apply surface-primary text-primary;
-  }
-
-  .cloud:hover {
-    @apply bg-inverse text-white;
-  }
-</style>
