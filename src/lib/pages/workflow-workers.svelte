d02f73d08769734303ba5284a9153de5ff7a72b8
Alex Tideman <alex.tideman@gmail.com>
2024-02-28 15:40:43 -0600

    Move reachability out of workflow run and to workflow workers page

--- src/lib/pages/workflow-workers.svelte
+++ src/lib/pages/workflow-workers.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
-  import WorkersList from '$lib/components/workers-list.svelte';
-  import { workflowRun } from '$lib/stores/workflow-run';
+  import { page } from '$app/stores';
 
-  $: ({ workers, workflow, compatibility, reachability } = $workflowRun);
+  import WorkersList from '$lib/components/workers-list.svelte';
+  import { getWorkerTaskReachability } from '$lib/services/pollers-service';
+  import { workflowRun } from '$lib/stores/workflow-run';
+  import {
+    getUniqueBuildIdsFromPollers,
+    pollerHasVersioning,
+  } from '$lib/utilities/task-queue-compatibility';
+
+  let { namespace } = $page.params;
+  $: ({ workers, workflow, compatibility } = $workflowRun);
+
+  $: taskQueue = workflow?.taskQueue;
+  $: versioningEnabled = pollerHasVersioning(workers.pollers);
 </script>
 
-<WorkersList
-  taskQueue={workflow?.taskQueue}
-  {workers}
-  {compatibility}
-  {reachability}
-/>
+{#if versioningEnabled}
+  {@const buildIds = getUniqueBuildIdsFromPollers(workers.pollers)}
+  {#await getWorkerTaskReachability( { namespace, buildIds, taskQueue }, ) then reachability}
+    <WorkersList {taskQueue} {workers} {compatibility} {reachability} />
+  {:catch}
+    <WorkersList {taskQueue} {workers} />
+  {/await}
+{:else}
+  <WorkersList {taskQueue} {workers} />
+{/if}
