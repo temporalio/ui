8bcfc30d8c0619195fd463e6a043bd23957de2f5
grace <grace.gardner@temporal.io>
2024-02-28 00:26:14 -0700

    add active color to plug in and update styling to match designs

--- src/lib/holocene/toggle-button/toggle-button.svelte
+++ src/lib/holocene/toggle-button/toggle-button.svelte
@@ -66,11 +66,11 @@
 
 <style lang="postcss">
   .toggle-button {
-    @apply flex items-center justify-center border-2  px-4 py-2 text-sm transition-colors hover:bg-gradient-to-br hover:from-blue-100 hover:to-purple-100 hover:text-primary focus:from-blue-100 focus:to-purple-100 focus:outline-none focus-visible:border-indigo-600 focus-visible:bg-gradient-to-br focus-visible:shadow-focus focus-visible:shadow-blue-600/50;
+    @apply flex items-center justify-center border-2 border-secondary px-4 py-2 text-sm text-primary hover:enabled:surface-interactive-secondary focus-visible:enabled:surface-interactive-secondary;
   }
 
   .toggle-button.active {
-    @apply bg-inverse text-white hover:text-primary focus-visible:text-primary;
+    @apply surface-secondary-active;
   }
 
   .group:first-child {
