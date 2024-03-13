dae6ad7de770e853bcdce77748838f788e5f1507
Roey Berman <roey.berman@gmail.com>
2024-03-12 17:31:11 -0700

    Delete unused src/lib/utilities/is-subrow-activity.ts (#1908)

--- src/lib/utilities/is-subrow-activity.ts
+++
@@ -1,7 +0,0 @@
-import type { IterableEvent } from '$lib/types/events';
-
-import { has } from './has';
-
-export const isSubrowActivity = (event: IterableEvent): boolean => {
-  return has(event?.attributes, 'workflowTaskCompletedEventId');
-};
