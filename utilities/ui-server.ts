f2df0eae39b2be601ec22660fe3ecdbb48740df8
Brendan Gerrity <brendan.gerrity@datadoghq.com>
2024-03-20 10:23:47 -0400

    remute ui server

--- utilities/ui-server.ts
+++ utilities/ui-server.ts
@@ -28,7 +28,7 @@ export const createUIServer = async (
 
   await $`make build`;
 
-  const uiServerProcess = $`./ui-server --env ${env} start`;
+  const uiServerProcess = $`./ui-server --env ${env} start`.quiet();
   console.log(`✨ ui-server running in ${env} mode on port ${portForEnv(env)}`);
 
   const shutdown = async () => {
