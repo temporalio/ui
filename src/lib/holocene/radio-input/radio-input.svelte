6e6024f8047b647c2b5cc4700e8333b2f6137de7
Laura Whitaker <laura.whitaker@temporal.io>
2024-04-09 12:47:53 -0600

    Audit radio input (#1966)
    * Add focus color
    
    * Fix text color in Radio Input story
    
    * Update style on hover and focus-visible
    
    * Add style for disabled state

--- src/lib/holocene/radio-input/radio-input.svelte
+++ src/lib/holocene/radio-input/radio-input.svelte
@@ -15,6 +15,7 @@
   export let label: string;
   export let description: string | undefined = undefined;
   export let labelHidden = false;
+  export let disabled = false;
 
   let internalGroup: Writable<T> = writable(value);
   let internalName: string = '';
@@ -31,7 +32,7 @@
 </script>
 
 <div class="flex flex-col gap-1">
-  <label>
+  <label class:disabled>
     <input
       bind:group={$group}
       type="radio"
@@ -39,6 +40,7 @@
       {name}
       {value}
       {id}
+      {disabled}
       {...$$restProps}
     />
     <span class="label" class:hidden={labelHidden}>
@@ -59,18 +61,40 @@
 
   label {
     @apply flex grow cursor-pointer flex-row items-center gap-2 text-sm font-normal text-primary focus:outline-none;
+
+    &.disabled {
+      @apply cursor-not-allowed;
+    }
   }
 
   input[type='radio'] {
-    @apply relative box-content h-4 w-4 min-w-[16px] cursor-pointer appearance-none rounded-full border border-2 border-subtle bg-transparent hover:border-white/30 hover:bg-interactive  hover:shadow-[inset_0_0_0_1px] hover:shadow-white hover:dark:border-black/30 hover:dark:shadow-black;
+    @apply relative box-content h-4 w-4 min-w-[16px] cursor-pointer appearance-none rounded-full border border-2 border-subtle bg-transparent outline outline-4 outline-transparent dark:text-black;
 
     &::after {
       @apply absolute left-1 top-1 h-0 w-0 scale-0 rounded-full bg-interactive transition-transform content-[''];
     }
+
+    &:enabled {
+      &:focus-visible,
+      &:hover {
+        @apply border-transparent border-white bg-interactive outline-offset-0 outline-focus dark:border-black;
+      }
+    }
+
+    &:disabled {
+      @apply cursor-not-allowed opacity-50;
+    }
   }
 
   input[type='radio']:checked {
-    @apply border-white/10 bg-interactive shadow-[inset_0_0_0_2px] shadow-white dark:shadow-black;
+    @apply bg-interactive shadow-[inset_0_0_0_2px] shadow-white dark:shadow-black;
+
+    &:enabled {
+      &:focus-visible,
+      &:hover {
+        @apply border-white shadow-none dark:border-black;
+      }
+    }
 
     &::after {
       @apply h-2 w-2 scale-100;
