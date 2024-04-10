2084b0deda07f4d9d76187c25d87c0c34b72a17e
Laura Whitaker <laura.whitaker@temporal.io>
2024-04-10 15:23:17 -0600

    Audit bulk action button

--- src/lib/holocene/table/bulk-action-button.svelte
+++ src/lib/holocene/table/bulk-action-button.svelte
@@ -15,15 +15,15 @@
 
 <style lang="postcss">
   .bulk-action-button {
-    @apply rounded border px-2 py-1 text-xs leading-3 text-black hover:text-black;
+    @apply rounded border px-2 py-1 text-xs leading-3 text-offWhite;
   }
 
   .bulk-action-button.primary {
-    @apply border-white bg-primary from-blue-100 to-purple-100 hover:border-primary hover:bg-gradient-to-br dark:hover:text-black;
+    @apply border-offWhite bg-offBlack from-blue-100 to-purple-100 hover:border-primary hover:bg-gradient-to-br hover:text-offBlack;
   }
 
   .bulk-action-button.destructive {
-    @apply border-danger bg-danger hover:border-red-900 hover:bg-red-900 hover:text-white;
+    @apply border-danger bg-danger hover:border-red-900 hover:bg-red-900;
   }
 
   .bulk-action-button:disabled {
