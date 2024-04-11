550c7c4141d66f2a8c18f99eb573365b83b8491a
grace <grace.gardner@temporal.io>
2024-04-11 00:23:25 -0600

    change varient back to primary

--- src/lib/components/lines-and-dots/event-sort-filter.svelte
+++ src/lib/components/lines-and-dots/event-sort-filter.svelte
@@ -57,7 +57,7 @@
   <MenuButton
     active={!!value}
     hasIndicator
-    variant="table-header"
+    variant="primary"
     controls="event-date-filter-menu"
     data-testid="event-date-filter-button"
   >
