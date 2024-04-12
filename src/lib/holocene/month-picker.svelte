6b858c68414275725c3e64f4604b49025128d3c4
Laura Whitaker <laura.whitaker@temporal.io>
2024-04-12 15:08:53 -0600

    Audit days of week and month (#1996)
    * Add back active style to Button
    
    * Update days of the week
    
    * Update days of the month

--- src/lib/holocene/month-picker.svelte
+++ src/lib/holocene/month-picker.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Button from '$lib/holocene/button.svelte';
   import { monthNames } from '$lib/utilities/calendar';
 
   export let months: string[];
@@ -21,22 +22,9 @@
 
 <div class="flex flex-wrap gap-4 text-center">
   {#each monthNames as { label, value }}
-    <button
-      class="cell"
-      class:active={months.includes(value)}
-      on:click|preventDefault={(e) => onClick(e, value)}
-    >
+    {@const active = months.includes(value)}
+    <Button variant="secondary" {active} on:click={(e) => onClick(e, value)}>
       {label}
-    </button>
+    </Button>
   {/each}
 </div>
-
-<style lang="postcss">
-  .cell {
-    @apply cursor-pointer rounded border-2 border-subtle bg-slate-300 from-blue-100 to-purple-100 p-2  hover:border-primary hover:bg-gradient-to-br hover:text-primary;
-  }
-
-  .active {
-    @apply bg-inverse text-white;
-  }
-</style>
