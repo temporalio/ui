d7b251a149ac07e935bd19e4baf2d71e4f818b6c
Alex Tideman <alex.tideman@gmail.com>
2024-04-18 10:37:30 -0500

    Fetch child workflows with ParentWorkflowId if Temporal server v1.23+ (#2013)
    * Fetch child workflows with ParentWorkflowId if 1.23+ and use new table
    
    * Refactor to fetchAllChildWorkflows

--- src/lib/components/workflow/workflow-relationships.svelte
+++ src/lib/components/workflow/workflow-relationships.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   import { page } from '$app/stores';
 
   import ChildWorkflowsTable from '$lib/components/workflow/child-workflows-table.svelte';
   import { translate } from '$lib/i18n/translate';
+  import { fetchAllChildWorkflows } from '$lib/services/workflow-service';
   import { fullEventHistory } from '$lib/stores/events';
   import { namespaces } from '$lib/stores/namespaces';
   import { workflowRun } from '$lib/stores/workflow-run';
+  import type { WorkflowExecution } from '$lib/types/workflows';
   import { getWorkflowRelationships } from '$lib/utilities/get-workflow-relationships';
 
   import FirstPreviousNextWorkflowTable from './first-previous-next-workflow-table.svelte';
+  import LiveChildWorkflowsTable from './live-child-workflows-table.svelte';
   import ParentWorkflowTable from './parent-workflow-table.svelte';
   import SchedulerTable from './scheduler-table.svelte';
 
   $: ({ workflow: workflowId, namespace } = $page.params);
   $: ({ workflow } = $workflowRun);
+
+  let liveChildren: WorkflowExecution[] = [];
+
+  onMount(async () => {
+    liveChildren = await fetchAllChildWorkflows(namespace, workflowId);
+  });
 
   $: workflowRelationships = getWorkflowRelationships(
     workflow,
@@ -52,7 +63,9 @@
         />
       {/if}
     </div>
-    {#if hasChildren}
+    {#if liveChildren.length}
+      <LiveChildWorkflowsTable children={liveChildren} />
+    {:else if hasChildren}
       <ChildWorkflowsTable
         {children}
         pendingChildren={$workflowRun.workflow.pendingChildren}
