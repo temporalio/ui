1c8eafea5fe6118209f3f586f3d47651f4469e4f
Laura Whitaker <laura.whitaker@temporal.io>
2024-04-18 16:29:45 -0600

    Make loading gif mp4 instead

--- src/lib/holocene/loading.svelte
+++ src/lib/holocene/loading.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
   import { translate } from '$lib/i18n/translate';
-  import Logo from '$lib/vendor/Temporal_Logo_Animation.gif';
+  import Logo from '$lib/vendor/Temporal_Logo_Animation.mp4';
 
   export let title = translate('common.loading');
 </script>
 
-<div class="my-12 flex flex-col items-center justify-start {$$props.class}">
-  <img
-    src={Logo}
-    style="margin-top: -40px;"
-    alt=""
-    width="200px"
-    height="200px"
-  />
-  <h2 class="text-xl font-medium" style="margin-top: -40px;">
+<div
+  class="my-12 flex flex-col items-center justify-start gap-4 {$$props.class}"
+>
+  <video
+    autoplay
+    loop
+    muted
+    playsinline
+    width="100"
+    height="100"
+    class="dark:invert-[.92]"
+  >
+    <source src={Logo} type="video/mp4" />
+  </video>
+  <h2 class="text-xl font-medium">
     {title}
   </h2>
 </div>
