57f69d62549507405f69cbc45f1b6cdeb931ab56
Steve Kinney <hello@stevekinney.net>
2024-04-22 16:50:03 -0600

    Update split-button.stories.svelte

--- src/lib/holocene/split-button.stories.svelte
+++ src/lib/holocene/split-button.stories.svelte
@@ -1,26 +1,61 @@
-<script lang="ts">
-  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+<script lang="ts" context="module">
+  import type { Meta } from '@storybook/svelte';
 
-  import iconNames from '$lib/holocene/icon';
+  import { iconNames } from '$lib/holocene/icon';
   import MenuItem from '$lib/holocene/menu/menu-item.svelte';
   import SplitButton from '$lib/holocene/split-button.svelte';
+
+  export const meta: Meta = {
+    title: 'Split Button',
+    component: SplitButton,
+    args: {
+      label: 'Split Button',
+      menuLabel: 'Actions',
+      variant: 'Primary',
+      position: 'left',
+      icon: undefined,
+      disabled: false,
+      primaryActionDisabled: false,
+    },
+    argTypes: {
+      position: {
+        name: 'Position',
+        control: 'radio',
+        options: ['left', 'right'],
+      },
+      label: { name: 'Label', control: 'text' },
+      menuLabel: {
+        name: 'Menu Label',
+        control: 'text',
+        table: {
+          category: 'Accessibility',
+        },
+      },
+      id: { control: 'text', table: { disable: true } },
+      icon: { name: 'Icon', control: 'select', options: iconNames },
+      variant: {
+        name: 'Variant',
+        control: 'select',
+        options: ['Primary', 'Secondary', 'Destructive', 'Ghost'],
+        mapping: {
+          Primary: 'primary',
+          Secondary: 'secondary',
+          Destructive: 'destructive',
+          Ghost: 'ghost',
+        },
+      },
+      disabled: { name: 'Disabled', control: 'boolean' },
+      primaryActionDisabled: {
+        name: 'Primary Action Disabled',
+        control: 'boolean',
+      },
+    },
+  } satisfies Meta;
 </script>
 
-<Meta
-  title="Split Button"
-  component={SplitButton}
-  argTypes={{
-    position: { control: 'radio', options: ['left', 'right'] },
-    label: { control: 'text' },
-    menuLabel: { control: 'text' },
-    id: { control: 'text' },
-    icon: { control: 'select', options: iconNames },
-    variant: {
-      control: 'select',
-      options: ['primary', 'secondary', 'destructive', 'ghost'],
-    },
-  }}
-/>
+<script lang="ts">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
 
 <Template let:args>
   <SplitButton {...args}>
@@ -29,13 +64,227 @@
   </SplitButton>
 </Template>
 
+<Story name="Primary" />
+
+<Story name="Primary Icon" args={{ icon: 'trash' }} />
+
 <Story
+  name="Primary with Right-Positioned Icon"
   args={{
-    label: 'Edit',
-    menuLabel: 'Actions',
-    id: 'button-1',
-    variant: 'primary',
-    position: 'left',
+    icon: 'trash',
+    position: 'right',
   }}
-  name="split button"
+/>
+
+<Story name="Primary, Disabled" args={{ disabled: true }} />
+
+<Story
+  name="Primary, Disabled with Icon"
+  args={{ icon: 'trash', disabled: true }}
+/>
+
+<Story name="Secondary" args={{ variant: 'Secondary' }} />
+
+<Story
+  name="Secondary with Icon"
+  args={{ variant: 'Secondary', icon: 'trash' }}
+/>
+
+<Story
+  name="Secondary with Right-Positioned Icon"
+  args={{
+    variant: 'Secondary',
+    icon: 'trash',
+    position: 'right',
+  }}
+/>
+
+<Story
+  name="Secondary, Disabled"
+  args={{ variant: 'Secondary', disabled: true }}
+/>
+
+<Story
+  name="Secondary, Disabled with Icon"
+  args={{ icon: 'trash', variant: 'Secondary', disabled: true }}
+/>
+
+<Story name="Destructive" args={{ variant: 'Destructive' }} />
+
+<Story
+  name="Destructive with Icon"
+  args={{ variant: 'Destructive', icon: 'trash' }}
+/>
+
+<Story
+  name="Destructive with Right-Positioned Icon"
+  args={{
+    variant: 'Destructive',
+    icon: 'trash',
+    position: 'right',
+  }}
+/>
+
+<Story
+  name="Destructive, Disabled"
+  args={{ variant: 'Destructive', disabled: true }}
+/>
+
+<Story
+  name="Destructive, Disabled with Icon"
+  args={{ icon: 'trash', variant: 'Destructive', disabled: true }}
+/>
+
+<Story name="Ghost" args={{ variant: 'Ghost' }} />
+
+<Story name="Ghost with Icon" args={{ variant: 'Ghost', icon: 'trash' }} />
+
+<Story
+  name="Ghost with Right-Positioned Icon"
+  args={{
+    variant: 'Ghost',
+    icon: 'trash',
+    position: 'right',
+  }}
+/>
+
+<Story name="Ghost, Disabled" args={{ variant: 'Ghost', disabled: true }} />
+
+<Story
+  name="Ghost, Disabled with Icon"
+  args={{ icon: 'trash', variant: 'Ghost', disabled: true }}
+/>
+
+<Story
+  name="Primary (Dark)"
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Primary with Icon (Dark)"
+  args={{ icon: 'trash' }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Primary with Right-Positioned Icon (Dark)"
+  args={{
+    icon: 'trash',
+    position: 'right',
+  }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Primary, Disabled (Dark)"
+  args={{ disabled: true }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Primary, Disabled with Icon (Dark)"
+  args={{ icon: 'trash', disabled: true }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Secondary (Dark)"
+  args={{ variant: 'Secondary' }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Secondary with Icon (Dark)"
+  args={{ variant: 'Secondary', icon: 'trash' }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Secondary with Right-Positioned Icon (Dark)"
+  args={{
+    variant: 'Secondary',
+    icon: 'trash',
+    position: 'right',
+  }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Secondary, Disabled (Dark)"
+  args={{ variant: 'Secondary', disabled: true }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Secondary, Disabled with Icon (Dark)"
+  args={{ icon: 'trash', variant: 'Secondary', disabled: true }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Destructive (Dark)"
+  args={{ variant: 'Destructive' }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Destructive with Icon (Dark)"
+  args={{ variant: 'Destructive', icon: 'trash' }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Destructive with Right-Positioned Icon (Dark)"
+  args={{
+    variant: 'Destructive',
+    icon: 'trash',
+    position: 'right',
+  }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Destructive, Disabled (Dark)"
+  args={{ variant: 'Destructive', disabled: true }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Destructive, Disabled with Icon (Dark)"
+  args={{ icon: 'trash', variant: 'Destructive', disabled: true }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Ghost (Dark)"
+  args={{ variant: 'Ghost' }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Ghost with Icon (Dark)"
+  args={{ variant: 'Ghost', icon: 'trash' }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Ghost with Right-Positioned Icon (Dark)"
+  args={{
+    variant: 'Ghost',
+    icon: 'trash',
+    position: 'right',
+  }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Ghost, Disabled (Dark)"
+  args={{ variant: 'Ghost', disabled: true }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
+/>
+
+<Story
+  name="Ghost, Disabled with Icon (Dark)"
+  args={{ icon: 'trash', variant: 'Ghost', disabled: true }}
+  parameters={{ themes: { themeOverride: 'dark' } }}
 />
