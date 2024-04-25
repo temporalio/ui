8a1cc2789937a3d5ba8bc311186d57c35e3fb4ec
Laura Whitaker <laura.whitaker@temporal.io>
2024-04-25 13:16:49 -0600

    Add autoplay check to loading component

--- src/lib/holocene/loading.svelte
+++ src/lib/holocene/loading.svelte
@@ -1,27 +1,54 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
+  import Logo from '$lib/holocene/logo.svelte';
   import { translate } from '$lib/i18n/translate';
   import mp4Logo from '$lib/vendor/Temporal_Logo_Animation.mp4';
   import webmLogo from '$lib/vendor/Temporal_Logo_Animation.webm';
 
   export let title = translate('common.loading');
+
+  let isAutoplayAllowed = true;
+  let videoElement: HTMLVideoElement;
+
+  onMount(async () => {
+    const autoplay = videoElement?.play();
+
+    if (autoplay !== undefined) {
+      try {
+        await autoplay;
+      } catch (e) {
+        if (videoElement) {
+          isAutoplayAllowed = false;
+        }
+      }
+    } else {
+      isAutoplayAllowed = false;
+    }
+  });
 </script>
 
 <div
   class="my-12 flex flex-col items-center justify-start gap-4 {$$props.class}"
 >
-  <video
-    autoplay
-    loop
-    muted
-    playsinline
-    preload="auto"
-    height="160"
-    width="160"
-    class="dark:invert"
-  >
-    <source src={mp4Logo} type="video/mp4;codecs=hvc1" />
-    <source src={webmLogo} type="video/webm" />
-  </video>
+  {#if isAutoplayAllowed}
+    <video
+      autoplay
+      loop
+      muted
+      playsinline
+      preload="auto"
+      height="160"
+      width="160"
+      class="dark:invert"
+      bind:this={videoElement}
+    >
+      <source src={mp4Logo} type="video/mp4;codecs=hvc1" />
+      <source src={webmLogo} type="video/webm" />
+    </video>
+  {:else}
+    <Logo height={160} width={160} />
+  {/if}
   <h2 class="text-xl font-medium">
     {title}
   </h2>
