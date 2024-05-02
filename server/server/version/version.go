79de54230bbee7a8bc4eaebfdb2b5f4550db3875
Alex Tideman <alex.tideman@gmail.com>
2024-05-02 13:50:13 -0500

    Update version.go to v2.27.0

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.26.2"
+	UIVersion               = "2.27.0"
 )
