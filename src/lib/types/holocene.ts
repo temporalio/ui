ee1e45d46c50a051e4a4da3685abae3397cebdc0
Steve Kinney <hello@stevekinney.net>
2024-05-03 16:05:17 -0600

    Update holocene.ts

--- src/lib/types/holocene.ts
+++ src/lib/types/holocene.ts
@@ -3,6 +3,8 @@ export type DataAttributes = {
   'data-testid'?: string;
 };
 
+export type ToastVariant = 'success' | 'error' | 'info' | 'warning' | 'primary';
+
 export interface Toast {
   message: string;
   variant?: ToastVariant;
