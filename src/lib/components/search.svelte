f6abd9e37ba3f2f35a94719454502477901e5996
Steve Kinney <hello@stevekinney.net>
2024-05-03 17:59:34 -0600

    Improve the search box

--- src/lib/components/search.svelte
+++ src/lib/components/search.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
+  import kebabCase from 'kebab-case';
+
   import Button from '$lib/holocene/button.svelte';
   import Input from '$lib/holocene/input/input.svelte';
   import { translate } from '$lib/i18n/translate';
 
-  export let placeholder = '';
-  export let value = '';
+  export let placeholder = `${translate('common.search')}…`;
   export let label = translate('common.search');
+  export let value = '';
   export let name = 'query';
   export let icon = false;
-  export let id = `${label.toLocaleUpperCase()}-input`;
+  export let id = kebabCase(`${label}-${name}`);
+
+  let testId = $$props['data-testid'] || id;
 </script>
 
-<form on:submit|preventDefault role="search" class="flex items-center">
+<form
+  on:submit|preventDefault
+  role="search"
+  class="flex items-center"
+  data-testid={`${testId}-form`}
+>
   <Input
     class="w-full"
     {label}
@@ -23,9 +32,10 @@
     {value}
     {placeholder}
     autocomplete="off"
+    data-testid={`${testId}-input`}
     on:input
   >
-    <Button slot="after-input" type="submit">
+    <Button slot="after-input" type="submit" data-testid={`${testId}-button`}>
       {label}
     </Button>
   </Input>
