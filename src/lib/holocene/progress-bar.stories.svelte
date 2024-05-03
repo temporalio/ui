d886ecba389dd1e40fd30a0f16509b6aba330f56
Steve Kinney <hello@stevekinney.net>
2024-05-03 12:37:58 -0600

    Create progress-bar.stories.svelte

---
+++ src/lib/holocene/progress-bar.stories.svelte
@@ -0,0 +1,24 @@
+<script lang="ts" context="module">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+
+  import ProgressBar from '$lib/holocene/progress-bar.svelte';
+
+  export const meta = {
+    title: 'Progress Bar',
+    component: ProgressBar,
+    args: {
+      width: '50%',
+    },
+  } as Meta<ProgressBar>;
+</script>
+
+<Template let:args>
+  <div class="relative mx-auto p-8">
+    <ProgressBar {...args} />
+  </div>
+</Template>
+
+<Story name="Partial" args={{ width: '60%' }} />
+<Story name="Full" args={{ width: '100%' }} />
+<Story name="Empty" args={{ width: '0%' }} />
