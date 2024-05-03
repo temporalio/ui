975cdbe32cd293223874ae80fd6f31c1f3347404
Steve Kinney <hello@stevekinney.net>
2024-05-03 13:59:35 -0600

    Create search.stories.svelte

---
+++ src/lib/components/search.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" context="module">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+  import type { Meta } from '@storybook/svelte';
+
+  import Search from './search.svelte';
+
+  export const meta = {
+    title: 'Holocene/Search',
+    component: Search,
+  } satisfies Meta<Search>;
+</script>
+
+<Template let:args>
+  <Search {...args} />
+</Template>
+
+<Story name="Default" />
