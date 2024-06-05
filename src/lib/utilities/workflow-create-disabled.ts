1791a7b885166065a14f664d81ad9cf0970ea5b6
Alex Tideman <alex.tideman@gmail.com>
2024-06-05 09:55:24 -0500

    Add flag check in workflowCreateDisabled

--- src/lib/utilities/workflow-create-disabled.ts
+++ src/lib/utilities/workflow-create-disabled.ts
@@ -12,5 +12,9 @@ export const workflowCreateDisabled = (
   const namespaceWriteDisabled = get(coreUser).namespaceWriteDisabled(
     namespace ?? page.params.namespace,
   );
-  return page.data.settings.disableWriteActions || namespaceWriteDisabled;
+  return (
+    page.data.settings.disableWriteActions ||
+    page.data.settings.startWorkflowDisabled ||
+    namespaceWriteDisabled
+  );
 };
