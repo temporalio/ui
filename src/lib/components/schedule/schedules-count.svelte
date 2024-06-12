56ab7cb5bdade81e863cd9c6ba0e6fdef392f9a4
Alex Tideman <alex.tideman@gmail.com>
2024-06-12 08:45:49 -0500

    Add plural translation and remove loading

--- src/lib/components/schedule/schedules-count.svelte
+++ src/lib/components/schedule/schedules-count.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
   import { page } from '$app/stores';
 
-  import Skeleton from '$lib/holocene/skeleton/index.svelte';
   import { translate } from '$lib/i18n/translate';
   import { fetchScheduleCount } from '$lib/services/workflow-counts';
   import { schedulesCount } from '$lib/stores/schedules';
 
   $: namespace = $page.params.namespace;
 
-  let loading = false;
-
   const fetchCounts = async () => {
-    loading = true;
     try {
       $schedulesCount = await fetchScheduleCount({
         namespace,
       });
     } catch (e) {
       console.error('Fetching schedules count failed: ', e?.message);
-    } finally {
-      loading = false;
     }
   };
 
@@ -27,10 +21,5 @@
 </script>
 
 <div class="flex flex-wrap items-center gap-2">
-  {#if !loading}
-    {$schedulesCount}
-  {:else}
-    <Skeleton class="h-6 w-6 rounded" />
-  {/if}
-  {translate('common.schedules')}
+  {translate('common.schedules-plural', { count: Number($schedulesCount) })}
 </div>
