6e0cf87f3771c409f4de42bc32f4d052166a7408
Alex Thomsen <alex.scott.thomsen@gmail.com>
2024-06-14 12:14:47 -0600

    Buf stuff 💪🌮💪

--- src/routes/(app)/namespaces/[namespace]/schedules/[schedule]/+page.svelte
+++ src/routes/(app)/namespaces/[namespace]/schedules/[schedule]/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+  import { CloudService } from '@buf/temporal_api-cloud.connectrpc_es/temporal/api/cloud/cloudservice/v1/service_connect';
+  import { createPromiseClient } from '@connectrpc/connect';
+  import { createGrpcWebTransport } from '@connectrpc/connect-web';
+
   import { page } from '$app/stores';
 
   import PageTitle from '$lib/components/page-title.svelte';
   import ScheduleView from '$lib/pages/schedule-view.svelte';
 
   const schedule = $page.params.schedule;
+
+  const transport = createGrpcWebTransport({ baseUrl: '', interceptors: [] });
+
+  const cloudService = createPromiseClient(CloudService, transport);
 </script>
 
 <PageTitle title={`Schedule | ${schedule}`} url={$page.url.href} />
