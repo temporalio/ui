7a28811e7620c8839b69eae9868b60dc8b0b5bec
Alex Tideman <alex.tideman@gmail.com>
2024-07-01 15:52:54 -0500

    bump version in package.json to v2.27.11 (#2175)
    * 2.27.11
    
    * Update fire extinguisher icon
    
    ---------
    
    Co-authored-by: Temporal Data (cicd) <commander-data@temporal.io>
    Co-authored-by: Laura Whitaker <laura.whitaker@temporal.io>

--- src/lib/holocene/icon/paths.ts
+++ src/lib/holocene/icon/paths.ts
@@ -44,7 +44,7 @@ import fileImport from './svg/file-import.svelte';
 import fileUpload from './svg/file-upload.svelte';
 import filterSolid from './svg/filter-solid.svelte';
 import filter from './svg/filter.svelte';
-import fireHydrant from './svg/fire-hydrant.svelte';
+import fireExtinguisher from './svg/fire-extinguisher.svelte';
 import github from './svg/github.svelte';
 import graph from './svg/graph.svelte';
 import hyphen from './svg/hyphen.svelte';
@@ -144,7 +144,7 @@ export const icons = {
   'file-upload': fileUpload,
   filter,
   'filter-solid': filterSolid,
-  'fire-hydrant': fireHydrant,
+  'fire-extinguisher': fireExtinguisher,
   github,
   graph,
   hyphen,
