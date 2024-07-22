efb8973f1dd68a84f87d3939fd78e939a125cc04
Alex Tideman <alex.tideman@gmail.com>
2024-07-22 08:37:49 -0500

    Return 404 page for endpoint not found. Use endpoint name on delete confirmation and remove the need to type DELETE.

--- src/routes/(app)/nexus/[id]/+layout.ts
+++ src/routes/(app)/nexus/[id]/+layout.ts
@@ -3,19 +3,28 @@ import { decodeSingleReadablePayloadWithCodec } from '$lib/utilities/decode-payl
 
 export const load = async ({ params, fetch, parent }) => {
   const { id } = params;
-  const { endpoint } = await fetchNexusEndpoint(id, fetch);
-  const data = await parent();
 
-  let description = '';
-  if (endpoint?.spec?.description?.data) {
-    const decodedDescription = await decodeSingleReadablePayloadWithCodec(
-      endpoint.spec.description,
-      data.settings,
-    );
-    description = decodedDescription;
+  try {
+    const { endpoint } = await fetchNexusEndpoint(id, fetch);
+    const data = await parent();
+
+    let description = '';
+    try {
+      if (endpoint?.spec?.description?.data) {
+        const decodedDescription = await decodeSingleReadablePayloadWithCodec(
+          endpoint.spec.description,
+          data.settings,
+        );
+        description = decodedDescription;
+      }
+    } catch (e) {
+      console.error('Error decoding Nexus Endpoint description:', e);
+    }
+    endpoint.spec.descriptionString = description;
+    return {
+      endpoint,
+    };
+  } catch (e) {
+    console.error('Error loading Nexus Endpoint:', e);
   }
-  endpoint.spec.descriptionString = description;
-  return {
-    endpoint,
-  };
 };
