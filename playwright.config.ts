084ad9717a308bd750639e3ea920f6a325aa3f4c
Alex Tideman <alex.tideman@gmail.com>
2024-08-12 09:30:26 -0500

    Try a new port

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 const PLAYWRIGHT_MODE = process.env.PW_MODE;
-const PORT = PLAYWRIGHT_MODE === 'e2e' ? 3000 : 3333;
+const PORT = PLAYWRIGHT_MODE === 'e2e' ? 3001 : 3333;
 
 export default defineConfig({
   testDir: './tests',
@@ -12,7 +12,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 3 : 1,
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   reporter: [
     ['html'],
     ['json', { outputFile: 'playwright-report/test-results.json' }],
