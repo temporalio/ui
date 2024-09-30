a1d2cff90906da3a03ce8d700f3b3d4b75a2dd8d
Ross Edfort <rossedfort@gmail.com>
2024-09-30 13:15:37 -0600

    update UI server version to v2.31.1 (#2358)

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.31.0"
+	UIVersion               = "2.31.1"
 )
