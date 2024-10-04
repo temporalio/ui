9448730d0ceea549e59042696b6ba56a68ce9879
Laura Whitaker <laura.whitaker@temporal.io>
2024-10-04 11:35:04 -0600

    Remove unnecessary Icon class

--- src/lib/holocene/combobox/combobox.svelte
+++ src/lib/holocene/combobox/combobox.svelte
@@ -339,7 +339,7 @@
 
   <div class="combobox-wrapper" class:disabled class:invalid={!valid}>
     {#if leadingIcon}
-      <Icon width={20} height={20} class="ml-2 shrink-0" name={leadingIcon} />
+      <Icon width={20} height={20} class="ml-2" name={leadingIcon} />
     {/if}
     <div
       class="input-wrapper"
