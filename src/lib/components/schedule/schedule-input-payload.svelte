605e906d7e20f18ace87e2c55783d9bd7450c671
Laura Whitaker <laura.whitaker@temporal.io>
2024-10-11 14:18:44 -0600

    Show correct encoding value in PayloadInput (#2385)
    * Prevent remount of PayloadInput after load which triggers clearValues
    
    * Default to json/plain encoding type if Codec Server is enabled

--- src/lib/components/schedule/schedule-input-payload.svelte
+++ src/lib/components/schedule/schedule-input-payload.svelte
@@ -7,6 +7,7 @@
 
   import PayloadDecoder from '../event/payload-decoder.svelte';
   import PayloadInput, {
+    isPayloadInputEncodingType,
     type PayloadInputEncoding,
   } from '../payload-input.svelte';
 
@@ -18,17 +19,18 @@
 
   const setInitialInput = (decodedValue: string): void => {
     input = getSinglePayload(decodedValue);
-    $encoding = atob(
+    const currentEncoding = atob(
       String(payloads?.payloads[0]?.metadata?.encoding ?? 'json/plain'),
-    ) as PayloadInputEncoding;
+    );
+    if (isPayloadInputEncodingType(currentEncoding)) {
+      $encoding = currentEncoding;
+    }
     loading = false;
   };
 </script>
 
 <div class="flex flex-col gap-1">
   <PayloadDecoder value={payloads} key="payloads" onDecode={setInitialInput}>
-    {#key loading}
-      <PayloadInput bind:input bind:encoding />
-    {/key}
+    <PayloadInput bind:input bind:encoding bind:loading />
   </PayloadDecoder>
 </div>
