2a002f52c2dcba9b61506510f9cc002d66f1bd23
Laura Whitaker <laura.whitaker@temporal.io>
2024-10-16 07:28:31 -0600

    Update Banner component

--- src/lib/holocene/banner/banner.svelte
+++ src/lib/holocene/banner/banner.svelte
@@ -1,32 +1,96 @@
 <script lang="ts" context="module">
+  import { cva, type VariantProps } from 'class-variance-authority';
+
   import { persistStore } from '$lib/stores/persist-store';
-  export const bannerClosed = persistStore<boolean>('bannerClosed', false);
+
+  export const dismissedBanners = persistStore<Record<string, boolean>>(
+    'dismissed-banners',
+    {},
+  );
+  export type BannerType = VariantProps<typeof types>['type'];
+
+  const type = {
+    default: 'bg-indigo-200 text-indigo-900',
+    danger: 'surface-danger',
+  };
+  const types = cva(
+    [
+      'w-full',
+      'md:sticky md:top-12',
+      'max-md:fixed max-md:bottom-16',
+      'z-[39]',
+      'flex items-center justify-between gap-2',
+      'px-4 py-2',
+    ],
+    {
+      variants: {
+        type,
+      },
+      defaultVariants: {
+        type: 'default',
+      },
+    },
+  );
 </script>
 
 <script lang="ts">
-  import IconButton from '$lib/holocene/icon-button.svelte';
+  import { twMerge as merge } from 'tailwind-merge';
 
+  import Button from '$lib/holocene/button.svelte';
+  import Icon from '$lib/holocene/icon/icon.svelte';
+
+  import type { IconName } from '../icon';
+
+  type BaseProps = {
+    id: string;
+    message: string;
+    dismissible?: boolean;
+    icon: IconName | null;
+    type: BannerType;
+    class?: string;
+  };
+
+  type DismissibleBanner = BaseProps & {
+    dismissable: true;
+    dismissLabel: string;
+  };
+
+  type $$Props = BaseProps | DismissibleBanner;
+
+  export let id: string;
   export let message: string;
-  export let closeLabel: string;
+  export let dismissLabel: string = '';
+  export let dismissable = false;
+  export let icon: IconName = null;
+  export let type: BannerType = 'default';
 
-  $: show = message && !$bannerClosed;
+  let className = '';
+  export { className as class };
+
+  $: show = message && !$dismissedBanners[id];
+
+  const dismissBanner = () => {
+    $dismissedBanners[id] = true;
+  };
 </script>
 
 {#if show}
-  <section class="banner" {...$$restProps}>
-    {message}
-    <IconButton
-      on:click={() => ($bannerClosed = true)}
-      data-testid="close-banner"
-      class="text-black-600 absolute right-0 top-0 mr-5 h-10 w-10"
-      icon="close"
-      label={closeLabel}
-    />
+  <section class={merge(types({ type }), className)} {...$$restProps}>
+    <span class="flex items-center gap-2">
+      {#if icon}
+        <Icon name={icon} />
+      {/if}
+      {message}
+    </span>
+    {#if dismissable}
+      <Button
+        on:click={dismissBanner}
+        leadingIcon="close"
+        variant="ghost"
+        size="xs"
+      >
+        <span class="sr-only">{dismissLabel}</span>
+      </Button>
+    {/if}
   </section>
 {/if}
-
-<style lang="postcss">
-  .banner {
-    @apply relative block flex items-center justify-center bg-indigo-200 text-center leading-10 text-indigo-900;
-  }
-</style>
