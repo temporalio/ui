0c54beca663d85dbc2f2208243f1fc950e4a5296
Laura Whitaker <laura.whitaker@temporal.io>
2024-10-22 12:07:33 -0600

    Add FileInput story

---
+++ src/lib/holocene/file-input.stories.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" context="module">
+  import type { Meta } from '@storybook/svelte';
+
+  export const meta = {
+    title: 'File Input',
+    component: FileInput,
+    args: {
+      id: 'file-input',
+      accept: '.json',
+    },
+    argTypes: {
+      id: { control: 'text' },
+      accept: { control: 'text' },
+    },
+  } satisfies Meta<FileInput>;
+</script>
+
+<script lang="ts">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+
+  import FileInput from './file-input.svelte';
+</script>
+
+<Template let:args>
+  <FileInput {...args} />
+</Template>
+
+<Story name="Default" />
