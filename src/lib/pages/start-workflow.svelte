1c40f873728d33ec67fe8c0f63429693ce1d8f8c
Alex Tideman <alex.tideman@gmail.com>
2024-10-22 10:15:47 -0500

    Add toast instead of redirect on start workflow with link to workflow

--- src/lib/pages/start-workflow.svelte
+++ src/lib/pages/start-workflow.svelte
@@ -4,7 +4,6 @@
   import { onMount } from 'svelte';
   import { v4 } from 'uuid';
 
-  import { goto } from '$app/navigation';
   import { page } from '$app/stores';
 
   import PayloadInput, {
@@ -15,7 +14,6 @@
   import Button from '$lib/holocene/button.svelte';
   import Input from '$lib/holocene/input/input.svelte';
   import Link from '$lib/holocene/link.svelte';
-  import Loading from '$lib/holocene/loading.svelte';
   import { translate } from '$lib/i18n/translate';
   import { getPollers } from '$lib/services/pollers-service';
   import {
@@ -26,9 +24,11 @@
     customSearchAttributes,
     type SearchAttributeInput,
   } from '$lib/stores/search-attributes';
+  import { toaster } from '$lib/stores/toaster';
   import { workflowsSearchParams } from '$lib/stores/workflows';
   import { pluralize } from '$lib/utilities/pluralize';
   import {
+    routeForEventHistory,
     routeForTaskQueue,
     routeForWorkflows,
   } from '$lib/utilities/route-for';
@@ -36,8 +36,6 @@
   import { workflowCreateDisabled } from '$lib/utilities/workflow-create-disabled';
 
   $: ({ namespace } = $page.params);
-
-  let loading = false;
 
   let workflowId = '';
   let taskQueue = '';
@@ -70,10 +68,9 @@
   });
 
   const onWorkflowStart = async () => {
-    loading = true;
     try {
       error = '';
-      await startWorkflow({
+      const { runId } = await startWorkflow({
         namespace,
         workflowId,
         taskQueue,
@@ -82,11 +79,22 @@
         encoding: $encoding,
         searchAttributes,
       });
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      goto(routeForWorkflows({ namespace }));
+      toaster.push({
+        variant: 'success',
+        duration: 5000,
+        message: translate('workflows.start-workflow-success'),
+        link: routeForEventHistory({
+          namespace,
+          workflow: workflowId,
+          run: runId,
+        }),
+      });
     } catch (e) {
-      error = e?.message || 'Error starting Workflow';
-      loading = false;
+      toaster.push({
+        variant: 'error',
+        duration: 5000,
+        message: e?.message || translate('workflows.start-workflow-error'),
+      });
     }
   };
 
@@ -161,103 +169,97 @@
 </script>
 
 <div class="flex w-full flex-col items-center pb-24">
-  {#if loading}
-    <Loading title={translate('workflows.starting-workflow')} />
-  {:else}
-    <div class="mb-6 flex w-full items-start">
-      <Link
-        href={`${routeForWorkflows({
-          namespace,
-        })}?${$workflowsSearchParams}`}
-        data-testid="back-to-workflows"
-        icon="chevron-left"
-      >
-        {translate('workflows.back-to-workflows')}
-      </Link>
-    </div>
-    <div class="flex w-full flex-col gap-4 lg:w-2/3 2xl:w-1/2">
-      <h1 class="mb-4 overflow-hidden" data-testid="start-workflow">
-        Start a Workflow
-      </h1>
-      <div
-        class="flex w-full flex-col items-center justify-between gap-2 md:flex-row md:gap-4"
-      >
-        <Input
-          id="workflowId"
-          required
-          bind:value={workflowId}
-          label="Workflow ID"
-          class="w-full grow"
-          on:blur={(e) => onInputChange(e, 'workflowId')}
-        />
-        <Button
-          class="mt-0 md:mt-6"
-          variant="secondary"
-          leadingIcon="retry"
-          on:click={generateRandomWorkflowId}>Random UUID</Button
-        >
-      </div>
-      <div class="flex w-full items-center justify-between gap-4">
-        <Input
-          id="taskQueue"
-          required
-          bind:value={taskQueue}
-          label="Task Queue"
-          class="grow"
-          on:blur={(e) => onInputChange(e, 'taskQueue')}
-        />
-      </div>
-      {#if pollerCount !== undefined}
-        <Alert
-          intent={pollerCount > 0 ? 'success' : 'warning'}
-          title={pollerCount
-            ? 'Task Queue is Active'
-            : 'Task Queue is Inactive'}
-        >
-          <div class="flex w-full items-center justify-between">
-            <p>
-              {pollerCount}
-              {pluralize('Worker', pollerCount)}
-            </p>
-            <Link
-              href={routeForTaskQueue({ namespace, queue: taskQueue })}
-              newTab
-            >
-              View Task Queue
-            </Link>
-          </div></Alert
-        >
-      {/if}
+  <div class="mb-6 flex w-full items-start">
+    <Link
+      href={`${routeForWorkflows({
+        namespace,
+      })}?${$workflowsSearchParams}`}
+      data-testid="back-to-workflows"
+      icon="chevron-left"
+    >
+      {translate('workflows.back-to-workflows')}
+    </Link>
+  </div>
+  <div class="flex w-full flex-col gap-4 lg:w-2/3 2xl:w-1/2">
+    <h1 class="mb-4 overflow-hidden" data-testid="start-workflow">
+      Start a Workflow
+    </h1>
+    <div
+      class="flex w-full flex-col items-center justify-between gap-2 md:flex-row md:gap-4"
+    >
       <Input
-        id="workflowType"
+        id="workflowId"
         required
-        bind:value={workflowType}
-        label="Workflow Type"
-        on:blur={(e) => onInputChange(e, 'workflowType')}
+        bind:value={workflowId}
+        label="Workflow ID"
+        class="w-full grow"
+        on:blur={(e) => onInputChange(e, 'workflowId')}
       />
-      {#key inputRetrieved}
-        <PayloadInput bind:input bind:encoding />
-      {/key}
-      {#if viewAdvancedOptions}
-        <AddSearchAttributes bind:attributesToAdd={searchAttributes} />
-      {/if}
-      <div class="mt-4 flex w-full justify-between">
-        <Button
-          variant="ghost"
-          trailingIcon={viewAdvancedOptions ? 'chevron-up' : 'chevron-down'}
-          on:click={() => (viewAdvancedOptions = !viewAdvancedOptions)}
-          >{translate('common.more-options')}</Button
-        >
-        <Button
-          disabled={!enableStart}
-          on:click={onWorkflowStart}
-          data-testid="start-workflow-button"
-          >{translate('workflows.start-workflow')}</Button
-        >
-      </div>
-      {#if error}
-        <Alert intent="error" title={error} />
-      {/if}
+      <Button
+        class="mt-0 md:mt-6"
+        variant="secondary"
+        leadingIcon="retry"
+        on:click={generateRandomWorkflowId}>Random UUID</Button
+      >
     </div>
-  {/if}
+    <div class="flex w-full items-center justify-between gap-4">
+      <Input
+        id="taskQueue"
+        required
+        bind:value={taskQueue}
+        label="Task Queue"
+        class="grow"
+        on:blur={(e) => onInputChange(e, 'taskQueue')}
+      />
+    </div>
+    {#if pollerCount !== undefined}
+      <Alert
+        intent={pollerCount > 0 ? 'success' : 'warning'}
+        title={pollerCount ? 'Task Queue is Active' : 'Task Queue is Inactive'}
+      >
+        <div class="flex w-full items-center justify-between">
+          <p>
+            {pollerCount}
+            {pluralize('Worker', pollerCount)}
+          </p>
+          <Link
+            href={routeForTaskQueue({ namespace, queue: taskQueue })}
+            newTab
+          >
+            View Task Queue
+          </Link>
+        </div></Alert
+      >
+    {/if}
+    <Input
+      id="workflowType"
+      required
+      bind:value={workflowType}
+      label="Workflow Type"
+      on:blur={(e) => onInputChange(e, 'workflowType')}
+    />
+    {#key inputRetrieved}
+      <PayloadInput bind:input bind:encoding />
+    {/key}
+    {#if viewAdvancedOptions}
+      <AddSearchAttributes bind:attributesToAdd={searchAttributes} />
+    {/if}
+    <div class="mt-4 flex w-full justify-between">
+      <Button
+        variant="ghost"
+        trailingIcon={viewAdvancedOptions ? 'chevron-up' : 'chevron-down'}
+        on:click={() => (viewAdvancedOptions = !viewAdvancedOptions)}
+        >{translate('common.more-options')}</Button
+      >
+      <Button
+        disabled={!enableStart}
+        on:click={onWorkflowStart}
+        data-testid="start-workflow-button"
+        >{translate('workflows.start-workflow')}</Button
+      >
+    </div>
+    {#if error}
+      <Alert intent="error" title={error} />
+    {/if}
+  </div>
 </div>
