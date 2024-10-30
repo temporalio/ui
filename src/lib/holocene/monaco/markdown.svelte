cc3c77d2a4aca40399d09a7af0ab4d75b76a46a5
Alex Tideman <alex.tideman@gmail.com>
2024-10-30 15:35:06 -0500

    Add dynamic page param replacement in markdown, allow pop-up scripts

--- src/lib/holocene/monaco/markdown.svelte
+++ src/lib/holocene/monaco/markdown.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import { page } from '$app/stores';
+
   import { useDarkMode } from '$lib/utilities/dark-mode';
 
   export let content: string;
+
   let iframe;
 
   $: theme = $useDarkMode ? 'dark' : 'light';
@@ -11,6 +14,23 @@
     iframe.height = '';
     iframe.height = iframe.contentWindow.document.body.scrollHeight + 2 + 'px';
   };
+
+  const { workflow: workflowId, run: runId, namespace } = $page.params;
+
+  const replaceTemplate = (content: string) => {
+    if (namespace) {
+      content = content.replace(/\{namespace\}/g, namespace);
+    }
+    if (workflowId) {
+      content = content.replace(/\{workflowId\}/g, workflowId);
+    }
+    if (runId) {
+      content = content.replace(/\{runId\}/g, runId);
+    }
+    return content;
+  };
+
+  const templatedContent = replaceTemplate(content);
 </script>
 
 <section class="h-full w-full">
@@ -19,7 +39,7 @@
       bind:this={iframe}
       on:load={resizeIframe}
       title="output"
-      src="/render?content={encodeURIComponent(content)}&theme={theme}"
+      src="/render?content={encodeURIComponent(templatedContent)}&theme={theme}"
       class="w-full rounded-md"
     />
   {/key}
