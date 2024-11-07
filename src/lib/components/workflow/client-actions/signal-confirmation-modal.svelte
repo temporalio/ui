593c03bb62235d870782abeb0484b497c44bbaa3
Alex Tideman <alex.tideman@gmail.com>
2024-11-07 14:22:56 -0600

    Use signal definitions as dropdown select for sending signal

--- src/lib/components/workflow/client-actions/signal-confirmation-modal.svelte
+++ src/lib/components/workflow/client-actions/signal-confirmation-modal.svelte
@@ -6,9 +6,12 @@
   } from '$lib/components/payload-input-with-encoding.svelte';
   import Input from '$lib/holocene/input/input.svelte';
   import Modal from '$lib/holocene/modal.svelte';
+  import Option from '$lib/holocene/select/option.svelte';
+  import Select from '$lib/holocene/select/select.svelte';
   import { translate } from '$lib/i18n/translate';
   import { signalWorkflow } from '$lib/services/workflow-service';
   import { toaster } from '$lib/stores/toaster';
+  import { workflowRun } from '$lib/stores/workflow-run';
   import type { WorkflowExecution } from '$lib/types/workflows';
   import { isNetworkError } from '$lib/utilities/is-network-error';
 
@@ -16,6 +19,9 @@
   export let workflow: WorkflowExecution;
   export let namespace: string;
   export let refresh: Writable<number>;
+
+  $: ({ metadata } = $workflowRun);
+  $: signalDefinitions = metadata?.definition?.signalDefinitions;
 
   const defaultEncoding: PayloadInputEncoding = 'json/plain';
 
@@ -74,12 +80,27 @@
 >
   <h3 slot="title">{translate('workflows.signal-modal-title')}</h3>
   <div slot="content" class="flex flex-col gap-4">
-    <Input
-      id="signal-name"
-      label={translate('workflows.signal-name-label')}
-      required
-      bind:value={name}
-    />
+    {#if signalDefinitions?.length > 0}
+      <Select
+        id="signal-select"
+        label={translate('workflows.signal-name-label')}
+        class="min-w-fit"
+        bind:value={name}
+        data-testid="signal-select"
+        required
+      >
+        {#each signalDefinitions as { name: value, description = '' }}
+          <Option {value} {description}>{value}</Option>
+        {/each}
+      </Select>
+    {:else}
+      <Input
+        id="signal-name"
+        label={translate('workflows.signal-name-label')}
+        required
+        bind:value={name}
+      />
+    {/if}
     <PayloadInputWithEncoding bind:input bind:encoding />
   </div>
 </Modal>
