10e52ae397fb57bb2a96510afad734b5378ab7e7
Alex Tideman <alex.tideman@temporal.io>
2024-11-20 11:34:16 -0600

    Update version.go to v2.32.0 (#2435)

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.31.2"
+	UIVersion               = "2.32.0"
 )
