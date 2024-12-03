50d11b5bd6fd88d0ac029194aaeda54f37a86f49
Ross Edfort <rossedfort@gmail.com>
2024-12-03 15:47:05 -0700

    add collapsible component and icons for billing plans page

---
+++ src/lib/holocene/collapsible/collapsible.stories.svelte
@@ -0,0 +1,29 @@
+<script lang="ts" context="module">
+  import type { Meta } from '@storybook/svelte';
+
+  import Collapsible from './collapsible.svelte';
+
+  export const meta = {
+    title: 'Collapsible',
+    component: Collapsible,
+    args: {
+      label: 'Expand me',
+    },
+
+    argTypes: {
+      label: { name: 'Label', control: 'text' },
+    },
+  } satisfies Meta<Collapsible>;
+</script>
+
+<script lang="ts">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+  <Collapsible label={args.label}>
+    <p>This is the content</p>
+  </Collapsible>
+</Template>
+
+<Story name="Default" />
---
+++ src/lib/holocene/collapsible/collapsible.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  import { twMerge as merge } from 'tailwind-merge';
+
+  import Icon from '../icon/icon.svelte';
+
+  type $$Props = HTMLAttributes<HTMLDivElement> & {
+    label: string;
+    open?: boolean;
+  };
+
+  let className: string | undefined = undefined;
+
+  export let label: string;
+  export let open: boolean = false;
+  export { className as class };
+
+  const toggleOpen = () => (open = !open);
+</script>
+
+<div
+  class={merge(
+    'flex w-full flex-row items-center justify-evenly gap-4',
+    className,
+  )}
+  {...$$restProps}
+>
+  <div class="w-full border border-subtle" />
+  <button
+    class="flex grow items-center gap-2 whitespace-nowrap"
+    on:click={toggleOpen}
+  >
+    {label}
+    <Icon name={open ? 'chevron-up' : 'chevron-down'} />
+  </button>
+  <div class="w-full border border-subtle" />
+</div>
+
+<div class:hidden={!open}>
+  <slot />
+</div>
