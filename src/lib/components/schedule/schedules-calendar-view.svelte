112879ab7067a77eb29027f8b9c763ce199f4476
Laura Whitaker <laura.whitaker@temporal.io>
2025-02-10 11:43:19 -0700

    Add cron string example description

--- src/lib/components/schedule/schedules-calendar-view.svelte
+++ src/lib/components/schedule/schedules-calendar-view.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { page } from '$app/stores';
 
+  import CodeBlock from '$lib/holocene/code-block.svelte';
   import Input from '$lib/holocene/input/input.svelte';
   import TabList from '$lib/holocene/tab/tab-list.svelte';
   import TabPanel from '$lib/holocene/tab/tab-panel.svelte';
@@ -116,6 +117,23 @@
           <h3>
             {translate('schedules.cron-view-title')}
           </h3>
+          <p class="text-secondary">
+            {translate('schedules.crow-view-example-description')}
+          </p>
+          <CodeBlock
+            inline
+            language="text"
+            content={`┌───────────── minute (0 - 59) 
+│ ┌───────────── hour (0 - 23)
+│ │ ┌───────────── day of the month (1 - 31)
+│ │ │ ┌───────────── month (1 - 12)
+│ │ │ │ ┌───────────── day of the week (0 - 6) (Sunday to Saturday)
+│ │ │ │ │
+* * * * * <command to execute>`}
+            copyable={false}
+          />
+        </div>
+        <div class="flex flex-col gap-2">
           <p class="text-secondary">
             {translate('schedules.cron-view-description')}
           </p>
