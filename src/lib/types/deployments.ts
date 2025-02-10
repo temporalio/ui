84428ebb35fe1ec5b59dc861c7b788a4dcfd4240
Alex Tideman <alex.tideman@gmail.com>
2025-02-10 09:20:36 -0600

    Bump api version, add new page for deployments. WIP

---
+++ src/lib/types/deployments.ts
@@ -0,0 +1,34 @@
+// interface Deployment {
+//   seriesName: string;
+//   buildId: string;
+// }
+
+// interface DeploymentListInfo {
+//   deployment: Deployment;
+//   createTime: string; // ISO timestamp
+//   isCurrent: boolean;
+// }
+
+// interface WorkerDeploymentOptions {
+//   deploymentName: string;
+//   buildId: string;
+//   workerVersioningMode: WorkerVersioningMode;
+// }
+
+// enum WorkerVersioningMode {
+//   UNSPECIFIED = 0,
+//   UNVERSIONED = 1,
+//   VERSIONED = 2
+// }
+
+// enum TaskQueueType {
+//   UNSPECIFIED = 0,
+//   WORKFLOW = 1,
+//   ACTIVITY = 2
+// }
+
+// enum VersionDrainageStatus {
+//   UNSPECIFIED = 0,
+//   DRAINING = 1,
+//   DRAINED = 2
+// }
