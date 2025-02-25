a9a01995b22476882254b30705b253054d2931eb
Alex Tideman <alex.tideman@gmail.com>
2025-02-25 10:39:27 -0600

    Add back versioning tab for v2 versioning

--- src/lib/pages/task-queue.svelte
+++ src/lib/pages/task-queue.svelte
@@ -2,9 +2,20 @@
   import { page } from '$app/stores';
 
   import WorkerTable from '$lib/components/worker-table.svelte';
+  import TabList from '$lib/holocene/tab/tab-list.svelte';
+  import Tab from '$lib/holocene/tab/tab.svelte';
+  import Tabs from '$lib/holocene/tab/tabs.svelte';
+  import { translate } from '$lib/i18n/translate';
   import { getPollers } from '$lib/services/pollers-service';
+  import { taskQueueView } from '$lib/stores/task-queue-view';
+
+  import TaskQueueVersioning from './task-queue-versioning.svelte';
 
   $: ({ queue: taskQueue, namespace } = $page.params);
+
+  const onTab = (view: 'workers' | 'versioning') => {
+    $taskQueueView = view;
+  };
 </script>
 
 {#await getPollers({ queue: taskQueue, namespace }) then workers}
@@ -12,6 +23,26 @@
     <h2 data-testid="task-queue-name">
       {taskQueue}
     </h2>
-    <WorkerTable {workers} />
+    <Tabs>
+      <TabList class="flex flex-wrap gap-6 p-4 pl-0" label="task queue detail">
+        <Tab
+          label={translate('workers.workers')}
+          id="worker-tab"
+          onClick={() => onTab('workers')}
+          active={$taskQueueView === 'workers'}
+        />
+        <Tab
+          label={translate('workers.versioning')}
+          id="versioning-tab"
+          onClick={() => onTab('versioning')}
+          active={$taskQueueView === 'versioning'}
+        />
+      </TabList>
+    </Tabs>
+    {#if $taskQueueView === 'versioning'}
+      <TaskQueueVersioning {taskQueue} {workers} />
+    {:else}
+      <WorkerTable {workers} />
+    {/if}
   </section>
 {/await}
