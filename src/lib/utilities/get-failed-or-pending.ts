7f99f03ae900d270a094171fa8b38c4d95f412e3
Your Name <alex.tideman@gmail.com>
2025-03-07 11:06:06 -0600

    Add unit tests, refactor logic, standarized and clean up pending rows

--- src/lib/utilities/get-failed-or-pending.ts
+++ src/lib/utilities/get-failed-or-pending.ts
@@ -1,13 +1,53 @@
 import { isEventGroup } from '$lib/models/event-groups';
 import type { EventGroup } from '$lib/models/event-groups/event-groups';
 import { isEvent } from '$lib/models/event-history';
-import type { IterableEventWithPending } from '$lib/types/events';
+import type {
+  IterableEventWithPending,
+  WorkflowEvent,
+} from '$lib/types/events';
 
 import { isLocalActivityMarkerEvent } from './is-event-type';
 import {
   isPendingActivity,
   isPendingNexusOperation,
 } from './is-pending-activity';
+
+const isFailedLocalActivity = (event: WorkflowEvent) => {
+  return (
+    isLocalActivityMarkerEvent(event) &&
+    event.markerRecordedEventAttributes.failure
+  );
+};
+
+const isFailedLocalActivityGroup = (group: EventGroup) => {
+  return isFailedLocalActivity(group.initialEvent);
+};
+
+const isFailedEvent = (iterable: IterableEventWithPending) => {
+  return (
+    isEvent(iterable) &&
+    (iterable.classification === 'Failed' ||
+      iterable.classification === 'TimedOut' ||
+      isFailedLocalActivity(iterable))
+  );
+};
+
+const isPendingEvent = (iterable: IterableEventWithPending) => {
+  return isPendingActivity(iterable) || isPendingNexusOperation(iterable);
+};
+
+const isFailedEventGroup = (iterable: IterableEventWithPending) => {
+  return (
+    isEventGroup(iterable) &&
+    (iterable.classification === 'Failed' ||
+      iterable.classification === 'TimedOut' ||
+      isFailedLocalActivityGroup(iterable))
+  );
+};
+
+const isPendingEventGroup = (iterable: IterableEventWithPending) => {
+  return isEventGroup(iterable) && iterable.isPending;
+};
 
 export const getFailedOrPendingEvents = (
   items: IterableEventWithPending[],
@@ -16,17 +56,10 @@ export const getFailedOrPendingEvents = (
   if (!filterForFailedOrPending) return items;
   return items.filter(
     (item) =>
-      (isEvent(item) && item.classification === 'Failed') ||
-      (isEvent(item) && item.classification === 'TimedOut') ||
-      (isEvent(item) &&
-        isLocalActivityMarkerEvent(item) &&
-        item.markerRecordedEventAttributes.failure) ||
-      isPendingActivity(item) ||
-      isPendingNexusOperation(item) ||
-      (isEventGroup(item) &&
-        (item.isPending ||
-          item.finalClassification === 'Failed' ||
-          item.finalClassification === 'TimedOut')),
+      isFailedEvent(item) ||
+      isPendingEvent(item) ||
+      isFailedEventGroup(item) ||
+      isPendingEventGroup(item),
   );
 };
 
@@ -36,10 +69,6 @@ export const getFailedOrPendingGroups = (
 ) => {
   if (!filterForFailedOrPending) return items;
   return items.filter(
-    (item) =>
-      isEventGroup(item) &&
-      (item.isPending ||
-        item.finalClassification === 'Failed' ||
-        item.finalClassification === 'TimedOut'),
+    (item) => isFailedEventGroup(item) || isPendingEventGroup(item),
   );
 };
