8da920aede9f9ef89f993844971f3fd5404afd89
Ross Edfort <ross.edfort@temporal.io>
2025-03-19 10:19:24 -0600

    add story

--- src/lib/holocene/select/select.stories.svelte
+++ src/lib/holocene/select/select.stories.svelte
@@ -61,3 +61,8 @@
 <Story name="Disabled and Selected" args={{ disabled: true, value: 'pizza' }} />
 
 <Story name="Label Hidden" args={{ labelHidden: true }} />
+
+<Story
+  name="Invalid with error message"
+  args={{ invalid: true, error: 'This value is invalid' }}
+/>
