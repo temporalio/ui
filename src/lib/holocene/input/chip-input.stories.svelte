46d89d603459bdf60cb7fe1f53c4f1e1d692d952
Laura Whitaker <laura.whitaker@temporal.io>
2025-03-25 15:42:38 -0600

    ChipInput Updates (#2612)
    * Update Textarea max length story
    
    * Add maxLength prop to ChipInput
    
    * Keep input inline with chips

--- src/lib/holocene/input/chip-input.stories.svelte
+++ src/lib/holocene/input/chip-input.stories.svelte
@@ -18,6 +18,7 @@
       removeChipButtonLabel: 'Remove',
       labelHidden: false,
       validator: isEmail,
+      maxLength: undefined,
       chips: ['tobias@temporal.io'],
     },
     argTypes: {
@@ -30,6 +31,7 @@
       labelHidden: { name: 'Label Hidden', control: 'boolean' },
       chips: { name: 'Chips', table: { disable: true } },
       validator: { table: { disable: true } },
+      maxLength: { name: 'Maximum Length', control: 'number' },
       removeChipButtonLabel: {
         name: 'Aria label for remove button',
         control: 'text',
@@ -80,11 +82,21 @@
 />
 
 <Story
-  name="Invalid"
+  name="Error"
   play={async ({ canvasElement, id }) => {
     const canvas = within(canvasElement);
     const input = canvas.getByTestId(id);
     await userEvent.type(input, 'bonbon');
     await userEvent.keyboard('{enter}');
+  }}
+/>
+
+<Story
+  name="With Maximum Length"
+  args={{ maxLength: 10 }}
+  play={async ({ canvasElement, id }) => {
+    const canvas = within(canvasElement);
+    const input = canvas.getByTestId(id);
+    await userEvent.click(input);
   }}
 />
