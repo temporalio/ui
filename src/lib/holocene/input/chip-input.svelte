f2c30f7ba115d995d1b4e04e8f759741d6a0588c
Laura Whitaker <laura.whitaker@temporal.io>
2025-03-27 11:02:10 -0600

    Revert "Revert "ChipInput Updates"" (#2621)
    * Revert "Revert "ChipInput Updates (#2612)" (#2619)"
    
    This reverts commit c98d5664176743d16efcc24002264ef68f82c2b2.
    
    * Fix scrollIntoView
    
    * Set initial size of input based on placeholder length
    
    * Fix spacing in Combobox

--- src/lib/holocene/input/chip-input.svelte
+++ src/lib/holocene/input/chip-input.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { writable } from 'svelte/store';
 
-  import { afterUpdate, onDestroy } from 'svelte';
+  import { afterUpdate } from 'svelte';
   import { twMerge as merge } from 'tailwind-merge';
 
   import Chip from '$lib/holocene/chip.svelte';
@@ -19,10 +19,10 @@
   export let validator: (value: string) => boolean = () => true;
   export let removeChipButtonLabel: string | ((chipValue: string) => string);
   export let external = false;
+  export let maxLength = 0;
 
   const values = writable<string[]>(chips);
   let displayValue = '';
-  let shouldScrollToInput = false;
   let inputContainer: HTMLDivElement;
   let input: HTMLInputElement;
 
@@ -32,25 +32,8 @@
   let className = '';
   export { className as class };
 
-  const scrollToInput = () => {
-    let rect = input.getBoundingClientRect();
-    inputContainer.scrollTo(rect.x, rect.y);
-    shouldScrollToInput = false;
-  };
-
-  const unsubscribe = values.subscribe((updatedChips) => {
-    shouldScrollToInput = updatedChips.length > chips.length;
-    chips = updatedChips;
-  });
-
   afterUpdate(() => {
-    if (shouldScrollToInput) {
-      scrollToInput();
-    }
-  });
-
-  onDestroy(() => {
-    unsubscribe();
+    input.scrollIntoView({ block: 'nearest', inline: 'start' });
   });
 
   const handleKeydown = (e: KeyboardEvent) => {
@@ -74,11 +57,17 @@
 
   const handlePaste = (e: ClipboardEvent) => {
     e.preventDefault();
+    if (maxLength && $values.length >= maxLength) return;
     const clipboardContents = e.clipboardData.getData('text/plain');
-    values.update((previous) => [
-      ...previous,
-      ...clipboardContents.split(',').map((content) => content.trim()),
-    ]);
+    let newValues = clipboardContents
+      .split(',')
+      .map((content) => content.trim());
+
+    if (maxLength) {
+      newValues = newValues.slice(0, maxLength - $values.length);
+    }
+
+    values.update((previous) => [...previous, ...newValues]);
   };
 
   const handleBlur = () => {
@@ -97,15 +86,14 @@
   };
 </script>
 
-<div class={merge(disabled && 'cursor-not-allowed', className)}>
-  <Label
-    class="pb-1"
-    {required}
-    {label}
-    {disabled}
-    hidden={labelHidden}
-    for={id}
-  />
+<div
+  class={merge(
+    'group flex flex-col gap-1',
+    disabled && 'cursor-not-allowed',
+    className,
+  )}
+>
+  <Label {required} {label} {disabled} hidden={labelHidden} for={id} />
   <div
     bind:this={inputContainer}
     class={merge(
@@ -144,15 +132,38 @@
       on:blur={handleBlur}
       on:keydown|stopPropagation={handleKeydown}
       on:paste={handlePaste}
+      maxlength={maxLength && $values.length >= maxLength ? 0 : undefined}
+      size={placeholder.length || undefined}
     />
   </div>
-  {#if invalid && hintText}
-    <span class="hint">
-      {hintText}
-    </span>
+
+  {#if (invalid && hintText) || (maxLength && !disabled)}
+    <div class="flex justify-between gap-2">
+      <div
+        class="error-msg"
+        class:min-width={maxLength}
+        aria-live={invalid ? 'assertive' : 'off'}
+      >
+        {#if invalid && hintText}
+          <p>{hintText}</p>
+        {/if}
+      </div>
+      {#if maxLength && !disabled}
+        <span class="count">
+          <span
+            class="text-information"
+            class:warn={maxLength - $values?.length <= 5}
+            class:error={maxLength === $values?.length}
+          >
+            {$values?.length ?? 0}
+          </span>&nbsp;/&nbsp;{maxLength}
+        </span>
+      {/if}
+    </div>
   {/if}
+
   {#if $values.length > 0 && external}
-    <div class="mt-1 flex flex-row flex-wrap gap-1">
+    <div class="flex flex-row flex-wrap gap-1">
       {#each $values as chip, i (`${chip}-${i}`)}
         {@const valid = validator(chip)}
         <Chip
@@ -174,10 +185,26 @@
   }
 
   input {
-    @apply surface-primary inline-block w-full focus:outline-none;
+    @apply surface-primary inline-block grow focus:outline-none;
   }
 
-  .hint {
-    @apply text-xs text-danger;
+  .error-msg {
+    @apply break-words text-sm text-danger;
+  }
+
+  .error-msg.min-width {
+    @apply w-[calc(100%-6rem)];
+  }
+
+  .count {
+    @apply invisible text-right text-sm font-medium text-primary group-focus-within:visible;
+  }
+
+  .count > .warn {
+    @apply text-warning;
+  }
+
+  .count > .error {
+    @apply text-danger;
   }
 </style>
