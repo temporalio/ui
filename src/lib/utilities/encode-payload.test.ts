d20a636b58a0a733ba1a077e13033c8db167f6dd
Your Name <alex.tideman@gmail.com>
2025-04-14 09:40:23 -0500

    Fix bigInt test

--- src/lib/utilities/encode-payload.test.ts
+++ src/lib/utilities/encode-payload.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from 'vitest';
 
-import { getSinglePayload } from './encode-payload';
+import { stringifyWithBigInt } from '$lib/utilities/parse-with-big-int';
+
+import { encodePayloads, getSinglePayload } from './encode-payload';
 
 describe('getSinglePayload', () => {
   it('should return single payload from single payload', () => {
@@ -10,8 +12,8 @@ describe('getSinglePayload', () => {
         data: 'eyJ0aXRsZSI6ImhlbGxvIn0=',
       },
     ];
-    const singlePayload = getSinglePayload(JSON.stringify(payload));
-    expect(singlePayload).toEqual(JSON.stringify(payload[0]));
+    const singlePayload = getSinglePayload(stringifyWithBigInt(payload));
+    expect(singlePayload).toEqual(stringifyWithBigInt(payload[0]));
   });
   it('should return single payload from multiple payloads', () => {
     const payload = [
@@ -21,8 +23,8 @@ describe('getSinglePayload', () => {
       },
       { metadata: { encoding: 'json/plain' }, data: 'cccccasdf' },
     ];
-    const singlePayload = getSinglePayload(JSON.stringify(payload));
-    expect(singlePayload).toEqual(JSON.stringify(payload[0]));
+    const singlePayload = getSinglePayload(stringifyWithBigInt(payload));
+    expect(singlePayload).toEqual(stringifyWithBigInt(payload[0]));
   });
   it('should return empty string from no payload', () => {
     const singlePayload = getSinglePayload('');
@@ -33,7 +35,56 @@ describe('getSinglePayload', () => {
       metadata: { encoding: 'json/plain' },
       data: 'eyJ0aXRsZSI6ImhlbGxvIn0=',
     };
-    const singlePayload = getSinglePayload(JSON.stringify(payload));
+    const singlePayload = getSinglePayload(stringifyWithBigInt(payload));
     expect(singlePayload).toEqual('');
+  });
+});
+
+describe('encodePayloads', () => {
+  it('should encode single simple string payload', async () => {
+    const payload = await encodePayloads({
+      input: stringifyWithBigInt('cats'),
+      encoding: 'json/plain',
+    });
+    const expectedEncodedPayload = [
+      {
+        data: 'ImNhdHMi',
+        metadata: {
+          encoding: 'anNvbi9wbGFpbg==',
+        },
+      },
+    ];
+    expect(payload).toEqual(expectedEncodedPayload);
+  });
+  it('should encode single simple string payload', async () => {
+    const payload = await encodePayloads({
+      input: stringifyWithBigInt('cats'),
+      encoding: 'json/plain',
+    });
+    const expectedEncodedPayload = [
+      {
+        data: 'ImNhdHMi',
+        metadata: {
+          encoding: 'anNvbi9wbGFpbg==',
+        },
+      },
+    ];
+    expect(payload).toEqual(expectedEncodedPayload);
+  });
+  it('should encode bigInt string payload', async () => {
+    const input = { foo: 1234213412398883n };
+    const payload = await encodePayloads({
+      input: stringifyWithBigInt(input),
+      encoding: 'json/plain',
+    });
+    const expectedEncodedPayload = [
+      {
+        data: 'eyJmb28iOjEyMzQyMTM0MTIzOTg4ODN9',
+        metadata: {
+          encoding: 'anNvbi9wbGFpbg==',
+        },
+      },
+    ];
+    expect(payload).toEqual(expectedEncodedPayload);
   });
 });
