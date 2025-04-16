82a983f8ad3f3651efa3368bf1109e87a617873b
Your Name <alex.tideman@gmail.com>
2025-04-16 14:31:22 -0500

    Fix refetching direct workflows on workflow refresh

--- src/lib/components/workflow/workflow-relationships.svelte
+++ src/lib/components/workflow/workflow-relationships.svelte
@@ -9,6 +9,7 @@
     fetchAllRootWorkflowsCount,
   } from '$lib/services/workflow-service';
   import { workflowRun } from '$lib/stores/workflow-run';
+  import type { WorkflowExecution } from '$lib/types/workflows';
 
   import WorkflowFamilyTree from './relationships/workflow-family-tree.svelte';
   import WorkflowRelationshipsOld from './workflow-relationships-old.svelte';
@@ -18,37 +19,45 @@
   $: ({ namespace } = $page.params);
   $: ({ workflow } = $workflowRun);
 
-  $: rootWorkflowId = workflow.rootExecution.workflowId;
-  $: rootRunId = workflow.rootExecution.runId;
-  $: parentWorkflowId = workflow?.parent?.workflowId;
-  $: parentRunId = workflow?.parent?.runId;
+  let initialWorkflow: WorkflowExecution | undefined = undefined;
+
+  $: rootWorkflowId = initialWorkflow?.rootExecution?.workflowId;
+  $: rootRunId = initialWorkflow?.rootExecution?.runId;
+  $: parentWorkflowId = initialWorkflow?.parent?.workflowId;
+  $: parentRunId = initialWorkflow?.parent?.runId;
+
+  $: {
+    if (!initialWorkflow && workflow) initialWorkflow = workflow;
+  }
 </script>
 
 <div class="pb-12">
   <div class="flex w-full flex-col justify-center gap-4">
-    {#await fetchAllRootWorkflowsCount(namespace, rootWorkflowId, rootRunId) then { count }}
-      {@const intCount = parseInt(count)}
-      {#if intCount > MAX_UPPER_LIMIT}
-        {#await fetchAllDirectWorkflows( { namespace, parentWorkflowId, parentRunId, workflow }, )}
-          <Loading />
-        {:then root}
-          <WorkflowFamilyTree {root} {namespace} />
-        {:catch}
-          <WorkflowRelationshipsOld />
-        {/await}
-      {:else if intCount > 0}
-        {#await fetchAllRootWorkflows(namespace, rootWorkflowId, rootRunId)}
-          <Loading />
-        {:then root}
-          <WorkflowFamilyTree fullTree {root} {namespace} />
-        {:catch}
-          <WorkflowRelationshipsOld />
-        {/await}
-      {:else}
-        <h4 class="px-8 py-4">{translate('workflows.no-relationships')}</h4>
-      {/if}
-    {:catch}
-      <WorkflowRelationshipsOld />
-    {/await}
+    {#if initialWorkflow}
+      {#await fetchAllRootWorkflowsCount(namespace, rootWorkflowId, rootRunId) then { count }}
+        {@const intCount = parseInt(count)}
+        {#if intCount > MAX_UPPER_LIMIT}
+          {#await fetchAllDirectWorkflows( { namespace, parentWorkflowId, parentRunId, workflow: initialWorkflow }, )}
+            <Loading />
+          {:then root}
+            <WorkflowFamilyTree {root} {namespace} />
+          {:catch}
+            <WorkflowRelationshipsOld />
+          {/await}
+        {:else}
+          {#await fetchAllRootWorkflows(namespace, rootWorkflowId, rootRunId)}
+            <Loading />
+          {:then root}
+            <WorkflowFamilyTree fullTree {root} {namespace} />
+          {:catch}
+            <WorkflowRelationshipsOld />
+          {/await}
+        {/if}
+      {:catch}
+        <WorkflowRelationshipsOld />
+      {/await}
+    {:else}
+      <h4 class="px-8 py-4">{translate('workflows.no-relationships')}</h4>
+    {/if}
   </div>
 </div>
