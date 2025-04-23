39c21e4e6792b81842a659510cbda76dc2eed020
Your Name <alex.tideman@gmail.com>
2025-04-23 09:23:13 -0500

    Set position on toaster

--- src/lib/holocene/toaster.svelte
+++ src/lib/holocene/toaster.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+  import type { Writable } from 'svelte/store';
+
+  import { cva } from 'class-variance-authority';
+
+  import type { ToastPosition } from '$lib/types/holocene';
+
   import type { Toaster } from '../stores/toaster';
 
   import Link from './link.svelte';
@@ -7,16 +13,30 @@
   export let pop: Toaster['pop'];
   export let toasts: Toaster['toasts'];
   export let closeButtonLabel: string;
+  export let position: Writable<ToastPosition>;
 
   const dismissToast = (event: CustomEvent<{ id: string }>) => {
     pop(event.detail.id);
   };
+
+  const toast = cva(['fixed z-[99999] flex flex-col items-end gap-2'], {
+    variants: {
+      position: {
+        'top-left': 'top-16 left-5',
+        'top-center': 'top-16 left-1/2 -translate-x-1/2',
+        'top-right': 'top-16 right-5',
+        'bottom-left': 'bottom-5 left-5',
+        'bottom-center': 'bottom-5 left-1/2 -translate-x-1/2',
+        'bottom-right': 'bottom-5 right-5',
+      },
+    },
+    defaultVariants: {
+      position: $position,
+    },
+  });
 </script>
 
-<div
-  class="fixed bottom-5 right-5 z-[99999] flex flex-col items-end gap-2"
-  role="log"
->
+<div class={toast({ position: $position })} role="log">
   {#each $toasts as { message, variant, id, link } (id)}
     <ToastComponent {closeButtonLabel} {variant} {id} on:dismiss={dismissToast}>
       {#if link}
