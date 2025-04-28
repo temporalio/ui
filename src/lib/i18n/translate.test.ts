98ec161612bd52e30796ad574591fa18c529653b
grace <grace.gardner@temporal.io>
2025-04-28 12:59:07 -0600

    try to fix mock

--- src/lib/i18n/translate.test.ts
+++ src/lib/i18n/translate.test.ts
@@ -1,60 +1,48 @@
 import * as i18next from 'i18next';
-import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 
 import { translate } from './translate';
 
-describe('translate', () => {
-  beforeEach(() => {
-    vi.mock('i18next', async (importOriginal) => {
-      const actual = (await importOriginal()) as i18next.i18n;
-      const mockT = vi.fn();
-      return {
-        default: {
-          ...actual,
-          t: mockT,
-        },
-        t: mockT,
-      };
-    });
-  });
+const mockT = vi.fn();
 
+vi.mock('i18next', async (importOriginal) => {
+  const actual = (await importOriginal()) as i18next.i18n;
+  return {
+    default: {
+      ...actual,
+      t: mockT,
+    },
+    t: mockT,
+  };
+});
+
+describe('translate', () => {
   afterEach(() => {
     vi.clearAllMocks();
   });
 
   test('accepts a namespace and key', () => {
     translate('common.loading');
-
     expect(i18next.t).toHaveBeenCalledWith('common:loading', {});
   });
 
   test('accepts a namespace, key, and replace params', () => {
     translate('common.loading', { foo: 'bar' });
-
-    expect(i18next.t).toHaveBeenCalledWith('common:loading', {
-      foo: 'bar',
-    });
+    expect(i18next.t).toHaveBeenCalledWith('common:loading', { foo: 'bar' });
   });
 
   test('accepts a namespace, key, and count', () => {
     translate('common.loading', { count: 10 });
-
-    expect(i18next.t).toHaveBeenCalledWith('common:loading', {
-      count: 10,
-    });
+    expect(i18next.t).toHaveBeenCalledWith('common:loading', { count: 10 });
   });
 
   test('accepts namespace, key, and a count of 0', () => {
     translate('common.loading', { count: 0 });
-
-    expect(i18next.t).toHaveBeenCalledWith('common:loading', {
-      count: 0,
-    });
+    expect(i18next.t).toHaveBeenCalledWith('common:loading', { count: 0 });
   });
 
   test('accepts a namespace, key, count, and replace parameters', () => {
     translate('common.loading', { count: 10, foo: 'bar' });
-
     expect(i18next.t).toHaveBeenCalledWith('common:loading', {
       foo: 'bar',
       count: 10,
