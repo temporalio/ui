0579c295b708dff9fbec3a06606ccba076e0bdcb
Steve Kinney <hello@stevekinney.net>
2025-05-12 08:30:18 -0600

    Refactor is-pending-activity and add tests

--- src/lib/utilities/is-pending-activity.test.ts
+++ src/lib/utilities/is-pending-activity.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, it } from 'vitest';
 
-import { isPendingActivity } from './is-pending-activity';
+import {
+  isPendingActivity,
+  isPendingNexusOperation,
+} from './is-pending-activity';
 
 describe('isPendingActivity', () => {
   it('should return true if the event has an activityType', () => {
@@ -27,5 +30,42 @@ describe('isPendingActivity', () => {
 
   it('should return false if passed an array', () => {
     expect(isPendingActivity([])).toBe(false);
+  });
+});
+
+describe('isPendingNexusOperation', () => {
+  it('should return true if the event has operation and endpoint properties', () => {
+    expect(
+      isPendingNexusOperation({
+        operation: 'test-operation',
+        endpoint: 'test-endpoint',
+      }),
+    ).toBe(true);
+  });
+
+  it('should return false if the event is missing the operation property', () => {
+    expect(isPendingNexusOperation({ endpoint: 'test-endpoint' })).toBe(false);
+  });
+
+  it('should return false if the event is missing the endpoint property', () => {
+    expect(isPendingNexusOperation({ operation: 'test-operation' })).toBe(
+      false,
+    );
+  });
+
+  it('should return false if passed null', () => {
+    expect(isPendingNexusOperation(null)).toBe(false);
+  });
+
+  it('should return false if passed a string', () => {
+    expect(isPendingNexusOperation('string')).toBe(false);
+  });
+
+  it('should return false if passed a number', () => {
+    expect(isPendingNexusOperation(4)).toBe(false);
+  });
+
+  it('should return false if passed an array', () => {
+    expect(isPendingNexusOperation([])).toBe(false);
   });
 });
