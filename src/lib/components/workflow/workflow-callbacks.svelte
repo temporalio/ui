f0bfe37f7bbec77fb5e328d30d1c7200501f592d
grace <grace.gardner@temporal.io>
2025-05-13 12:40:23 -0600

    update workflow callbacks

--- src/lib/components/workflow/workflow-callbacks.svelte
+++ src/lib/components/workflow/workflow-callbacks.svelte
@@ -5,28 +5,35 @@
 
   import WorkflowCallback from './workflow-callback.svelte';
 
-  export let callbacks: Callback[];
+  type Props = {
+    callbacks: Callback[];
+  };
 
-  $: initialEvent = $fullEventHistory[0];
-  $: link = initialEvent?.links?.[0];
+  const { callbacks }: Props = $props();
+  const initialEvent = $derived(() => $fullEventHistory[0]);
+  const link = $derived(() => initialEvent()?.links?.[0]);
 
-  $: firstCallback = callbacks[0];
+  const firstCallback = $derived(() => callbacks[0]);
 </script>
 
-<WorkflowCallback callback={firstCallback} {link}>
+<WorkflowCallback callback={firstCallback()} link={link()}>
   {#if callbacks.length > 1}
     <div class="py-2">
-      <AccordionLight let:open>
-        <p class="p-1" slot="title">
-          View {callbacks.length - 1} other Callbacks
-        </p>
-        {#if open}
-          <div class="flex flex-col gap-2">
-            {#each callbacks.slice(1) as callback}
-              <WorkflowCallback {callback} />
-            {/each}
-          </div>
-        {/if}
+      <AccordionLight>
+        {#snippet titleName()}
+          <p class="p-1" slot="title">
+            View {callbacks.length - 1} other Callbacks
+          </p>
+        {/snippet}
+        {#snippet children({ open })}
+          {#if open}
+            <div class="flex flex-col gap-2">
+              {#each callbacks.slice(1) as callback}
+                <WorkflowCallback {callback} />
+              {/each}
+            </div>
+          {/if}
+        {/snippet}
       </AccordionLight>
     </div>
   {/if}
