819eaf74ca585ae04906a29e4c8f3c7c93684206
Alex Thomsen <alex.scott.thomsen@gmail.com>
2025-05-15 11:58:59 -0600

    Fix icon packaging errors (#2732)
    * Fix icon packaging errors
    
    * Fix labs icon

--- src/lib/holocene/icon/svg/converter-down.svelte
+++ src/lib/holocene/icon/svg/converter-down.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import Svg from '../svg.svelte';
+  let props = $props();
 </script>
 
-<Svg {...$$props}>
+<Svg {...props}>
   <path
     d="M11.1818 23.0868L11.9976 24L12.8134 23.0868L20.1842 14.8362L21 13.923L19.3732 12.1021L18.5575 13.0152L13.1493 19.0636V1.28917V0H10.8459V1.28917V19.0582L5.44255 13.0098L4.62677 12.0967L3 13.923L3.81578 14.8362L11.1818 23.0868Z"
     fill="currentcolor"
