c762347d85adaf3b183b24bc1641ec011fa493ae
Andrew Zamojc <me@andrewzamojc.com>
2025-05-28 10:01:41 -0400

    add DetailList component and subcomponents

---
+++ src/lib/components/detail-list/detail-list-column.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  interface Props {
+    children: Snippet;
+  }
+
+  const { children }: Props = $props();
+</script>
+
+<div
+  class="grid grid-cols-subgrid grid-rows-subgrid items-center gap-3 [grid-column:auto/span_2] [grid-row:1/-1]"
+>
+  {@render children()}
+</div>
---
+++ src/lib/components/detail-list/detail-list-text-value.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import type { IconName } from '$lib/holocene/icon';
+  import Tooltip from '$lib/holocene/tooltip.svelte';
+
+  import DetailListValue from './detail-list-value.svelte';
+
+  interface Props {
+    copyable?: boolean;
+    copyableText?: string;
+    text: string;
+    tooltipText?: string;
+    isBadge?: boolean;
+    iconName?: IconName | undefined;
+  }
+
+  let { copyable, copyableText, text, tooltipText, isBadge }: Props = $props();
+</script>
+
+{#snippet content()}
+  <span
+    class="w-fit select-all truncate rounded-sm leading-4"
+    class:surface-subtle={isBadge}>{text}</span
+  >
+{/snippet}
+
+<DetailListValue {copyable} {copyableText}>
+  {#if tooltipText}
+    <Tooltip text={tooltipText} top>
+      {@render content()}
+    </Tooltip>
+  {:else}
+    {@render content()}
+  {/if}
+</DetailListValue>
