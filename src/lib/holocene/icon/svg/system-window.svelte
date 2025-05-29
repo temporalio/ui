4b1a6f72ffa4156d8017549945019f16b71085ef
Andrew Zamojc <me@andrewzamojc.com>
2025-05-29 10:56:47 -0400

    fix system-window icon

--- src/lib/holocene/icon/svg/system-window.svelte
+++ src/lib/holocene/icon/svg/system-window.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import Svg from '../svg.svelte';
+  let props = $props();
 </script>
 
-<Svg {...$$props}>
+<Svg {...props}>
   <g
     clip-path="url(#clip0_21712_726)"
     id="g1"
