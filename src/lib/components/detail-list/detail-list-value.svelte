c5244d6fe4b6a6e99a8de5c57c3bf761efa80730
Andrew Zamojc <me@andrewzamojc.com>
2025-06-04 10:55:42 -0400

    use holocene copy button

--- src/lib/components/detail-list/detail-list-value.svelte
+++ src/lib/components/detail-list/detail-list-value.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
 
-  import CopyButton from '$lib/holocene/copyable/copy-button.svelte';
+  import CopyButton from '$lib/holocene/copyable/button.svelte';
   import { translate } from '$lib/i18n/translate';
+  import { copyToClipboard } from '$lib/utilities/copy-to-clipboard';
 
   interface Props {
     copyable?: boolean;
@@ -10,16 +11,29 @@
     children: Snippet;
   }
 
-  let { children, copyable, copyableText }: Props = $props();
+  const { children, copyable, copyableText }: Props = $props();
+  const { copy, copied } = copyToClipboard();
+
+  const handleCopy = (e: Event) => {
+    copy(e, copyableText);
+  };
 </script>
 
-<dt class="col-[2] flex gap-2">
+<dt class="col-[2] flex">
   {@render children()}
   {#if copyable}
-    <CopyButton
-      iconTitle={translate('common.copy-icon-title')}
-      successIconTitle={translate('common.copy-success-icon-title')}
-      {copyableText}
-    />
+    <!-- 
+      The CopyButton is larger than the line height, which pushes around grid items. Rather than removing its margin/padding, which makes the 
+      click box too small, it is absolute so it can slightly spill over the edges of the dt.
+    -->
+    <div class="relative flex w-6 items-center">
+      <CopyButton
+        copyIconTitle={translate('common.copy-icon-title')}
+        copySuccessIconTitle={translate('common.copy-success-icon-title')}
+        copied={$copied}
+        on:click={handleCopy}
+        class="absolute left-0"
+      />
+    </div>
   {/if}
 </dt>
