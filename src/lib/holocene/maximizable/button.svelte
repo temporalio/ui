61796948fc7ca51c1d7fb9f9b2169b59bd5f1422
Andrew Zamojc <me@andrewzamojc.com>
2025-06-13 16:43:56 -0400

    add maximizable component

---
+++ src/lib/holocene/maximizable/button.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import type { HTMLButtonAttributes } from 'svelte/elements';
+
+  import { twMerge as merge } from 'tailwind-merge';
+
+  import Icon from '$lib/holocene/icon/icon.svelte';
+
+  interface Props extends HTMLButtonAttributes {
+    maximized: boolean;
+    onClick: (e: MouseEvent) => void;
+    class?: string;
+    'data-testid'?: string;
+    'data-theme'?: string;
+  }
+
+  let {
+    maximized,
+    onClick,
+    class: className = undefined,
+    ...restProps
+  }: Props = $props();
+</script>
+
+<button
+  class={merge(
+    'surface-interactive-secondary m-1 border border-[transparent] bg-transparent p-1 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary/70',
+    className,
+  )}
+  onclick={onClick}
+  {...restProps}
+>
+  <Icon
+    title={maximized ? 'minimize' : 'expand'}
+    name={maximized ? 'minimize' : 'expand'}
+  />
+</button>
