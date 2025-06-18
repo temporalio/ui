392152a68e7f8413e84911abaf205f12a4be1cdd
Alex-Tideman <7967403+Alex-Tideman@users.noreply.github.com>
2025-06-18 14:06:17 +0000

    chore: bump version to 2.38.3
    Auto-generated version bump from 2.38.2 to 2.38.3
    
    Bump type: patch
    
    Changes included:\nManual version bump

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.38.2"
+	UIVersion               = "2.38.3"
 )
