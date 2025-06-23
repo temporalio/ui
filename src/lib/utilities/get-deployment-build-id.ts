af61be4126247bce13728d35b871641d658bb518
Alex Tideman <alex.tideman@temporal.io>
2025-06-23 13:15:51 -0500

    Deployment Updates - Pinned Workers and Unversioned current (#2778)
    * Better No Current Version label and hide deployed at timestamp. Show only pinned workers if workflow is pinned
    
    * Use current label
    
    * WIP: autoUpgrade
    
    * Show only current and ramping build ids for AutoUpgrade workflows
    
    * Use correct version checks on version-row
    
    * Add rune to get workflow versions and add tests
    
    * Move to Public Preview

--- src/lib/utilities/get-deployment-build-id.ts
+++ src/lib/utilities/get-deployment-build-id.ts
@@ -1,3 +1,8 @@
+import {
+  isVersionSummaryNew,
+  type VersionSummary,
+} from '$lib/types/deployments';
+
 export const getBuildIdFromVersion = (version: string | undefined): string => {
   if (!version) return '';
   const newDelimiter = version.split(':')[1];
@@ -5,4 +10,22 @@ export const getBuildIdFromVersion = (version: string | undefined): string => {
   const oldDelimiter = version.split('.')[1];
   if (oldDelimiter) return oldDelimiter;
   return version;
+};
+
+export const getDeploymentFromVersion = (
+  version: string | undefined,
+): string => {
+  if (!version) return '';
+  const newDelimiter = version.split(':');
+  if (newDelimiter && newDelimiter.length === 2) return newDelimiter[0];
+  const oldDelimiter = version.split('.');
+  if (oldDelimiter && oldDelimiter.length === 2) return oldDelimiter[0];
+  return version;
+};
+
+export const getDeploymentVersionFromStruct = (version: VersionSummary) => {
+  if (isVersionSummaryNew(version) && version.deploymentVersion) {
+    return `${version.deploymentVersion.deploymentName}:${version.deploymentVersion.buildId}`;
+  }
+  return `${version.version}`;
 };
