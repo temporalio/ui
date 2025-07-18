01ea5e1a76e71a1764475aae8e16de0a1faf63a0
Your Name <alex.tideman@gmail.com>
2025-07-18 09:26:58 -0500

    Update column order and add links

--- src/lib/pages/workflow-nexus-links.svelte
+++ src/lib/pages/workflow-nexus-links.svelte
@@ -12,6 +12,7 @@
   import { getEventLinkHref } from '$lib/utilities/event-link-href';
   import { isNexusOperationScheduledEvent } from '$lib/utilities/is-event-type';
   import {
+    routeForEventHistory,
     routeForEventHistoryEvent,
     routeForNamespace,
   } from '$lib/utilities/route-for';
@@ -38,6 +39,20 @@
       (callback) => callback?.callback?.links?.length,
     ),
   );
+
+  // const requestIds = $derived(
+  //   workflow?.workflowExtendedInfo?.requestIdInfos
+  //     ? Object.values(workflow?.workflowExtendedInfo?.requestIdInfos)
+  //     : [],
+  // );
+  // const getLinkFromRequestId = $derived((requestId) => {
+  //   const event = $fullEventHistory.find(
+  //     (event) => event.id === requestId.eventId,
+  //   );
+  //   if (event?.links?.length) {
+  //     return event.links[0];
+  //   }
+  // });
 </script>
 
 <section class="flex flex-col gap-4">
@@ -51,31 +66,51 @@
         >{translate('workflows.workers-tab')}</caption
       >
       <TableHeaderRow slot="headers">
-        <th>{translate('nexus.caller-link')}</th>
-        <th>{translate('nexus.caller-namespace')}</th>
         <th>{translate('nexus.caller-event')}</th>
+        <th>{translate('nexus.caller-workflow')}</th>
+        <th>{translate('nexus.caller-namespace')}</th>
+        <th>{translate('nexus.handler-event')}</th>
       </TableHeaderRow>
-      {#each callbacks as { callback }}
-        {@const link = callback.links[0]}
+      {#each callbacks as callback}
+        {@const link = callback?.callback?.links?.[0]}
         <TableRow data-testid="worker-row">
-          <td class="text-left" data-testid="link-event"> </td>
-          <td class="text-left" data-testid="link-namespace">
-            <Link
-              href={routeForNamespace({
-                namespace: link.workflowEvent.namespace,
-              })}>{link.workflowEvent.namespace}</Link
-            >
-          </td>
-          <td class="text-left" data-testid="link-href">
+          <td class="break-all text-left" data-testid="caller-event">
             {#if link?.workflowEvent}
               <Link href={getEventLinkHref(link)}
                 >{fromScreamingEnum(
                   link.workflowEvent?.eventRef?.eventType ||
                     link.workflowEvent?.requestIdRef?.eventType,
                   'EventType',
-                )}</Link
+                )}
+                {#if link.workflowEvent?.eventRef?.eventId}
+                  ({link.workflowEvent.eventRef.eventId})
+                {/if}
+              </Link>
+            {/if}
+          </td>
+          <td class="break-all text-left" data-testid="link-event">
+            {#if link?.workflowEvent}
+              <Link
+                href={routeForEventHistory({
+                  namespace: link.workflowEvent.namespace,
+                  workflow: link.workflowEvent.workflowId,
+                  run: link.workflowEvent.runId,
+                })}>{link.workflowEvent.workflowId}</Link
               >
             {/if}
+          </td>
+          <td class="break-all text-left" data-testid="link-namespace">
+            {#if link?.workflowEvent}
+              <Link
+                href={routeForNamespace({
+                  namespace: link.workflowEvent.namespace,
+                })}>{link.workflowEvent.namespace}</Link
+              >
+            {/if}
+          </td>
+          <td class="break-all text-left" data-testid="handler-event">
+            How do I associate callback to handler event? No requestId on
+            callback
           </td>
         </TableRow>
       {/each}
@@ -89,21 +124,20 @@
       >
       <TableHeaderRow slot="headers">
         <th>{translate('nexus.source-event')}</th>
-        <th>{translate('nexus.source-namespace')}</th>
         <th>{translate('nexus.nexus-endpoint-simple')}</th>
         <th>{translate('nexus.nexus-service')}</th>
         <th>{translate('nexus.nexus-operation')}</th>
-        <th>{translate('nexus.target-link')}</th>
-        <th>{translate('common.event-type')}</th>
+        <th>{translate('nexus.handler-namespace')}</th>
+        <th>{translate('nexus.handler-workflow')}</th>
+        <th>{translate('nexus.handler-event')}</th>
       </TableHeaderRow>
       {#each nexusGroups as group}
-        {@const link = group.links[0]}
-        {console.log('link', link)}
+        {@const link = group.links?.[0]}
         {@const scheduledEvent = group.eventList.find((e) =>
           isNexusOperationScheduledEvent(e),
         )}
         <TableRow data-testid="worker-row">
-          <td class="text-left" data-testid="link-event">
+          <td class="break-all text-left" data-testid="link-event">
             <Link
               href={routeForEventHistoryEvent({
                 namespace,
@@ -113,38 +147,47 @@
               })}>{scheduledEvent.id}</Link
             >
           </td>
-          <td class="text-left" data-testid="link-namespace">
-            <Link
-              href={routeForNamespace({
-                namespace,
-              })}>{namespace}</Link
-            >
-          </td>
-          <td class="text-left" data-testid="link-endpoint">
+          <td class="break-all text-left" data-testid="link-endpoint">
             {scheduledEvent?.attributes?.endpoint}
           </td>
-          <td class="text-left" data-testid="link-service">
+          <td class="break-all text-left" data-testid="link-service">
             {scheduledEvent?.attributes?.service}
           </td>
-          <td class="text-left" data-testid="link-operation">
+          <td class="break-all text-left" data-testid="link-operation">
             {scheduledEvent?.attributes?.operation}
           </td>
-          <td class="text-left" data-testid="link-namespace">
-            <Link
-              href={routeForNamespace({
-                namespace: link.workflowEvent.namespace,
-              })}>{link.workflowEvent.namespace}</Link
-            >
+          <td class="break-all text-left" data-testid="link-namespace">
+            {#if link?.workflowEvent}
+              <Link
+                href={routeForNamespace({
+                  namespace: link?.workflowEvent?.namespace,
+                })}>{link?.workflowEvent?.namespace}</Link
+              >
+            {/if}
           </td>
-          <td class="text-left" data-testid="link-href">
+          <td class="break-all text-left" data-testid="link-href">
+            {#if link?.workflowEvent}
+              <Link
+                href={routeForEventHistory({
+                  namespace: link.workflowEvent.namespace,
+                  workflow: link.workflowEvent.workflowId,
+                  run: link.workflowEvent.runId,
+                })}>{link.workflowEvent.workflowId}</Link
+              >
+            {/if}
+          </td>
+          <td class="break-all text-left" data-testid="link-href">
             {#if link?.workflowEvent}
               <Link href={getEventLinkHref(link)}
                 >{fromScreamingEnum(
                   link.workflowEvent?.eventRef?.eventType ||
                     link.workflowEvent?.requestIdRef?.eventType,
                   'EventType',
-                )}</Link
-              >
+                )}
+                {#if link.workflowEvent?.eventRef?.eventId}
+                  ({link.workflowEvent.eventRef.eventId})
+                {/if}
+              </Link>
             {/if}
           </td>
         </TableRow>
