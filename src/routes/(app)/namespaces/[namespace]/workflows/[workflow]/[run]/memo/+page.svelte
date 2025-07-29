ba71313974def0be9404ce76effe51a3e1bbd0f1
grace <grace.gardner@temporal.io>
2025-07-29 16:18:27 -0600

    move memo into component

--- src/routes/(app)/namespaces/[namespace]/workflows/[workflow]/[run]/memo/+page.svelte
+++ src/routes/(app)/namespaces/[namespace]/workflows/[workflow]/[run]/memo/+page.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
   import { page } from '$app/state';
 
-  import PayloadDecoder from '$lib/components/event/payload-decoder.svelte';
   import PageTitle from '$lib/components/page-title.svelte';
-  import CodeBlock from '$lib/holocene/code-block.svelte';
+  import WorkflowMemo from '$lib/components/workflow/metadata/workflow-memo.svelte';
   import { translate } from '$lib/i18n/translate';
   import WorkflowPaddedLayout from '$lib/layouts/workflow-padded-layout.svelte';
-  import { workflowRun } from '$lib/stores/workflow-run';
-
-  const { workflow } = $derived($workflowRun);
 </script>
 
 <PageTitle
@@ -16,20 +12,5 @@
   url={page.url.href}
 />
 <WorkflowPaddedLayout>
-  <div class="mt-4 flex flex-col gap-2">
-    <h3>{translate('common.memo')}</h3>
-    {#if workflow?.memo}
-      <PayloadDecoder value={{ memo: workflow.memo }} key="memo">
-        {#snippet children(decodedValue)}
-          <CodeBlock
-            content={decodedValue}
-            copyIconTitle={translate('common.copy-icon-title')}
-            copySuccessIconTitle={translate('common.copy-success-icon-title')}
-          />
-        {/snippet}
-      </PayloadDecoder>
-    {:else}
-      <p>{translate('events.empty-memo-attributes')}</p>
-    {/if}
-  </div>
+  <WorkflowMemo />
 </WorkflowPaddedLayout>
