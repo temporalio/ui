ee76a5909113a6c6a5eecd00a6920e076c87dbb7
Laura Whitaker <laura.whitaker@temporal.io>
2025-07-29 16:07:30 -0600

    Use solid spinner as default spinner icon

--- src/lib/holocene/icon/svg/spinner.svelte
+++ src/lib/holocene/icon/svg/spinner.svelte
@@ -5,45 +5,10 @@
 
 <Svg {...props}>
   <path
-    d="M11.5 3.5C11.5 3.77614 11.7239 4 12 4C12.2761 4 12.5 3.77614 12.5 3.5C12.5 3.22386 12.2761 3 12 3C11.7239 3 11.5 3.22386 11.5 3.5Z"
+    d="M12 3C16.9706 3 21 7.02944 21 12C21 14.4853 19.9926 16.7353 18.364 18.364"
     stroke="currentcolor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-  <path
-    d="M5.5 6C5.5 6.27614 5.72386 6.5 6 6.5C6.27614 6.5 6.5 6.27614 6.5 6C6.5 5.72386 6.27614 5.5 6 5.5C5.72386 5.5 5.5 5.72386 5.5 6Z"
-    stroke="currentcolor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-  <path
-    d="M3 12C3 12.2761 3.22386 12.5 3.5 12.5C3.77614 12.5 4 12.2761 4 12C4 11.7239 3.77614 11.5 3.5 11.5C3.22386 11.5 3 11.7239 3 12Z"
-    stroke="currentcolor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-  <path
-    d="M5.5 18C5.5 18.2761 5.72386 18.5 6 18.5C6.27614 18.5 6.5 18.2761 6.5 18C6.5 17.7239 6.27614 17.5 6 17.5C5.72386 17.5 5.5 17.7239 5.5 18Z"
-    stroke="currentcolor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-  <path
-    d="M11.5 20.5C11.5 20.7761 11.7239 21 12 21C12.2761 21 12.5 20.7761 12.5 20.5C12.5 20.2239 12.2761 20 12 20C11.7239 20 11.5 20.2239 11.5 20.5Z"
-    stroke="currentcolor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-  <path
-    d="M17.5 18C17.5 18.2761 17.7239 18.5 18 18.5C18.2761 18.5 18.5 18.2761 18.5 18C18.5 17.7239 18.2761 17.5 18 17.5C17.7239 17.5 17.5 17.7239 17.5 18Z"
-    stroke="currentcolor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-  <path
-    d="M20 12C20 12.2761 20.2239 12.5 20.5 12.5C20.7761 12.5 21 12.2761 21 12C21 11.7239 20.7761 11.5 20.5 11.5C20.2239 11.5 20 11.7239 20 12Z"
-    stroke="currentcolor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    stroke-width="3"
+    stroke-linecap="square"
+    stroke-linejoin="miter"
   />
 </Svg>
