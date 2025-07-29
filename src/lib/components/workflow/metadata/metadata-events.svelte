1b07d4c784612bde749174cf83926ca89cddbdd0
grace <grace.gardner@temporal.io>
2025-07-29 10:20:40 -0600

    add summary back in and remove event id

--- src/lib/components/workflow/metadata/metadata-events.svelte
+++ src/lib/components/workflow/metadata/metadata-events.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import MetadataDecoder from '$lib/components/event/metadata-decoder.svelte';
   import Badge from '$lib/holocene/badge.svelte';
   import Icon from '$lib/holocene/icon/icon.svelte';
   import TableHeaderRow from '$lib/holocene/table/table-header-row.svelte';
@@ -27,42 +28,57 @@
   };
 </script>
 
-<Table class="w-full" data-testid="metadata-events-table">
-  <caption class="sr-only" slot="caption">
-    {translate('workflows.user-metadata-tab')}
-  </caption>
-  <TableHeaderRow slot="headers">
-    <th class="w-20 text-left">{translate('common.id')}</th>
-    <th class="w-40 text-left">{translate('common.event-type')}</th>
-    <th class="w-24 text-left">{translate('common.status')}</th>
-    <th class="w-36 text-left">{translate('common.time')}</th>
-  </TableHeaderRow>
+<div class="overflow-x-auto">
+  <Table class="min-w-full table-fixed" data-testid="metadata-events-table">
+    <caption class="sr-only" slot="caption">
+      {translate('workflows.user-metadata-tab')}
+    </caption>
+    <TableHeaderRow slot="headers">
+      <th class="w-1/5 px-3 py-3 text-left">{translate('common.event-type')}</th
+      >
+      <th class="w-1/6 px-3 py-3 text-left">{translate('common.status')}</th>
+      <th class="w-1/4 px-3 py-3 text-left">{translate('common.time')}</th>
+      <th class="w-auto px-3 py-3 text-left">{translate('common.summary')}</th>
+    </TableHeaderRow>
 
-  {#each groups as group}
-    <TableRow class="hover:bg-interactive-table-hover">
-      <td class="px-3 py-3 text-left">
-        <span class="font-mono text-sm text-secondary">{group.id}</span>
-      </td>
-      <td class="px-3 py-3 text-left">
-        <div class="flex flex-col gap-1 capitalize">
-          {group.category}
-        </div>
-      </td>
-      <td class="px-3 py-3 text-left">
-        <Badge type={getBadgeType(group.finalClassification)} class="text-xs">
-          {group.finalClassification}
-        </Badge>
-      </td>
-      <td class="px-3 py-3 text-left">
-        <div class="flex items-center gap-2">
-          <Icon name="clock" class="h-3 w-3 text-secondary/60" />
-          <span class="truncate text-sm">
-            {formatDate(group.eventTime, $timeFormat, {
-              relative: $relativeTime,
-            })}
-          </span>
-        </div>
-      </td>
-    </TableRow>
-  {/each}
-</Table>
+    {#each groups as group}
+      <TableRow class="hover:bg-interactive-table-hover">
+        <td class="px-3 py-3 text-left">
+          <div class="flex flex-col gap-1 capitalize">
+            {group.category}
+          </div>
+        </td>
+        <td class="px-3 py-3 text-left">
+          <Badge type={getBadgeType(group.finalClassification)} class="text-xs">
+            {group.finalClassification}
+          </Badge>
+        </td>
+        <td class="px-3 py-3 text-left">
+          <div class="flex items-center gap-2">
+            <Icon name="clock" class="h-3 w-3 text-secondary/60" />
+            <span class="truncate text-sm">
+              {formatDate(group.eventTime, $timeFormat, {
+                relative: $relativeTime,
+              })}
+            </span>
+          </div>
+        </td>
+        <td class="px-3 py-3 text-left">
+          {#if group.userMetadata?.summary}
+            <MetadataDecoder
+              value={group.userMetadata.summary}
+              fallback={translate('events.decode-failed')}
+              let:decodedValue
+            >
+              <span class="font-mono text-sm text-secondary"
+                >{decodedValue}</span
+              >
+            </MetadataDecoder>
+          {:else}
+            <span class="font-mono text-sm text-secondary">-</span>
+          {/if}
+        </td>
+      </TableRow>
+    {/each}
+  </Table>
+</div>
