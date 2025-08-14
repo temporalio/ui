83a126e70fe31f0751e9de7ac05cd6ea9e0a7aa4
Laura Whitaker <laura.whitaker@temporal.io>
2025-08-14 10:30:41 -0600

    Add Loading component story

---
+++ src/lib/holocene/loading.stories.svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+  import type { Meta } from '@storybook/svelte';
+
+  import Loading from './loading.svelte';
+
+  export const meta = {
+    title: 'Loading',
+    component: Loading,
+    args: {
+      title: 'Loading...',
+    },
+    argTypes: {
+      title: { control: 'text' },
+    },
+  } satisfies Meta<Loading>;
+</script>
+
+<script lang="ts">
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+  <Loading {...args} />
+</Template>
+
+<Story name="Default" />
