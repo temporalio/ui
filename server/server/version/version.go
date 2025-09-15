512bf5ace41a46d9a64a76ff3f8a8fb43bdd6f75
rossnelson <146089+rossnelson@users.noreply.github.com>
2025-09-15 19:37:19 +0000

    chore: bump version to 2.41.0
    Auto-generated version bump from 2.40.1 to 2.41.0
    
    Bump type: minor
    
    Changes included:
    - [`904ca347`](https://github.com/temporalio/ui/commit/904ca347b988396cf9d14fc4b1e6c8a0e80cc974) fix(workflows): add GitHub App token to version bump PR creation (#2910)
    - [`bd50889e`](https://github.com/temporalio/ui/commit/bd50889ebd5898fde79be1e9e50c5f71a79a55fd) fix(workflows): fix changelog generation and PR body formatting in version bump (#2912)

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.40.1"
+	UIVersion               = "2.41.0"
 )
