a729ce061718d4a7530dbf1aa4db91da74b4277e
Your Name <alex.tideman@gmail.com>
2025-10-01 15:17:57 -0500

    Update resfresh rate tests

--- src/lib/utilities/refresh-rate.test.ts
+++ src/lib/utilities/refresh-rate.test.ts
@@ -2,36 +2,46 @@ import { describe, expect, it } from 'vitest';
 
 import { getExponentialBackoff } from './refresh-rate';
 
-describe('getExponentialBackoff', () => {
+describe('getExponentialBackoff with 5 seconds', () => {
   it('should get initial backoff interval of 5 seconds', () => {
-    expect(getExponentialBackoff(5, 1, 100)).toEqual(5000);
+    expect(getExponentialBackoff(5, 1)).toEqual(5000);
   });
 
   it('should get backoff interval of 6 seconds on attempt 2', () => {
-    expect(getExponentialBackoff(5, 2, 100)).toEqual(6000);
+    expect(getExponentialBackoff(5, 2)).toEqual(6000);
   });
 
-  it('should get backoff interval of 6 seconds on attempt 3', () => {
-    expect(getExponentialBackoff(5, 3, 100)).toEqual(6000);
+  it('should get backoff interval of 7 seconds on attempt 3', () => {
+    expect(getExponentialBackoff(5, 3)).toEqual(7000);
   });
 
-  it('should get backoff interval of 10 seconds on attempt 10', () => {
-    expect(getExponentialBackoff(5, 10, 100)).toEqual(10000);
+  it('should get backoff interval of 26 seconds on attempt 10', () => {
+    expect(getExponentialBackoff(5, 10)).toEqual(26000);
   });
 
-  it('should get backoff interval of 19 seconds on attempt 20', () => {
-    expect(getExponentialBackoff(5, 20, 100)).toEqual(19000);
+  it('should get backoff interval of 160 seconds on attempt 20', () => {
+    expect(getExponentialBackoff(5, 20)).toEqual(160000);
   });
 
-  it('should get backoff interval of 134 seconds on attempt 50', () => {
-    expect(getExponentialBackoff(5, 50, 100)).toEqual(134000);
+  it('should get max backoff interval of five minutes on attempt 50', () => {
+    expect(getExponentialBackoff(5, 50)).toEqual(300000);
+  });
+});
+
+describe('getExponentialBackoff with 60 seconds', () => {
+  it('should get initial backoff interval of 60 seconds', () => {
+    expect(getExponentialBackoff(60, 1)).toEqual(60000);
   });
 
-  it('should get max backoff interval of one hour on attempt 100', () => {
-    expect(getExponentialBackoff(5, 100, 100)).toEqual(3600000);
+  it('should get backoff interval of 72 seconds on attempt 2', () => {
+    expect(getExponentialBackoff(60, 2)).toEqual(72000);
   });
 
-  it('should still get max backoff interval of one hour on attempt 1000', () => {
-    expect(getExponentialBackoff(5, 1000, 100)).toEqual(3600000);
+  it('should get backoff interval of 86 seconds on attempt 3', () => {
+    expect(getExponentialBackoff(60, 3)).toEqual(86000);
+  });
+
+  it('should get max backoff interval of 5 minutes on attempt 10', () => {
+    expect(getExponentialBackoff(60, 10)).toEqual(300000);
   });
 });
