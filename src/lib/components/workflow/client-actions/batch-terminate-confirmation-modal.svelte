3d60eee9a51f1d023057a73d472d152ce9e4e208
Grace Iris Gardner <42048868+GraceGardner@users.noreply.github.com>
2025-10-02 10:00:10 -0600

    Update src/lib/components/workflow/client-actions/batch-terminate-confirmation-modal.svelte
    Co-authored-by: Laura Whitaker <laura.whitaker@temporal.io>

--- src/lib/components/workflow/client-actions/batch-terminate-confirmation-modal.svelte
+++ src/lib/components/workflow/client-actions/batch-terminate-confirmation-modal.svelte
@@ -27,7 +27,7 @@
   const reasonPlaceholder = getPlaceholder(Action.Terminate, identity);
   const jobId = writable('');
   const jobIdValid = writable(true);
-  let jobIdPlaceholder: string = crypto.randomUUID();
+  let jobIdPlaceholder = crypto.randomUUID();
   let error = '';
 
   const { allSelected, terminableWorkflows } =
