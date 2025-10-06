a713eab48f6514c75a4265d78819dfd1b3a84d51
Laura Whitaker <laura.whitaker@temporal.io>
2025-10-06 14:55:59 -0600

    Migrate layout files to Svelte 5

--- src/routes/(app)/import/events/[namespace]/[workflow]/[run]/history/+layout.svelte
+++ src/routes/(app)/import/events/[namespace]/[workflow]/[run]/history/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+  import type { Snippet } from 'svelte';
+
   import ImportEventsView from '$lib/pages/import-events-view.svelte';
+
+  interface Props {
+    children: Snippet;
+  }
+  let { children }: Props = $props();
 </script>
 
 <ImportEventsView>
-  <slot />
+  {@render children()}
 </ImportEventsView>
