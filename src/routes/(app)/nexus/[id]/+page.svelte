c9e55b718680d918c3f4d469b503227d993365c7
grace <grace.gardner@temporal.io>
2025-10-06 12:50:21 -0600

    rm unneeded changes

--- src/routes/(app)/nexus/[id]/+page.svelte
+++ src/routes/(app)/nexus/[id]/+page.svelte
@@ -7,28 +7,17 @@
   import TaskQueueStatus from '$lib/components/task-queue-status.svelte';
   import { translate } from '$lib/i18n/translate';
   import NexusEndpoint from '$lib/pages/nexus-endpoint.svelte';
-  import { coreUserStore } from '$lib/stores/core-user';
-  import { editDisabled } from '$lib/utilities/edit-disabled';
 
   let { data }: { data: LayoutData } = $props();
 
   const { endpoint } = $derived(data);
-  const coreUser = coreUserStore();
-
-  const isEditDisabled = $derived(
-    editDisabled(
-      $page.data.settings,
-      $coreUser,
-      endpoint?.spec?.target?.worker?.namespace,
-    ),
-  );
 </script>
 
 <PageTitle
   title={translate('nexus.nexus-endpoint', { id: $page.params.id })}
   url={$page.url.href}
 />
-<NexusEndpoint {endpoint} editDisabled={isEditDisabled}>
+<NexusEndpoint {endpoint}>
   {#snippet taskQueueStatus()}
     <TaskQueueStatus {endpoint} />
   {/snippet}
