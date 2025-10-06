30256197141832fbe258aa7c26e25f9914bd2f21
Laura Whitaker <laura.whitaker@temporal.io>
2025-10-06 14:57:08 -0600

    Update FeedbackButton

--- src/lib/components/feedback-button.svelte
+++ src/lib/components/feedback-button.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
-  import { page } from '$app/stores';
+  import { page } from '$app/state';
 
-  import Icon from '$lib/holocene/icon/icon.svelte';
+  import Button from '$lib/holocene/button.svelte';
 
-  const href =
-    $page.data?.settings?.feedbackURL ||
-    'https://github.com/temporalio/ui/issues/new/choose';
+  const href = $derived(
+    page.data?.settings?.feedbackURL ||
+      'https://github.com/temporalio/ui/issues/new/choose',
+  );
 </script>
 
-<a {href} target="_blank" data-testid="give-feedback" rel="noreferrer">
-  <div class="feedback-button">
-    <Icon class="mr-2 text-purple-200" name="feedback" height={12} width={12} />
-    Give Feedback
-  </div>
-</a>
-
-<style lang="postcss">
-  .feedback-button {
-    @apply flex cursor-pointer items-center justify-between px-4 py-2 text-sm text-purple-200 shadow-md;
-  }
-
-  .feedback-button:hover {
-    @apply bg-slate-700;
-  }
-</style>
+<Button
+  size="sm"
+  leadingIcon="feedback"
+  variant="ghost"
+  class="text-brand shadow-md"
+  {href}
+  target="_blank"
+  data-testid="give-feedback"
+  rel="noreferrer"
+>
+  Give Feedback
+</Button>
