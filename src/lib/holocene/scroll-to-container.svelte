3f7e46ca972910f8bba9cd9b33c9bb0efabb8ba0
Ross Edfort <ross.edfort@temporal.io>
2025-10-22 14:32:09 -0600

    move scroll up/down buttons to left of main content

--- src/lib/holocene/scroll-to-container.svelte
+++ src/lib/holocene/scroll-to-container.svelte
@@ -1,17 +1,40 @@
 <script lang="ts">
+  import { type ClassNameValue, twMerge } from 'tailwind-merge';
+
   import ScrollToBottom from '$lib/holocene/scroll-to-bottom.svelte';
   import ScrollToTop from '$lib/holocene/scroll-to-top.svelte';
   import { fullEventHistory } from '$lib/stores/events';
+  import { navOpen } from '$lib/stores/nav-open';
 
-  export let scrollToTopHidden = true;
-  export let scrollToBottomHidden = false;
-  export let onScrollToTopClick: () => void;
-  export let onScrollToBottomClick: () => void;
-  export let scrollToTopAriaLabel: string;
-  export let scrollToBottomAriaLabel: string;
+  interface Props {
+    scrollToTopHidden?: boolean;
+    scrollToBottomHidden?: boolean;
+    onScrollToTopClick: () => void;
+    onScrollToBottomClick: () => void;
+    scrollToTopAriaLabel: string;
+    scrollToBottomAriaLabel: string;
+    class?: ClassNameValue;
+  }
+
+  let {
+    scrollToTopHidden = true,
+    scrollToBottomHidden = false,
+    class: className = '',
+    onScrollToTopClick,
+    onScrollToBottomClick,
+    scrollToTopAriaLabel,
+    scrollToBottomAriaLabel,
+  }: Props = $props();
 </script>
 
-<div id="scroll-container" class={$$props.class}>
+<div
+  id="scroll-container"
+  class={twMerge(
+    'fixed bottom-5 z-50 hidden w-auto gap-2 transition-all md:flex',
+    $navOpen ? 'left-[196px]' : 'left-20',
+    className,
+  )}
+>
   <ScrollToTop
     hidden={scrollToTopHidden}
     {onScrollToTopClick}
@@ -25,9 +48,3 @@
     />
   {/if}
 </div>
-
-<style lang="postcss">
-  #scroll-container {
-    @apply fixed bottom-5 right-8 z-50 hidden w-auto gap-2 md:flex;
-  }
-</style>
