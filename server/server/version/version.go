1f7be515d1f90328e46388dbf2f55659ad9a2e7c
laurakwhit <15069288+laurakwhit@users.noreply.github.com>
2025-11-04 19:48:57 +0000

    chore: bump version to 2.43.0
    Auto-generated version bump from 2.42.1 to 2.43.0
    
    Bump type: minor
    
    Changes included:
    - [`c772936b`](https://github.com/temporalio/ui/commit/c772936bd1c277d4be491d004efb92014a6b6253) add new logos (#2950)
    - [`e4659afc`](https://github.com/temporalio/ui/commit/e4659afc08eb5185c1bdd6cdf40a9b52f3134077) Add TLS support for ui-server (#2745)
    - [`35333bc0`](https://github.com/temporalio/ui/commit/35333bc0595c3f81a3b0d670e1934ff8adf2ba72) remove scroll up/down buttons in main content (#2975)
    - [`25f4cd50`](https://github.com/temporalio/ui/commit/25f4cd503bcc9a5a39628d6387c589e7ec0babb1) Bump sveltekit-superforms from 2.27.1 to 2.27.4 (#2965)
    - [`133577d5`](https://github.com/temporalio/ui/commit/133577d57eeb411b5c0f3f3364b91c24a8fe8ccb) Change sticky header to surface-background and include border (#2979)
    - [`c90bbe85`](https://github.com/temporalio/ui/commit/c90bbe85b0e263cb07ad68cf57dbc036d947cbd0) Add gradient for completed Activities with retries (#2977)
    - [`015de88a`](https://github.com/temporalio/ui/commit/015de88aee47d44ff92bfe0d07fb3865cf2bc28f) Set day as max unit in formatDate and getDuration (#2978)
    - [`edf98d97`](https://github.com/temporalio/ui/commit/edf98d97ddffb5dbd881bb256579371859f05cd4) Allow custom keyword SAs to be quick filterable (#2980)
    - [`6183f331`](https://github.com/temporalio/ui/commit/6183f331d9ace0438a6d84cf5a3847871810be39) Account for units less than a day in formatDate (#2982)
    - [`2688adac`](https://github.com/temporalio/ui/commit/2688adac0defc3e59c2f77cf3043e1e09c2b13e7) Hide timeout if 0s (#2983)
    - [`7732e4b8`](https://github.com/temporalio/ui/commit/7732e4b85952520258b74312aef3d1a0cced80ab) feat(search-attributes): add disableTypeForExisting prop (#2984)
    - [`66a6323f`](https://github.com/temporalio/ui/commit/66a6323f8cf718bd2e9f3a1b66d24d57cdf32cf0) Fix holocene component prop types (#2985)
    - [`c8673e6c`](https://github.com/temporalio/ui/commit/c8673e6cffe09261b74f5e30630cbb90cf741bae) Fix handleFocusOut on maximize to allow highlighting text to copy (#2986)
    - [`547045af`](https://github.com/temporalio/ui/commit/547045af5a1571e7769bb6e56e3e2ce20ee9e828) SVG loading (#2955)
    - [`ecacb1e0`](https://github.com/temporalio/ui/commit/ecacb1e0bfeead94f413bcbd7c58b12bad2e144d) fix(search-attributes): use $derived for reactive props pattern (#2992)
    - [`9f00bdf3`](https://github.com/temporalio/ui/commit/9f00bdf34de0acd8654053873f6f77c9240e2da7) Use CurrentVersion createTime (#2995)
    - [`6bf4c09c`](https://github.com/temporalio/ui/commit/6bf4c09cb0db1e36a3db1b0f1c1cdf9123b25e9b) DT-2423 - move theme selector to user dropdown menu (#2988)
    - [`49746aba`](https://github.com/temporalio/ui/commit/49746aba508da5786353613df5f4ceec3d082edc) DT-3415 - move data encoder settings for into a modal (#2981)
    - [`8046e1ac`](https://github.com/temporalio/ui/commit/8046e1acb3becdb1ac60bb2a30ac8ecbac3e07e8) Add laptop-code and shield icons to Holocene icon system (#2997)
    - [`05d8686b`](https://github.com/temporalio/ui/commit/05d8686bfb4af75c775fb6651908ec0a932433cf) Add a slot to the bottom nav for user profile picture in cloud (#3004)
    - [`18d60d43`](https://github.com/temporalio/ui/commit/18d60d4318b75b034e735326985ada5f33bcc13f) Use page.data.namespace to access namespaceInfo (#3002)
    - [`2f24a1b5`](https://github.com/temporalio/ui/commit/2f24a1b5426458b01d497118ba2c62ced3c6e357) Task Failure System View (#2991)

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.42.1"
+	UIVersion               = "2.43.0"
 )
