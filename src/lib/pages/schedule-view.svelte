07dec31dd93c155f066c9a2b83d6f9fc9e41832d
Alex Tideman <alex.tideman@temporal.io>
2025-11-21 08:39:53 -0600

    Clear Schedule timeouts (#3030)
    * Clear timeouts
    
    * Use one codeblock for schedule spec
    
    * Make max height 600

--- src/lib/pages/schedule-view.svelte
+++ src/lib/pages/schedule-view.svelte
@@ -2,6 +2,7 @@
   import { writable } from 'svelte/store';
 
   import { addDays, addHours, startOfDay } from 'date-fns';
+  import { onDestroy } from 'svelte';
 
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
@@ -75,6 +76,9 @@
   let error = '';
   let scheduleUpdating = false;
   let overlapPolicy = writable<OverlapPolicy>('Unspecified');
+  let deleteTimeout: ReturnType<typeof setTimeout>;
+  let triggerTimeout: ReturnType<typeof setTimeout>;
+  let backfillTimeout: ReturnType<typeof setTimeout>;
   let policies: { label: string; description: string; value: OverlapPolicy }[] =
     [
       {
@@ -124,7 +128,8 @@
       $loading = true;
       await deleteSchedule({ identity, namespace, scheduleId });
       deleteConfirmationModalOpen = false;
-      setTimeout(() => {
+      clearTimeout(deleteTimeout);
+      deleteTimeout = setTimeout(() => {
         $loading = false;
         goto(routeForSchedules({ namespace }));
       }, 2000);
@@ -169,7 +174,8 @@
       scheduleId,
       overlapPolicy: $overlapPolicy,
     });
-    setTimeout(() => {
+    clearTimeout(triggerTimeout);
+    triggerTimeout = setTimeout(() => {
       scheduleFetch = fetchSchedule(parameters);
       closeTriggerModal();
       scheduleUpdating = false;
@@ -245,7 +251,8 @@
       startTime,
       endTime,
     });
-    setTimeout(() => {
+    clearTimeout(backfillTimeout);
+    backfillTimeout = setTimeout(() => {
       scheduleFetch = fetchSchedule(parameters);
       closeBackfillModal();
       scheduleUpdating = false;
@@ -255,6 +262,12 @@
   const resetReason = () => {
     reason = '';
   };
+
+  onDestroy(() => {
+    clearTimeout(deleteTimeout);
+    clearTimeout(triggerTimeout);
+    clearTimeout(backfillTimeout);
+  });
 </script>
 
 {#await scheduleFetch}
@@ -297,16 +310,9 @@
           dateTime={schedule?.info?.createTime}
         >
           {#snippet leading()}
-            {translate('common.created')}
+            Created
           {/snippet}
         </Timestamp>
-        <!-- <p class="flex items-center gap-2 text-right text-sm">
-          {translate('common.created', {
-            created: formatDate(schedule?.info?.createTime, $timeFormat, {
-              relative: $relativeTime,
-            }),
-          })}
-        </p> -->
         {#if schedule?.info?.updateTime}
           <Timestamp
             as="p"
@@ -314,16 +320,9 @@
             dateTime={schedule?.info?.updateTime}
           >
             {#snippet leading()}
-              {translate('common.last-updated')}
+              Last Updated
             {/snippet}
           </Timestamp>
-          <!-- <p class="flex items-center gap-2 text-right text-sm">
-            {translate('common.last-updated', {
-              updated: formatDate(schedule?.info?.updateTime, $timeFormat, {
-                relative: $relativeTime,
-              }),
-            })}
-          </p> -->
         {/if}
       </div>
       <SplitButton
