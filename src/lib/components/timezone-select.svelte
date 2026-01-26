beec6b03d752439dc7aab87b77a2fd94dd18791a
Andrew Zamojc <me@andrewzamojc.com>
2026-01-26 16:13:17 -0500

    ux improvements

--- src/lib/components/timezone-select.svelte
+++ src/lib/components/timezone-select.svelte
@@ -16,7 +16,6 @@
   } from '$lib/holocene/menu';
   import ToggleButton from '$lib/holocene/toggle-button/toggle-button.svelte';
   import ToggleButtons from '$lib/holocene/toggle-button/toggle-buttons.svelte';
-  import ToggleSwitch from '$lib/holocene/toggle-switch.svelte';
   import { translate } from '$lib/i18n/translate';
   import {
     hourFormat,
@@ -54,6 +53,7 @@
   let intervalId: number | undefined = undefined;
   let currentDate = new Date().setMilliseconds(0);
   let openUnsubscriber: Unsubscriber | undefined;
+  let timezoneListElement: HTMLDivElement;
 
   $: filteredOptions = !search
     ? TimezoneOptions
@@ -67,21 +67,14 @@
       });
 
   const selectTimezone = (value: string) => {
-    if ($relativeTime && value !== BASE_TIME_FORMAT_OPTIONS.LOCAL)
-      $relativeTime = false;
     $timeFormat = value;
     search = '';
-  };
-
-  const handleRelativeToggle = (e: Event) => {
-    const target = e.target as HTMLInputElement;
-    if (target.checked && $timeFormat !== BASE_TIME_FORMAT_OPTIONS.LOCAL) {
-      $timeFormat = BASE_TIME_FORMAT_OPTIONS.LOCAL;
-    }
+    $open = false;
   };
 
   const setTimestampFormat = (format: TimestampFormat) => {
     $timestampFormat = format;
+    $relativeTime = false;
   };
 
   const setHourFormat = (format: HourFormat) => {
@@ -96,6 +89,20 @@
       intervalId = window.setInterval(() => {
         currentDate = new Date().setMilliseconds(0);
       }, 1000);
+
+      setTimeout(() => {
+        if (timezoneListElement) {
+          const selectedItem = timezoneListElement.querySelector(
+            '[aria-selected="true"], .selected',
+          );
+          if (selectedItem) {
+            selectedItem.scrollIntoView({
+              block: 'center',
+              behavior: 'instant',
+            });
+          }
+        }
+      }, 0);
     } else {
       window.clearInterval(intervalId);
     }
@@ -130,124 +137,124 @@
   <Menu
     id="timezones-menu"
     {position}
-    class="w-[10rem] sm:w-[20rem] md:w-[28rem]"
+    keepOpen={true}
+    maxHeight="max-h-[26rem]"
+    class="flex w-[10rem] flex-col sm:w-[20rem] md:w-[28rem]"
   >
-    <Input
-      label={translate('common.search')}
-      labelHidden
-      id="timezone-search"
-      noBorder
-      bind:value={search}
-      icon="search"
-      placeholder={translate('common.search')}
-    />
-
-    <MenuDivider />
-
-    <div class="m-4">
-      <ToggleSwitch
-        label={translate('common.relative')}
-        id="relative-toggle"
-        bind:checked={$relativeTime}
-        labelPosition="left"
-        on:change={handleRelativeToggle}
-        data-testid="timezones-relative-toggle"
+    <div class="mx-4 mb-2 mt-4 flex-shrink-0">
+      <p class="font-medium">Timestamp Format</p>
+      <Timestamp
+        as="p"
+        class="mb-4 text-sm text-secondary"
+        dateTime={currentDate}
       />
-    </div>
-
-    {#if !$relativeTime}
       <div
-        class="mx-4 mb-2 mt-4 flex gap-2 max-md:flex-col md:flex-row md:items-center md:justify-between"
+        class="flex flex-col gap-2 md:flex-row md:items-center md:justify-between"
       >
-        <p class="font-medium">Timestamp Format</p>
         <ToggleButtons>
           <ToggleButton
             size="xs"
-            active={$timestampFormat === 'short'}
+            active={$relativeTime}
+            on:click={() => ($relativeTime = true)}
+            data-testid="timezones-relative-toggle"
+          >
+            Relative
+          </ToggleButton>
+          <ToggleButton
+            size="xs"
+            active={!$relativeTime && $timestampFormat === 'short'}
             on:click={() => setTimestampFormat('short')}>Short</ToggleButton
           >
           <ToggleButton
             size="xs"
-            active={$timestampFormat === 'medium'}
+            active={!$relativeTime && $timestampFormat === 'medium'}
             on:click={() => setTimestampFormat('medium')}>Default</ToggleButton
           >
           <ToggleButton
             size="xs"
-            active={$timestampFormat === 'long'}
+            active={!$relativeTime && $timestampFormat === 'long'}
             on:click={() => setTimestampFormat('long')}>Long</ToggleButton
           >
           <ToggleButton
             size="xs"
-            active={$timestampFormat === 'iso'}
+            active={!$relativeTime && $timestampFormat === 'iso'}
             on:click={() => setTimestampFormat('iso')}>ISO</ToggleButton
           >
         </ToggleButtons>
-      </div>
 
-      <div
-        class="mx-4 mb-2 flex gap-2 max-md:flex-col md:flex-row md:items-center md:justify-between"
-      >
-        <p class="font-medium">Hour Format</p>
         <ToggleButtons>
           <ToggleButton
             size="xs"
             active={$hourFormat === 'system'}
-            disabled={$timestampFormat === 'iso'}
-            on:click={() => setHourFormat('system')}>System</ToggleButton
+            disabled={$timestampFormat === 'iso' || $relativeTime}
+            on:click={() => setHourFormat('system')}
           >
+            <Icon name="system-window" />
+          </ToggleButton>
           <ToggleButton
             size="xs"
             active={$hourFormat === '12'}
-            disabled={$timestampFormat === 'iso'}
-            on:click={() => setHourFormat('12')}>12-hour</ToggleButton
+            disabled={$timestampFormat === 'iso' || $relativeTime}
+            on:click={() => setHourFormat('12')}>12h</ToggleButton
           >
           <ToggleButton
             size="xs"
             active={$hourFormat === '24'}
-            disabled={$timestampFormat === 'iso'}
-            on:click={() => setHourFormat('24')}>24-hour</ToggleButton
+            disabled={$timestampFormat === 'iso' || $relativeTime}
+            on:click={() => setHourFormat('24')}>24h</ToggleButton
           >
         </ToggleButtons>
       </div>
+    </div>
 
-      <div class="mx-4 mb-4 mt-3">
-        <Timestamp
-          as="p"
-          class="text-xs text-secondary"
-          dateTime={currentDate}
-        />
-      </div>
-    {/if}
+    <div class="mx-4 mt-1 flex-shrink-0">
+      <p class="mb-1 font-medium">Timezone</p>
+    </div>
+
+    <div class="flex-shrink-0 px-4">
+      <Input
+        label={translate('common.search')}
+        labelHidden
+        id="timezone-search"
+        noBorder
+        bind:value={search}
+        icon="search"
+        placeholder={translate('common.search')}
+        disabled={$relativeTime || $timestampFormat === 'iso'}
+      />
+    </div>
 
     <MenuDivider />
 
-    {#if !search}
-      {#each QuickTimezoneOptions as { value, label }}
+    <div class="max-h-[18rem] overflow-y-auto" bind:this={timezoneListElement}>
+      {#if !search}
+        {#each QuickTimezoneOptions as { value, label }}
+          <MenuItem
+            onclick={() => selectTimezone(value)}
+            data-testid="timezones-{value}"
+            selected={value === $timeFormat}
+            disabled={$relativeTime || $timestampFormat === 'iso'}
+            description={value === BASE_TIME_FORMAT_OPTIONS.LOCAL
+              ? localTime
+              : undefined}
+          >
+            {label}
+          </MenuItem>
+        {/each}
+      {/if}
+
+      {#each filteredOptions as { value, label, offset, abbr }}
         <MenuItem
-          onclick={() => selectTimezone(value)}
-          data-testid="timezones-{value}"
           selected={value === $timeFormat}
-          description={value === BASE_TIME_FORMAT_OPTIONS.LOCAL
-            ? localTime
-            : undefined}
+          onclick={() => selectTimezone(value)}
+          disabled={$relativeTime || $timestampFormat === 'iso'}
+          description={formatUTCOffset(offset, translate('common.utc'))}
         >
-          {label}
+          {label} ({abbr})
         </MenuItem>
+      {:else}
+        <MenuItem disabled>{translate('common.no-results')}</MenuItem>
       {/each}
-
-      <MenuDivider />
-    {/if}
-
-    {#each filteredOptions as { value, label, offset, abbr }}
-      <MenuItem
-        selected={value === $timeFormat}
-        onclick={() => selectTimezone(value)}
-        description={formatUTCOffset(offset, translate('common.utc'))}
-      >
-        {label} ({abbr})
-      </MenuItem>
-    {:else}
-      <MenuItem disabled>{translate('common.no-results')}</MenuItem>
-    {/each}
+    </div>
   </Menu>
 </MenuContainer>
