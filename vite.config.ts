c67cc8816f0679b1612649facb9796ab506d6e95
Ross Nelson <ross.nelson@temporal.io>
2026-02-09 09:20:01 -0500

    Reapply "Refresh token POC (#2966)" (#3139) (#3141)
    This reverts commit 3ca9d3c3e08e11a403b13d28ab9fdf003f36cfdb.

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+import { oidcServerPlugin } from './plugins/vite-plugin-oidc-server';
 import { temporalServer } from './plugins/vite-plugin-temporal-server';
 import { uiServerPlugin } from './plugins/vite-plugin-ui-server';
 
 export default defineConfig({
-  plugins: [sveltekit(), temporalServer(), uiServerPlugin()],
+  plugins: [
+    sveltekit(),
+    oidcServerPlugin(),
+    temporalServer(),
+    uiServerPlugin(),
+  ],
   optimizeDeps: {
     include: ['date-fns', 'date-fns-tz'],
   },
