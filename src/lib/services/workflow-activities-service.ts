90dfec88b1d31ee61833676e7d83181cedca7833
Laura Whitaker <laura.whitaker@temporal.io>
2026-02-12 11:00:53 -0700

    Add fallbacks for activities routes (#3154)
    * Add fallback for /activities-deprecated routes
    
    * Add minimum version check

--- src/lib/services/workflow-activities-service.ts
+++ src/lib/services/workflow-activities-service.ts
@@ -1,3 +1,5 @@
+import { page } from '$app/state';
+
 import type {
   ActivityPauseRequest,
   ActivityPauseResponse,
@@ -8,14 +10,38 @@ import type {
   ActivityUpdateOptionsRequest,
   ActivityUpdateOptionsResponse,
 } from '$lib/types';
+import { isNotFound, isNotImplemented } from '$lib/utilities/handle-error';
 import { stringifyWithBigInt } from '$lib/utilities/parse-with-big-int';
 import { requestFromAPI } from '$lib/utilities/request-from-api';
 import { routeForApi } from '$lib/utilities/route-for-api';
+import { minimumVersionRequired } from '$lib/utilities/version-check';
 
-type WorkflowInformation = {
-  workflowId: string;
-  runId: string;
-  activityId: string;
+const requestWithActivityFallback = async <T>(
+  route: string,
+  init: Parameters<typeof requestFromAPI>[1],
+): Promise<T> => {
+  const fallbackRoute = route.replace(
+    '/activities-deprecated/',
+    '/activities/',
+  );
+  const version = page.data?.settings?.version;
+
+  if (version && !minimumVersionRequired('2.45.3', version)) {
+    return requestFromAPI<T>(fallbackRoute, init);
+  }
+
+  try {
+    return await requestFromAPI<T>(route, {
+      ...init,
+      notifyOnError: false,
+    });
+  } catch (error: unknown) {
+    if (isNotImplemented(error) || isNotFound(error)) {
+      return requestFromAPI<T>(fallbackRoute, init);
+    }
+
+    throw error;
+  }
 };
 
 export const pauseActivity = async ({
@@ -32,7 +58,7 @@ export const pauseActivity = async ({
     namespace,
   });
 
-  return requestFromAPI(route, {
+  return requestWithActivityFallback(route, {
     options: {
       method: 'POST',
       body: stringifyWithBigInt({
@@ -57,7 +83,7 @@ export const unpauseActivity = async ({
     namespace,
   });
 
-  return requestFromAPI(route, {
+  return requestWithActivityFallback(route, {
     options: {
       method: 'POST',
       body: stringifyWithBigInt({
@@ -82,7 +108,7 @@ export const resetActivity = async ({
     namespace,
   });
 
-  return requestFromAPI(route, {
+  return requestWithActivityFallback(route, {
     options: {
       method: 'POST',
       body: stringifyWithBigInt({
@@ -110,7 +136,7 @@ export const updateActivityOptions = async ({
 
   const fullMask =
     'taskQueue.name,scheduleToCloseTimeout,scheduleToStartTimeout,startToCloseTimeout,heartbeatTimeout,retryPolicy.initialInterval,retryPolicy.backoffCoefficient,retryPolicy.maximumInterval,retryPolicy.maximumAttempts';
-  return requestFromAPI(route, {
+  return requestWithActivityFallback(route, {
     options: {
       method: 'POST',
       body: stringifyWithBigInt({
