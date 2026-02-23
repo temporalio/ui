6367d761923f7d311f5a6ae486802114415168ed
rossedfort <11775628+rossedfort@users.noreply.github.com>
2026-02-23 16:50:35 +0000

    chore: bump version to 2.45.4
    Auto-generated version bump from 2.45.3 to 2.45.4
    
    Bump type: patch
    
    Changes included:
    - [`02b30a6e`](https://github.com/temporalio/ui/commit/02b30a6e917f6afca1cc6ae7eddfdcf07a91c906) Use CountSchedules for counting schedules (#3100)
    - [`90dfec88`](https://github.com/temporalio/ui/commit/90dfec88b1d31ee61833676e7d83181cedca7833) Add fallbacks for activities routes (#3154)
    - [`f160628c`](https://github.com/temporalio/ui/commit/f160628c187b30b8894b76e8e62a17eb91c80ec5) DT-3564 - migrate schedules form to superforms (#3159)
    - [`50d7bdf3`](https://github.com/temporalio/ui/commit/50d7bdf34fa8da8aae596106f1f8e0c7a3b42c84) small css changes to nav components for better responsiveness (#3162)
    - [`e3b4028b`](https://github.com/temporalio/ui/commit/e3b4028b4597e1845245cbcea6e9a28052538145) UI Copy Updates (#3165)
    - [`3638919c`](https://github.com/temporalio/ui/commit/3638919c3790708f50c23b86558ae37d4e031d15) add snippet for pre-release badge on standalone activities page (#3166)
    - [`1ee50fcb`](https://github.com/temporalio/ui/commit/1ee50fcb16308a719d47b7b9bb197b288f4a929e) fix standalone activity routes and prep for release (#3168)
    - [`7553e5b4`](https://github.com/temporalio/ui/commit/7553e5b48071f0ce971fccb9a386b645cc903989) Event history legend (#3160)
    - [`562067a8`](https://github.com/temporalio/ui/commit/562067a83c890329ac2dac81ea7669ff9722cdf6) Environment variable audit (#3163)
    - [`51bdfd75`](https://github.com/temporalio/ui/commit/51bdfd75cdc07a8fde7003e351df16845a3dc2f7) Card typography fixes (#3169)
    - [`dfc6b453`](https://github.com/temporalio/ui/commit/dfc6b4532ee40d32bfcd2d4451ea4efb24f2df39) fix schedule edit form infinite effect loop (#3172)
    - [`07995e61`](https://github.com/temporalio/ui/commit/07995e611ef1ebecba5b2f447c6377b2578c2484) fix infinite effect bug when editing schedule with large input payload (#3174)

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.45.3"
+	UIVersion               = "2.45.4"
 )
