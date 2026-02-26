1f262dbca9bd257c478a8f4373905bab262c3ae8
rossedfort <11775628+rossedfort@users.noreply.github.com>
2026-02-26 21:54:03 +0000

    chore: bump version to 2.47.1
    Auto-generated version bump from 2.47.0 to 2.47.1
    
    Bump type: patch
    
    Changes included:
    - [`44f326b6`](https://github.com/temporalio/ui/commit/44f326b636306c106bb34acda4551985054ca29a) Add check for default color (#3194)
    - [`952ae5ef`](https://github.com/temporalio/ui/commit/952ae5ef9ba14bcc582be612c5d45059ee750610) Bump dependencies (#3176)
    - [`b62ebe7a`](https://github.com/temporalio/ui/commit/b62ebe7a0e845f62f386624bc9df855c3af3957b) Detail list text and link value fix (#3195)
    - [`7f84fe09`](https://github.com/temporalio/ui/commit/7f84fe090e82acf196a10152772c29bccedb60fc) Bump server image and dockerize versions (#3192)

--- server/server/version/version.go
+++ server/server/version/version.go
@@ -28,5 +28,5 @@ const (
 	clientNameHeaderName    = "client-name"
 	clientNameHeaderValue   = "temporal-ui"
 	clientVersionHeaderName = "client-version"
-	UIVersion               = "2.47.0"
+	UIVersion               = "2.47.1"
 )
