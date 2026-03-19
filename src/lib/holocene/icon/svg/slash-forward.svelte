5eba5b9e5ed60837e1a6edf1705b219561d47f71
Katie Toler <katie.toler@temporal.io>
2026-03-19 00:02:30 -0400

    Add new icons

---
+++ src/lib/holocene/icon/svg/slash-forward.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Svg from '../svg.svelte';
+  let props = $props();
+</script>
+
+<Svg {...props}>
+  <path d="M 18.75,0 H 16.3674 L 5.25,24 h 2.382615 z" fill="currentColor" />
+</Svg>
