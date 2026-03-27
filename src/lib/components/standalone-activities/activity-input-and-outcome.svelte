5be311eb955ffa6b3eb2530b2fab637fccba85c1
Ross Edfort <ross.edfort@temporal.io>
2026-03-27 10:51:10 -0600

    use existing input/result component for SAA

--- src/lib/components/standalone-activities/activity-input-and-outcome.svelte
+++ src/lib/components/standalone-activities/activity-input-and-outcome.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import CodeBlock from '$lib/holocene/code-block.svelte';
   import type { Payloads } from '$lib/types';
   import type { ActivityExecutionOutcome } from '$lib/types/activity-execution';
   import { has } from '$lib/utilities/has';
+  import { stringifyWithBigInt } from '$lib/utilities/parse-with-big-int';
 
-  import PayloadDecoder from '../event/payload-decoder.svelte';
+  import InputAndResultsPayload from '../workflow/input-and-results-payload.svelte';
 
   interface Props {
     input: Payloads | undefined;
@@ -16,28 +16,24 @@
 </script>
 
 <div class="grid w-full grid-cols-2 gap-4 max-md:grid-cols-1">
-  <div class="flex flex-col gap-2">
-    <h5>Input</h5>
-    <PayloadDecoder value={input} key="payloads">
-      {#snippet children(decodedValue)}
-        <CodeBlock content={decodedValue} />
-      {/snippet}
-    </PayloadDecoder>
-  </div>
-  <div class="flex flex-col gap-2">
-    <h5>Result</h5>
-    {#if has(outcome, 'failure')}
-      <CodeBlock content={JSON.stringify(outcome.failure, null, 2)} />
-    {:else if has(outcome, 'result')}
-      <PayloadDecoder value={outcome.result} key="payloads">
-        {#snippet children(decodedValue)}
-          <CodeBlock content={decodedValue} />
-        {/snippet}
-      </PayloadDecoder>
-    {:else if pending}
-      <CodeBlock content="Results will appear upon completion." />
-    {:else}
-      <CodeBlock content={JSON.stringify({}, null, 2)} />
-    {/if}
-  </div>
+  <InputAndResultsPayload title="Input" content={stringifyWithBigInt(input)} />
+  {#if has(outcome, 'failure')}
+    <InputAndResultsPayload
+      title="Result"
+      content={stringifyWithBigInt(outcome.failure)}
+      isPending={pending}
+    />
+  {:else if has(outcome, 'result')}
+    <InputAndResultsPayload
+      title="Result"
+      content={stringifyWithBigInt(outcome.result)}
+      isPending={pending}
+    />
+  {:else}
+    <InputAndResultsPayload
+      title="Result"
+      content={stringifyWithBigInt(outcome)}
+      isPending={pending}
+    />
+  {/if}
 </div>
