fa383136f3f25acda2df276b7d989b89f1f8d450
Katie Toler <katie.toler@temporal.io>
2026-04-02 15:33:40 -0400

    Fix: rename import to match new type name.

--- src/lib/holocene/navigation/navigation-utils.ts
+++ src/lib/holocene/navigation/navigation-utils.ts
@@ -1,15 +1,15 @@
-import type { NavLinkListItem } from '$lib/types/global';
+import type { NavLinkItem } from '$lib/types/global';
 
-export type { NavLinkListItem };
+export type { NavLinkItem };
 
 export function isNavLinkItem(
-  item: NavLinkListItem,
-): item is Extract<NavLinkListItem, { href: string }> {
+  item: NavLinkItem,
+): item is Extract<NavLinkItem, { href: string }> {
   return 'href' in item;
 }
 
 export function isNavDividerItem(
-  item: NavLinkListItem,
-): item is Extract<NavLinkListItem, { divider: true }> {
+  item: NavLinkItem,
+): item is Extract<NavLinkItem, { divider: true }> {
   return 'divider' in item;
 }
