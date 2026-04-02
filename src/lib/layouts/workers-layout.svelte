ed0d9e67d284970b3d09d7a97754c8f4768befe4
Laura Whitaker <laura.whitaker@temporal.io>
2026-04-02 15:41:29 -0600

    Pass hrefs to workers layout

--- src/lib/layouts/workers-layout.svelte
+++ src/lib/layouts/workers-layout.svelte
@@ -8,21 +8,14 @@
   import Tab from '$lib/holocene/tab/tab.svelte';
   import Tabs from '$lib/holocene/tab/tabs.svelte';
   import { translate } from '$lib/i18n/translate';
-  import { pathMatches } from '$lib/utilities/path-matches';
-  import {
-    routeForWorkerDeployments,
-    routeForWorkers,
-  } from '$lib/utilities/route-for';
 
   interface Props {
-    namespace: string;
     children: Snippet;
+    deploymentsHref: string;
+    workersHref: string;
   }
 
-  const { children, namespace }: Props = $props();
-
-  const workersHref = $derived(routeForWorkers({ namespace }));
-  const deploymentsHref = $derived(routeForWorkerDeployments({ namespace }));
+  const { children, deploymentsHref, workersHref }: Props = $props();
 </script>
 
 <header class="flex flex-col gap-2">
@@ -35,11 +28,7 @@
         label={translate('workers.instance', { count: 2 })}
         id="workers-tab"
         href={workersHref}
-        active={pathMatches(
-          page.url.pathname,
-          routeForWorkers({ namespace }),
-          true,
-        )}
+        active={page.url.pathname.endsWith('/workers')}
       >
         <Badge class="shrink-0">Pre-Release</Badge>
       </Tab>
@@ -47,11 +36,7 @@
         label={translate('deployments.deployments')}
         id="deployments-tab"
         href={deploymentsHref}
-        active={pathMatches(
-          page.url.pathname,
-          routeForWorkerDeployments({ namespace }),
-          true,
-        )}
+        active={page.url.pathname.endsWith('/deployments')}
       />
     </TabList>
   </Tabs>
--- src/routes/(app)/namespaces/[namespace]/workers/deployments/+page.svelte
+++ src/routes/(app)/namespaces/[namespace]/workers/deployments/+page.svelte
@@ -5,14 +5,20 @@
   import { translate } from '$lib/i18n/translate';
   import WorkersLayout from '$lib/layouts/workers-layout.svelte';
   import WorkerDeployments from '$lib/pages/deployments.svelte';
+  import {
+    routeForWorkerDeployments,
+    routeForWorkers,
+  } from '$lib/utilities/route-for';
 
   const namespace = $derived(page.params.namespace);
+  const deploymentsHref = $derived(routeForWorkerDeployments({ namespace }));
+  const workersHref = $derived(routeForWorkers({ namespace }));
 </script>
 
 <PageTitle
   title={translate('deployments.worker-deployments')}
   url={page.url.href}
 />
-<WorkersLayout {namespace}>
+<WorkersLayout {workersHref} {deploymentsHref}>
   <WorkerDeployments />
 </WorkersLayout>
