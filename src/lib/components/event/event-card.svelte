c5d4c996c8693ca5fbfd8104c72d6d60e051788c
Alex Tideman <alex.tideman@temporal.io>
2026-04-07 14:07:37 -0500

    Add link from Event Card to jump to event id page from Timeline. Remove unnecessary padding (#3277)

--- src/lib/components/event/event-card.svelte
+++ src/lib/components/event/event-card.svelte
@@ -22,13 +22,17 @@
     shouldDisplayAsTime,
   } from '$lib/utilities/get-single-attribute-for-event';
   import { isLocalActivityMarkerEvent } from '$lib/utilities/is-event-type';
-  import { routeForNamespace } from '$lib/utilities/route-for';
+  import {
+    routeForEventHistoryEvent,
+    routeForNamespace,
+  } from '$lib/utilities/route-for';
 
   import EventDetailsLink from './event-details-link.svelte';
   import MetadataDecoder from './metadata-decoder.svelte';
   import PayloadDecoder from './payload-decoder.svelte';
 
   let { event }: { event: WorkflowEvent } = $props();
+  const { namespace, workflow, run } = $derived(page.params);
 
   const displayName = $derived(
     isLocalActivityMarkerEvent(event)
@@ -78,7 +82,14 @@
 >
   <div class="flex flex-wrap items-center justify-between gap-2">
     <div class="flex items-center gap-2 text-base">
-      <p class="font-mono">{event.id}</p>
+      <Link
+        href={routeForEventHistoryEvent({
+          eventId: event.id,
+          run,
+          workflow,
+          namespace,
+        })}>{event.id}</Link
+      >
       <p class="font-medium">
         {displayName}
       </p>
