96f6fd28c65c0bee300ec4fde22282580b053c47
Your Name <alex.tideman@gmail.com>
2026-04-07 08:49:47 -0500

    Remove common errors from header snippet

--- src/routes/(app)/namespaces/[namespace]/workflows/[workflow]/[run]/+layout.svelte
+++ src/routes/(app)/namespaces/[namespace]/workflows/[workflow]/[run]/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { onDestroy, type Snippet } from 'svelte';
 
-  import WorkflowCommonErrors from '$lib/components/workflow/workflow-common-errors.svelte';
   import WorkflowRunLayout from '$lib/layouts/workflow-run-layout.svelte';
   import { clearPreviousEventParameters } from '$lib/stores/previous-events';
 
