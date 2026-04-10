c102320cd217c7e5ea459c059e5d1f8132761af4
Laura Whitaker <laura.whitaker@temporal.io>
2026-04-10 12:59:09 -0600

    Create generic sortAlphabetically util

--- src/lib/utilities/sort-namespaces.ts
+++
@@ -1,6 +0,0 @@
-import type { NamespaceListItem } from '$lib/types/global';
-
-export const sortNamespaces = (
-  list: NamespaceListItem[],
-): NamespaceListItem[] =>
-  [...list].sort((a, b) => a.namespace.localeCompare(b.namespace));
