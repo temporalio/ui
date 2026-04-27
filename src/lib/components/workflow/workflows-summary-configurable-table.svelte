14a52aae9de0f51dcbd67725bacd3f1cf0302208
Tegan Churchill <tegan.churchill@temporal.io>
2026-04-27 13:11:45 -0700

    Fix type check

--- src/lib/components/workflow/workflows-summary-configurable-table.svelte
+++ src/lib/components/workflow/workflows-summary-configurable-table.svelte
@@ -219,7 +219,7 @@
           // this is required due to how the underlying Checkbox component
           // get's it's onclick type from svelte event forwarding. It does not
           // know what the current event target type is a checkbox input
-          if ('checked' in event.currentTarget) {
+          if (event.currentTarget instanceof HTMLInputElement) {
             selectWorkflows(
               Boolean(event.currentTarget.checked),
               targetedWorkflows,
