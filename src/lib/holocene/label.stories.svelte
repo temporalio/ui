d5654a40d701c78208b520e8fc77ff04c938e3c9
Tegan Churchill <tegan.churchill@temporal.io>
2026-04-30 10:03:42 -0700

    Update Label component Svelte 5 syntax

--- src/lib/holocene/label.stories.svelte
+++ src/lib/holocene/label.stories.svelte
@@ -1,6 +1,7 @@
 <script lang="ts" context="module">
   import { Story, Template } from '@storybook/addon-svelte-csf';
   import type { Meta } from '@storybook/svelte';
+  import type { ComponentProps } from 'svelte';
 
   import Label from '$lib/holocene/label.svelte';
 
@@ -23,7 +24,7 @@
         table: { category: 'Accessibility' },
       },
     },
-  } satisfies Meta<Label>;
+  } satisfies Meta<ComponentProps<typeof Label>>;
 </script>
 
 <Template let:args>
