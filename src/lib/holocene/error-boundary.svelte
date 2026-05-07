40a029ea539ab08a0c36bfa4c3a9ceedc536a703
Tegan Churchill <tegan.churchill@temporal.io>
2026-05-07 11:17:19 -0700

    refactor(DT-3906): UI Svelte 5 Migrations medium (#3368)

--- src/lib/holocene/error-boundary.svelte
+++ src/lib/holocene/error-boundary.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import type { Snippet } from 'svelte';
+
   import Error from './error.svelte';
+
+  let { children }: { children?: Snippet } = $props();
 </script>
 
 <svelte:boundary>
-  <slot />
+  {@render children?.()}
   {#snippet failed(error, reset)}
     <Error {reset} {error} />
   {/snippet}
