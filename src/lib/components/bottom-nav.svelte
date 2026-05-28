5a890d67f9d285ccd6b986679aff69e205199fb1
Ross Nelson <axcess1@me.com>
2026-05-28 14:25:15 -0400

    feat: add centerButton snippet to BottomNavigation for custom center content

--- src/lib/components/bottom-nav.svelte
+++ src/lib/components/bottom-nav.svelte
@@ -34,6 +34,14 @@
         },
       ]
     >;
+    centerButton?: Snippet<
+      [
+        {
+          open: boolean;
+          onClick: () => void;
+        },
+      ]
+    >;
     profilePicture?: Snippet;
     class?: ClassNameValue;
   }
@@ -45,6 +53,7 @@
     showNamespacePicker = true,
     children,
     nsPicker,
+    centerButton,
     profilePicture,
     class: className = '',
   }: Props = $props();
@@ -159,24 +168,31 @@
     {/if}
   </button>
   {#if showNamespacePicker}
-    <div class="namespace-wrapper">
-      <Button
-        variant="ghost"
-        data-testid="namespace-switcher"
-        leadingIcon="namespace-switcher"
-        size="xs"
-        class="grow text-white"
-        on:click={onNamespaceClick}>{truncateNamespace(namespace)}</Button
-      >
-      <div class="ml-1 h-full w-1 border-l border-subtle"></div>
-      <Button
-        variant="ghost"
-        size="xs"
-        href={routeForNamespace({ namespace })}
-        disabled={!namespaceExists}
-        ><Icon class="text-white" name="external-link" /></Button
-      >
-    </div>
+    {#if centerButton}
+      {@render centerButton({
+        open: viewNamespaces,
+        onClick: onNamespaceClick,
+      })}
+    {:else}
+      <div class="namespace-wrapper">
+        <Button
+          variant="ghost"
+          data-testid="namespace-switcher"
+          leadingIcon="namespace-switcher"
+          size="xs"
+          class="grow text-white"
+          on:click={onNamespaceClick}>{truncateNamespace(namespace)}</Button
+        >
+        <div class="ml-1 h-full w-1 border-l border-subtle"></div>
+        <Button
+          variant="ghost"
+          size="xs"
+          href={routeForNamespace({ namespace })}
+          disabled={!namespaceExists}
+          ><Icon class="text-white" name="external-link" /></Button
+        >
+      </div>
+    {/if}
   {/if}
   <button
     class="nav-button"
