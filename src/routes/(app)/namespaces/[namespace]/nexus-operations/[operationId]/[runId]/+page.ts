72777d353ae6472203c8a32ce75847f5240e6411
Ross Edfort <ross.edfort@temporal.io>
2026-06-12 10:55:02 -0600

    fix import

--- src/routes/(app)/namespaces/[namespace]/nexus-operations/[operationId]/[runId]/+page.ts
+++ src/routes/(app)/namespaces/[namespace]/nexus-operations/[operationId]/[runId]/+page.ts
@@ -1,6 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
-import type { PageLoad } from '../$types';
+import type { PageLoad } from './$types';
 
 export const load: PageLoad = async function ({ url }) {
   redirect(302, `${url.pathname}/details`);
