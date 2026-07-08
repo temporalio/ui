670e6df90ddc19f2320e17c01aafc7097a80c45c
Laura Whitaker <laura.whitaker@temporal.io>
2026-07-07 17:07:35 -0700

    Add worker count to workers layout

--- src/lib/stores/workers.ts
+++ src/lib/stores/workers.ts
@@ -1,3 +1,19 @@
-import { writable } from 'svelte/store';
+import { derived, writable } from 'svelte/store';
+
+import { minimumVersionRequired } from '$lib/utilities/version-check';
+
+import { isCloud } from './advanced-visibility';
+import { temporalVersion } from './versions';
 
 export const refresh = writable(0);
+
+export const workerCount = writable({
+  count: 0,
+  newCount: 0,
+});
+
+export const workerCountEnabled = derived(
+  [isCloud, temporalVersion],
+  ([$isCloud, $temporalVersion]) =>
+    $isCloud || minimumVersionRequired('1.31.2', $temporalVersion),
+);
