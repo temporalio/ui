a972535ca567531d805ff5587c66b060e15f61b8
Tegan Churchill <tegan.churchill@temporal.io>
2026-07-23 16:04:14 -0700

    Update holocene accordion-light to runes syntax

--- src/lib/components/workflow/workflow-callbacks.svelte
+++ src/lib/components/workflow/workflow-callbacks.svelte
@@ -15,17 +15,21 @@
 <WorkflowCallback callback={firstCallback} {link}>
   {#if callbacks.length > 1}
     <div class="py-2">
-      <AccordionLight let:open>
-        <p class="p-1" slot="title">
-          View {callbacks.length - 1} other Callbacks
-        </p>
-        {#if open}
-          <div class="flex flex-col gap-2">
-            {#each callbacks.slice(1) as callback}
-              <WorkflowCallback {callback} />
-            {/each}
-          </div>
-        {/if}
+      <AccordionLight>
+        {#snippet title()}
+          <p class="p-1">
+            View {callbacks.length - 1} other Callbacks
+          </p>
+        {/snippet}
+        {#snippet children(open)}
+          {#if open}
+            <div class="flex flex-col gap-2">
+              {#each callbacks.slice(1) as callback}
+                <WorkflowCallback {callback} />
+              {/each}
+            </div>
+          {/if}
+        {/snippet}
       </AccordionLight>
     </div>
   {/if}
