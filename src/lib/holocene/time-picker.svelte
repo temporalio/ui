97dd71e32815804af01f92575d283e88a6f128eb
Tegan Churchill <tegan.churchill@temporal.io>
2026-07-29 13:59:11 -0700

    Update holocene time-picker to runes syntax

--- src/lib/holocene/time-picker.svelte
+++ src/lib/holocene/time-picker.svelte
@@ -1,25 +1,37 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
-
   import Input from '$lib/holocene/input/input.svelte';
   import ToggleButton from '$lib/holocene/toggle-button/toggle-button.svelte';
   import ToggleButtons from '$lib/holocene/toggle-button/toggle-buttons.svelte';
 
-  export let hour = '';
-  export let minute = '';
-  export let second = '';
-  export let half: 'AM' | 'PM' = 'AM';
-  export let twelveHourClock = true;
-  export let includeSeconds = true;
-  export let disabled = false;
-  export let error = false;
-  export let idPrefix = '';
+  interface Props {
+    hour?: string;
+    minute?: string;
+    second?: string;
+    half?: 'AM' | 'PM';
+    twelveHourClock?: boolean;
+    includeSeconds?: boolean;
+    disabled?: boolean;
+    error?: boolean;
+    idPrefix?: string;
+    onTimeChange?: (value: string) => void;
+  }
 
-  const dispatch = createEventDispatcher();
+  let {
+    hour = $bindable(''),
+    minute = $bindable(''),
+    second = $bindable(''),
+    half = $bindable('AM'),
+    twelveHourClock = true,
+    includeSeconds = true,
+    disabled = false,
+    error = false,
+    idPrefix = '',
+    onTimeChange,
+  }: Props = $props();
 
   const onInput = (e: Event) => {
     const target = e.target as HTMLInputElement;
-    dispatch('timechange', target.value);
+    onTimeChange?.(target.value);
   };
 </script>
 
