b29b16bbd101e3e76dcc90313c7ed77783723ad9
jscalio <jscalio@gmail.com>
2026-08-13 08:10:59 -0400

    feat: add owner and retention period columns to namespaces table

--- src/routes/(app)/namespaces/+page.svelte
+++ src/routes/(app)/namespaces/+page.svelte
@@ -11,6 +11,7 @@
   import { translate } from '$lib/i18n/translate';
   import { namespaces } from '$lib/stores/namespaces';
   import { routeForNamespace } from '$lib/utilities/route-for';
+  import { fromSecondsToDaysOrHours } from '$lib/utilities/format-time';
 </script>
 
 <PageTitle title="Namespaces" url={$page.url.href} />
@@ -33,6 +34,8 @@
         {#snippet headers()}
           <TableHeaderRow>
             <th>{translate('common.name')}</th>
+            <th>{translate('namespaces.owner')}</th>
+            <th>{translate('namespaces.retention-period')}</th>
           </TableHeaderRow>
         {/snippet}
         {#each visibleItems as namespace (namespace.namespaceInfo?.name)}
@@ -44,6 +47,8 @@
                 })}>{namespace.namespaceInfo?.name}</Link
               >
             </td>
+            <td>{namespace.namespaceInfo?.ownerEmail ?? '')}</td>
+            <td>{fromSecondsToDaysOrHours(namespace.config?.workflowExecutionRetentionTtl.toString())}</td>
           </TableRow>
         {/each}
       </Table>
