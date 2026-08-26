eb4dd52b9e997edb9d21235710a8b92270327900
Tegan Churchill <tegan.churchill@temporal.io>
2026-08-26 13:22:51 -0700

    Use overlay background for CodeMirror

--- src/lib/vendor/codemirror/custom-extensions.ts
+++ src/lib/vendor/codemirror/custom-extensions.ts
@@ -34,7 +34,7 @@ export type EditorLanguage =
 const baseTheme = {
   '&': {
     color: 'var(--color-content-primary)',
-    backgroundColor: 'var(--color-surface-secondary)',
+    backgroundColor: 'var(--color-overlay-primary)',
     height: '100%',
   },
   '.cm-scroller': {
