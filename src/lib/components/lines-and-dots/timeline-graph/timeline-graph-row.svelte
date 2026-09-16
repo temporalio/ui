2b7dbd1066cc8ad7c8fb351e65dfe01d4f39a973
Laura Whitaker <laura.whitaker@temporal.io>
2026-09-16 09:46:52 -0700

    Fix clipped timeline dot icons on 1x displays

--- src/lib/components/lines-and-dots/timeline-graph/timeline-graph-row.svelte
+++ src/lib/components/lines-and-dots/timeline-graph/timeline-graph-row.svelte
@@ -233,7 +233,7 @@
   >
     {#if icon}
       <svg
-        class="absolute left-1/2 top-1/2 h-[55%] w-[55%] -translate-x-1/2 -translate-y-1/2 text-black"
+        class="absolute left-1/2 top-1/2 h-1/2 w-1/2 -translate-x-1/2 -translate-y-1/2 text-black"
         viewBox="0 0 16 16"
       >
         <use href="#ti-{icon}" />
