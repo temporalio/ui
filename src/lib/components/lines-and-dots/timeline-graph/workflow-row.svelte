c7913484afc7af41a06839d81ff93b170c5fb590
Laura Whitaker <laura.whitaker@temporal.io>
2026-09-16 10:39:45 -0700

    Derive dot icon size from an even-pixel token

--- src/lib/components/lines-and-dots/timeline-graph/workflow-row.svelte
+++ src/lib/components/lines-and-dots/timeline-graph/workflow-row.svelte
@@ -66,7 +66,7 @@
       style:background={colors.fill}
     >
       <svg
-        class="absolute left-1/2 top-1/2 h-1/2 w-1/2 -translate-x-1/2 -translate-y-1/2 text-black"
+        class="absolute left-1/2 top-1/2 h-[var(--dot-icon)] w-[var(--dot-icon)] -translate-x-1/2 -translate-y-1/2 text-black"
         viewBox="0 0 16 16"
       >
         <use href="#ti-workflow" />
